0fde4092c6f842193666b70b2f7cad02c784d4e3
Brandon Williams <mbw234@gmail.com>
2016-03-01 09:21:16 -0500

    Remove stub code.

--- Kickstarter-iOS/Tests/KickstarterTests.swift
+++ Kickstarter-iOS/Tests/KickstarterTests.swift
@@ -1,36 +1,5 @@
-//
-//  KickstarterTests.swift
-//  KickstarterTests
-//
-//  Created by Brandon Williams on 2/29/16.
-//  Copyright © 2016 Kickstarter. All rights reserved.
-//
-
 import XCTest
 @testable import Kickstarter_iOS
 
 class KickstarterTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
