38f05f686fb02e5c67c93e8c58a008873d270b12
Brandon Williams <mbw234@gmail.com>
2016-03-08 17:23:43 -0500

    Made library code for MVVM table view controllers.

---
+++ Library/MVVM/MVVMTableViewController.swift
@@ -0,0 +1,29 @@
+import func Foundation.dispatch_async
+import func Foundation.dispatch_get_main_queue
+import class UIKit.UITableViewController
+
+/// A UICollectionViewController base class that interfaces nicely with
+/// cells of the type MVVMCollectionViewCell.
+///
+/// Subclasses will be most interested in overriding `bindViewModel`. This
+/// is where one should observe signals exposed in the view model.
+public class MVVMTableViewController : UITableViewController {
+  private var needsToBind = true
+
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if needsToBind {
+      // guarantee that `bindViewModel` is called after `viewDidLoad`
+      dispatch_async(dispatch_get_main_queue()) {
+        self.bindViewModel()
+      }
+      needsToBind = false
+    }
+  }
+
+  /// All signal observations should happen in here. This method is called
+  /// immediately after `viewDidLoad`.
+  public func bindViewModel() {
+  }
+}
