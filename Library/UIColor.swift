f283159c349d94b9812abc015d31be9184e7be68
Gina Binetti <theginbin@gmail.com>
2016-03-08 14:36:47 -0500

    UIColor extension with hex conversion

---
+++ Library/UIColor.swift
@@ -0,0 +1,24 @@
+import class UIKit.UIColor
+import struct CoreGraphics.CGFloat
+import func CoreGraphics./
+
+extension UIColor {
+  @nonobjc static func hexa(value: UInt32) -> UIColor {
+    let a = CGFloat((value & 0xFF000000) >> 24) / 255.0
+    let r = CGFloat((value & 0xFF0000) >> 16) / 255.0
+    let g = CGFloat((value & 0xFF00) >> 8) / 255.0
+    let b = CGFloat((value & 0xFF)) / 255.0
+
+    return UIColor(red: r, green: g, blue: b, alpha: a)
+  }
+
+  @nonobjc static func hex(value: UInt32) -> UIColor {
+    let r = CGFloat((value & 0xFF0000) >> 16) / 255.0
+    let g = CGFloat((value & 0xFF00) >> 8) / 255.0
+    let b = CGFloat((value & 0xFF)) / 255.0
+
+    return UIColor(red: r, green: g, blue: b, alpha: 1.0)
+  }
+}
+
+
