cd9ecae45631e2eba7edefcffbe2ffa189dfbd11
Gina Binetti <theginbin@gmail.com>
2016-03-10 18:16:04 -0500

    update syntax

--- Library/UIColor.swift
+++ Library/UIColor.swift
@@ -10,10 +10,10 @@ extension UIColor {
     let g = CGFloat((value & 0xFF00) >> 8) / 255.0
     let b = CGFloat((value & 0xFF)) / 255.0
 
-    let aRounded = UIColor.toDecimalPlace(a, decimalNum: 1)
-    let rRounded = UIColor.toDecimalPlace(r, decimalNum: 2)
-    let gRounded = UIColor.toDecimalPlace(g, decimalNum: 2)
-    let bRounded = UIColor.toDecimalPlace(b, decimalNum: 2)
+    let aRounded = UIColor.toDecimalPlace(a, 1)
+    let rRounded = UIColor.toDecimalPlace(r, 2)
+    let gRounded = UIColor.toDecimalPlace(g, 2)
+    let bRounded = UIColor.toDecimalPlace(b, 2)
 
     return UIColor(red: rRounded, green: gRounded, blue: bRounded, alpha: aRounded)
   }
@@ -23,14 +23,14 @@ extension UIColor {
     let g = CGFloat((value & 0xFF00) >> 8) / 255.0
     let b = CGFloat((value & 0xFF)) / 255.0
 
-    let rRounded = UIColor.toDecimalPlace(r, decimalNum: 2)
-    let gRounded = UIColor.toDecimalPlace(g, decimalNum: 2)
-    let bRounded = UIColor.toDecimalPlace(b, decimalNum: 2)
+    let rRounded = UIColor.toDecimalPlace(r, 2)
+    let gRounded = UIColor.toDecimalPlace(g, 2)
+    let bRounded = UIColor.toDecimalPlace(b, 2)
 
     return UIColor(red: rRounded, green: gRounded, blue: bRounded, alpha: 1.0)
   }
 
-  private static func toDecimalPlace(num: CGFloat, decimalNum: Int) -> CGFloat {
+  private static func toDecimalPlace(num: CGFloat, _ decimalNum: Int) -> CGFloat {
     switch decimalNum {
     case 1:
       return CGFloat(round(Double(num) * 10) / 10)
