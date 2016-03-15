b4800c5117dea470624af41b9500b3fb716b30dc
Gina Binetti <theginbin@gmail.com>
2016-03-15 14:03:39 -0400

    add current user token to environment

--- Kickstarter-iOS/AppDelegate.swift
+++ Kickstarter-iOS/AppDelegate.swift
@@ -6,7 +6,9 @@ import protocol Library.HockeyManagerType
 import struct Library.AppEnvironment
 import struct KsApi.Service
 import struct KsApi.ServerConfig
+import struct KsApi.OauthToken
 import enum Library.Language
+import struct Library.CurrentUser
 import class Foundation.NSLocale
 import class Foundation.NSObject
 
@@ -17,9 +19,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   let viewModel: AppDelegateViewModelType = AppDelegateViewModel()
 
   override init() {
+    let userToken = CurrentUser.shared.accessToken
+    var serviceToken: OauthToken? = nil
+    if let token = userToken {
+      serviceToken = OauthToken.init(token: token)
+    }
+
     AppEnvironment.pushEnvironment(
       apiService: Service(
         serverConfig: ServerConfig.production,
+        oauthToken: serviceToken,
         language: Language.en.rawValue
       ),
       language: .en,
