1ddd2d888aae47bdf33550b43f5af153f41eded9
Brandon Williams <mbw234@gmail.com>
2016-04-27 14:28:52 -0400

    Swizzle viewDidLoad for binding. (#52)

--- Library/DataSource/UIViewController-BindToViewModel.swift
+++ Library/DataSource/UIViewController-BindToViewModel.swift
@@ -1,13 +1,49 @@
 import UIKit
+import ObjectiveC
 
 extension UIViewController {
-  public override func awakeFromNib() {
+  public override class func initialize() {
+    struct Static {
+      static var token: dispatch_once_t = 0
+    }
+
+    // make sure this isn't a subclass
+    guard self === UIViewController.self else { return }
+
+    dispatch_once(&Static.token) {
+      let originalSelector = #selector(viewDidLoad)
+      let swizzledSelector = #selector(ksr_viewDidLoad)
+
+      let originalMethod = class_getInstanceMethod(self, originalSelector)
+      let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+
+      let didAddMethod = class_addMethod(self,
+                                         originalSelector,
+                                         method_getImplementation(swizzledMethod),
+                                         method_getTypeEncoding(swizzledMethod))
+
+      if didAddMethod {
+        class_replaceMethod(self,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod))
+      } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod)
+      }
+    }
+  }
+
+  internal func ksr_viewDidLoad(animated: Bool) {
     bindViewModel()
+    self.ksr_viewDidLoad(animated)
   }
 
   public func bindViewModel() {
   }
 
+}
+
+extension UIViewController {
   public static var defaultNib: String {
     return self.description().componentsSeparatedByString(".").dropFirst().joinWithSeparator(".")
   }
