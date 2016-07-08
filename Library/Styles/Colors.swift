6a13a8157c25425a77f6535029570ccd54a3b802
Lisa Luo <lluo92@gmail.com>
2016-07-08 15:23:24 -0400

    Update Colors with mars lib

--- Library/Styles/Colors.swift
+++ Library/Styles/Colors.swift
@@ -10,9 +10,17 @@ extension UIColor {
       ("Royal Blue", .ksr_royalBlue),
       ("Clear", .ksr_clear),
       ("Facebook Blue", .ksr_facebookBlue),
-      ("Dark Green", .ksr_darkGreen),
-      ("Green", .ksr_green),
-      ("Light Green", .ksr_lightGreen),
+      ("Golden Bell", .ksr_goldenBell),
+      ("Coral", .ksr_coral(weight: 400)),
+      ("Green", .ksr_green(weight: 400)),
+      ("Grey", .ksr_grey(weight: 400)),
+      ("Navy", .ksr_navy(weight: 400)),
+      ("Teal", .ksr_orange(weight: 400)),
+      ("Teal", .ksr_peach(weight: 400)),
+      ("Sage", .ksr_sage(weight: 400)),
+      ("Red", .ksr_red(weight: 500)),
+      ("Teal", .ksr_teal(weight: 400)),
+      ("Violet", .ksr_violet(weight: 400)),
       ("Gray", .ksr_gray),
       ("Black Gray", .ksr_blackGray),
       ("Dark Gray", .ksr_darkGray),
@@ -25,27 +33,151 @@ extension UIColor {
       ("Dark Gray Text", .ksr_darkGrayText),
       ("Light Gray Text", .ksr_lightGrayText),
       ("Twitter Blue", .ksr_twitterBlue),
+      ("Whisper", .ksr_whisper),
       ("White", .ksr_white),
-      ("Yellow", .ksr_yellow)
+      ("Yellow", .ksr_yellow),
+      ("Custom Indigo", .ksr_customIndigo)
     ]
   }
 
+  public static func ksr_coral(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200:            return .hex(0xF0E9EF)
+    case 300, 400:            return .hex(0xFE8485)
+    case 500:                 return .hex(0xF46969)
+    case 600:                 return .hex(0xFE446B)
+    case 700, 800, 900:       return .hex(0x4A0A3F)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_green(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200:            return .hex(0xe5f3e9)
+    case 300:                 return .hex(0xceebd7)
+    case 400:                 return .hex(0x2bde73)
+    case 500:                 return .hex(0x25cb68)
+    case 600, 700, 800, 900:  return .hex(0x05af3c)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_green(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200:            return .hex(0xe5f3e9)
+    case 300:                 return .hex(0xceebd7)
+    case 400:                 return .hex(0x2bde73)
+    case 500:                 return .hex(0x25cb68)
+    case 600, 700, 800, 900:  return .hex(0x05af3c)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_grey(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100:                     return .hex(0xFBFBFA)
+    case 200:                     return .hex(0xF7F7F6)
+    case 300:                     return .hex(0xF2F2F2)
+    case 400:                     return .hex(0xEDEDED)
+    case 500, 600, 700, 800, 900: return .hex(0xDCDEDD)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_green(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200:            return .hex(0xe5f3e9)
+    case 300:                 return .hex(0xceebd7)
+    case 400:                 return .hex(0x2bde73)
+    case 500:                 return .hex(0x25cb68)
+    case 600, 700, 800, 900:  return .hex(0x05af3c)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_peach(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200, 300:            return .hex(0xF4E9D8)
+    case 350:                      return .hex(0xF6E3D8)
+    case 400:                      return .hex(0xF4DACC)
+    case 500, 600:                 return .hex(0xFF6A59)
+    case 700, 800, 900:            return .hex(0xFD4616)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_red(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200, 300, 400, 500, 600, 700, 800, 900:
+      return .hex(0xEF0707)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_sage(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200, 300:           return .hex(0xE6EBE2)
+    case 400:                     return .hex(0xD1E4DE)
+    case 500, 600, 700, 800, 900: return .hex(0xD7DED9)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_teal(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100, 200, 300:       return .hex(0x96D8DB)
+    case 400:                 return .hex(0x2DBECF)
+    case 500:                 return .hex(0x21ABBB)
+    case 600, 700, 800, 900:  return .hex(0x122C49)
+    default:
+      fatalError()
+    }
+  }
+
+  public static func ksr_violet(weight weight: Int) -> UIColor {
+    switch weight {
+    case 100:                 return .hex(0xF8F9FD)
+    case 200:                 return .hex(0xE3ECFA)
+    case 300:                 return .hex(0xCACDF6)
+    case 400:                 return .hex(0xD4C2F2)
+    case 500:                 return .hex(0x997EF2)
+    case 600, 700, 800:       return .hex(0x504083)
+    case 850:                 return .hex(0x392B84)
+    case 900:                 return .hex(0x0B055E)
+    default:
+      fatalError()
+    }
+  }
+
+  @available(*, deprecated=1.0)
   public static var ksr_black: UIColor {
     return .hex(0x000000)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_blue: UIColor {
     return .hex(0x00a0ff)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_darkBlue: UIColor {
     return .hex(0x081245)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_lightBlue: UIColor {
     return .hex(0xe8f6ff)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_royalBlue: UIColor {
     return .hex(0x00a0ff)
   }
@@ -58,58 +190,62 @@ extension UIColor {
     return .hex(0x3b5998)
   }
 
-  public static var ksr_darkGreen: UIColor {
-    return .hex(0x25CB68)
+  @available(*, deprecated=1.0)
+  public static var ksr_goldenBell: UIColor {
+    return .hex(0xD79211)
   }
 
-  public static var ksr_green: UIColor {
-    return .hex(0x2bde73)
-  }
-
-  public static var ksr_lightGreen: UIColor {
-    return .hex(0xdef7e0)
-  }
-
+  @available(*, deprecated=1.0)
   public static var ksr_gray: UIColor {
     return .hex(0xd9d9de)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_blackGray: UIColor {
     return .hex(0x464646)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_darkGray: UIColor {
     return .hex(0x828587)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_lightGray: UIColor {
     return .hex(0xf7fafa)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_mediumGray: UIColor {
     return .hex(0xebebee)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_offWhite: UIColor {
     return .hex(0xfafafa)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_mint: UIColor {
     return .hex(0xe3fdf9)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_pink: UIColor {
     return .hex(0xffecf0)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_textDefault: UIColor {
     return .hex(0x000000)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_darkGrayText: UIColor {
     return .hex(0x828587)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_lightGrayText: UIColor {
     return .hex(0xf7fafa)
   }
@@ -118,11 +254,23 @@ extension UIColor {
     return .hex(0x00aced)
   }
 
+  @available(*, deprecated=1.0)
+  public static var ksr_whisper: UIColor {
+    return .hex(0xFBFAF9)
+  }
+
+  @available(*, deprecated=1.0)
   public static var ksr_white: UIColor {
     return .hex(0xffffff)
   }
 
+  @available(*, deprecated=1.0)
   public static var ksr_yellow: UIColor {
     return .hex(0xffffc9)
+  }
+
+  @available(*, deprecated=1.0)
+  public static var ksr_customIndigo: UIColor {
+    return .hex(0x392B84)
   }
 }
