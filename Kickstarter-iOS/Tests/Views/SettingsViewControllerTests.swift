1f6f81f17442cad9651f257adc87ef8b9de4a1ba
Brandon Williams <mbw234@gmail.com>
2016-09-16 21:21:43 -0400

    Added screenshots for creator and member settings. (#253)

--- Kickstarter-iOS/Tests/Views/SettingsViewControllerTests.swift
+++ Kickstarter-iOS/Tests/Views/SettingsViewControllerTests.swift
@@ -7,7 +7,7 @@ import XCTest
 
 internal final class SettingsViewControllerTests: TestCase {
 
-  func testView() {
+  func testNonCreator() {
     let currentUser = .template |> User.lens.stats.backedProjectsCount .~ 1234
 
     Language.allLanguages.forEach { language in
@@ -17,7 +17,41 @@ internal final class SettingsViewControllerTests: TestCase {
         let (parent, _) = traitControllers(device: .phone4_7inch, orientation: .portrait, child: vc)
         parent.view.frame.size.height = 1_450
 
-        FBSnapshotVerifyView(vc.view, identifier: "Settings - \(language)")
+        FBSnapshotVerifyView(vc.view, identifier: "lang_\(language)")
+      }
+    }
+  }
+
+  func testCreator() {
+    let currentUser = .template
+      |> User.lens.stats.backedProjectsCount .~ 1234
+      |> User.lens.stats.createdProjectsCount .~ 2
+
+    Language.allLanguages.forEach { language in
+      withEnvironment(currentUser: currentUser, language: language, mainBundle: NSBundle.framework) {
+
+        let vc = SettingsViewController.instantiate()
+        let (parent, _) = traitControllers(device: .phone4_7inch, orientation: .portrait, child: vc)
+        parent.view.frame.size.height = 1_650
+
+        FBSnapshotVerifyView(vc.view, identifier: "lang_\(language)")
+      }
+    }
+  }
+
+  func testMember() {
+    let currentUser = .template
+      |> User.lens.stats.backedProjectsCount .~ 1234
+      |> User.lens.stats.memberProjectsCount .~ 2
+
+    Language.allLanguages.forEach { language in
+      withEnvironment(currentUser: currentUser, language: language, mainBundle: NSBundle.framework) {
+
+        let vc = SettingsViewController.instantiate()
+        let (parent, _) = traitControllers(device: .phone4_7inch, orientation: .portrait, child: vc)
+        parent.view.frame.size.height = 1_450
+
+        FBSnapshotVerifyView(vc.view, identifier: "lang_\(language)")
       }
     }
   }
