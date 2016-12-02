5c028e12de691d31b7bce7d185670a5abca26fe7
Gina Binetti <theginbin@gmail.com>
2016-12-01 19:38:18 -0500

    style friends/facebook

--- Kickstarter-iOS/Views/Cells/FindFriendsHeaderCell.swift
+++ Kickstarter-iOS/Views/Cells/FindFriendsHeaderCell.swift
@@ -10,7 +10,9 @@ protocol FindFriendsHeaderCellDelegate: class {
 
 internal final class FindFriendsHeaderCell: UITableViewCell, ValueCell {
 
+  @IBOutlet private weak var cardView: UIView!
   @IBOutlet private weak var closeButton: UIButton!
+  @IBOutlet private weak var containerView: UIView!
   @IBOutlet private weak var findFriendsButton: UIButton!
   @IBOutlet private weak var subtitleLabel: UILabel!
   @IBOutlet private weak var titleLabel: UILabel!
@@ -38,35 +40,44 @@ internal final class FindFriendsHeaderCell: UITableViewCell, ValueCell {
   internal override func bindStyles() {
     super.bindStyles()
 
+    self
+      |> baseTableViewCellStyle()
+      |> UITableViewCell.lens.contentView.layoutMargins %~~ { _, cell in
+        cell.traitCollection.isRegularRegular
+          ? .init(topBottom: Styles.grid(10), leftRight: Styles.grid(20))
+          : .init(top: Styles.grid(3), left: Styles.grid(2), bottom: Styles.gridHalf(3),
+                  right: Styles.grid(2))
+    }
+
+    self.cardView
+      |> dropShadowStyle()
+
+    self.containerView
+      |> UIView.lens.layoutMargins .~ .init(all: Styles.grid(2))
+
     self.titleLabel
-      |> UILabel.lens.font .~ .ksr_title3()
-      |> UILabel.lens.textColor .~ .ksr_text_navy_900
+      |> UILabel.lens.font .~ .ksr_headline(size: 14)
+      |> UILabel.lens.textColor .~ .ksr_text_navy_700
       |> UILabel.lens.text %~ { _ in Strings.Discover_more_projects() }
 
     self.subtitleLabel
-      |> UILabel.lens.font .~ .ksr_subhead()
+      |> UILabel.lens.font .~ .ksr_subhead(size: 12)
       |> UILabel.lens.textColor .~ .ksr_text_navy_600
       |> UILabel.lens.text %~ { _ in Strings.Follow_your_Facebook_friends_and_get_notified() }
 
     self.closeButton
+      |> UIButton.lens.tintColor .~ .ksr_navy_700
       |> UIButton.lens.targets .~ [(self, action: #selector(closeButtonTapped), .TouchUpInside)]
+      |> UIButton.lens.contentEdgeInsets .~ .init(top: Styles.grid(1), left: Styles.grid(3), bottom: Styles.grid(3), right: Styles.grid(2))
 
     self.findFriendsButton
       |> navyButtonStyle
+      |> UIButton.lens.titleLabel.font .~ .ksr_headline(size: 12)
       |> UIButton.lens.targets .~ [(self, action: #selector(findFriendsButtonTapped), .TouchUpInside)]
+      |> UIButton.lens.contentEdgeInsets .~ .init(topBottom: 8, leftRight: 24)
       |> UIButton.lens.title(forState: .Normal) %~ { _ in
         Strings.social_following_header_button_find_your_friends()
     }
-
-    self
-      |> baseTableViewCellStyle()
-      |> UITableViewCell.lens.backgroundColor .~ .whiteColor()
-      |> UITableViewCell.lens.contentView.layoutMargins %~~ { _, cell in
-        cell.traitCollection.isRegularRegular
-          ? .init(topBottom: Styles.grid(6), leftRight: Styles.grid(20))
-          : .init(top: Styles.gridHalf(3), left: Styles.grid(2), bottom: Styles.gridHalf(3),
-                  right: Styles.grid(2))
-      }
   }
 
   @objc func closeButtonTapped() {
