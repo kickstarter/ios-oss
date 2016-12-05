fdbe6b4e63ab5f974174e0c1d4860fbecc42b741
Brandon Williams <mbw234@gmail.com>
2016-12-05 10:33:16 -0500

    Fix footer for support email. (#408)

--- Kickstarter-iOS/Library/MFMailComposeViewController.swift
+++ Kickstarter-iOS/Library/MFMailComposeViewController.swift
@@ -13,12 +13,21 @@ internal extension MFMailComposeViewController {
 
     mcvc.setToRecipients([Strings.support_email_to()])
 
-    let body = Strings.support_email_body() +
-      "\(NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"]) | " +
-      "\(UIDevice.currentDevice().systemVersion) | " +
-      "\(UIDevice.currentDevice().model)\n" // todo: should be full deviceModel as used in Koala
+    let app: AnyObject = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] ?? ""
+    let os = UIDevice.currentDevice().systemVersion
+    let user = (AppEnvironment.current.currentUser?.id).flatMap(String.init) ?? "Logged out"
+
+    let body = "\(Strings.support_email_body())\n\(user) | \(app) | \(os) | \(deviceModel())\n"
     mcvc.setMessageBody(body, isHTML: false)
 
     return mcvc
   }
+}
+
+private func deviceModel() -> String {
+  var size: Int = 0
+  sysctlbyname("hw.machine", nil, &size, nil, 0)
+  var machine = [CChar](count: Int(size), repeatedValue: 0)
+  sysctlbyname("hw.machine", &machine, &size, nil, 0)
+  return String.fromCString(machine) ?? "Unknown"
 }
