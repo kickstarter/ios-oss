1ef0e59854d06633377d7b9e6a5bf90be9b22d9c
Justin Swart <jwswart@gmail.com>
2016-12-21 11:54:25 +0200

    LiveStreamContainerViewModel tests, update KsLive

---
+++ Library/Tests/ViewModels/LiveStreamContainerViewModelTests.swift
@@ -0,0 +1,164 @@
+import Prelude
+import ReactiveCocoa
+import Result
+import XCTest
+@testable import KsApi
+@testable import KsLive
+@testable import Library
+@testable import ReactiveExtensions_TestHelpers
+
+internal final class LiveStreamContainerViewModelTests: TestCase {
+  private let vm: LiveStreamContainerViewModelType = LiveStreamContainerViewModel()
+
+  private let createAndConfigureLiveStreamViewController = TestObserver<(Project, LiveStreamEvent), NoError>()
+  private let dismiss = TestObserver<(), NoError>()
+  private let liveStreamState = TestObserver<LiveStreamViewControllerState, NoError>()
+  private let loaderText = TestObserver<String, NoError>()
+  private let projectImageUrl = TestObserver<NSURL, NoError>()
+  private let showVideoView = TestObserver<Bool, NoError>()
+  private let titleViewText = TestObserver<String, NoError>()
+
+  override func setUp() {
+    super.setUp()
+
+    self.vm.outputs.createAndConfigureLiveStreamViewController.observe(
+      self.createAndConfigureLiveStreamViewController.observer)
+    self.vm.outputs.dismiss.observe(self.dismiss.observer)
+    self.vm.outputs.liveStreamState.observe(self.liveStreamState.observer)
+    self.vm.outputs.loaderText.observe(self.loaderText.observer)
+    self.vm.outputs.projectImageUrl.observe(self.projectImageUrl.observer)
+    self.vm.outputs.showVideoView.observe(self.showVideoView.observer)
+    self.vm.outputs.titleViewText.observe(self.titleViewText.observer)
+  }
+
+  func testCreateLiveStreamViewController() {
+    let project = Project.template
+    let event = LiveStreamEvent.template
+
+    self.vm.inputs.viewDidLoad()
+    self.vm.inputs.configureWith(project: project, event: event)
+
+    XCTAssertTrue(self.createAndConfigureLiveStreamViewController.lastValue == (project, event))
+  }
+
+  func testDismiss() {
+    self.vm.inputs.closeButtonTapped()
+
+    self.dismiss.assertValueCount(1)
+  }
+
+  func testLiveStreamStates() {
+    let project = Project.template
+    let event = LiveStreamEvent.template
+
+    self.vm.inputs.viewDidLoad()
+    self.vm.inputs.configureWith(project: project, event: event)
+
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .greenRoom)
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .loading)
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .live(playbackState: .loading, startTime: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .live(playbackState: .playing, startTime: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .live(playbackState: .error(error: .sessionInterrupted), startTime: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .replay(playbackState: .loading, replayAvailable: false, duration: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .replay(playbackState: .playing, replayAvailable: false, duration: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .replay(playbackState: .error(error: .failedToConnect), replayAvailable: false,
+        duration: 123))
+
+    // Test begins with an implicit loading state before any others
+    self.liveStreamState.assertValues([
+      .loading,
+      .greenRoom,
+      .loading,
+      .live(playbackState: .loading, startTime: 123),
+      .live(playbackState: .playing, startTime: 123),
+      .live(playbackState: .error(error: .sessionInterrupted), startTime: 123),
+      .replay(playbackState: .loading, replayAvailable: false, duration: 123),
+      .replay(playbackState: .playing, replayAvailable: false, duration: 123),
+      .replay(playbackState: .error(error: .failedToConnect), replayAvailable: false, duration: 123)
+    ])
+  }
+
+  func testLoaderText() {
+    let project = Project.template
+    let event = LiveStreamEvent.template
+
+    self.vm.inputs.viewDidLoad()
+    self.vm.inputs.configureWith(project: project, event: event)
+
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .greenRoom)
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .loading)
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .replay(playbackState: .loading, replayAvailable: false, duration: 123))
+
+    self.loaderText.assertValues([
+      "Loading",
+      "The live stream will start soon",
+      "Loading",
+      "The replay will start soon"
+    ])
+  }
+
+  func testProjectImageUrl() {
+    let project = Project.template
+
+    self.vm.inputs.configureWith(project: project, event: nil)
+    self.vm.inputs.viewDidLoad()
+
+    XCTAssertTrue(self.projectImageUrl.lastValue?.absoluteString == "http://www.kickstarter.com/full.jpg")
+  }
+
+  func testShowVideoView() {
+    let project = Project.template
+    let event = LiveStreamEvent.template
+
+    self.vm.inputs.viewDidLoad()
+    self.vm.inputs.configureWith(project: project, event: event)
+
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .greenRoom)
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .live(playbackState: .playing, startTime: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .loading)
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .replay(playbackState: .playing, replayAvailable: false, duration: 123))
+
+    self.showVideoView.assertValues([
+      false,
+      true,
+      false,
+      true
+    ])
+  }
+
+  func testTitleViewText() {
+    let project = Project.template
+    let event = LiveStreamEvent.template
+
+    self.vm.inputs.viewDidLoad()
+    self.vm.inputs.configureWith(project: project, event: event)
+
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .greenRoom)
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .live(playbackState: .loading, startTime: 123))
+    self.vm.inputs.liveStreamViewControllerStateChanged(state: .loading)
+    self.vm.inputs.liveStreamViewControllerStateChanged(
+      state: .replay(playbackState: .loading, replayAvailable: false, duration: 123))
+
+    self.titleViewText.assertValues([
+      "Loading",
+      "Starting soon",
+      "Live",
+      "Loading",
+      "Recorded Live"
+    ])
+  }
+}
+
+private func == (tuple1: (Project, LiveStreamEvent)?, tuple2: (Project, LiveStreamEvent)) -> Bool {
+  if let tuple1 = tuple1 {
+    return tuple1.0 == tuple2.0 && tuple1.1 == tuple2.1
+  }
+
+  return false
+}
