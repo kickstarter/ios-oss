d3474a784051fccf6185690a9a383f018f7e09e4
Brandon Williams <mbw234@gmail.com>
2016-12-23 12:17:34 -0500

    swiftlint

--- Library/UIAlertController.swift
+++ Library/UIAlertController.swift
@@ -176,7 +176,7 @@ public extension UIAlertController {
     friendsCount: Int,
     yesHandler: @escaping ((UIAlertAction) -> Void),
     noHandler: @escaping ((UIAlertAction) -> Void)) -> UIAlertController {
-    
+
     let alertController = UIAlertController(
       title: Strings.social_following_stats_button_follow_all_friends(remote_friends_count: friendsCount),
       message: Strings.social_following_nice_youre_about_to_follow_all_friends(),
