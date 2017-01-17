ed9b9ec6dea32c468a3bbb142206a470f7f11cfe
Brandon Williams <mbw234@gmail.com>
2017-01-17 12:29:45 +0200

    update to share logic

--- Library/LiveStreamActivityItemProvider.swift
+++ Library/LiveStreamActivityItemProvider.swift
@@ -20,9 +20,7 @@
         if activityType == .mail || activityType == .message {
           return liveStreamEvent.stream.projectName
         } else if activityType == .postToTwitter {
-          return Strings.Creator_name_is_streaming_live_on_Kickstarter(
-            creator_name: liveStreamEvent.creator.name
-          )
+          return twitterInitialText(forLiveStreamEvent: liveStreamEvent)
         } else if activityType == .copyToPasteboard || activityType == .postToFacebook {
           return liveStreamEvent.stream.webUrl
         }
@@ -30,5 +28,22 @@
 
       return self.activityViewControllerPlaceholderItem(activityViewController)
     }
+  }
+
+  private func twitterInitialText(forLiveStreamEvent liveStreamEvent: LiveStreamEvent) -> String {
+    if liveStreamEvent.stream.liveNow {
+      return Strings.Creator_name_is_streaming_live_on_Kickstarter(creator_name: liveStreamEvent.creator.name)
+    }
+
+    if liveStreamEvent.stream.startDate < AppEnvironment.current.dateType.init().date {
+      return Strings.Creator_name_was_streaming_live_on_Kickstarter(
+        creator_name: liveStreamEvent.creator.name
+      )
+    }
+
+    return Strings.Creator_name_will_be_streaming_live_on_Kickstarter_in_duration(
+      creator_name: liveStreamEvent.creator.name,
+      in_duration: Format.relative(secondsInUTC: liveStreamEvent.stream.startDate.timeIntervalSince1970)
+    )
   }
 #endif
