485cc9c77342d206e303cba6535428ba28e1b262
Christella Dolmo <cdolm92@gmail.com>
2017-01-30 14:36:14 -0500

    test pass

--- Library/Tests/ViewModels/SearchEmptyStateCellViewModelTests.swift
+++ Library/Tests/ViewModels/SearchEmptyStateCellViewModelTests.swift
@@ -23,8 +23,9 @@ internal final class SearchEmptyStateCellViewModelTests: TestCase {
 
     self.vm.inputs.configureWith(param: discoveryParams)
 
-    self.searchTermNotFoundLabelText.assertValues
-    ([Strings.We_couldnt_find_anything_for_search_term(search_term: "abcdefgh")],
-                                         "Emits to the user that the search term could not be found.")
+    self.searchTermNotFoundLabelText.assertValues(
+      [Strings.We_couldnt_find_anything_for_search_term(search_term: "abcdefgh")],
+      "Emits to the user that the search term could not be found."
+    )
   }
 }
