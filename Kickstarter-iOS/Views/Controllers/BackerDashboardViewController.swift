a629b92901336eea18a77190f4235d6e03af1738
Gina Binetti <theginbin@gmail.com>
2017-02-28 19:58:15 -0500

    basic switching and scrolling wip

--- Kickstarter-iOS/Views/Controllers/BackerDashboardViewController.swift
+++ Kickstarter-iOS/Views/Controllers/BackerDashboardViewController.swift
@@ -6,21 +6,27 @@ import UIKit
 internal final class BackerDashboardViewController: UIViewController {
 
   @IBOutlet private weak var avatarView: CircleAvatarImageView!
-  @IBOutlet private weak var backedSortLabel: UILabel!
+  @IBOutlet private weak var backedContainerView: UIView!
+  @IBOutlet private weak var backedSortButton: UIButton!
   @IBOutlet private weak var backerNameLabel: UILabel!
   @IBOutlet private weak var backerNumberLabel: UILabel!
   @IBOutlet private weak var dividerView: UIView!
-  @IBOutlet private weak var headerBorderView: UIView!
   @IBOutlet fileprivate weak var headerTopBackgroundView: UIView!
   @IBOutlet fileprivate weak var headerView: UIView!
   @IBOutlet fileprivate weak var headerViewTopConstraint: NSLayoutConstraint!
-  @IBOutlet private weak var savedSortLabel: UILabel!
+  @IBOutlet private weak var savedContainerView: UIView!
+  @IBOutlet private weak var savedSortButton: UIButton!
+  @IBOutlet private weak var selectedLineView: UIView!
+  @IBOutlet private weak var selectedLineLeadingConstraint: NSLayoutConstraint!
+  @IBOutlet private weak var selectedLineWidthConstraint: NSLayoutConstraint!
 
   private weak var backedProjectsViewController: ProfileBackedProjectsViewController!
+  private weak var savedProjectsViewController: ProfileSavedProjectsViewController!
 
   private let viewModel: BackerDashboardViewModelType = BackerDashboardViewModel()
 
-  var previousScrollOffset: CGFloat = 0;
+  private var currentScrollOffset: CGFloat = 0 // temp
+  private var isCollapsed: Bool = false
 
   internal static func instantiate() -> BackerDashboardViewController {
     return Storyboard.BackerDashboard.instantiate(BackerDashboardViewController.self)
@@ -33,6 +39,18 @@ internal final class BackerDashboardViewController: UIViewController {
       .flatMap { $0 as? ProfileBackedProjectsViewController }.first
 
     self.backedProjectsViewController.delegate = self
+
+    self.savedProjectsViewController = self.childViewControllers
+      .flatMap { $0 as? ProfileSavedProjectsViewController }.first
+
+    self.savedProjectsViewController.delegate = self
+
+    self.savedProjectsViewController.view.isHidden = true
+    self.savedContainerView.isHidden = true
+
+    self.backedSortButton.addTarget(self, action: #selector(backedButtonTapped), for: .touchUpInside)
+
+    self.savedSortButton.addTarget(self, action: #selector(savedButtonTapped), for: .touchUpInside)
   }
 
   internal override func viewWillAppear(_ animated: Bool) {
@@ -46,25 +64,71 @@ internal final class BackerDashboardViewController: UIViewController {
   internal override func bindStyles() {
     super.bindStyles()
   }
+
+  fileprivate func moveHeader(with scrollView: UIScrollView) {
+    let minHeaderHeight = self.headerView.frame.size.height - self.headerTopBackgroundView.frame.size.height
+      + Styles.grid(2)
+
+    if scrollView.contentOffset.y > 0 {
+      self.headerViewTopConstraint.constant = -scrollView.contentOffset.y
+    }
+
+    if self.headerViewTopConstraint.constant <= -minHeaderHeight {
+      self.headerViewTopConstraint.constant = -minHeaderHeight
+      isCollapsed = true
+    } else {
+      isCollapsed = false
+    }
+
+    self.currentScrollOffset = abs(self.headerViewTopConstraint.constant)
+
+    print("offset = \(scrollView.contentOffset.y)")
+  }
+
+  @objc private func backedButtonTapped() {
+    self.backedProjectsViewController.view.isHidden = false
+    self.backedContainerView.isHidden = false
+    self.savedProjectsViewController.view.isHidden = true
+    self.savedContainerView.isHidden = true
+
+    self.selectedLineLeadingConstraint.constant = 0
+    self.selectedLineWidthConstraint.constant = self.backedSortButton.frame.size.width
+  }
+
+  @objc private func savedButtonTapped() {
+    self.backedProjectsViewController.view.isHidden = true
+    self.backedContainerView.isHidden = true
+    self.savedProjectsViewController.view.isHidden = false
+    self.savedContainerView.isHidden = false
+
+    self.selectedLineLeadingConstraint.constant = self.savedSortButton.frame.origin.x
+    self.selectedLineWidthConstraint.constant = self.savedSortButton.frame.size.width
+  }
 }
 
 extension BackerDashboardViewController: ProfileBackedProjectsViewControllerDelegate {
   func profileBackedProjectsDidScroll(_ scrollView: UIScrollView) {
-    let minHeaderHeight = self.headerView.frame.size.height - self.headerTopBackgroundView.frame.size.height
-      + Styles.grid(2)
-
-    // super basic
-    self.headerViewTopConstraint.constant = -scrollView.contentOffset.y
-
-    if self.headerViewTopConstraint.constant <= -minHeaderHeight {
-      self.headerViewTopConstraint.constant = -minHeaderHeight
-    }
+    self.moveHeader(with: scrollView)
   }
 
   func profileBackedProjectsDidEndDecelerating(_ scrollView: UIScrollView) {
   }
 
   func profileBackedProjectsDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    if !decelerate {
+    }
+  }
+}
+
+extension BackerDashboardViewController: ProfileSavedProjectsViewControllerDelegate {
+  func profileSavedProjectsDidScroll(_ scrollView: UIScrollView) {
+    self.moveHeader(with: scrollView)
+  }
+
+  func profileSavedProjectsDidEndDecelerating(_ scrollView: UIScrollView) {
+  }
+
+  func profileSavedProjectsDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
     if !decelerate {
     }
   }
---
+++ Kickstarter-iOS/Views/Controllers/ProfileSavedProjectsViewController.swift
@@ -0,0 +1,92 @@
+import KsApi
+import Library
+import Prelude
+import UIKit
+
+internal protocol ProfileSavedProjectsViewControllerDelegate: class {
+  func profileSavedProjectsDidEndDecelerating(_ scrollView: UIScrollView)
+  func profileSavedProjectsDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool)
+  func profileSavedProjectsDidScroll(_ scrollView: UIScrollView)
+}
+
+internal final class ProfileSavedProjectsViewController: UITableViewController {
+
+  //  fileprivate let viewModel: ProfileSavedProjectsViewModelType = ProfileSavedProjectsViewModel()
+  //  fileprivate let dataSource = ProfileSavedProjectsDataSource()
+
+  internal weak var delegate: ProfileSavedProjectsViewControllerDelegate?
+
+  //  internal static func configureWith(sort: BackedSort) {
+  //  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    //self.tableView.estimatedRowHeight = 100.0
+    self.tableView.rowHeight = UITableViewAutomaticDimension
+    //self.tableView.dataSource = dataSource
+
+    //self.viewModel.inputs.viewDidLoad()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+
+  override func bindViewModel() {
+    super.bindViewModel()
+  }
+
+  override func bindStyles() {
+    super.bindStyles()
+
+    _ = self
+      |> baseTableControllerStyle()
+      |> UIViewController.lens.title %~ { _ in Strings.Follow_friends() }
+
+    _ = self.navigationController?.navigationBar
+      ?|> baseNavigationBarStyle
+  }
+
+  override internal func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell,
+                                   forRowAt indexPath: IndexPath) {
+
+    //    if let statsCell = cell as? FindFriendsStatsCell, statsCell.delegate == nil {
+    //      statsCell.delegate = self
+    //    } else if let fbConnectCell = cell as? FindFriendsFacebookConnectCell, fbConnectCell.delegate == nil {
+    //      fbConnectCell.delegate = self
+    //    }
+    //
+    //    self.viewModel.inputs.willDisplayRow(self.dataSource.itemIndexAt(indexPath),
+    //                                         outOf: self.dataSource.numberOfItems())
+  }
+
+  /// placeholder
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel!.text = "Saved project #\(indexPath.row)"
+    return cell
+  }
+  ///
+
+  override internal func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    self.delegate?.profileSavedProjectsDidScroll(scrollView)
+  }
+
+  override internal func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    self.delegate?.profileSavedProjectsDidEndDecelerating(scrollView)
+  }
+
+  override internal func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    self.delegate?.profileSavedProjectsDidEndDragging(scrollView, willDecelerate: decelerate)
+  }
+}
+
