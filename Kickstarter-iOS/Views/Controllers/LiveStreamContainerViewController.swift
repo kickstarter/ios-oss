7998067c85355de1d4c4c3f28bbb56c6d0cbc298
Justin Swart <jwswart@gmail.com>
2017-03-16 15:37:03 +0200

    Reorder modifier

--- Kickstarter-iOS/Views/Controllers/LiveStreamContainerViewController.swift
+++ Kickstarter-iOS/Views/Controllers/LiveStreamContainerViewController.swift
@@ -192,7 +192,7 @@ public final class LiveStreamContainerViewController: UIViewController {
     }
   }
 
-  override public var prefersStatusBarHidden: Bool {
+  public override var prefersStatusBarHidden: Bool {
     return true
   }
 
