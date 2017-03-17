410a59d00521d48f2ac9071b5fd5bcd8649a6eeb
Justin Swart <jwswart@gmail.com>
2017-03-17 15:14:22 +0200

    Fix live screenshot tests

--- Kickstarter-iOS/Tests/Views/LiveStreamContainerViewControllerTests.swift
+++ Kickstarter-iOS/Tests/Views/LiveStreamContainerViewControllerTests.swift
@@ -18,40 +18,6 @@ internal final class LiveStreamContainerViewControllerTests: TestCase {
     UIView.setAnimationsEnabled(true)
     super.tearDown()
   }
-//swiftlint:disable line_length
-//
-//  func testLive() {
-//    let liveStreamEvent = .template
-//      |> LiveStreamEvent.lens.startDate .~ (MockDate().addingTimeInterval(-86_400)).date
-//      |> LiveStreamEvent.lens.liveNow .~ true
-//      |> LiveStreamEvent.lens.name .~ "Title of the live stream goes here and can be 60 chr max"
-//      |> LiveStreamEvent.lens.description .~ ("175 char max. 175 char max 175 char max message with " +
-//        "a max character count. Hi everyone! We’re doing an exclusive performance of one of our new tracks!")
-//
-//    let devices = [Device.phone4_7inch, .phone4inch, .pad]
-//    let orientations = [Orientation.landscape, .portrait]
-//
-//    let liveStreamService = MockLiveStreamService(fetchEventResult: Result(liveStreamEvent))
-//
-//    combos(Language.allLanguages, devices, orientations).forEach { lang, device, orientation in
-//      withEnvironment(apiDelayInterval: .seconds(3), language: lang, liveStreamService: liveStreamService) {
-//        let vc = LiveStreamContainerViewController.configuredWith(project: .template,
-//                                                                  liveStreamEvent: liveStreamEvent,
-//                                                                  refTag: .projectPage,
-//                                                                  presentedFromProject: false)
-//
-//        let (parent, _) = traitControllers(device: device, orientation: orientation, child: vc)
-//        self.scheduler.advance(by: .seconds(3))
-//
-//        vc.liveStreamViewControllerNumberOfPeopleWatchingChanged(controller: nil, numberOfPeople: 2_532)
-//
-//        FBSnapshotVerifyView(
-//          parent.view, identifier: "lang_\(lang)_device_\(device)_orientation_\(orientation)"
-//        )
-//      }
-//    }
-//  }
-//
 //  func testFreshLiveStreamEventFetchFailed() {
 //    let liveStreamEvent = .template
 //      |> LiveStreamEvent.lens.startDate .~ (MockDate().addingTimeInterval(-86_400)).date
@@ -206,4 +172,35 @@ internal final class LiveStreamContainerViewControllerTests: TestCase {
       }
     }
   }
+
+  func testLive() {
+    let liveStreamEvent = .template
+      |> LiveStreamEvent.lens.startDate .~ (MockDate().addingTimeInterval(-86_400)).date
+      |> LiveStreamEvent.lens.liveNow .~ true
+      |> LiveStreamEvent.lens.name .~ "Title of the live stream goes here and can be 60 chr max"
+      |> LiveStreamEvent.lens.description .~ ("175 char max. 175 char max 175 char max message with " +
+        "a max character count. Hi everyone! We’re doing an exclusive performance of one of our new tracks!")
+
+    let devices = [Device.phone4_7inch, .phone4inch, .pad]
+    let orientations = [Orientation.landscape, .portrait]
+
+    let liveStreamService = MockLiveStreamService(fetchEventResult: Result(liveStreamEvent))
+
+    combos(Language.allLanguages, devices, orientations).forEach { lang, device, orientation in
+      withEnvironment(apiDelayInterval: .seconds(3), language: lang, liveStreamService: liveStreamService) {
+        let vc = LiveStreamContainerViewController.configuredWith(project: .template,
+                                                                  liveStreamEvent: liveStreamEvent,
+                                                                  refTag: .projectPage,
+                                                                  presentedFromProject: false)
+
+        let (parent, _) = traitControllers(device: device, orientation: orientation, child: vc)
+        self.scheduler.advance(by: .seconds(3))
+
+        FBSnapshotVerifyView(
+          parent.view, identifier: "lang_\(lang)_device_\(device)_orientation_\(orientation)"
+        )
+      }
+    }
+  }
+
 }
