17a139767ff8dd0ac6bc09365eb0f942e9e435a3
Gina Binetti <theginbin@gmail.com>
2017-06-21 12:52:46 -0400

    add empty state for no messages

--- Kickstarter-iOS/Views/Cells/MessagesEmptyStateCell.swift
+++ Kickstarter-iOS/Views/Cells/MessagesEmptyStateCell.swift
@@ -18,6 +18,14 @@ internal final class MessagesEmptyStateCell: UITableViewCell, ValueCell {
   internal override func bindStyles() {
     super.bindStyles()
 
+    _ = self
+      |> baseTableViewCellStyle()
+      |> UITableViewCell.lens.contentView.layoutMargins %~~ { _, cell in
+        cell.traitCollection.isRegularRegular
+          ? .init(topBottom: Styles.grid(5), leftRight: Styles.grid(12))
+          : .init(topBottom: Styles.grid(4), leftRight: Styles.grid(2))
+    }
+
     _ = self.titleLabel
       |> UILabel.lens.textColor .~ .ksr_text_navy_700
       |> UILabel.lens.font .~ UIFont.ksr_headline(size: 18.0)
