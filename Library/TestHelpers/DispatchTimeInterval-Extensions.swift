0ed838221518cb3be654273e4997e512940afb71
Justin Swart <justinswart@users.noreply.github.com>
2017-07-26 15:47:37 -0400

    Fix compiler directive

--- Library/TestHelpers/DispatchTimeInterval-Extensions.swift
+++ Library/TestHelpers/DispatchTimeInterval-Extensions.swift
@@ -3,20 +3,31 @@ import Foundation
 extension DispatchTimeInterval {
 
   public func scale(by scale: Float) -> DispatchTimeInterval {
-    switch self {
-    case let .seconds(interval):
-      return .milliseconds(Int(Float(interval * 1_000) * scale))
-    case let .milliseconds(interval):
-      return .microseconds(Int(Float(interval * 1_000) * scale))
-    case let .microseconds(interval):
-      return .nanoseconds(Int(Float(interval * 1_000) * scale))
-    case let .nanoseconds(interval):
-      return .nanoseconds(Int(Float(interval) * scale))
-      #if swift(>=3.2)
-    case .never:
-      return .never
-      #endif
-    }
+    #if swift(>=3.2)
+      switch self {
+      case let .seconds(interval):
+        return .milliseconds(Int(Float(interval * 1_000) * scale))
+      case let .milliseconds(interval):
+        return .microseconds(Int(Float(interval * 1_000) * scale))
+      case let .microseconds(interval):
+        return .nanoseconds(Int(Float(interval * 1_000) * scale))
+      case let .nanoseconds(interval):
+        return .nanoseconds(Int(Float(interval) * scale))
+      case .never:
+        return .never
+      }
+    #else
+      switch self {
+      case let .seconds(interval):
+        return .milliseconds(Int(Float(interval * 1_000) * scale))
+      case let .milliseconds(interval):
+        return .microseconds(Int(Float(interval * 1_000) * scale))
+      case let .microseconds(interval):
+        return .nanoseconds(Int(Float(interval * 1_000) * scale))
+      case let .nanoseconds(interval):
+        return .nanoseconds(Int(Float(interval) * scale))
+      }
+    #endif
   }
 
   public func halved() -> DispatchTimeInterval {
@@ -24,20 +35,31 @@ extension DispatchTimeInterval {
   }
 
   fileprivate var nanoseconds: Int64 {
-    switch self {
-    case .seconds(let s):
-      return Int64(s) * Int64(NSEC_PER_SEC)
-    case .milliseconds(let ms):
-      return Int64(ms) * Int64(NSEC_PER_MSEC)
-    case .microseconds(let us):
-      return Int64(us) * Int64(NSEC_PER_USEC)
-    case .nanoseconds(let ns):
-      return Int64(ns)
-      #if swift(>=3.2)
-    case .never:
-      return Int64(0)
-      #endif
-    }
+    #if swift(>=3.2)
+      switch self {
+      case .seconds(let s):
+        return Int64(s) * Int64(NSEC_PER_SEC)
+      case .milliseconds(let ms):
+        return Int64(ms) * Int64(NSEC_PER_MSEC)
+      case .microseconds(let us):
+        return Int64(us) * Int64(NSEC_PER_USEC)
+      case .nanoseconds(let ns):
+        return Int64(ns)
+      case .never:
+        return .infinity
+      }
+    #else
+      switch self {
+      case .seconds(let s):
+        return Int64(s) * Int64(NSEC_PER_SEC)
+      case .milliseconds(let ms):
+        return Int64(ms) * Int64(NSEC_PER_MSEC)
+      case .microseconds(let us):
+        return Int64(us) * Int64(NSEC_PER_USEC)
+      case .nanoseconds(let ns):
+        return Int64(ns)
+      }
+    #endif
   }
 
   public static func + (lhs: DispatchTimeInterval, rhs: DispatchTimeInterval) -> DispatchTimeInterval {
