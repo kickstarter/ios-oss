93ab5baab5f43c11ffd6b7b3cb50fb3d8bf33f59
Boris Schneiderman <bormind@users.noreply.github.com>
2017-08-04 10:27:46 -0700

    Commenting on signalProducer properties

--- Library/ViewModels/DashboardViewModel.swift
+++ Library/ViewModels/DashboardViewModel.swift
@@ -102,11 +102,11 @@ public protocol DashboardViewModelType {
 public final class DashboardViewModel: DashboardViewModelInputs, DashboardViewModelOutputs,
   DashboardViewModelType {
 
-  //We use producers ot this properties during view appearance and projects retreaval process 
-  //to avoid any race condisions and keep last selected project param "handy" 
-  //for the moment projects are reteaved
+  //Project selected by user in the drawer ot by pysh for the project message thread
   private let selectedProjectProperty: SignalProducer<Param?, NoError>
+  //Selected project or the first project in the projects collection if no project was selected
   private let selectProjectPropertyOrFirst = MutableProperty<Param?>(nil)
+  //Last recieved push for message or nil if view is about to disappear
   private let messageThreadReceived = MutableProperty<(Param, MessageThread)?>(nil)
 
   public init() {
