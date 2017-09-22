cbf1c5390d3e2510aeca59fd03fddbe1725cd9db
Justin Swart <justinswart@users.noreply.github.com>
2017-09-21 18:55:24 -0700

    Fix ProjectPamphletContentViewControllerTests snapshot tests

--- Kickstarter-iOS/Views/Cells/ProjectPamphletMainCell.swift
+++ Kickstarter-iOS/Views/Cells/ProjectPamphletMainCell.swift
@@ -42,6 +42,11 @@ internal final class ProjectPamphletMainCell: UITableViewCell, ValueCell {
   @IBOutlet fileprivate weak var readMoreButton: UIButton!
   @IBOutlet fileprivate weak var stateLabel: UILabel!
   @IBOutlet fileprivate weak var statsStackView: UIStackView!
+  /* NB: We've introduced a height constraint here since iOS 11.0
+   * Try removing it in subsequent versions, it worked just fine without it before 11.0
+   * ¯\_(ツ)_/¯
+   */
+  @IBOutlet fileprivate weak var statsStackViewHeightConstraint: NSLayoutConstraint!
   @IBOutlet fileprivate weak var youreABackerContainerView: UIView!
   @IBOutlet fileprivate weak var youreABackerLabel: UILabel!
 
@@ -171,6 +176,8 @@ internal final class ProjectPamphletMainCell: UITableViewCell, ValueCell {
 
     _ = self.statsStackView
       |> UIStackView.lens.isAccessibilityElement .~ true
+
+    self.statsStackViewHeightConstraint.constant = Styles.grid(5)
 
     _ = self.youreABackerContainerView
       |> roundedStyle(cornerRadius: 2)
