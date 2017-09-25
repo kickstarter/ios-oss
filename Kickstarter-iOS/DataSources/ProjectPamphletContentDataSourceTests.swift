a22a1a814b5b51b48d1525c2b45d87fbb4b11cf0
Saturnino Collaço Teixeira Filho <scollaco@gmail.com>
2017-09-25 17:02:31 -0400

    Unavailable reward placement (native) (#248)
    * Created new section on rewards list to separate available and sold out rewards

--- Kickstarter-iOS/DataSources/ProjectPamphletContentDataSourceTests.swift
+++ Kickstarter-iOS/DataSources/ProjectPamphletContentDataSourceTests.swift
@@ -124,4 +124,34 @@ final class ProjectPamphletContentDataSourceTests: TestCase {
                      self.dataSource[IndexPath(row: 6, section: section)] as? ProjectPamphletSubpage)
     }
   }
+
+  func testAvailableRewardsSection_ShowsCorrectValues() {
+    let availableSection = ProjectPamphletContentDataSource.Section.availableRewards.rawValue
+    let unavailableSection = ProjectPamphletContentDataSource.Section.unavailableRewards.rawValue
+
+    let reward = Reward.template
+      |> Reward.lens.remaining .~ 1
+    let project = Project.template
+      |> Project.lens.rewards .~ [reward]
+
+    dataSource.load(project: project, liveStreamEvents: [])
+
+    XCTAssertEqual(1, self.dataSource.tableView(self.tableView, numberOfRowsInSection: availableSection))
+    XCTAssertEqual(0, self.dataSource.tableView(self.tableView, numberOfRowsInSection: unavailableSection))
+  }
+
+  func testUnavailableRewardsSection_ShowsCorrectValues() {
+    let availableSection = ProjectPamphletContentDataSource.Section.availableRewards.rawValue
+    let unavailableSection = ProjectPamphletContentDataSource.Section.unavailableRewards.rawValue
+
+    let reward = Reward.template
+      |> Reward.lens.remaining .~ 0
+    let project = Project.template
+      |> Project.lens.rewards .~ [reward]
+
+    dataSource.load(project: project, liveStreamEvents: [])
+
+    XCTAssertEqual(0, self.dataSource.tableView(self.tableView, numberOfRowsInSection: availableSection))
+    XCTAssertEqual(1, self.dataSource.tableView(self.tableView, numberOfRowsInSection: unavailableSection))
+  }
 }
