3c38bf7e5dd56e293291712a9f62fcfe554f2c41
Nino Collaco <scollaco@gmail.com>
2017-10-09 14:47:08 -0400

    Category template cleanup

--- KsApi/models/templates/RootCategoriesEnvelope.CategoryTemplates.swift
+++ KsApi/models/templates/RootCategoriesEnvelope.CategoryTemplates.swift
@@ -21,41 +21,27 @@ extension KsApi.RootCategoriesEnvelope.Category {
   internal static let filmAndVideo = template
     |> RootCategoriesEnvelope.Category.lens.id .~ "Q2F0ZWdvcnktMTE="
     <> RootCategoriesEnvelope.Category.lens.name .~ "Film & Video"
-    <> RootCategoriesEnvelope.Category.lens.subcategories .~
-    RootCategoriesEnvelope.Category.SubcategoryConnection(
-      totalCount: 0,
-      nodes: [RootCategoriesEnvelope.Category.SubcategoryConnection.Node(
-        id: "Q2F0ZWdvcnktMzA=",
-        name: "Documentary",
-        parentCategory: ParentCategory(id: "Q2F0ZWdvcnktMTE=", name: "Film & Video"),
-        totalProjectCount: 0)]
-  )
 
   internal static let games = template
     |> RootCategoriesEnvelope.Category.lens.id .~ "Q2F0ZWdvcnktMTI="
     <> RootCategoriesEnvelope.Category.lens.name .~ "Games"
-    <> RootCategoriesEnvelope.Category.lens.subcategories .~
-    RootCategoriesEnvelope.Category.SubcategoryConnection(
-      totalCount: 0,
-      nodes: [RootCategoriesEnvelope.Category.SubcategoryConnection.Node(
-        id: "Q2F0ZWdvcnktMzQ=",
-        name: "Tabletop Games",
-        parentCategory: ParentCategory(id: "Q2F0ZWdvcnktMTI=", name: "Games"),
-        totalProjectCount: 0)]
-  )
 
   internal static let illustration = template
     |> RootCategoriesEnvelope.Category.lens.id .~ "Q2F0ZWdvcnktMjI="
     <> RootCategoriesEnvelope.Category.lens.name .~ "Illustration"
     <> RootCategoriesEnvelope.Category.lens.parentId .~ RootCategoriesEnvelope.Category.art.id
+    <> RootCategoriesEnvelope.Category.lens.parent .~ RootCategoriesEnvelope.Category.art.parentCategory
 
   internal static let documentary = template
     |> RootCategoriesEnvelope.Category.lens.id .~ "Q2F0ZWdvcnktMzA="
     <> RootCategoriesEnvelope.Category.lens.name .~ "Documentary"
     <> RootCategoriesEnvelope.Category.lens.parentId .~ RootCategoriesEnvelope.Category.filmAndVideo.id
+    <> RootCategoriesEnvelope.Category.lens.parent .~
+    RootCategoriesEnvelope.Category.filmAndVideo.parentCategory
 
   internal static let tabletopGames = template
     |> RootCategoriesEnvelope.Category.lens.id .~ "Q2F0ZWdvcnktMzQ="
     <> RootCategoriesEnvelope.Category.lens.name .~ "Tabletop Games"
     <> RootCategoriesEnvelope.Category.lens.parentId .~ RootCategoriesEnvelope.Category.games.id
+    <> RootCategoriesEnvelope.Category.lens.parent .~ RootCategoriesEnvelope.Category.games.parentCategory
 }
