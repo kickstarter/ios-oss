3c09e1cff0f46b69d7be37aef6de2978f7ad9cee
Christella Dolmo <cdolm92@gmail.com>
2017-11-02 14:58:24 -0400

    wip test

--- Library/ViewModels/CreatorDigestSettingsViewModelTests.swift
+++ Library/ViewModels/CreatorDigestSettingsViewModelTests.swift
@@ -1,9 +1,44 @@
-//
-//  CreatorDigestSettingsViewModelTests.swift
-//  Library-iOSTests
-//
-//  Created by Christella Dolmo on 11/2/17.
-//  Copyright © 2017 Kickstarter. All rights reserved.
-//
-
 import Foundation
+import XCTest
+import ReactiveSwift
+import Result
+import Prelude
+@testable import KsApi
+@testable import Library
+@testable import ReactiveExtensions_TestHelpers
+
+internal final class CreatorDigestSettingsViewModelTests: TestCase {
+  let vm = CreatorDigestSettingsViewModel()
+
+  let dailyDigestSelected = TestObserver<Bool, NoError>()
+  let individualEmailSelected = TestObserver<Bool, NoError>()
+  let unableToSaveError = TestObserver<String, NoError>()
+  let updateCurrentUser = TestObserver<User, NoError>()
+
+  internal override func setUp() {
+    super.setUp()
+    self.vm.outputs.dailyDigestSelected.observe(self.dailyDigestSelected.observer)
+    self.vm.outputs.individualEmailSelected.observe(self.individualEmailSelected.observer)
+    self.vm.outputs.unableToSaveError.observe(self.unableToSaveError.observer)
+    self.vm.outputs.updateCurrentUser.observe(self.updateCurrentUser.observer)
+  }
+
+  func testDailyDigestToggle() {
+    let user = User.template
+    AppEnvironment.login(AccessTokenEnvelope(accessToken: "deadbeef", user: user))
+
+    self.vm.inputs.viewDidLoad()
+
+    self.dailyDigestSelected.assertValues([false])
+    self.individualEmailSelected.assertValues([false])
+
+    self.vm.inputs.dailyDigestTapped(on: true)
+    self.dailyDigestSelected.assertValues([false, true], "Daily digest toggled on.")
+    self.individualEmailSelected.assertValues([false, false], "Daily digest toggled off.")
+
+    self.vm.inputs.dailyDigestTapped(on: false)
+    self.dailyDigestSelected.assertValues([false, true, false], "Daily digest toggled off.")
+    self.individualEmailSelected.assertValues([false, false, true], "Daily digest toggled on." )
+  }
+
+}
