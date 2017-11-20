5d3e171fba9db1efab42b22a9b91855f4bcfd1a6
Nino Collaco <scollaco@gmail.com>
2017-11-20 12:21:16 -0500

    Added UIPageViewController extension

---
+++ Library/UIPageViewController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIPageViewController+Extensions.swift
+//  Kickstarter-iOS
+//
+//  Created by Saturnino Texeira on 17/11/2017.
+//  Copyright © 2017 Kickstarter. All rights reserved.
+//
+
+import Foundation
