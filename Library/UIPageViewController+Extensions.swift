deb351f6754f3b0bd214e0230e2af268d6565aa9
Nino Collaco <scollaco@gmail.com>
2017-11-21 16:55:01 -0500

    Removed empty extension

--- Library/UIPageViewController+Extensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIPageViewController+Extensions.swift
-//  Kickstarter-iOS
-//
-//  Created by Saturnino Texeira on 17/11/2017.
-//  Copyright © 2017 Kickstarter. All rights reserved.
-//
-
-import Foundation
