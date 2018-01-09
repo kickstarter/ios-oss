aafb6c8d48c78cd0ed04d8d8ed12f85d9d684150
Christella Dolmo <cdolm92@gmail.com>
2018-01-09 12:07:15 -0500

    tests

--- Library/ViewModels/ActivityFriendBackingViewModelTests.swift
+++ Library/ViewModels/ActivityFriendBackingViewModelTests.swift
@@ -9,18 +9,62 @@ internal final class ActivityFriendBackingViewModelTests: TestCase {
   fileprivate let vm: ActivityFriendBackingViewModelType = ActivityFriendBackingViewModel()
 
   fileprivate let cellAccessibilityLabel = TestObserver<String, NoError>()
+  fileprivate let friendTitleLabel = TestObserver<String, NoError>()
 
   override func setUp() {
     super.setUp()
 
     self.vm.outputs.cellAccessibilityLabel.observe(self.cellAccessibilityLabel.observer)
+    self.vm.outputs.friendTitle.map{$0.string}.observe(self.friendTitleLabel.observer)
   }
 
-  func testAccessibility() {
+  func testFrientTitle_ParentCategory() {
+    let games = Category.template
+      |> \.id .~ "12"
+      |> \.name .~ "Games"
+      |> \.subcategories
+      .~ Category.SubcategoryConnection(totalCount: 1, nodes: [.tabletopGames])
+
+
     self.vm.inputs.configureWith(activity:
       .template
         |> Activity.lens.category .~ .backing
-        |> Activity.lens.project .~ (.template |> Project.lens.category .~ .games)
+        |> Activity.lens.project .~ (.template |> Project.lens.category .~ games)
+
+    )
+
+    self.friendTitleLabel.assertValues(["Blob backed a Games project."])
+  }
+
+  func testFrientTitle_SubCategory() {
+    let illustration = Category.template
+      |> \.id .~ "25"
+      |> \.name .~ "Illustration"
+      |> \.parentId .~ "1"
+      |> Category.lens.parent .~ ParentCategory(id: "1", name: "Art")
+
+    self.vm.inputs.configureWith(activity:
+      .template
+        |> Activity.lens.category .~ .backing
+        |> Activity.lens.project .~ (.template |> Project.lens.category .~ illustration)
+
+    )
+
+    self.friendTitleLabel.assertValues(["Blob backed an Art project."])
+  }
+
+
+  func testAccessibility() {
+    let games = Category.template
+      |> \.id .~ "12"
+      |> \.name .~ "Games"
+      |> \.subcategories
+      .~ Category.SubcategoryConnection(totalCount: 1, nodes: [.tabletopGames])
+
+    self.vm.inputs.configureWith(activity:
+      .template
+        |> Activity.lens.category .~ .backing
+        |> Activity.lens.project .~ (.template |> Project.lens.category .~ games)
     )
 
     self.cellAccessibilityLabel.assertValues(["Blob backed a Games project., The Project"])
