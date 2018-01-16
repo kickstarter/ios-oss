bf9c0e4557c1f528ad4c72eca6e8411e121a23f9
Nino Collaco <scollaco@gmail.com>
2018-01-16 14:49:17 -0500

    Fixed ProjectPamphletContent snapshots

--- Kickstarter-iOS/Views/Cells/ProjectPamphletMainCell.swift
+++ Kickstarter-iOS/Views/Cells/ProjectPamphletMainCell.swift
@@ -103,10 +103,11 @@ internal final class ProjectPamphletMainCell: UITableViewCell, ValueCell {
     _ = [self.backersSubtitleLabel, self.deadlineSubtitleLabel, self.pledgeSubtitleLabel]
       ||> UILabel.lens.textColor .~ .ksr_text_dark_grey_500
       ||> UILabel.lens.font .~ .ksr_caption1(size: 13)
-      ||> UILabel.lens.numberOfLines .~ 2
+      ||> UILabel.lens.numberOfLines .~ 1
 
     _ = [self.backersTitleLabel, self.deadlineTitleLabel, self.pledgedTitleLabel]
       ||> UILabel.lens.font .~ .ksr_headline(size: 13)
+      ||> UILabel.lens.numberOfLines .~ 1
 
     _ = self.blurbAndReadMoreStackView
       |> UIStackView.lens.spacing .~ 0
