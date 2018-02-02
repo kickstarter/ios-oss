a92cf8c5a5e0dfbb09ab821525bbaeb0caaad138
Christella Dolmo <cdolm92@gmail.com>
2018-02-02 17:37:51 -0500

    deleted comment

--- Kickstarter-iOS/Views/Controllers/MessageThreadsViewController.swift
+++ Kickstarter-iOS/Views/Controllers/MessageThreadsViewController.swift
@@ -11,16 +11,9 @@ internal final class MessageThreadsViewController: UITableViewController {
   @IBOutlet fileprivate weak var footerView: UIView!
   @IBOutlet fileprivate weak var mailboxLabel: UILabel!
 
-//  internal static func configuredWith(project: Project?) -> MessageThreadsViewController {
-//    let vc = Storyboard.Messages.instantiate(MessageThreadsViewController.self)
-//    vc.viewModel.inputs.configureWith(project: project)
-//    return vc
-//  }
-
-  public static func configuredWith(project: Project?, refTag: RefTag?) -> MessageThreadsViewController {
+  internal static func configuredWith(project: Project?, refTag: RefTag?) -> MessageThreadsViewController {
     let vc = Storyboard.Messages.instantiate(MessageThreadsViewController.self)
     vc.viewModel.inputs.configureWith(project: project, refTag: refTag)
-
     return vc
   }
 
