999fe9abb2545baccb2802168d4511c2a86921ed
Christella Dolmo <cdolm92@gmail.com>
2018-02-05 12:28:38 -0500

    testing faq link

--- Library/NavigationTests.swift
+++ Library/NavigationTests.swift
@@ -56,6 +56,9 @@ public final class NavigationTests: XCTestCase {
     KSRAssertMatch(.project(.slug("project"), .root, refTag: nil),
                    "/projects/creator/project/description")
 
+    KSRAssertMatch(.project(.slug("project"), .faqs, refTag: nil),
+                   "/projects/creator/project/faqs")
+
     KSRAssertMatch(.project(.slug("project"), .friends, refTag: nil),
                    "/projects/creator/project/friends")
 
