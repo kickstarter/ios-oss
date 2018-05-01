464ad0cbee8dafbc9f5f0f4d3ae1d8913ece43c4
Isabel Barrera <ifbarrera@me.com>
2018-05-01 13:42:57 -0400

    Adding view model

--- Kickstarter-iOS/ViewModels/DiscoveryProjectCategoryViewModel.swift
+++ Kickstarter-iOS/ViewModels/DiscoveryProjectCategoryViewModel.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2018 Kickstarter. All rights reserved.
 //
 
-import Foundation
+import KsApi
+import Result
+import Prelude
+import ReactiveSwift
+import ReactiveExtensions
+
+public protocol DiscoveryProjectCategoryViewModelInputs {
+  func updateImageString(imageString: String)
+  func updateCategoryName(name: String)
+}
+
+public protocol DiscoveryProjectCategoryViewModelOutputs {
+  var categoryNameText: Signal<String, NoError> { get }
+  var categoryImage: Signal<UIImage, NoError> { get }
+}
+
+public protocol DiscoveryProjectCategoryViewModelType {
+  var inputs: DiscoveryProjectCategoryViewModelInputs { get }
+  var outputs: DiscoveryProjectCategoryViewModelOutputs { get }
+}
+
+public final class DiscoveryProjectCategoryViewModel: DiscoveryProjectCategoryViewModelType, DiscoveryProjectCategoryViewModelInputs, DiscoveryProjectCategoryViewModelOutputs {
+  public var inputs: DiscoveryProjectCategoryViewModelInputs { return self }
+  public var outputs: DiscoveryProjectCategoryViewModelOutputs { return self }
+  
+  init() {
+    self.categoryNameText = categoryNameTextProperty.signal.skipNil().map{ $0 }
+    self.categoryImage = imageStringProperty.signal.skipNil().map{ (imageString) -> UIImage in
+      return UIImage(named: imageString)!
+    }
+  }
+  
+  public var categoryNameText: Signal<String, NoError>
+  public var categoryImage: Signal<UIImage, NoError>
+  
+  private let categoryNameTextProperty = MutableProperty<String?>(nil)
+  public func updateCategoryName(name: String) {
+    categoryNameTextProperty.value = name
+  }
+  
+  private let imageStringProperty = MutableProperty<String?>(nil)
+  public func updateImageString(imageString: String) {
+    imageStringProperty.value = imageString
+  }
+}
