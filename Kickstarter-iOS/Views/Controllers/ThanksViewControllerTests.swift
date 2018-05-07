cd1b514b169fa92165b40f97e208e7787bcc2451
Isabel Barrera <ifbarrera@me.com>
2018-05-07 17:00:26 -0400

    Swiftlint

--- Kickstarter-iOS/Views/Controllers/ThanksViewControllerTests.swift
+++ Kickstarter-iOS/Views/Controllers/ThanksViewControllerTests.swift
@@ -14,37 +14,37 @@ import Prelude
 @testable import Library
 
 class ThanksViewControllerTests: TestCase {
-    
+
     override func setUp() {
       super.setUp()
-      
+
       AppEnvironment.pushEnvironment(mainBundle: Bundle.framework)
       UIView.setAnimationsEnabled(false)
     }
-    
+
     override func tearDown() {
       AppEnvironment.popEnvironment()
       UIView.setAnimationsEnabled(true)
-      
+
       super.tearDown()
     }
-    
+
     func testThanksViewController() {
       let discoveryEnvelope = DiscoveryEnvelope.template
       let rootCategories = RootCategoriesEnvelope(rootCategories: [Category.art])
       let mockService = MockService(fetchGraphCategoriesResponse: rootCategories, fetchDiscoveryResponse: discoveryEnvelope)
-      
+
       combos(Language.allLanguages, [Device.phone4_7inch, Device.pad]).forEach { language, device in
         withEnvironment(apiService: mockService, language: language) {
           let project = Project.cosmicSurgery
           |> Project.lens.id .~ 3
-          
+
           let controller = ThanksViewController.configuredWith(project: project)
-          
+
           let (parent, _) = traitControllers(device: device, orientation: .portrait, child: controller)
-          
+
           self.scheduler.run()
-          
+
           FBSnapshotVerifyView(parent.view, identifier: "lang_\(language)_device_\(device)")
         }
       }
