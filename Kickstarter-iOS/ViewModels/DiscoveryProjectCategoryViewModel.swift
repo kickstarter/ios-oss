fd000f86551dde615375ab9e96d144852ac42460
Isabel Barrera <ifbarrera@me.com>
2018-05-08 14:28:43 -0400

    Removing header comments, updating snapshots

--- Kickstarter-iOS/ViewModels/DiscoveryProjectCategoryViewModel.swift
+++ Kickstarter-iOS/ViewModels/DiscoveryProjectCategoryViewModel.swift
@@ -1,11 +1,3 @@
-//
-//  DiscoveryProjectCategoryViewModel.swift
-//  Kickstarter-iOS
-//
-//  Created by Isabel Barrera on 4/30/18.
-//  Copyright © 2018 Kickstarter. All rights reserved.
-//
-
 import KsApi
 import Result
 import Prelude
