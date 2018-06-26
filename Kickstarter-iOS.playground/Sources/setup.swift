a8351e3109cfde2122b4af2a684cd948a01f27f9
Nino Collaco <scollaco@gmail.com>
2018-06-26 12:07:42 -0400

    Fixed color paletes playgrounds

--- Kickstarter-iOS.playground/Sources/setup.swift
+++ Kickstarter-iOS.playground/Sources/setup.swift
@@ -1,9 +1,9 @@
+import Library
 import UIKit
-import Foundation
 
 // This needs to be called manually in playgrounds.
 public func initialize() {
 
-    UIView.self.perform(Selector(("doBadSwizzleStuff")), with: nil)
-    UIViewController.self.perform(Selector(("doBadSwizzleStuff")), with: nil)
+    UIView.doBadSwizzleStuff()
+    UIViewController.doBadSwizzleStuff()
 }
