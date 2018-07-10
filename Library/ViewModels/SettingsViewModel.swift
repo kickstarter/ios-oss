3afaeb533ee411fd6eb5eb4be5defddbcf092bc6
Saturnino Collaço Teixeira Filho <scollaco@gmail.com>
2018-07-10 14:27:31 -0400

    Notifications settings refactor (#388)

--- Library/ViewModels/SettingsViewModel.swift
+++ Library/ViewModels/SettingsViewModel.swift
@@ -7,76 +7,37 @@ import Result
 
 public protocol SettingsViewModelInputs {
   func artsAndCultureNewsletterTapped(on: Bool)
-  func backingsTapped(selected: Bool)
-  func commentsTapped(selected: Bool)
-  func creatorTipsTapped(selected: Bool)
   func deleteAccountTapped()
-  func emailFrequencyTapped()
   func exportDataTapped()
-  func findFriendsTapped()
-  func followerTapped(selected: Bool)
   func followingSwitchTapped(on: Bool, didShowPrompt: Bool)
-  func friendActivityTapped(selected: Bool)
   func gamesNewsletterTapped(on: Bool)
   func happeningNewsletterTapped(on: Bool)
   func inventNewsletterTapped(on: Bool)
   func logoutCanceled()
   func logoutConfirmed()
   func logoutTapped()
-  func manageProjectNotificationsTapped()
-  func messagesTapped(selected: Bool)
-  func mobileBackingsTapped(selected: Bool)
-  func mobileCommentsTapped(selected: Bool)
-  func mobileFollowerTapped(selected: Bool)
-  func mobileFriendActivityTapped(selected: Bool)
-  func mobileMessagesTapped(selected: Bool)
-  func mobilePostLikesTapped(selected: Bool)
-  func mobileUpdatesTapped(selected: Bool)
-  func postLikesTapped(selected: Bool)
   func privateProfileSwitchDidChange(isOn: Bool)
   func promoNewsletterTapped(on: Bool)
   func rateUsTapped()
   func recommendationsTapped(on: Bool)
-  func updatesTapped(selected: Bool)
   func viewDidLoad()
   func weeklyNewsletterTapped(on: Bool)
 }
 
 public protocol SettingsViewModelOutputs {
   var artsAndCultureNewsletterOn: Signal<Bool, NoError> { get }
-  var backingsSelected: Signal<Bool, NoError> { get }
-  var commentsSelected: Signal<Bool, NoError> { get }
-  var creatorNotificationsHidden: Signal<Bool, NoError> { get }
-  var creatorTipsSelected: Signal<Bool, NoError> { get }
-  var emailFrequencyButtonEnabled: Signal<Bool, NoError> { get }
   var exportDataButtonEnabled: Signal<Bool, NoError> { get }
   var exportDataExpirationDate: Signal<String, NoError> { get }
   var exportDataLoadingIndicator: Signal<Bool, NoError> { get }
   var exportDataText: Signal<String, NoError> { get }
-  var followerSelected: Signal<Bool, NoError> { get }
   var followingPrivacyOn: Signal<Bool, NoError> { get }
-  var friendActivitySelected: Signal<Bool, NoError> { get }
   var gamesNewsletterOn: Signal<Bool, NoError> { get }
   var goToAppStoreRating: Signal<String, NoError> { get }
   var goToDeleteAccountBrowser: Signal<URL, NoError> { get }
-  var goToEmailFrequency: Signal<User, NoError> { get }
-  var goToFindFriends: Signal<Void, NoError> { get }
-  var goToManageProjectNotifications: Signal<Void, NoError> { get }
   var happeningNewsletterOn: Signal<Bool, NoError> { get }
   var inventNewsletterOn: Signal<Bool, NoError> { get }
   var logoutWithParams: Signal<DiscoveryParams, NoError> { get }
-  var manageProjectNotificationsButtonAccessibilityHint: Signal<String, NoError> { get }
-  var messagesSelected: Signal<Bool, NoError> { get }
-  var mobileBackingsSelected: Signal<Bool, NoError> { get }
-  var mobileCommentsSelected: Signal<Bool, NoError> { get }
-  var mobileFollowerSelected: Signal<Bool, NoError> { get }
-  var mobileFriendActivitySelected: Signal<Bool, NoError> { get }
-  var mobileMessagesSelected: Signal<Bool, NoError> { get }
-  var mobilePostLikesSelected: Signal<Bool, NoError> { get }
-  var mobileUpdatesSelected: Signal<Bool, NoError> { get }
-  var postLikesSelected: Signal<Bool, NoError> { get }
   var privateProfileEnabled: Signal<Bool, NoError> { get }
-  var projectNotificationsCount: Signal<String, NoError> { get }
   var promoNewsletterOn: Signal<Bool, NoError> { get }
   var requestExportData: Signal<(), NoError> { get }
   var recommendationsOn: Signal<Bool, NoError> { get }
@@ -85,7 +46,6 @@ public protocol SettingsViewModelOutputs {
   var showPrivacyFollowingPrompt: Signal<(), NoError> { get }
   var showOptInPrompt: Signal<String, NoError> { get }
   var unableToSaveError: Signal<String, NoError> { get }
-  var updatesSelected: Signal<Bool, NoError> { get }
   var updateCurrentUser: Signal<User, NoError> { get }
   var versionText: Signal<String, NoError> { get }
   var weeklyNewsletterOn: Signal<Bool, NoError> { get }
@@ -143,62 +103,62 @@ SettingsViewModelOutputs {
         (UserAttribute.newsletter(Newsletter.weekly), $0)
       },
       self.backingsTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.backings), $0)
+        (UserAttribute.notification(UserAttribute.Notification.backings), $0)
       },
       self.commentsTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.comments), $0)
+        (UserAttribute.notification(UserAttribute.Notification.comments), $0)
       },
       self.followerTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.follower), $0)
+        (UserAttribute.notification(UserAttribute.Notification.follower), $0)
       },
       self.friendActivityTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.friendActivity), $0)
+        (UserAttribute.notification(UserAttribute.Notification.friendActivity), $0)
       },
       self.messagesTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.messages), $0)
+        (UserAttribute.notification(UserAttribute.Notification.messages), $0)
       },
       self.mobileBackingsTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobileBackings), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobileBackings), $0)
       },
       self.mobileCommentsTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobileComments), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobileComments), $0)
       },
       self.mobileFollowerTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobileFollower), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobileFollower), $0)
       },
       self.mobileFriendActivityTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobileFriendActivity), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobileFriendActivity), $0)
       },
       self.mobileMessagesTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobileMessages), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobileMessages), $0)
       },
       self.mobilePostLikesTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobilePostLikes), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobilePostLikes), $0)
       },
       self.mobileUpdatesTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.mobileUpdates), $0)
+        (UserAttribute.notification(UserAttribute.Notification.mobileUpdates), $0)
       },
       self.postLikesTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.postLikes), $0)
+        (UserAttribute.notification(UserAttribute.Notification.postLikes), $0)
       },
       self.privateProfileEnabledProperty.signal.negate().map {
-        (UserAttribute.privacy(Privacy.showPublicProfile), $0)
+        (UserAttribute.privacy(UserAttribute.Privacy.showPublicProfile), $0)
       },
       self.creatorTipsProperty.signal.map {
-        (UserAttribute.notification(Notification.creatorTips), $0)
+        (UserAttribute.notification(UserAttribute.Notification.creatorTips), $0)
       },
       self.updatesTappedProperty.signal.map {
-        (UserAttribute.notification(Notification.updates), $0)
+        (UserAttribute.notification(UserAttribute.Notification.updates), $0)
       },
       self.followingSwitchTappedProperty.signal
         .filter { (on, didShowPrompt) in
           didShowPrompt == true || (on == true && didShowPrompt == false)
         }
         .map {
-        (UserAttribute.privacy(Privacy.following), $0.0)
+        (UserAttribute.privacy(UserAttribute.Privacy.following), $0.0)
       },
       self.recommendationsTappedProperty.signal.map {
-        (UserAttribute.privacy(Privacy.recommendations), !$0)
+        (UserAttribute.privacy(UserAttribute.Privacy.recommendations), !$0)
       }
     )
 
@@ -229,12 +189,6 @@ SettingsViewModelOutputs {
 
     self.updateCurrentUser = Signal.merge(initialUser, updatedUser, previousUserOnError)
 
-    self.creatorNotificationsHidden = self.updateCurrentUser.map { !$0.isCreator }.skipRepeats()
-
-    self.projectNotificationsCount = self.updateCurrentUser
-      .map { Format.wholeNumber($0.stats.backedProjectsCount ?? 0) }
-      .skipRepeats()
-
     self.goToAppStoreRating = self.rateUsTappedProperty.signal
       .map { AppEnvironment.current.config?.iTunesLink ?? "" }
 
@@ -242,10 +196,6 @@ SettingsViewModelOutputs {
       .map {
         AppEnvironment.current.apiService.serverConfig.webBaseUrl.appendingPathComponent("/profile/destroy")
       }
-
-    self.goToFindFriends = self.findFriendsTappedProperty.signal
-
-    self.goToManageProjectNotifications = self.manageProjectNotificationsTappedProperty.signal
 
     self.showConfirmLogoutPrompt = self.logoutTappedProperty.signal
       .map {
@@ -278,44 +228,10 @@ SettingsViewModelOutputs {
     self.artsAndCultureNewsletterOn = self.updateCurrentUser
       .map { $0.newsletters.arts }.skipNil().skipRepeats()
 
-    self.backingsSelected = self.updateCurrentUser.map { $0.notifications.backings }.skipNil().skipRepeats()
-    self.creatorTipsSelected = self.updateCurrentUser
-      .map { $0.notifications.creatorTips }.skipNil().skipRepeats()
-    self.commentsSelected = self.updateCurrentUser
-      .map { $0.notifications.comments }.skipNil().skipRepeats()
-    self.followerSelected = self.updateCurrentUser
-      .map { $0.notifications.follower }.skipNil().skipRepeats()
-    self.friendActivitySelected = self.updateCurrentUser
-      .map { $0.notifications.friendActivity }.skipNil().skipRepeats()
-    self.messagesSelected = self.updateCurrentUser
-      .map { $0.notifications.messages }.skipNil().skipRepeats()
-    self.mobileBackingsSelected = self.updateCurrentUser
-      .map { $0.notifications.mobileBackings }.skipNil().skipRepeats()
-    self.mobileCommentsSelected = self.updateCurrentUser
-      .map { $0.notifications.mobileComments }.skipNil().skipRepeats()
-    self.mobileFollowerSelected = self.updateCurrentUser
-      .map { $0.notifications.mobileFollower }.skipNil().skipRepeats()
-    self.mobileFriendActivitySelected = self.updateCurrentUser
-      .map { $0.notifications.mobileFriendActivity }.skipNil().skipRepeats()
-    self.mobileMessagesSelected = self.updateCurrentUser
-      .map { $0.notifications.mobileMessages }.skipNil().skipRepeats()
-    self.mobilePostLikesSelected = self.updateCurrentUser
-      .map { $0.notifications.mobilePostLikes }.skipNil().skipRepeats()
-    self.mobileUpdatesSelected = self.updateCurrentUser
-      .map { $0.notifications.mobileUpdates }.skipNil().skipRepeats()
     self.privateProfileEnabled = self.updateCurrentUser
       .map { $0.showPublicProfile }.skipNil().negate().skipRepeats()
-    self.postLikesSelected = self.updateCurrentUser
-      .map { $0.notifications.postLikes }.skipNil().skipRepeats()
-    self.updatesSelected = self.updateCurrentUser
-      .map { $0.notifications.updates }.skipNil().skipRepeats()
     self.recommendationsOn = self.updateCurrentUser
       .map { $0.optedOutOfRecommendations }.skipNil().map { $0 ? false : true }.skipRepeats()
-
-    self.emailFrequencyButtonEnabled = self.backingsSelected
-
-    self.goToEmailFrequency = self.updateCurrentUser
-      .takeWhen(self.emailFrequencyTappedProperty.signal)
 
     self.versionText = viewDidLoadProperty.signal
       .map {
@@ -361,10 +277,6 @@ SettingsViewModelOutputs {
     self.showDataExpirationAndChevron = self.exportDataLoadingIndicator.signal
       .map { $0 }
 
-    // a11y
-    self.manageProjectNotificationsButtonAccessibilityHint = self.updateCurrentUser
-      .map { Strings.profile_project_count_projects_backed(project_count: $0.stats.backedProjectsCount ?? 0) }
-
     // Koala
     userAttributeChanged
       .observeValues { attribute, on in
@@ -385,6 +297,7 @@ SettingsViewModelOutputs {
                .comments, .follower, .friendActivity, .messages, .postLikes, .creatorTips, .updates:
             AppEnvironment.current.koala.trackChangeEmailNotification(type: notification.trackingString,
                                                                       on: on)
+          default: break
           }
         case let .privacy(privacy):
           switch privacy {
@@ -561,39 +474,18 @@ SettingsViewModelOutputs {
   }
 
   public let artsAndCultureNewsletterOn: Signal<Bool, NoError>
-  public let backingsSelected: Signal<Bool, NoError>
-  public let commentsSelected: Signal<Bool, NoError>
-  public let creatorNotificationsHidden: Signal<Bool, NoError>
-  public let creatorTipsSelected: Signal<Bool, NoError>
-  public let emailFrequencyButtonEnabled: Signal<Bool, NoError>
   public let exportDataLoadingIndicator: Signal<Bool, NoError>
   public let exportDataText: Signal<String, NoError>
   public let exportDataExpirationDate: Signal<String, NoError>
   public let exportDataButtonEnabled: Signal<Bool, NoError>
-  public let followerSelected: Signal<Bool, NoError>
   public let followingPrivacyOn: Signal<Bool, NoError>
-  public let friendActivitySelected: Signal<Bool, NoError>
   public let gamesNewsletterOn: Signal<Bool, NoError>
   public let goToAppStoreRating: Signal<String, NoError>
   public let goToDeleteAccountBrowser: Signal<URL, NoError>
-  public let goToEmailFrequency: Signal<User, NoError>
-  public let goToFindFriends: Signal<Void, NoError>
-  public let goToManageProjectNotifications: Signal<Void, NoError>
   public let happeningNewsletterOn: Signal<Bool, NoError>
   public let inventNewsletterOn: Signal<Bool, NoError>
   public let logoutWithParams: Signal<DiscoveryParams, NoError>
-  public var manageProjectNotificationsButtonAccessibilityHint: Signal<String, NoError>
-  public let messagesSelected: Signal<Bool, NoError>
-  public let mobileBackingsSelected: Signal<Bool, NoError>
-  public let mobileCommentsSelected: Signal<Bool, NoError>
-  public let mobileFollowerSelected: Signal<Bool, NoError>
-  public let mobileFriendActivitySelected: Signal<Bool, NoError>
-  public let mobileMessagesSelected: Signal<Bool, NoError>
-  public let mobilePostLikesSelected: Signal<Bool, NoError>
-  public let mobileUpdatesSelected: Signal<Bool, NoError>
-  public let postLikesSelected: Signal<Bool, NoError>
   public let privateProfileEnabled: Signal<Bool, NoError>
-  public let projectNotificationsCount: Signal<String, NoError>
   public let promoNewsletterOn: Signal<Bool, NoError>
   public let requestExportData: Signal<(), NoError>
   public let recommendationsOn: Signal<Bool, NoError>
@@ -602,7 +494,6 @@ SettingsViewModelOutputs {
   public let showOptInPrompt: Signal<String, NoError>
   public let showPrivacyFollowingPrompt: Signal<(), NoError>
   public let unableToSaveError: Signal<String, NoError>
-  public let updatesSelected: Signal<Bool, NoError>
   public let updateCurrentUser: Signal<User, NoError>
   public let weeklyNewsletterOn: Signal<Bool, NoError>
   public let versionText: Signal<String, NoError>
@@ -623,93 +514,4 @@ private func dateFormatter(for dateString: String?, state: ExportDataEnvelope.St
   if state == .expired {
     return ""
   } else { return Strings.Expires_date_at_time(date: expirationDate, time: expirationTime) }
-}
-
-private enum UserAttribute {
-  case newsletter(Newsletter)
-  case notification(Notification)
-  case privacy(Privacy)
-
-  fileprivate var lens: Lens<User, Bool?> {
-    switch self {
-    case let .newsletter(newsletter):
-      switch newsletter {
-      case .arts:       return User.lens.newsletters.arts
-      case .games:      return User.lens.newsletters.games
-      case .happening:  return User.lens.newsletters.happening
-      case .invent:     return User.lens.newsletters.invent
-      case .promo:      return User.lens.newsletters.promo
-      case .weekly:     return User.lens.newsletters.weekly
-      }
-    case let .notification(notification):
-      switch notification {
-      case .backings:             return User.lens.notifications.backings
-      case .comments:             return User.lens.notifications.comments
-      case .creatorTips:          return User.lens.notifications.creatorTips
-      case .follower:             return User.lens.notifications.follower
-      case .friendActivity:       return User.lens.notifications.friendActivity
-      case .messages:             return User.lens.notifications.messages
-      case .mobileBackings:       return User.lens.notifications.mobileBackings
-      case .mobileComments:       return User.lens.notifications.mobileComments
-      case .mobileFollower:       return User.lens.notifications.mobileFollower
-      case .mobileFriendActivity: return User.lens.notifications.mobileFriendActivity
-      case .mobileMessages:       return User.lens.notifications.mobileMessages
-      case .mobilePostLikes:      return User.lens.notifications.mobilePostLikes
-      case .mobileUpdates:        return User.lens.notifications.mobileUpdates
-      case .postLikes:            return User.lens.notifications.postLikes
-      case .updates:              return User.lens.notifications.updates
-      }
-    case let .privacy(privacy):
-      switch privacy {
-      case .following:          return User.lens.social
-      case .recommendations:    return User.lens.optedOutOfRecommendations
-      case .showPublicProfile:  return User.lens.showPublicProfile
-      }
-    }
-  }
-}
-
-private enum Notification {
-  case backings
-  case comments
-  case creatorTips
-  case follower
-  case friendActivity
-  case messages
-  case mobileBackings
-  case mobileComments
-  case mobileFollower
-  case mobileFriendActivity
-  case mobileMessages
-  case mobilePostLikes
-  case mobileUpdates
-  case postLikes
-  case updates
-
-  fileprivate var trackingString: String {
-    switch self {
-    case .backings, .mobileBackings:                return "New pledges"
-    case .comments, .mobileComments:                return "New comments"
-    case .creatorTips:                              return "Creator tips"
-    case .follower, .mobileFollower:                return "New followers"
-    case .friendActivity, .mobileFriendActivity:    return "Friend backs a project"
-    case .messages, .mobileMessages:                return "New messages"
-    case .postLikes, .mobilePostLikes:              return "New likes"
-    case .updates, .mobileUpdates:                  return "Project updates"
-    }
-  }
-}
-
-private enum Privacy {
-  case following
-  case recommendations
-  case showPublicProfile
-
-  fileprivate var trackingString: String {
-    switch self {
-    case .following: return Strings.Following()
-    case .recommendations: return Strings.Recommendations()
-    default: return ""
-    }
-  }
 }
