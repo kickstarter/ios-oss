83bd3d0cd047b11afe200b3d7888ea8c0c0cd4e1
Christella <cdolm92@gmail.com>
2018-08-03 14:24:09 -0400

    Saved projects null state (#386)

--- Kickstarter-iOS/Views/Cells/BackerDashboardEmptyStateCell.swift
+++ Kickstarter-iOS/Views/Cells/BackerDashboardEmptyStateCell.swift
@@ -5,9 +5,17 @@ import UIKit
 
 internal final class BackerDashboardEmptyStateCell: UITableViewCell, ValueCell {
 
-  @IBOutlet private weak var iconImageView: UIImageView!
+  @IBOutlet private weak var filledHeartIconImageView: UIImageView!
   @IBOutlet private weak var messageLabel: UILabel!
   @IBOutlet private weak var titleLabel: UILabel!
+  @IBOutlet private weak var iconImageView: UIImageView!
+
+  private var isAnimating: Bool = false
+  private let duration = 0.4
+  private let hiddenHeartAlpha = CGFloat(0.0)
+  private let visibleHeartAlpha = CGFloat(1.0)
+  private let shrunkHeartTransform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+  private let expandedHearTransform = CGAffineTransform(scaleX: 1.0, y: 1.0)
 
   internal func configureWith(value: ProfileProjectsType) {
     switch value {
@@ -19,15 +27,74 @@ internal final class BackerDashboardEmptyStateCell: UITableViewCell, ValueCell {
         .~ UIImage(named: "icon--eye",
                    in: .framework,
                    compatibleWith: nil)
+      _ = self.filledHeartIconImageView
+        |> UIImageView.lens.isHidden .~ true
     case .saved:
       self.messageLabel.text = Strings.Tap_the_heart_on_a_project_to_get_notified()
       self.titleLabel.text = Strings.No_saved_projects()
-      _ = self.iconImageView
-        |> UIImageView.lens.image
-        .~ UIImage(named: "icon--heart-outline",
-                   in: .framework,
-                   compatibleWith: nil)
+
+      NotificationCenter.default.addObserver(
+        self,
+        selector: #selector(animateToFilledHeart),
+        name: .ksr_savedProjectEmptyStateTapped,
+        object: nil
+      )
+      animateToFilledHeart()
     }
+  }
+
+  @objc internal func animateToFilledHeart() {
+
+    guard self.isAnimating == false else { return }
+
+    UIView.animate(
+      withDuration: self.duration,
+      delay: 1.0,
+      usingSpringWithDamping: 1.0,
+      initialSpringVelocity: 0.0,
+      options: .curveEaseInOut,
+      animations: { [weak self] in
+
+        guard let _self = self else { return }
+
+        _self.isAnimating = true
+
+        _self.filledHeartIconImageView.alpha = _self.hiddenHeartAlpha
+        _self.iconImageView.alpha = _self.hiddenHeartAlpha
+        _self.iconImageView.transform = _self.shrunkHeartTransform
+        _self.filledHeartIconImageView.transform = _self.expandedHearTransform
+        _self.filledHeartIconImageView.alpha = _self.visibleHeartAlpha
+        },
+      completion: { [weak self] _ in
+        guard let _self = self else { return }
+        _self.animateToOutlineHeart()
+      }
+    )
+  }
+
+  internal func animateToOutlineHeart() {
+    UIView.animate(
+      withDuration: self.duration,
+      delay: 0.6,
+      usingSpringWithDamping: 1.0,
+      initialSpringVelocity: 0.0,
+      options: .curveEaseInOut,
+      animations: { [weak self] in
+
+        guard let _self = self else { return }
+
+        _self.filledHeartIconImageView.alpha = _self.hiddenHeartAlpha
+        _self.iconImageView.alpha = _self.hiddenHeartAlpha
+        _self.filledHeartIconImageView.transform = _self.shrunkHeartTransform
+        _self.iconImageView.transform = _self.expandedHearTransform
+        _self.iconImageView.alpha = _self.visibleHeartAlpha
+      },
+      completion: { [weak self] _ in
+
+        guard let _self = self else { return }
+        _self.isAnimating = false
+      }
+    )
   }
 
   internal override func bindStyles() {
