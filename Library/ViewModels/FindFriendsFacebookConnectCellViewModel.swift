c7321808ed34c77307b12ab4723b8cada44945cd
Isabel Barrera <ifbarrera@me.com>
2018-08-08 10:08:36 -0400

    Facebook reconnect needed handling (#387)
    * strings
    
    * Updating FacebookConnectCell to support reconnection messaging
    
    * Adding needsFreshFacebookToken variable to User object
    
    * Tests
    
    * Activities handling
    
    * Activities facebook reconnect
    
    * Fix tests
    
    * Swiftlint
    
    * Removing String type
    
    * Strings
    
    * Update screenshots
    
    * Cleanup
    
    * Updating snapshots
    
    * Clean up and adding close button test
    
    * PR comments

--- Library/ViewModels/FindFriendsFacebookConnectCellViewModel.swift
+++ Library/ViewModels/FindFriendsFacebookConnectCellViewModel.swift
@@ -49,6 +49,15 @@ public protocol FindFriendsFacebookConnectCellViewModelOutputs {
 
   /// Emits a User that can be used to replace the current user in the environment
   var updateUserInEnvironment: Signal<User, NoError> { get }
+
+  /// Emits the cell's title
+  var facebookConnectCellTitle: Signal<String, NoError> { get }
+
+  /// Emits the cell's description
+  var facebookConnectCellSubtitle: Signal<String, NoError> { get }
+
+  /// Emits the button's title
+  var facebookConnectButtonTitle: Signal<String, NoError> { get }
 }
 
 public protocol FindFriendsFacebookConnectCellViewModelType {
@@ -125,9 +134,22 @@ public final class FindFriendsFacebookConnectCellViewModel: FindFriendsFacebookC
       facebookLoginAttemptFailAlert
     ])
 
-    let source = configureWithProperty.signal
+    let source = configureWithProperty.signal.skipNil().map { $0 }
+    let connectionType = configureWithProperty.signal.skipNil().map { _ in
+      FindFriendsFacebookConnectCellViewModel.connectionType(user: AppEnvironment.current.currentUser)
+    }
 
     self.hideCloseButton = source.map { $0 == FriendsSource.findFriends }
+
+    self.facebookConnectCellTitle = connectionType.signal
+      .skipNil()
+      .map { $0.titleText }
+    self.facebookConnectCellSubtitle = connectionType.signal
+      .skipNil()
+      .map { $0.subtitleText }
+    self.facebookConnectButtonTitle = connectionType.signal
+      .skipNil()
+      .map { $0.buttonText }
 
     source
       .takeWhen(self.showErrorAlert)
@@ -150,7 +172,7 @@ public final class FindFriendsFacebookConnectCellViewModel: FindFriendsFacebookC
     closeButtonTappedProperty.value = ()
   }
 
-  fileprivate let configureWithProperty = MutableProperty<FriendsSource>(FriendsSource.findFriends)
+  fileprivate let configureWithProperty = MutableProperty<FriendsSource?>(nil)
   public func configureWith(source: FriendsSource) {
     configureWithProperty.value = source
   }
@@ -176,11 +198,42 @@ public final class FindFriendsFacebookConnectCellViewModel: FindFriendsFacebookC
   }
 
   public let attemptFacebookLogin: Signal<(), NoError>
+  public let facebookConnectButtonTitle: Signal<String, NoError>
+  public let facebookConnectCellTitle: Signal<String, NoError>
+  public let facebookConnectCellSubtitle: Signal<String, NoError>
+  public let hideCloseButton: Signal<Bool, NoError>
   public let isLoading: Signal<Bool, NoError>
   public let notifyDelegateToDismissHeader: Signal<(), NoError>
   public let notifyDelegateUserFacebookConnected: Signal<(), NoError>
   public let postUserUpdatedNotification: Signal<Notification, NoError>
   public let updateUserInEnvironment: Signal<User, NoError>
   public let showErrorAlert: Signal<AlertError, NoError>
-  public let hideCloseButton: Signal<Bool, NoError>
+}
+
+extension FindFriendsFacebookConnectCellViewModel {
+  public static func showFacebookConnectionSection(for user: User?) -> Bool {
+    guard let isFacebookConnected = user?.facebookConnected else {
+      return true
+    }
+
+    let needsFreshFacebookToken = user?.needsFreshFacebookToken ?? false
+
+    // Show section in "reconnect" state if facebook connected but requiring a new token
+    return !isFacebookConnected || (isFacebookConnected
+      && needsFreshFacebookToken)
+  }
+
+  private static func connectionType(user: User?) -> FacebookConnectionType? {
+    guard let isFacebookConnected = user?.facebookConnected else {
+      return .connect
+    }
+
+    let needsRefresh = user?.needsFreshFacebookToken ?? false
+
+    if isFacebookConnected && needsRefresh {
+      return .reconnect
+    }
+
+    return .connect
+  }
 }
