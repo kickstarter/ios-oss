a874c0fac4d646a4912c8fbed46da801aea2ffa8
Isabel Barrera <ifbarrera@me.com>
2018-08-09 15:53:04 -0400

    Updating snapshots

--- Kickstarter-iOS/Views/Controllers/BetaToolsViewController.swift
+++ Kickstarter-iOS/Views/Controllers/BetaToolsViewController.swift
@@ -38,7 +38,7 @@ internal final class BetaToolsViewController: UIViewController {
       |> UIButton.lens.title(for: .normal) .~ "Submit feedback for beta"
 
     _ = self.betaTitleLabel
-      |> settingsTitleLabelStyle
+      |> settingsSectionLabelStyle
       |> UILabel.lens.text .~ "Beta tools"
 
     _ = self.languageSwitcher
