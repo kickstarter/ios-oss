13fc70794b0e1966b49b143edb7d8c48354fc189
Christella Dolmo <cdolm92@gmail.com>
2018-08-17 17:24:16 -0400

    new snapshot tests for settings

--- Kickstarter-iOS/Views/Controllers/SettingsViewControllerTests.swift
+++ Kickstarter-iOS/Views/Controllers/SettingsViewControllerTests.swift
@@ -17,8 +17,8 @@ internal final class SettingsViewControllerTests: TestCase {
     super.tearDown()
   }
 
-  func testNonCreator() {
-    let currentUser = .template |> User.lens.stats.backedProjectsCount .~ 1234
+  func testView() {
+    let currentUser = User.template
 
     Language.allLanguages.forEach { language in
       withEnvironment(
@@ -26,52 +26,8 @@ internal final class SettingsViewControllerTests: TestCase {
         currentUser: currentUser,
         language: language) {
 
-            let vc = SettingsViewController.instantiate()
-            let (parent, _) = traitControllers(device: .phone4_7inch, orientation: .portrait, child: vc)
-            parent.view.frame.size.height = 1_600
-
-          self.scheduler.run()
-
-          FBSnapshotVerifyView(vc.view, identifier: "lang_\(language)")
-      }
-    }
-  }
-
-  func testCreator() {
-    let currentUser = .template
-      |> User.lens.stats.backedProjectsCount .~ 1234
-      |> User.lens.stats.createdProjectsCount .~ 2
-
-    Language.allLanguages.forEach { language in
-      withEnvironment(
-        apiService: MockService(fetchUserSelfResponse: currentUser),
-        currentUser: currentUser,
-        language: language) {
-
-          let vc = SettingsViewController.instantiate()
+          let vc = SettingsV2ViewController.instantiate()
           let (parent, _) = traitControllers(device: .phone4_7inch, orientation: .portrait, child: vc)
-          parent.view.frame.size.height = 1_600
-
-          self.scheduler.run()
-
-          FBSnapshotVerifyView(vc.view, identifier: "lang_\(language)")}
-      }
-  }
-
-  func testMember() {
-    let currentUser = .template
-      |> User.lens.stats.backedProjectsCount .~ 1234
-      |> User.lens.stats.memberProjectsCount .~ 2
-
-    Language.allLanguages.forEach { language in
-      withEnvironment(
-        apiService: MockService(fetchUserSelfResponse: currentUser),
-        currentUser: currentUser,
-        language: language) {
-
-          let vc = SettingsViewController.instantiate()
-          let (parent, _) = traitControllers(device: .phone4_7inch, orientation: .portrait, child: vc)
-          parent.view.frame.size.height = 1_600
 
           self.scheduler.run()
 
