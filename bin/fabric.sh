8b8762daf7a4d08b2aaeec52b3e8f6ac02a9a022
Isabel Barrera <ifbarrera@me.com>
2018-08-28 10:54:31 -0400

    Space in script

--- bin/fabric.sh
+++ bin/fabric.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -z "$CI" ] && [ ! -z "$FABRIC_API_KEY" ] && [ ! -z "$FABRIC_BUILD_SECRET"]; then
+if [ ! -z "$CI" ] && [ ! -z "$FABRIC_API_KEY" ] && [ ! -z "$FABRIC_BUILD_SECRET" ]; then
 	echo "Configuring Fabric..."
 	"${PROJECT_DIR}"/Frameworks/Fabric/Fabric.framework/run "$FABRIC_API_KEY" "$FABRIC_BUILD_SECRET"
 else
