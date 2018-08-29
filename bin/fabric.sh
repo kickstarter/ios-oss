3fea2c73b721bce21e555b13083db6eed5cbd7c1
Isabel Barrera <ifbarrera@me.com>
2018-08-29 10:17:56 -0400

    Automatically refresh dSYMs and upload to Crashlytics (#420)
    * refresh_dsyms lane
    
    * test_refresh_dsyms job
    
    * Require swiftlint-and-cache
    
    * Indentation
    
    * Verbose
    
    * Space in script
    
    * Change binary path
    
    * checking in upload-symbols tool
    
    * Rename job, link it to alpha-dist builds
    
    * Add slack alert for refreshed dsyms

--- bin/fabric.sh
+++ bin/fabric.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -z "$CI" ] && [ ! -z "$FABRIC_API_KEY" ]; then
+if [ ! -z "$CI" ] && [ ! -z "$FABRIC_API_KEY" ] && [ ! -z "$FABRIC_BUILD_SECRET" ]; then
 	echo "Configuring Fabric..."
-	"${PROJECT_DIR}"/Frameworks/Fabric/Fabric.framework/run "$FABRIC_API_KEY"
+	"${PROJECT_DIR}"/Frameworks/Fabric/Fabric.framework/run "$FABRIC_API_KEY" "$FABRIC_BUILD_SECRET"
 else
 	echo "Skipping Fabric configuration..."
 fi
