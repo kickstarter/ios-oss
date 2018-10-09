ba55446c8681f146676dc11652ce2d3513307210
Christella Dolmo <cdolm92@gmail.com>
2018-10-09 16:45:14 -0400

    disable swiftlint todo

--- Kickstarter-iOS/Views/Cells/SettingsCurrencyPickerCell.swift
+++ Kickstarter-iOS/Views/Cells/SettingsCurrencyPickerCell.swift
@@ -65,6 +65,7 @@ extension SettingsCurrencyPickerCell: UIPickerViewDelegate {
       return
     }
 
+    // swiftlint:disable:next todo
     //TODO - remove this notification once the currency can be read from the user object
     NotificationCenter.default.post(name: Notification.Name.ksr_updatedCurrencyCellDetailText,
                                     object: nil,
