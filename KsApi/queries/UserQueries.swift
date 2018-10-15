87a1bff7778cfb36f2b6e1d9fb36bd7198d3750c
Isabel Barrera <ifbarrera@me.com>
2018-10-15 16:11:14 -0400

    Queryable

--- KsApi/queries/UserQueries.swift
+++ KsApi/queries/UserQueries.swift
@@ -1,5 +1,20 @@
 import Prelude
 
+protocol Queryable {
+  var query: NonEmptySet<Query> { get }
+}
+
+public enum UserQueries: Queryable {
+  case chosenCurrency
+
+  public var query: NonEmptySet<Query> {
+    switch self {
+    case .chosenCurrency:
+      return NonEmptySet(Query.user(chosenCurrencyQueryFields()))
+    }
+  }
+}
+
 public func chosenCurrencyQueryFields() -> NonEmptySet<Query.User> {
   return .chosenCurrency +| []
 }
