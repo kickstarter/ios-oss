97146599bb74a021bd90066ff624dcbc4990cbc0
Isabel Barrera <ifbarrera@me.com>
2018-10-15 15:52:11 -0400

    Remove baseUserQueryFields

--- KsApi/queries/UserQueries.swift
+++ KsApi/queries/UserQueries.swift
@@ -1,9 +1,5 @@
 import Prelude
 
-public func baseUserQueryFields() -> NonEmptySet<Query.User> {
-  return .id +| [.userId, .name]
-}
-
 public func chosenCurrencyQueryFields() -> NonEmptySet<Query.User> {
-  return baseUserQueryFields().op(.chosenCurrency +| [])
+  return .chosenCurrency +| []
 }
--- KsApi/queries/UserQueriesTests.swift
+++ KsApi/queries/UserQueriesTests.swift
@@ -3,17 +3,10 @@ import XCTest
 @testable import KsApi
 
 final class UserQueriesTests: XCTestCase {
-  func testBaseUserQuery() {
-    let query = Query.user(baseUserQueryFields())
-
-    XCTAssertEqual("me { id name uid }", query.description)
-    XCTAssertEqual("{ me { id name uid } }", Query.build(NonEmptySet(query)))
-  }
-
   func testUserCurrencyQuery() {
     let query = Query.user(chosenCurrencyQueryFields())
 
-    XCTAssertEqual("me { chosenCurrency id name uid }", query.description)
-    XCTAssertEqual("{ me { chosenCurrency id name uid } }", Query.build(NonEmptySet(query)))
+    XCTAssertEqual("me { chosenCurrency }", query.description)
+    XCTAssertEqual("{ me { chosenCurrency } }", Query.build(NonEmptySet(query)))
   }
 }
