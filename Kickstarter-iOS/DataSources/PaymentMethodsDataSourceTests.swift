fc303de547dd84eac07bb5bb6cbc26901b3ae8da
Justin Swart <justinswart@users.noreply.github.com>
2018-11-08 16:59:31 -0800

    Test datasource

--- Kickstarter-iOS/DataSources/PaymentMethodsDataSourceTests.swift
+++ Kickstarter-iOS/DataSources/PaymentMethodsDataSourceTests.swift
@@ -10,12 +10,35 @@ internal final class PaymentMethodsDataSourceTests: XCTestCase {
   let tableView = UITableView(frame: .zero)
 
   func testDataSource() {
-
     let cards = GraphUserCreditCard.template.storedCards.nodes
     self.dataSource.load(creditCards: cards)
 
     XCTAssertEqual(7, self.dataSource.tableView(self.tableView, numberOfRowsInSection: 0))
 
     XCTAssertEqual("CreditCardCell", self.dataSource.reusableId(item: 0, section: 0))
+  }
+
+  func testCardDeletion() {
+    let cards = GraphUserCreditCard.template.storedCards.nodes
+    self.dataSource.load(creditCards: cards)
+
+    XCTAssertEqual(7, self.dataSource.tableView(self.tableView, numberOfRowsInSection: 0))
+
+    XCTAssertEqual("CreditCardCell", self.dataSource.reusableId(item: 0, section: 0))
+
+    guard let card = cards.first else {
+      XCTFail("Card should exist")
+      return
+    }
+
+    var deletedCard: GraphUserCreditCard.CreditCard?
+    let deletionHandler = { card in
+      deletedCard = card
+    }
+
+    self.dataSource.deletionHandler = deletionHandler
+    self.dataSource.tableView(self.tableView, commit: .delete, forRowAt: .init(row: 0, section: 0))
+
+    XCTAssertEqual(deletedCard, card)
   }
 }
