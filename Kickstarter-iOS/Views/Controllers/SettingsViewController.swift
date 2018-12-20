16f2f5fc9b41f59b34b3d1bcd8ba9a431bba2ad6
Pavel Dušátko <pavel.dusatko@gmail.com>
2018-12-20 15:19:39 -0800

    Accessibility - Settings - Dismiss button (#532)
    * Add accessibility label to dismiss button & fix the minimum width
    
    * Set navigation item properties using key paths

--- Kickstarter-iOS/Views/Controllers/SettingsViewController.swift
+++ Kickstarter-iOS/Views/Controllers/SettingsViewController.swift
@@ -28,12 +28,7 @@ final class SettingsViewController: UIViewController {
     self.tableView.registerHeaderFooter(nib: .SettingsHeaderView)
 
     if self.presentingViewController != nil {
-      let image = UIImage(named: "icon--cross")
-      self.navigationItem.leftBarButtonItem =
-        UIBarButtonItem(image: image,
-                        style: .plain,
-                        target: self,
-                        action: #selector(closeButtonPressed))
+      self.navigationItem.leftBarButtonItem = self.leftBarButtonItem()
     }
 
     self.userUpdatedObserver = NotificationCenter.default
@@ -87,6 +82,21 @@ final class SettingsViewController: UIViewController {
     self.viewModel.outputs.goToAppStoreRating
       .observeForControllerAction()
       .observeValues { [weak self] link in self?.goToAppStore(link: link) }
+  }
+
+  private func leftBarButtonItem() -> UIBarButtonItem {
+    let leftBarButtonItem = UIBarButtonItem(
+      image: UIImage(named: "icon--cross"),
+      style: .plain,
+      target: self,
+      action: #selector(closeButtonPressed)
+    )
+
+    _ = leftBarButtonItem
+      |> \.accessibilityLabel %~ { _ in Strings.Dismiss() }
+      |> \.width .~ 44
+
+    return leftBarButtonItem
   }
 
   @objc fileprivate func closeButtonPressed() {
