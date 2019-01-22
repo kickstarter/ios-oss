bf4e82f347b1691b7e67ad080eeebe3ea281d4f6
Isabel Barrera <ifbarrera@me.com>
2019-01-22 17:27:59 -0500

    Simplify add card button

--- Kickstarter-iOS/Views/PaymentMethodsFooterView.swift
+++ Kickstarter-iOS/Views/PaymentMethodsFooterView.swift
@@ -10,24 +10,25 @@ public final class PaymentMethodsFooterView: UITableViewHeaderFooterView {
 
   public weak var delegate: PaymentMethodsFooterViewDelegate?
 
-  @IBOutlet private weak var plusLabel: UILabel!
-  @IBOutlet private weak var titleLabel: UILabel!
+  @IBOutlet private weak var addCardButton: UIButton!
   @IBOutlet private weak var separatorView: UIView!
 
   override public func bindStyles() {
     super.bindViewModel()
 
-    _ = self
-      |> \.backgroundColor .~ .ksr_grey_200
-
-    _ = self.plusLabel
-      |> \.textColor .~ .ksr_green_700
-
-    _ = self.titleLabel
-      |> \.text %~ { _ in
+    _ = self.addCardButton
+      |> UIButton.lens.titleColor(for: .normal) .~ .ksr_green_700
+      |> UIButton.lens.titleEdgeInsets .~ UIEdgeInsets(left: Styles.grid(4))
+      |> UIButton.lens.imageEdgeInsets .~ UIEdgeInsets(left: Styles.grid(2))
+      |> UIButton.lens.title(for: .normal) %~ { _ in
         Strings.Add_new_card()
       }
-      |> \.textColor .~ .ksr_green_700
+
+    _ = self.addCardButton.imageView
+      ?|> \.tintColor .~ .ksr_green_700
+
+    _ = self
+      |> \.backgroundColor .~ .ksr_grey_200
 
     _ = self.separatorView
       |> separatorStyle
