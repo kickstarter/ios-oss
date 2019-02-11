35b7d823e844406ac9226cdcf967358ebf7e76fe
Nino Collaco <scollaco@gmail.com>
2019-02-11 18:34:12 -0500

    Added more unit tests

--- Library/ViewModels/PaymentMethodsViewModelTests.swift
+++ Library/ViewModels/PaymentMethodsViewModelTests.swift
@@ -59,11 +59,9 @@ internal final class PaymentMethodsViewModelTests: TestCase {
       self.editButtonIsEnabled.assertDidNotEmitValue()
 
       self.vm.inputs.viewDidLoad()
-
       self.editButtonIsEnabled.assertValues([false])
 
       self.vm.inputs.viewWillAppear()
-
       self.scheduler.advance()
 
       self.editButtonIsEnabled.assertValues([false, true])
@@ -78,7 +76,6 @@ internal final class PaymentMethodsViewModelTests: TestCase {
     withEnvironment(apiService: apiService) {
 
       self.editButtonIsEnabled.assertDidNotEmitValue()
-
       self.vm.inputs.viewDidLoad()
 
       self.editButtonIsEnabled.assertValues([false])
@@ -104,10 +101,9 @@ internal final class PaymentMethodsViewModelTests: TestCase {
       self.editButtonIsEnabled.assertValues([false])
 
       self.vm.inputs.didDelete(card)
-
       self.scheduler.advance()
 
-      self.editButtonIsEnabled.assertValues([false, false])
+      self.editButtonIsEnabled.assertValues([false, true])
     }
   }
 
