27392c33dca761c801e8425c557722915723cc92
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-02-12 17:23:53 -0800

    Remove reference to weak self

--- Kickstarter-iOS/Views/Controllers/PaymentMethodsViewController.swift
+++ Kickstarter-iOS/Views/Controllers/PaymentMethodsViewController.swift
@@ -183,15 +183,13 @@ extension PaymentMethodsViewController: PaymentMethodsFooterViewDelegate {
 
 extension PaymentMethodsViewController: AddNewCardViewControllerDelegate {
   func addNewCardViewControllerSucceeded(with message: String) {
-    self.dismiss(animated: true) { [weak self] in
-      guard let self = self else { return }
+    self.dismiss(animated: true) {
       self.viewModel.inputs.cardAddedSuccessfully(message)
     }
   }
 
   func addNewCardViewControllerDismissed() {
-    self.dismiss(animated: true) { [weak self] in
-      guard let self = self else { return }
+    self.dismiss(animated: true) {
       self.viewModel.inputs.refresh()
     }
   }
