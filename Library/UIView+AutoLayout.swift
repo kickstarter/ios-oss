b34e515cbce02f101c6731daa99601cd7dd9d892
Justin Swart <justinswart@users.noreply.github.com>
2019-02-19 14:40:18 -0800

    Fix autolayout warnings

--- Library/UIView+AutoLayout.swift
+++ Library/UIView+AutoLayout.swift
@@ -1,14 +1,18 @@
 import UIKit
 
 extension UIView {
-  public func constrainEdges(to view: UIView) {
+  public func constrainEdges(to view: UIView, priority: UILayoutPriority = .required) {
     self.translatesAutoresizingMaskIntoConstraints = false
 
-    NSLayoutConstraint.activate([
+    let constraints = [
       self.leadingAnchor.constraint(equalTo: view.leadingAnchor),
       self.trailingAnchor.constraint(equalTo: view.trailingAnchor),
       self.topAnchor.constraint(equalTo: view.topAnchor),
       self.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-    ])
+    ]
+
+    constraints.forEach { $0.priority = priority }
+
+    NSLayoutConstraint.activate(constraints)
   }
 }
