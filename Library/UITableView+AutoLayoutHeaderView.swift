337b31f1cc9aecbe6be2901ad1824d2bccc1b016
Justin Swart <justinswart@users.noreply.github.com>
2019-02-22 11:29:59 -0800

    Currency Selection Feature (#575)
    * ① [Currency selection] Remove currency picker (#568)
    
    * Remove currency picker
    
    * swiftlint
    
    * Remove redundant enum case
    
    * Remove reference
    
    * ③ [Currency selection] Add table view again (#582)
    
    * Remove currency picker
    
    * swiftlint
    
    * Add SelectCurrencyViewController
    
    * Fix tests
    
    * Update snapshots
    
    * Remove redundant enum case
    
    * Remove reference
    
    * Remove SettingsCurrencyCell
    
    * Fix snapshot tests
    
    * Remove CaseIterable conformance on SettingsAccountCellType
    
    * Use reuseIdentifier local constant
    
    * Update pragma
    
    * Update tableview instantiation
    
    * Set nav title
    
    * Fix yet another merge issue
    
    * Fix merge
    
    * ③ [Currency selection] Add table view header (#570)
    
    * Add SelectCurrencyViewController
    
    * Fix tests
    
    * Add tableview header
    
    * Update snapshots
    
    * Added ViewController tests
    
    * Removed recordMode line from test
    
    * Simplify layout code for header
    
    * Remove SettingsCurrencyCell
    
    * Fix snapshot tests
    
    * Remove CaseIterable conformance on SettingsAccountCellType
    
    * Use reuseIdentifier local constant
    
    * Update pragma
    
    * Update tableview instantiation
    
    * Implicit returns in lazy vars
    
    * Fix alignment
    
    * ① [Currency selection] Remove currency picker (#568)
    
    * Remove currency picker
    
    * swiftlint
    
    * Remove redundant enum case
    
    * Remove reference
    
    * Remove local var, improve function name
    
    * Set label colour
    
    * Update snapshots
    
    * Fix project file
    
    * Correctly size header/footer views
    
    * Make the tests pass (#585)
    
    * Run snapshot tests on all devices
    
    * Remove duplicate extension, use keypath setter
    
    * Fix autolayout warnings
    
    * ④ [Currency selection] Add change currency api calls (#571)
    
    * Remove currency picker
    
    * swiftlint
    
    * Add SelectCurrencyViewController
    
    * Fix tests
    
    * Update snapshots
    
    * Add tableview header
    
    * Added ViewController tests
    
    * Removed recordMode line from test
    
    * Simplify layout code for header
    
    * Add update calls
    
    * swiftlint
    
    * Add update calls
    
    * Add view model tests
    
    * Remove stray file ref
    
    * Fix tests
    
    * swiftlint
    
    * Remove redundant enum case
    
    * Fix rebase duplication
    
    * Remove reference
    
    * Remove SettingsCurrencyCell
    
    * Fix snapshot tests
    
    * Remove CaseIterable conformance on SettingsAccountCellType
    
    * Use reuseIdentifier local constant
    
    * Update pragma
    
    * Update tableview instantiation
    
    * Implicit returns in lazy vars
    
    * Fix alignment
    
    * Add SelectCurrencyViewController
    
    * Fix tests
    
    * Add tableview header
    
    * Update snapshots
    
    * Added ViewController tests
    
    * Removed recordMode line from test
    
    * Simplify layout code for header
    
    * Remove SettingsCurrencyCell
    
    * Fix snapshot tests
    
    * Remove CaseIterable conformance on SettingsAccountCellType
    
    * Use reuseIdentifier local constant
    
    * Update pragma
    
    * Update tableview instantiation
    
    * Implicit returns in lazy vars
    
    * Fix alignment
    
    * ① [Currency selection] Remove currency picker (#568)
    
    * Remove currency picker
    
    * swiftlint
    
    * Remove redundant enum case
    
    * Remove reference
    
    * Remove local var, improve function name
    
    * Set label colour
    
    * Update snapshots
    
    * Fix project file
    
    * Correctly size header/footer views
    
    * Make the tests pass (#585)
    
    * Fix merge issue
    
    * Run snapshot tests on all devices
    
    * Remove duplicate extension, use keypath setter
    
    * Fix autolayout warnings
    
    * Fix merge issue
    
    * Add weak self, fix project file
    
    * Renamed signal
    
    * Refactor view model and tests
    
    * Use more inference
    
    * swiftlint
    
    * Add success banner

---
+++ Library/UITableView+AutoLayoutHeaderView.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+extension UITableView {
+  public func ksr_sizeHeaderFooterViewsToFit() {
+    let keyPaths: [ReferenceWritableKeyPath<UITableView, UIView?>] = [
+      (\.tableHeaderView),
+      (\.tableFooterView)
+    ]
+
+    keyPaths.forEach { keyPath in
+      if let view = self[keyPath: keyPath] {
+        let size = view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+
+        if view.frame.height != size.height {
+          view.frame.size.height = size.height
+
+          self[keyPath: keyPath] = view
+        }
+      }
+    }
+  }
+}
