3789c51ecb8b76872137e530dbff12ba8c6d6ac2
Nino Collaco <scollaco@gmail.com>
2019-03-04 17:08:43 -0500

    fixed identation on XCTestManifests file

--- bin/StringsScript/Tests/StringsScriptTests/XCTestManifests.swift
+++ bin/StringsScript/Tests/StringsScriptTests/XCTestManifests.swift
@@ -2,8 +2,8 @@ import XCTest
 
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(StringsScriptTests.allTests),
-    ]
+  return [
+    testCase(StringsScriptTests.allTests),
+  ]
 }
 #endif
