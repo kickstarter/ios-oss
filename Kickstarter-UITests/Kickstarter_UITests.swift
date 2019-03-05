49ac0a296649bb37c572fc0f5103b5573965532a
Nino Collaco <scollaco@gmail.com>
2019-03-05 15:04:13 -0500

    swiftlint

--- Kickstarter-UITests/Kickstarter_UITests.swift
+++ Kickstarter-UITests/Kickstarter_UITests.swift
@@ -1,34 +1,8 @@
-//
-//  Kickstarter_UITests.swift
-//  Kickstarter-UITests
-//
-//  Created by Saturnino Teixeira on 04/03/2019.
-//  Copyright © 2019 Kickstarter. All rights reserved.
-//
-
 import XCTest
 
 class Kickstarter_UITests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {}
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+    override func tearDown() {}
 }
