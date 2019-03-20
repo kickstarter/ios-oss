124b7ca8f195a8d809c365d5b35bfef21996b428
Saturnino Collaço Teixeira Filho <scollaco@gmail.com>
2019-03-20 10:42:15 -0400

    Add UnionPay support (#631)

--- KsApi/models/GraphUserCreditCard.swift
+++ KsApi/models/GraphUserCreditCard.swift
@@ -24,17 +24,22 @@ public struct GraphUserCreditCard: Swift.Decodable {
 
   public enum CreditCardType: String, Decodable, CaseIterable {
     case amex = "AMEX"
+    case diners = "DINERS"
     case discover = "DISCOVER"
     case jcb = "JCB"
     case mastercard = "MASTERCARD"
+    case unionPay = "UNION_PAY"
     case visa = "VISA"
-    case diners = "DINERS"
     case generic = "----"
 
     public var description: String? {
       switch self {
       case .amex, .discover, .jcb, .mastercard, .visa, .diners:
         return self.rawValue.capitalized
+      case .unionPay:
+        return self.rawValue
+          .capitalized
+          .replacingOccurrences(of: "_", with: " ")
       default:
         return nil
       }
