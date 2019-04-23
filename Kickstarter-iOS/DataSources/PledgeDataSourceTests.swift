757ab89d71606cecf76caae6de91b15c8010775b
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-04-23 09:48:35 -0700

    Fix tests

--- Kickstarter-iOS/DataSources/PledgeDataSourceTests.swift
+++ Kickstarter-iOS/DataSources/PledgeDataSourceTests.swift
@@ -4,15 +4,15 @@ import XCTest
 @testable import KsApi
 
 final class PledgeDataSourceTests: XCTestCase {
-  let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
   let dataSource = PledgeDataSource()
+  let tableView = UITableView(frame: .zero, style: .plain)
 
   func testLoad() {
     dataSource.load(amount: 100, currency: "USD")
 
-    XCTAssertEqual(3, self.dataSource.numberOfSections(in: collectionView))
-    XCTAssertEqual(1, self.dataSource.collectionView(collectionView, numberOfItemsInSection: 0))
-    XCTAssertEqual(2, self.dataSource.collectionView(collectionView, numberOfItemsInSection: 1))
-    XCTAssertEqual(1, self.dataSource.collectionView(collectionView, numberOfItemsInSection: 2))
+    XCTAssertEqual(3, self.dataSource.numberOfSections(in: self.tableView))
+    XCTAssertEqual(1, self.dataSource.tableView(self.tableView, numberOfRowsInSection: 0))
+    XCTAssertEqual(2, self.dataSource.tableView(self.tableView, numberOfRowsInSection: 1))
+    XCTAssertEqual(1, self.dataSource.tableView(self.tableView, numberOfRowsInSection: 2))
   }
 }
