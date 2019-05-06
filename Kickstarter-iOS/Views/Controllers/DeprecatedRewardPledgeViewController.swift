6a7bfbde784aa920b821b95815c1921a6ea3349c
Christella Dolmo <cdolm92@gmail.com>
2019-05-06 13:56:56 -0400

    snapshot tests

--- Kickstarter-iOS/Views/Controllers/DeprecatedRewardPledgeViewController.swift
+++ Kickstarter-iOS/Views/Controllers/DeprecatedRewardPledgeViewController.swift
@@ -230,7 +230,8 @@ internal final class DeprecatedRewardPledgeViewController: UIViewController {
 
     _ = self.disclaimerButton
       |> UIButton.lens.accessibilityLabel %~ { _ in
-        Strings.Kickstarter_is_not_a_store_Its_a_way_to_bring_creative_projects_to_life()
+        Strings.Kickstarter_is_not_a_store()
+          + " " + Strings.Its_a_way_to_bring_creative_projects_to_life()
           + " " + Strings.Learn_more_about_accountability()
     }
 
@@ -242,15 +243,14 @@ internal final class DeprecatedRewardPledgeViewController: UIViewController {
       |> UILabel.lens.textColor .~ UIColor.ksr_text_dark_grey_400
       |> UILabel.lens.textAlignment .~ .center
       |> UILabel.lens.numberOfLines .~ 2
-      |> UILabel.lens.text %~ { _ in
-        Strings.Kickstarter_is_not_a_store_Its_a_way_to_bring_creative_projects_to_life()
-      }
+      |> UILabel.lens.text %~ { _ in Strings.Kickstarter_is_not_a_store() }
 
     _ = self.disclaimerSecondaryLabel
       |> UILabel.lens.font .~ UIFont.ksr_caption1(size: 12)
       |> UILabel.lens.textColor .~ UIColor.ksr_text_dark_grey_400
       |> UILabel.lens.textAlignment .~ .center
       |> UILabel.lens.numberOfLines .~ 2
+      |> UILabel.lens.text %~ { _ in Strings.Its_a_way_to_bring_creative_projects_to_life() }
 
     _ = self.disclaimerTertiaryLabel
       |> UILabel.lens.font .~ UIFont.ksr_caption1(size: 12)
