3bad8c344b11df32dffe1635eb482beb695bae84
Isabel Barrera <isabel@kickstarter.com>
2019-05-22 07:53:38 -0400

    💲[Native Checkout] Sheet Overlay Container (#674)
    * Sheet overlay container VC and animator
    
    * Renaming, fixing rotation issues
    
    * Revert testing code
    
    * Cleanup and fixing a snapshot
    
    * Using keypath lenses
    
    * Updating the comment
    
    * PR comments
    
    * Renaming
    
    * Cleanup
    
    * View helpers
    
    * Style naming
    
    * Fix build
    
    * Remove iOS 11 Availability Checks From Native Checkout (#679)
    
    * Remove iOS 11 availability references
    
    * Fix merge conflict issues
    
    * Renaming suggestions
    
    * Swiftlint

---
+++ Kickstarter-iOS/Views/Transitions/SheetOverlayTransitionAnimator.swift
@@ -0,0 +1,104 @@
+import UIKit
+import Library
+import Prelude
+
+final class SheetOverlayTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+  private let darkOverlayView: UIView = {
+    UIView(frame: .zero)
+      |> \.backgroundColor .~ UIColor.ksr_soft_black.withAlphaComponent(0.8)
+  }()
+
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0.3
+  }
+
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    guard
+      let fromVC = transitionContext.viewController(forKey: .from),
+      let toVC = transitionContext.viewController(forKey: .to) else { return }
+
+    let containerView = transitionContext.containerView
+
+    if toVC.isBeingPresented {
+      self.animatePresentation(fromViewController: fromVC,
+                               toViewController: toVC,
+                               containerView: containerView,
+                               transitionContext: transitionContext)
+    } else {
+      self.animateDismissal(fromViewController: fromVC,
+                            toViewController: toVC,
+                            containerView: containerView,
+                            transitionContext: transitionContext)
+    }
+  }
+
+  // MARK: - Presentation
+
+  private func animatePresentation(
+    fromViewController fromVC: UIViewController,
+    toViewController toVC: UIViewController,
+    containerView: UIView,
+    transitionContext: UIViewControllerContextTransitioning) {
+    _ = self.darkOverlayView
+      |> \.alpha .~ 0.0
+      |> \.frame .~ fromVC.view.frame
+
+    _ = toVC.view
+      |> \.backgroundColor .~ .clear
+      |> \.frame .~ fromVC.view.frame.offsetBy(dx: 0, dy: toVC.view.frame.height)
+
+    _ = (self.darkOverlayView, containerView)
+      |> ksr_addSubviewToParent()
+
+    _ = (toVC.view, containerView)
+      |> ksr_addSubviewToParent()
+
+    let toFrame = fromVC.view.frame
+
+    UIView.animate(
+      withDuration: self.transitionDuration(using: transitionContext),
+      delay: 0,
+      options: .curveEaseInOut,
+      animations: { [weak self] in
+        toVC.view.frame = toFrame
+        self?.darkOverlayView.alpha = 1.0
+    }, completion: { [weak self] _ in
+      toVC.view.backgroundColor = self?.darkOverlayView.backgroundColor
+      self?.darkOverlayView.removeFromSuperview()
+
+      transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+    })
+  }
+
+  // MARK: - Dismissal
+
+  private func animateDismissal(fromViewController fromVC: UIViewController,
+                                toViewController toVC: UIViewController,
+                                containerView: UIView,
+                                transitionContext: UIViewControllerContextTransitioning) {
+    _ = self.darkOverlayView
+      |> \.frame .~ fromVC.view.frame
+      |> \.alpha .~ 1.0
+
+    _ = fromVC.view
+      |> \.backgroundColor .~ .clear
+
+    _ = containerView
+      |> ksr_insertSubview(self.darkOverlayView, belowSubview: fromVC.view)
+
+    let toFrame = toVC.view.frame.offsetBy(dx: 0, dy: fromVC.view.frame.height)
+
+    UIView.animate(
+      withDuration: self.transitionDuration(using: transitionContext),
+      delay: 0,
+      options: .curveEaseInOut,
+      animations: { [weak self] in
+        fromVC.view.frame = toFrame
+        self?.darkOverlayView.alpha = 0.0
+    }, completion: { [weak self] _ in
+      self?.darkOverlayView.removeFromSuperview()
+
+      transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+    })
+  }
+}
