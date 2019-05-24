7324bf0d485a0082db9ca9268d849d3e8cff1173
Isabel Barrera <isabel@kickstarter.com>
2019-05-24 16:37:48 +0200

    💲[Native Checkout] Reward Selection (no animation) (#683)
    * Navigate to PledgeVC when reward is selected
    
    * One more test
    
    * Renaming
    
    * Updates based on feedback

--- Library/ViewModels/RewardsCollectionViewModelTests.swift
+++ Library/ViewModels/RewardsCollectionViewModelTests.swift
@@ -9,11 +9,17 @@ import ReactiveExtensions_TestHelpers
 final class RewardsCollectionViewModelTests: TestCase {
   private let vm: RewardsCollectionViewModelType = RewardsCollectionViewModel()
 
+  private let goToPledgeProject = TestObserver<Project, NoError>()
+  private let goToPledgeRefTag = TestObserver<RefTag?, NoError>()
+  private let goToPledgeReward = TestObserver<Reward, NoError>()
   private let reloadDataWithRewards = TestObserver<[Reward], NoError>()
 
   override func setUp() {
     super.setUp()
 
+    self.vm.outputs.goToPledge.map { $0.project }.observe(self.goToPledgeProject.observer)
+    self.vm.outputs.goToPledge.map { $0.reward }.observe(self.goToPledgeReward.observer)
+    self.vm.outputs.goToPledge.map { $0.refTag }.observe(self.goToPledgeRefTag.observer)
     self.vm.outputs.reloadDataWithRewards.observe(self.reloadDataWithRewards.observer)
   }
 
@@ -32,5 +38,30 @@ final class RewardsCollectionViewModelTests: TestCase {
     let value = self.reloadDataWithRewards.values.last
 
     XCTAssertTrue(value?.count == rewardsCount)
+  }
+
+  func testGoToPledge() {
+    let project = Project.cosmicSurgery
+
+    self.vm.inputs.configure(with: project, refTag: .activity)
+    self.vm.inputs.viewDidLoad()
+
+    self.goToPledgeProject.assertDidNotEmitValue()
+    self.goToPledgeReward.assertDidNotEmitValue()
+    self.goToPledgeRefTag.assertDidNotEmitValue()
+
+    self.vm.inputs.rewardSelected(at: 0)
+
+    self.goToPledgeProject.assertValues([project])
+    self.goToPledgeReward.assertValues([project.rewards[0]])
+    self.goToPledgeRefTag.assertValues([.activity])
+
+    let endIndex = project.rewards.endIndex
+
+    self.vm.inputs.rewardSelected(at: endIndex - 1)
+
+    self.goToPledgeProject.assertValues([project, project])
+    self.goToPledgeReward.assertValues([project.rewards[0], project.rewards[endIndex - 1]])
+    self.goToPledgeRefTag.assertValues([.activity, .activity])
   }
 }
