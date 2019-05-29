361fa81b4f21e5d8319954dcf2245d40be734ae7
Justin Swart <justinswart@users.noreply.github.com>
2019-05-29 16:08:24 -0700

    Don't waste precious guard statements

--- Library/UILabel+IsTruncated.swift
+++ Library/UILabel+IsTruncated.swift
@@ -4,9 +4,7 @@ public extension UILabel {
 
   public func isTruncated() -> Bool {
     // Determines if Label is truncated
-    guard let string = self.text else { return false }
-
-    guard let font = self.font else { return false }
+    guard let string = self.text, let font = self.font else { return false }
 
     let size: CGSize = string.boundingRect(
       with: CGSize(width: self.frame.size.width, height: CGFloat.greatestFiniteMagnitude),
