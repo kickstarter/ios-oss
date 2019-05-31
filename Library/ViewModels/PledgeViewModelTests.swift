5e93ad0534476f2339f33541652370defc535aaf
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-05-31 11:06:04 -0700

    💲[Native Checkout] Shipping location cell plus sign (#691)
    * Add the ability to concatenate two NSAttributedStrings
    
    * Add plus sign attributed string
    
    * Prefix shipping location amount with a plus sign

--- Library/ViewModels/PledgeViewModelTests.swift
+++ Library/ViewModels/PledgeViewModelTests.swift
@@ -46,7 +46,7 @@ final class PledgeViewModelTests: TestCase {
         [Format.date(secondsInUTC: estimatedDelivery, template: "MMMMyyyy", timeZone: UTCTimeZone)]
       )
       self.shippingLocation.assertValues(["Brooklyn"])
-      self.shippingAmount.assertValues(["$7.50"])
+      self.shippingAmount.assertValues(["+$7.50"])
       self.isLoggedIn.assertValues([false])
     }
   }
@@ -67,7 +67,7 @@ final class PledgeViewModelTests: TestCase {
         [Format.date(secondsInUTC: estimatedDelivery, template: "MMMMyyyy", timeZone: UTCTimeZone)]
       )
       self.shippingLocation.assertValues(["Brooklyn"])
-      self.shippingAmount.assertValues(["$7.50"])
+      self.shippingAmount.assertValues(["+$7.50"])
       self.isLoggedIn.assertValues([true])
     }
   }
