b37b50a408f7d008ccb76c2b05d4cf4bd618eadf
Nino Collaco <scollaco@gmail.com>
2019-06-25 11:46:37 -0400

    Added PledgePaymentMethodsCell file

---
+++ Kickstarter-iOS/Views/Cells/PledgePaymentMethodsCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+final class PledgePaymentMethodsCell: UITableViewCell, ValueCell {
+
+  // MARK: - Lifecycle
+
+
+  // MARK: - Lifecycle
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
