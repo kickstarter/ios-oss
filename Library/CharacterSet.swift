55c7ca1226c24c29a92c7671b79557d757a55695
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-06-25 23:35:02 -0700

    Simplify return statement

--- Library/CharacterSet.swift
+++ Library/CharacterSet.swift
@@ -2,10 +2,7 @@ import Foundation
 
 public extension CharacterSet {
   static func ksr_decimalSeparators() -> CharacterSet {
-    if let decimalSeparator = AppEnvironment.current.locale.decimalSeparator {
-      return CharacterSet(charactersIn: decimalSeparator)
-    }
-    return CharacterSet()
+    return CharacterSet(charactersIn: AppEnvironment.current.locale.decimalSeparator ?? "")
   }
 
   static func ksr_numericCharacters() -> CharacterSet {
