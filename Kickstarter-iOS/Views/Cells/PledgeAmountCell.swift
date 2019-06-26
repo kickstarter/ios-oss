20703955659a5e5056faada9944d5feea262f31c
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-06-26 14:39:10 -0700

    Use coalesceWith

--- Kickstarter-iOS/Views/Cells/PledgeAmountCell.swift
+++ Kickstarter-iOS/Views/Cells/PledgeAmountCell.swift
@@ -141,7 +141,7 @@ extension PledgeAmountCell: UITextFieldDelegate {
     _ textField: UITextField, shouldChangeCharactersIn _: NSRange, replacementString string: String
   ) -> Bool {
     let decimalSeparatorCharacters = CharacterSet.ksr_decimalSeparators()
-    let existingCharacters = CharacterSet(charactersIn: textField.text ?? "")
+    let existingCharacters = CharacterSet(charactersIn: textField.text.coalesceWith(""))
     let inputCharacters = CharacterSet(charactersIn: string)
     let numericCharacters = CharacterSet.ksr_numericCharacters()
 
