c092f7427f098a2e4b73507ff7c02ec6558d3485
Christella Dolmo <cdolm92@gmail.com>
2019-07-09 12:27:59 -0400

    snapshot tests fix for error state

--- Kickstarter-iOS/Views/PledgeCTAContainerView.swift
+++ Kickstarter-iOS/Views/PledgeCTAContainerView.swift
@@ -137,7 +137,6 @@ private func pledgeCTAButtonStyle(
 
     return button
       |> roundedStyle(cornerRadius: 12)
-      |> UIButton.lens.titleColor(for: .normal) .~ UIColor.white
       |> UIButton.lens.titleLabel.font .~ UIFont.ksr_headline(size: 15)
       |> UIButton.lens.layer.borderWidth .~ 0
       |> UIButton.lens.titleEdgeInsets .~ .init(topBottom: Styles.grid(1), leftRight: Styles.grid(2))
