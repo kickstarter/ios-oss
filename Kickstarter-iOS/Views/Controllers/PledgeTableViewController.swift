09d39c917b7ff42433cd5689eccc5f688c52f185
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-07-09 16:43:56 -0700

    💲[Native Checkout] Shipping Picker - Navigation Workflow - 1/4 (#738)
    * Rename button
    
    * Hook the button up to its action
    
    * Fit target action on one line
    
    * Refactor shipping rules logic
    
    * Revert MockService changes
    
    * Hook up selection of a shipping rule to presenting a picker
    
    * Rename button signal
    
    * Rename a property
    
    * Add comments
    
    * Extract tuple to a private function for allowing point free assignment
    
    * Explicitly state masked corners type to help the compiler
    
    * Rename view controller
    
    * Extract offset value to a private enum constant
    
    * Rename delegate method
    
    * Rename signals
    
    * Update top offset
    
    * Rename output signal

--- Kickstarter-iOS/Views/Controllers/PledgeTableViewController.swift
+++ Kickstarter-iOS/Views/Controllers/PledgeTableViewController.swift
@@ -3,6 +3,12 @@ import Library
 import Prelude
 import UIKit
 
+private enum Layout {
+  enum Sheet {
+    static let offset: CGFloat = 222
+  }
+}
+
 class PledgeTableViewController: UITableViewController {
   // MARK: - Properties
 
@@ -67,6 +73,14 @@ class PledgeTableViewController: UITableViewController {
         }
       }
 
+    self.viewModel.outputs.presentShippingRules
+      .observeForUI()
+      .observeValues { [weak self] project, shippingRules, selectedShippingRule in
+        self?.presentShippingRules(
+          project, shippingRules: shippingRules, selectedShippingRule: selectedShippingRule
+        )
+      }
+
     self.viewModel.outputs.configureShippingLocationCellWithData
       .observeForUI()
       .observeValues { [weak self] isLoading, project, selectedShippingRule in
@@ -80,12 +94,6 @@ class PledgeTableViewController: UITableViewController {
       .observeValues { [weak self] project, pledgeTotal in
         self?.pledgeSummaryCell?.configureWith(value: (project, pledgeTotal))
       }
-  }
-
-  // MARK: - Actions
-
-  @objc func dismissKeyboard() {
-    self.tableView.endEditing(true)
   }
 
   // MARK: - UITableViewDelegate
@@ -118,10 +126,24 @@ class PledgeTableViewController: UITableViewController {
 
   // MARK: - Actions
 
+  @objc func dismissKeyboard() {
+    self.tableView.endEditing(true)
+  }
+
   private func presentHelpWebViewController(with helpType: HelpType) {
     let vc = HelpWebViewController.configuredWith(helpType: helpType)
-    let nav = UINavigationController(rootViewController: vc)
-    self.present(nav, animated: true, completion: nil)
+    let nc = UINavigationController(rootViewController: vc)
+    self.present(nc, animated: true)
+  }
+
+  private func presentShippingRules(
+    _: Project, shippingRules _: [ShippingRule], selectedShippingRule _: ShippingRule
+  ) {
+    let vc = UIViewController()
+    vc.view.backgroundColor = UIColor.cyan
+    let nc = UINavigationController(rootViewController: vc)
+    let sheetVC = SheetOverlayViewController(child: nc, offset: Layout.Sheet.offset)
+    self.present(sheetVC, animated: true)
   }
 }
 
@@ -131,21 +153,24 @@ extension PledgeTableViewController: PledgeDescriptionCellDelegate {
   }
 }
 
-extension PledgeTableViewController: PledgeSummaryCellDelegate {
-  internal func pledgeSummaryCell(_: PledgeSummaryCell, didOpen helpType: HelpType) {
-    self.presentHelpWebViewController(with: helpType)
+extension PledgeTableViewController: PledgeAmountCellDelegate {
+  func pledgeAmountCell(_: PledgeAmountCell, didUpdateAmount amount: Double) {
+    self.viewModel.inputs.pledgeAmountDidUpdate(to: amount)
   }
 }
 
 extension PledgeTableViewController: PledgeShippingLocationCellDelegate {
-  func pledgeShippingCell(_: PledgeShippingLocationCell, didSelectShippingRule rule: ShippingRule) {
-    self.viewModel.inputs.shippingRuleDidUpdate(to: rule)
+  func pledgeShippingCellWillPresentShippingRules(
+    _: PledgeShippingLocationCell,
+    selectedShippingRule rule: ShippingRule
+  ) {
+    self.viewModel.inputs.pledgeShippingCellWillPresentShippingRules(with: rule)
   }
 }
 
-extension PledgeTableViewController: PledgeAmountCellDelegate {
-  func pledgeAmountCell(_: PledgeAmountCell, didUpdateAmount amount: Double) {
-    self.viewModel.inputs.pledgeAmountDidUpdate(to: amount)
+extension PledgeTableViewController: PledgeSummaryCellDelegate {
+  internal func pledgeSummaryCell(_: PledgeSummaryCell, didOpen helpType: HelpType) {
+    self.presentHelpWebViewController(with: helpType)
   }
 }
 
