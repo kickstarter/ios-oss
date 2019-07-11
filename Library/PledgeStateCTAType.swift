7d307de55aa9969c1f507ae03c715359deecf82a
Isabel Barrera <isabel@kickstarter.com>
2019-07-11 15:32:25 -0400

    Revert "💲[Native Checkout] Pledge Error State CTA (#746)" (#748)
    This reverts commit f073a51e26a7aa87c19cc060d6b92ffc64c2821c.

--- Library/PledgeStateCTAType.swift
+++ Library/PledgeStateCTAType.swift
@@ -1,16 +1,24 @@
 import UIKit
 
 public enum PledgeStateCTAType {
-  case fix
   case pledge
   case manage
   case viewBacking
   case viewRewards
 
+  public var buttonBackgroundColor: UIColor {
+    switch self {
+    case .pledge:
+      return .ksr_green_500
+    case .manage:
+      return .ksr_blue_500
+    case .viewBacking, .viewRewards:
+      return .ksr_soft_black
+    }
+  }
+
   public var buttonTitle: String {
     switch self {
-    case .fix:
-      return Strings.Fix()
     case .pledge:
       return Strings.Back_this_project()
     case .manage:
@@ -22,62 +30,11 @@ public enum PledgeStateCTAType {
     }
   }
 
-  public var buttonBackgroundColor: UIColor {
-    switch self {
-    case .fix:
-      return .ksr_apricot_600
-    case .pledge:
-      return .ksr_green_500
-    case .manage:
-      return .ksr_blue_500
-    case .viewBacking, .viewRewards:
-      return .ksr_soft_black
-    }
-  }
-
-  public var buttonTitleTextColor: UIColor {
-    switch self {
-    case .pledge, .manage, .viewBacking, .viewRewards:
-      return .white
-    case .fix:
-      return .ksr_soft_black
-    }
-  }
-
-  public var stackViewIsHidden: Bool {
-    switch self {
-    case .pledge, .viewBacking, .viewRewards:
-      return true
-    case .fix, .manage:
-      return false
-    }
-  }
-
-  public var titleLabel: String? {
-    switch self {
-    case .fix:
-      return Strings.Check_your_payment_details()
-    case .manage:
-      return Strings.Youre_a_backer()
-    default:
-      return nil
-    }
-  }
-
-  public var subtitleLabel: String? {
-    switch self {
-    case .fix:
-      return Strings.We_couldnt_process_your_pledge()
-    default:
-      return nil
-    }
-  }
-
   public var stackViewAndSpacerAreHidden: Bool {
     switch self {
     case .pledge, .viewBacking, .viewRewards:
       return true
-    case .fix, .manage:
+    case .manage:
       return false
     }
   }
