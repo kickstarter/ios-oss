1c6c76a064187eea4fed05d018de30b1b388319e
Isabel Barrera <isabel@kickstarter.com>
2019-07-12 11:47:49 -0400

    💲[Native Checkout] Login & Sign-up on the pledge screen (Part 1) (#727)
    * Continue button functionality
    
    * Tests
    
    * Fixing merge conflict issues
    
    * Fix more issues from merge
    
    * SwiftFormat
    
    * Cleanup
    
    * Fixing merge issues
    
    * Code review comments
    
    * Formatting
    
    * PR comment
    
    * Renaming signals
    
    * Swiftformat

--- Kickstarter-iOS/Views/Cells/PledgeContinueCell.swift
+++ Kickstarter-iOS/Views/Cells/PledgeContinueCell.swift
@@ -2,17 +2,39 @@ import Foundation
 import Library
 import Prelude
 
+protocol PledgeContinueCellDelegate: AnyObject {
+  func pledgeContinueCellDidTapContinue(_ cell: PledgeContinueCell)
+}
+
 final class PledgeContinueCell: UITableViewCell, ValueCell {
+  // MARK: - Properties
+
   private let continueButton = MultiLineButton(type: .custom)
+  internal weak var delegate: PledgeContinueCellDelegate?
+  private let viewModel = PledgeContinueCellViewModel()
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
 
     self.setupSubviews()
+
+    self.bindViewModel()
   }
 
   required init?(coder _: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+  override func bindViewModel() {
+    super.bindViewModel()
+
+    self.viewModel.outputs.notifyDelegateContinueButtonTapped
+      .observeForControllerAction()
+      .observeValues { [weak self] in
+        guard let self = self else { return }
+
+        self.delegate?.pledgeContinueCellDidTapContinue(self)
+      }
   }
 
   override func bindStyles() {
@@ -42,5 +64,15 @@ final class PledgeContinueCell: UITableViewCell, ValueCell {
       |> ksr_constrainViewToMarginsInParent()
 
     self.continueButton.heightAnchor.constraint(greaterThanOrEqualToConstant: Styles.grid(8)).isActive = true
+
+    self.continueButton.addTarget(
+      self,
+      action: #selector(PledgeContinueCell.continueButtonTapped),
+      for: .touchUpInside
+    )
+  }
+
+  @objc private func continueButtonTapped() {
+    self.viewModel.inputs.continueButtonTapped()
   }
 }
