ea4e371341a619381f06bc1f6b26d61151dbb042
christella <cdolm92@gmail.com>
2019-07-24 17:24:28 -0400

    new green 200 color

--- Library/Styles/Colors.swift
+++ Library/Styles/Colors.swift
@@ -28,6 +28,7 @@ extension UIColor {
       ],
 
       "Green": [
+        200: .ksr_green_200,
         400: .ksr_green_400,
         500: .ksr_green_500,
         700: .ksr_green_700,
@@ -111,6 +112,11 @@ extension UIColor {
   /// 0x3B5998
   public static var ksr_facebookBlue: UIColor {
     return .hex(0x3B5998)
+  }
+
+  /// 0x57BFA4
+  public static var ksr_green_200: UIColor {
+    return .hex(0x57BFA4)
   }
 
   /// 0x2BDE73
