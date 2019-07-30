6bb5ff965450341e52c6d838e52f708bff5ef408
Justin Swart <justinswart@users.noreply.github.com>
2019-07-30 10:16:44 -0700

    Add test for featureNativeCheckoutPledgeViewEnabled()

--- Library/Extensions/Feature+HelpersTests.swift
+++ Library/Extensions/Feature+HelpersTests.swift
@@ -5,6 +5,8 @@ import Prelude
 import XCTest
 
 final class FeatureHelpersTests: TestCase {
+  // MARK: - nativeCheckout
+
   func testFeatureNativeCheckoutEnabled_isTrue() {
     let config = Config.template
       |> \.features .~ [Feature.nativeCheckout.rawValue: true]
@@ -26,6 +28,32 @@ final class FeatureHelpersTests: TestCase {
   func testFeatureNativeCheckoutEnabled_isFalse_whenNil() {
     withEnvironment(config: .template) {
       XCTAssertFalse(featureNativeCheckoutEnabled())
+    }
+  }
+
+  // MARK: - nativeCheckoutPledgeView
+
+  func testFeatureNativeCheckoutPledgeViewEnabled_isTrue() {
+    let config = Config.template
+      |> \.features .~ [Feature.nativeCheckoutPledgeView.rawValue: true]
+
+    withEnvironment(config: config) {
+      XCTAssertTrue(featureNativeCheckoutPledgeViewEnabled())
+    }
+  }
+
+  func testFeatureNativeCheckoutPledgeViewEnabled_isFalse() {
+    let config = Config.template
+      |> \.features .~ [Feature.nativeCheckoutPledgeView.rawValue: false]
+
+    withEnvironment(config: config) {
+      XCTAssertFalse(featureNativeCheckoutPledgeViewEnabled())
+    }
+  }
+
+  func testFeatureNativeCheckoutPledgeViewEnabled_isFalse_whenNil() {
+    withEnvironment(config: .template) {
+      XCTAssertFalse(featureNativeCheckoutPledgeViewEnabled())
     }
   }
 }
