4ea97d568a4dc4f1365f94aa0183f775c2e1cb36
Christella <cdolm92@gmail.com>
2019-08-02 12:44:05 -0400

    Feature native checkout reward pledge states (#764)
    * reward state type
    
    * icon to reward states
    
    * time based and limited reward states
    
    * some renaming
    
    * time based and limited reward states
    
    * disabled pledge button
    
    * fix payment copy
    
    * new green 200 color
    
    * new snapshots
    
    * new copy
    
    * updating business logic
    
    * rewardstates enum
    
    * snapshot tests
    
    * swift format
    
    * swift lint fix
    
    * swift lint fix
    
    * frameworks not needed
    
    * removed strings
    
    * new copy in Strings file
    
    * fix payment copy translations
    
    * fix failing tests
    
    * arg name change
    
    * new  snapshots
    
    * delete orig files
    
    * removing colors
    
    * removed latests copy
    
    * remove copy for error state
    
    * removing new localized copy
    
    * removing fix copy
    
    * snapshot fix
    
    * noreward test
    
    * noreward test
    
    * noreward test
    
    * Simplified and rename enum, updated tests

---
+++ Library/RewardCellProjectBackingStateTypeTests.swift
@@ -0,0 +1,73 @@
+@testable import KsApi
+@testable import Library
+import Prelude
+import XCTest
+
+final class RewardCellProjectBackingStateTypeTests: TestCase {
+  func test_Nonbacked_Live() {
+    let project = .template
+      |> Project.lens.personalization.isBacking .~ false
+      |> Project.lens.personalization.backing .~ nil
+      |> Project.lens.state .~ .live
+
+    XCTAssertEqual(
+      .nonBacked(live: .live),
+      RewardCellProjectBackingStateType.state(with: project)
+    )
+  }
+
+  func test_Nonbacked_NonLive() {
+    let project = .template
+      |> Project.lens.personalization.isBacking .~ false
+      |> Project.lens.personalization.backing .~ nil
+      |> Project.lens.state .~ .successful
+
+    XCTAssertEqual(
+      .nonBacked(live: .nonLive),
+      RewardCellProjectBackingStateType.state(with: project)
+    )
+  }
+
+  func test_Backed_NonLive() {
+    let backing = Backing.template
+
+    let project = .template
+      |> Project.lens.personalization.isBacking .~ true
+      |> Project.lens.personalization.backing .~ backing
+      |> Project.lens.state .~ .successful
+
+    XCTAssertEqual(
+      .backed(live: .nonLive),
+      RewardCellProjectBackingStateType.state(with: project)
+    )
+  }
+
+  func test_Backed_Live() {
+    let backing = Backing.template
+
+    let project = .template
+      |> Project.lens.personalization.isBacking .~ true
+      |> Project.lens.personalization.backing .~ backing
+      |> Project.lens.state .~ .live
+
+    XCTAssertEqual(
+      .backed(live: .live),
+      RewardCellProjectBackingStateType.state(with: project)
+    )
+  }
+
+  func test_Backed_Error() {
+    let backing = Backing.template
+      |> Backing.lens.status .~ .errored
+
+    let project = .template
+      |> Project.lens.personalization.isBacking .~ true
+      |> Project.lens.personalization.backing .~ backing
+      |> Project.lens.state .~ .live
+
+    XCTAssertEqual(
+      .backedError,
+      RewardCellProjectBackingStateType.state(with: project)
+    )
+  }
+}
