2c1859e44c6d98b77df328e960e71db4e07d6ee6
christella <cdolm92@gmail.com>
2019-08-21 17:37:22 -0400

    a refactor

--- Library/Styles/ButtonStyles.swift
+++ Library/Styles/ButtonStyles.swift
@@ -89,14 +89,7 @@ public let facebookButtonStyle = baseButtonStyle
 
   <> UIButton.lens.image(for: .normal) %~ { _ in image(named: "fb-logo-white") }
 
-public let fbFollowButtonStyle = baseButtonStyle
-  <> UIButton.lens.titleColor(for: .normal) .~ .white
-  <> UIButton.lens.backgroundColor(for: .normal) .~ .ksr_facebookBlue
-  <> UIButton.lens.titleColor(for: .highlighted) .~ .white
-  <> UIButton.lens.backgroundColor(for: .highlighted) .~ UIColor.ksr_facebookBlue.mixDarker(0.36)
-  <> UIButton.lens.backgroundColor(for: .disabled) .~ UIColor.ksr_facebookBlue.mixLighter(0.36)
-  <> UIButton.lens.tintColor .~ .white
-  <> UIButton.lens.imageEdgeInsets .~ .init(top: 0, left: 0, bottom: 0, right: 18.0)
+public let fbFollowButtonStyle = facebookButtonStyle
   <> UIButton.lens.contentEdgeInsets %~~ { _, button in
     button.traitCollection.verticalSizeClass == .compact
     ? .init(top: 10.0, left: 10.0, bottom: 10.0, right: 20.0)
