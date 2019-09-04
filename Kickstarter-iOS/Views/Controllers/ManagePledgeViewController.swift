cbcd2ba4baccc6a3be35197f5d26bfcfa9562009
Saturnino Collaço Teixeira Filho <scollaco@gmail.com>
2019-09-04 17:19:17 -0400

    View pledge screen (#826)

---
+++ Kickstarter-iOS/Views/Controllers/ManagePledgeViewController.swift
@@ -0,0 +1,95 @@
+import KsApi
+import Library
+import Prelude
+import UIKit
+
+final class ManagePledgeViewController: UIViewController {
+  // MARK: - Properties
+
+  private lazy var closeButton: UIBarButtonItem = {
+    UIBarButtonItem(
+      image: UIImage(named: "icon--cross"),
+      style: .plain,
+      target: self,
+      action: #selector(ManagePledgeViewController.closeButtonTapped)
+    )
+  }()
+
+  private lazy var editButton: UIBarButtonItem = {
+    UIBarButtonItem(
+      image: UIImage(named: "icon--more-menu"),
+      style: .plain,
+      target: self,
+      action: #selector(ManagePledgeViewController.editButtonTapped)
+    )
+  }()
+
+  private lazy var navigationBarShadowImage: UIImage? = {
+    UIImage(in: CGRect(x: 0, y: 0, width: 1, height: 0.5), with: .ksr_dark_grey_400)
+  }()
+
+  private let viewModel: ManagePledgeViewModelType = ManagePledgeViewModel()
+
+  // MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    _ = self
+      |> \.title %~ { _ in Strings.Manage_your_pledge() }
+      |> \.extendedLayoutIncludesOpaqueBars .~ true
+
+    _ = self.navigationController?.navigationBar
+      ?|> \.shadowImage .~ UIImage()
+      ?|> \.isTranslucent .~ false
+      ?|> \.barTintColor .~ .ksr_grey_400
+
+    _ = self.navigationItem
+      ?|> \.leftBarButtonItem .~ self.closeButton
+      ?|> \.rightBarButtonItem .~ self.editButton
+  }
+
+  // MARK: - Styles
+
+  override func bindStyles() {
+    super.bindStyles()
+
+    _ = self.view
+      |> viewStyle
+
+    _ = self.closeButton
+      |> \.accessibilityLabel %~ { _ in Strings.Dismiss() }
+      |> \.width .~ Styles.minTouchSize.width
+  }
+
+  // MARK: - Configuration
+
+  func configureWith(project: Project, reward: Reward) {
+    self.viewModel.inputs.configureWith(project, reward: reward)
+  }
+
+  // MARK: Actions
+
+  @objc private func editButtonTapped() {
+    let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+    actionSheet.addAction(
+      UIAlertAction(title: Strings.Contact_creator(), style: .default)
+    )
+    actionSheet.addAction(
+      UIAlertAction(title: Strings.Cancel(), style: .cancel)
+    )
+    self.present(actionSheet, animated: true)
+  }
+
+  @objc private func closeButtonTapped() {
+    self.dismiss(animated: true)
+  }
+}
+
+// MARK: Styles
+
+public let viewStyle: ViewStyle = { (view: UIView) in
+  view
+    |> \.backgroundColor .~ UIColor.ksr_grey_400
+}
