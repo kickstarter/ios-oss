278d57af40d8e69c2ea98c1d3d81ace03a01994d
christella <cdolm92@gmail.com>
2019-09-04 10:48:58 -0400

    checkout file

--- KsApi/models/CreateBackingEnvelope.swift
+++ KsApi/models/CreateBackingEnvelope.swift
@@ -7,25 +7,3 @@ public struct CreateBackingEnvelope: Decodable {
     public var checkout: Checkout
   }
 }
-
-public struct Checkout: Decodable {
-  public var state: State
-
-  public enum State: String, Decodable, CaseIterable {
-    case authorizing = "AUTHORIZING"
-    case verifying = "VERIFYING"
-    case successful = "SUCCESSFUL"
-    case failed = "FAILED"
-  }
-}
-
-extension Checkout {
-  enum CodingKeys: String, CodingKey {
-    case state
-  }
-
-  public init(from decoder: Decoder) throws {
-    let values = try decoder.container(keyedBy: CodingKeys.self)
-    self.state = try values.decode(State.self, forKey: .state)
-  }
-}
