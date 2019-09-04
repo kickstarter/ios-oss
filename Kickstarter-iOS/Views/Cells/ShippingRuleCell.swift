4391c1ea56f30c4b7b02642548eb92fa23e5ca69
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-09-04 08:12:56 -0700

    [1/2] Shipping rules - Selection UI (#823)
    * Use data wrapper
    
    * Rename loadValues to reloadDataWithShippingRules
    
    * Add the ability to select shipping rule
    
    * Format code
    
    * Scroll to selected shipping rule after the initial reload
    
    * Format code
    
    * Update snapshots
    
    * Rename parameter
    
    * Test reload values explicitly

--- Kickstarter-iOS/Views/Cells/ShippingRuleCell.swift
+++ Kickstarter-iOS/Views/Cells/ShippingRuleCell.swift
@@ -1,13 +1,40 @@
 import Library
-import Prelude
-import Prelude_UIKit
 import UIKit
 
 final class ShippingRuleCell: UITableViewCell, ValueCell {
+  // MARK: - Properties
+
+  private let viewModel: ShippingRuleCellViewModelType = ShippingRuleCellViewModel()
+
+  // MARK: - Lifecycle
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+    self.bindViewModel()
+  }
+
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - View model
+
+  override func bindViewModel() {
+    super.bindViewModel()
+
+    self.textLabel?.rac.text = self.viewModel.outputs.textLabelText
+
+    self.viewModel.outputs.isSelected
+      .observeForUI()
+      .observeValues { [weak self] isSelected in
+        self?.accessoryType = isSelected ? .checkmark : .none
+      }
+  }
+
   // MARK: - Configuration
 
-  func configureWith(value: String) {
-    _ = self.textLabel
-      ?|> \.text .~ value
+  func configureWith(value: ShippingRuleData) {
+    self.viewModel.inputs.configureWith(value)
   }
 }
--- Library/ViewModels/ShippingRulesViewModelTests.swift
+++ Library/ViewModels/ShippingRulesViewModelTests.swift
@@ -2,31 +2,233 @@ import Foundation
 @testable import KsApi
 @testable import Library
 import Prelude
-import ReactiveExtensions
 import ReactiveExtensions_TestHelpers
 
 final class ShippingRulesViewModelTests: TestCase {
   private let vm: ShippingRulesViewModelType = ShippingRulesViewModel()
 
-  private let loadValues = TestObserver<[String], Never>()
+  private let deselectCellAtIndex = TestObserver<Int, Never>()
+  private let flashScrollIndicators = TestObserver<Void, Never>()
+  private let reloadDataWithShippingRulesData = TestObserver<[ShippingRuleData], Never>()
+  private let reloadDataWithShippingRulesReload = TestObserver<Bool, Never>()
+  private let scrollToCellAtIndex = TestObserver<Int, Never>()
+  private let selectCellAtIndex = TestObserver<Int, Never>()
 
   override func setUp() {
     super.setUp()
 
-    self.vm.outputs.loadValues.observe(self.loadValues.observer)
+    self.vm.outputs.deselectCellAtIndex.observe(self.deselectCellAtIndex.observer)
+    self.vm.outputs.flashScrollIndicators.observe(self.flashScrollIndicators.observer)
+
+    self.vm.outputs.reloadDataWithShippingRules.map(first).observe(
+      self.reloadDataWithShippingRulesData.observer
+    )
+    self.vm.outputs.reloadDataWithShippingRules.map(second).observe(
+      self.reloadDataWithShippingRulesReload.observer
+    )
+    self.vm.outputs.scrollToCellAtIndex.observe(self.scrollToCellAtIndex.observer)
+    self.vm.outputs.selectCellAtIndex.observe(self.selectCellAtIndex.observer)
   }
 
-  func testValues() {
+  func testShippingRuleSelection() {
     let project = Project.template
       |> Project.lens.country .~ Project.Country.ca
 
-    let shippingRule = ShippingRule.template
-      |> ShippingRule.lens.cost .~ 25
-      |> (ShippingRule.lens.location .. Location.lens.localizedName) .~ "Canada"
+    let shippingRule1 = ShippingRule.template
+      |> ShippingRule.lens.location .~ .canada
 
-    self.vm.inputs.configureWith(project, shippingRules: [shippingRule], selectedShippingRule: .template)
+    let shippingRule2 = ShippingRule.template
+      |> ShippingRule.lens.location .~ .greatBritain
+
+    let shippingRule3 = ShippingRule.template
+      |> ShippingRule.lens.location .~ .usa
+
+    let shippingRules = [shippingRule1, shippingRule2, shippingRule3]
+
+    self.vm.inputs.configureWith(
+      project, shippingRules: shippingRules, selectedShippingRule: shippingRule2
+    )
     self.vm.inputs.viewDidLoad()
 
-    self.loadValues.assertValues([["Canada (+CA$ 25)"]])
+    self.flashScrollIndicators.assertValueCount(1)
+    self.reloadDataWithShippingRulesData.assertValues([
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule3
+        )
+      ]
+    ])
+    self.reloadDataWithShippingRulesReload.assertValues([true])
+    self.scrollToCellAtIndex.assertValues([1])
+
+    self.vm.inputs.didSelectShippingRule(at: 0)
+
+    self.flashScrollIndicators.assertValueCount(1)
+    self.reloadDataWithShippingRulesData.assertValues([
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule3
+        )
+      ],
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule3
+        )
+      ]
+    ])
+    self.reloadDataWithShippingRulesReload.assertValues([true, false])
+    self.scrollToCellAtIndex.assertValues([1])
+
+    // Selecting out of bounds index does nothing Jon Snow
+    self.vm.inputs.didSelectShippingRule(at: Int.min)
+
+    self.flashScrollIndicators.assertValueCount(1)
+    self.reloadDataWithShippingRulesData.assertValues([
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule3
+        )
+      ],
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule3
+        )
+      ]
+    ])
+    self.reloadDataWithShippingRulesReload.assertValues([true, false])
+    self.scrollToCellAtIndex.assertValues([1])
+
+    self.vm.inputs.didSelectShippingRule(at: Int.max)
+
+    self.flashScrollIndicators.assertValueCount(1)
+    self.reloadDataWithShippingRulesData.assertValues([
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule3
+        )
+      ],
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule3
+        )
+      ]
+    ])
+    self.reloadDataWithShippingRulesReload.assertValues([true, false])
+    self.scrollToCellAtIndex.assertValues([1])
+
+    // Selecting index within bounds does work
+    self.vm.inputs.didSelectShippingRule(at: 2)
+
+    self.flashScrollIndicators.assertValueCount(1)
+    self.reloadDataWithShippingRulesData.assertValues([
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule2, shippingRule: shippingRule3
+        )
+      ],
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule1, shippingRule: shippingRule3
+        )
+      ],
+      [
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule3, shippingRule: shippingRule1
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule3, shippingRule: shippingRule2
+        ),
+        ShippingRuleData(
+          project: project, selectedShippingRule: shippingRule3, shippingRule: shippingRule3
+        )
+      ]
+    ])
+    self.reloadDataWithShippingRulesReload.assertValues([true, false, false])
+    self.scrollToCellAtIndex.assertValues([1])
+  }
+
+  func testCellSelectionDeselection() {
+    let selectedShippingRule = ShippingRule.template
+      |> ShippingRule.lens.location .~ Location.australia
+
+    self.vm.inputs.configureWith(
+      .template,
+      shippingRules: [.template, .template, selectedShippingRule, .template],
+      selectedShippingRule: selectedShippingRule
+    )
+    self.vm.inputs.viewDidLoad()
+
+    self.deselectCellAtIndex.assertDidNotEmitValue()
+    self.selectCellAtIndex.assertValues([2])
+
+    self.vm.inputs.didSelectShippingRule(at: 0)
+
+    self.deselectCellAtIndex.assertValues([2])
+    self.selectCellAtIndex.assertValues([2, 0])
+
+    // Selecting the same index as is currently selected does nothing Jon Snow
+    self.vm.inputs.didSelectShippingRule(at: 0)
+
+    self.deselectCellAtIndex.assertValues([2])
+    self.selectCellAtIndex.assertValues([2, 0])
+
+    self.vm.inputs.didSelectShippingRule(at: 1)
+
+    self.deselectCellAtIndex.assertValues([2, 0])
+    self.selectCellAtIndex.assertValues([2, 0, 1])
   }
 }
