30a320c2a4d042a3bc4e40a11406296ac174dbc4
Nino Collaco <scollaco@gmail.com>
2019-09-05 17:05:54 -0400

    Changed logic on DeprecatedRewardCellViewModel

--- Library/ViewModels/DeprecatedRewardCellViewModel.swift
+++ Library/ViewModels/DeprecatedRewardCellViewModel.swift
@@ -69,9 +69,9 @@ public final class DeprecatedRewardCellViewModel: DeprecatedRewardCellViewModelT
         ) ?? (.us, project.stats.staticUsdRate)
         switch rewardOrBacking {
         case let .left(reward):
-          let min = minPledgeAmount(forProject: project, reward: reward)
+          let amount = reward.convertedMinimum ?? max(1, reward.minimum * Double(rate))
           return Format.currency(
-            max(1, Int(Float(min) * rate)),
+            amount,
             country: country,
             omitCurrencyCode: project.stats.omitUSCurrencyCode
           )
