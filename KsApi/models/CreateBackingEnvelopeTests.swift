8ab704ff379437d88534c44ed8e16b3f322c6ba7
christella <cdolm92@gmail.com>
2019-09-05 17:26:22 -0400

    removed extra envelope test

--- KsApi/models/CreateBackingEnvelopeTests.swift
+++ KsApi/models/CreateBackingEnvelopeTests.swift
@@ -2,27 +2,6 @@
 import XCTest
 
 final class CreateBackingEnvelopeTests: XCTestCase {
-  private var json: String {
-    return """
-    createBacking {
-      createBacking(input: {
-        projectId: 1622745379,
-        amount: "20",
-        locationId: "4oCcTG9jYXRpb24tMjQ2OTk0OeKAnQ==",
-        rewardId: nil,
-        paymentSourceId: "paymentid",
-        paymentType: "card"
-      })
-    }
-    """
-  }
-
-  func testDecode() {
-    if let decodeData = envelopeFromJSON() {
-      let checkout = decodeData.createBacking.checkout
-      XCTAssertEqual(checkout.state, .verifying)
-    }
-  }
 
   func testCreateBackingEnvelopeDecoding() {
     let jsonString =
@@ -44,17 +23,5 @@ final class CreateBackingEnvelopeTests: XCTestCase {
     } catch {
       XCTFail("CreateBackingEnvelope should be decoded!")
     }
-  }
-
-  private func envelopeFromJSON() -> CreateBackingEnvelope? {
-    if let jsonData = json.data(using: .utf8) {
-      do {
-        let decodedData = try JSONDecoder().decode(CreateBackingEnvelope.self, from: jsonData)
-        return decodedData
-      } catch {
-        return nil
-      }
-    }
-    return nil
   }
 }
