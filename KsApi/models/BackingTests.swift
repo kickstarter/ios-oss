491c504d20d0dab6952f1b6e2ddce537efc37760
Nino Collaco <scollaco@gmail.com>
2019-09-17 10:54:00 -0400

    Added locationName to Backing model

--- KsApi/models/BackingTests.swift
+++ KsApi/models/BackingTests.swift
@@ -8,6 +8,7 @@ final class BackingTests: XCTestCase {
       "backer_id": 1,
       "id": 1,
       "location_id": 1,
+      "location_name": "United States",
       "pledged_at": 1_000,
       "project_country": "US",
       "project_id": 1,
@@ -16,7 +17,15 @@ final class BackingTests: XCTestCase {
     ])
 
     XCTAssertNil(backing.error)
+    XCTAssertEqual(1.0, backing.value?.amount)
+    XCTAssertEqual(1, backing.value?.backerId)
     XCTAssertEqual(1, backing.value?.id)
+    XCTAssertEqual(1, backing.value?.locationId)
+    XCTAssertEqual("United States", backing.value?.locationName)
+    XCTAssertEqual(1_000, backing.value?.pledgedAt)
+    XCTAssertEqual("US", backing.value?.projectCountry)
+    XCTAssertEqual(1, backing.value?.projectId)
+    XCTAssertEqual(1, backing.value?.sequence)
     XCTAssertEqual(Backing.Status.pledged, backing.value?.status)
   }
 }
