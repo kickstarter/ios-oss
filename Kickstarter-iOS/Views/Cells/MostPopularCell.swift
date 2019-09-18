8a162cd71fe8966af267c0c929dbc2a695ff3ea1
Justin Swart <justinswart@users.noreply.github.com>
2019-09-18 16:45:13 -0700

    [NT-308] Update strings and nokogiri gem (#841)
    * Make strings and update gem
    
    * Revert nokogiri
    
    * Update swiftformat, nokogiri, swift version that swiftformat uses
    
    * Add single file exclusions to Dangerfile
    
    * Use correct Strings functions
    
    * Fix encoding type
    
    * Increased height of snapshots
    
    * SwiftFormat
    
    * Remove tolerance

--- Kickstarter-iOS/Views/Cells/MostPopularCell.swift
+++ Kickstarter-iOS/Views/Cells/MostPopularCell.swift
@@ -24,7 +24,7 @@ internal final class MostPopularCell: UITableViewCell, ValueCell {
       |> UILabel.lens.font .~ .ksr_title1(size: 22)
       |> UILabel.lens.textColor .~ .ksr_soft_black
       |> UILabel.lens.text %~ { _ in
-        localizedString(key: "Popular_Projects", defaultValue: "Popular Projects")
+        Strings.Popular_Projects()
       }
   }
 }
