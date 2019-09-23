4786c873f77254603cb1274d4db0ec5874994567
Nino Collaco <scollaco@gmail.com>
2019-09-23 15:40:48 -0400

    Fixed / incremented tests

--- KsApi/models/BackingTests.swift
+++ KsApi/models/BackingTests.swift
@@ -8,6 +8,14 @@ final class BackingTests: XCTestCase {
       "backer_id": 1,
       "id": 1,
       "location_id": 1,
+      "payment_source" : [
+        "expiration_date": "2019-09-23",
+        "id": 20,
+        "last_four": "1234",
+        "payment_type": "CREDIT_CARD",
+        "state": "ACTIVE",
+        "type": "VISA",
+      ],
       "pledged_at": 1_000,
       "project_country": "US",
       "project_id": 1,
@@ -17,6 +25,14 @@ final class BackingTests: XCTestCase {
 
     XCTAssertNil(backing.error)
     XCTAssertEqual(1, backing.value?.id)
+    XCTAssertEqual(1, backing.value?.id)
+    XCTAssertEqual("2019-09-23", backing.value?.paymentSource?.expirationDate)
+    // id is converted to a base64 encoded string to keep graphQL compatibility (used in other API calls).
+    XCTAssertEqual("VXNlci0yMA==", backing.value?.paymentSource?.id)
+    XCTAssertEqual("1234", backing.value?.paymentSource?.lastFour)
+    XCTAssertEqual("CREDIT_CARD", backing.value?.paymentSource?.paymentType)
+    XCTAssertEqual("ACTIVE", backing.value?.paymentSource?.state)
+    XCTAssertEqual(GraphUserCreditCard.CreditCardType.visa, backing.value?.paymentSource?.type)
     XCTAssertEqual(Backing.Status.pledged, backing.value?.status)
   }
 }
