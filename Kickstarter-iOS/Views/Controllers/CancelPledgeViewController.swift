3db9574b791b858c3967128f5d3ba758c70b40e6
Isabel Barrera <ifbarrera@me.com>
2019-09-30 15:55:33 -0400

    Cancel pledge subviews and view configuration

--- Kickstarter-iOS/Views/Controllers/CancelPledgeViewController.swift
+++ Kickstarter-iOS/Views/Controllers/CancelPledgeViewController.swift
@@ -5,6 +5,22 @@ import Prelude
 import UIKit
 
 final class CancelPledgeViewController: UIViewController {
+  // MARK: - Properties
+  private lazy var cancelButton = { UIButton(type: .custom)
+    |> \.translatesAutoresizingMaskIntoConstraints .~ false
+  }()
+  private lazy var cancellationDetailsTextLabel = { UILabel(frame: .zero) }()
+  private lazy var cancellationReasonTextField = { UITextField(frame: .zero) }()
+  private lazy var cancellationReasonDisclaimerLabel = { UILabel(frame: .zero) }()
+  private lazy var goBackButton = { UIButton(type: .custom)
+    |> \.translatesAutoresizingMaskIntoConstraints .~ false
+  }()
+  private lazy var rootStackView = {
+    UIStackView(frame: .zero)
+      |> \.translatesAutoresizingMaskIntoConstraints .~ false
+  }()
+  private lazy var scrollView = { UIScrollView(frame: .zero) }()
+
   private let viewModel: CancelPledgeViewModelType = CancelPledgeViewModel()
 
   // MARK: - Lifecycle
@@ -13,22 +29,116 @@ final class CancelPledgeViewController: UIViewController {
     super.viewDidLoad()
 
     _ = self
-      |> \.title %~ { _ in Strings.Cancel_pledge()
-      }
+      |> \.title %~ { _ in Strings.Cancel_pledge() }
+
+    _ = (self.scrollView, self.view)
+      |> ksr_addSubviewToParent()
+      |> ksr_constrainViewToEdgesInParent()
+
+    _ = (self.rootStackView, self.scrollView)
+      |> ksr_addSubviewToParent()
+
+    _ = ([self.cancellationDetailsTextLabel,
+          self.cancellationReasonTextField,
+          self.cancellationReasonDisclaimerLabel,
+          self.cancelButton,
+          self.goBackButton], self.rootStackView)
+      |> ksr_addArrangedSubviewsToStackView()
+
+    self.rootStackView.setCustomSpacing(Styles.grid(10), after: self.cancellationReasonDisclaimerLabel) // TODO: move this to a custom ksr function
+    self.rootStackView.setCustomSpacing(Styles.grid(6), after: self.cancellationDetailsTextLabel)
+    self.rootStackView.setCustomSpacing(Styles.grid(1), after: self.cancellationReasonTextField)
+
+    self.setupConstraints()
+
+    self.viewModel.inputs.viewDidLoad()
   }
 
   // MARK: - Configuration
 
-  internal func configure(with _: Project, backing _: Backing) {}
+  internal func configure(with project: Project, backing: Backing) {
+    self.viewModel.inputs.configure(with: project, backing: backing)
+  }
 
   override func bindStyles() {
     super.bindStyles()
 
     _ = self.view
       |> \.backgroundColor .~ UIColor.ksr_grey_400
+
+    _ = self.scrollView
+      |> \.alwaysBounceVertical .~ true
+
+    _ = self.rootStackView
+      |> checkoutRootStackViewStyle
+      |> \.spacing .~ Styles.grid(2)
+
+    _ = self.cancellationDetailsTextLabel
+      |> cancellationDetailsTextLabelStyle
+
+    _ = self.cancellationReasonTextField
+      |> cancellationReasonTextFieldStyle
+      |> UITextField.lens.placeholder %~ { _ in Strings.Tell_us_why_optional() }
+
+    _ = self.cancellationReasonDisclaimerLabel
+      |> cancellationDisclaimerLabelStyle
+      |> \.text %~ { _ in Strings.We_wont_share_this_with_the_creator() }
+
+    _ = self.cancelButton
+      |> apricotButtonStyle
+      |> UIButton.lens.title(for: .normal) %~ { _ in Strings.Yes_cancel_it() }
+
+    _ = self.goBackButton
+      |> greyButtonStyle
+      |> UIButton.lens.title(for: .normal) %~ { _ in Strings.No_go_back() }
   }
 
   override func bindViewModel() {
     super.bindViewModel()
+
+    self.viewModel.outputs.cancellationDetailsTextLabelValue
+      .observeForUI()
+      .observeValues { [weak self] amount, projectName in
+        _ = self?.cancellationDetailsTextLabel
+          ?|> \.text %~ { _ in Strings.Are_you_sure_you_wish_to_cancel_your_amount_pledge_to_project_name(amount: amount, project_name: projectName) }
+    }
   }
+
+  private func setupConstraints() {
+    NSLayoutConstraint.activate([
+      self.rootStackView.leftAnchor.constraint(equalTo: self.scrollView.leftAnchor),
+      self.rootStackView.rightAnchor.constraint(equalTo: self.scrollView.rightAnchor),
+      self.rootStackView.centerYAnchor.constraint(equalTo: self.scrollView.centerYAnchor),
+      self.rootStackView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+      self.cancelButton.heightAnchor.constraint(equalToConstant: Styles.minTouchSize.height),
+      self.goBackButton.heightAnchor.constraint(equalToConstant: Styles.minTouchSize.height),
+      self.cancellationReasonTextField.heightAnchor.constraint(equalTo: self.cancelButton.heightAnchor)
+      ])
+  }
+}
+
+// MARK: - Styles
+
+private let cancellationDisclaimerLabelStyle: LabelStyle = { label in
+  label
+    |> \.font .~ .ksr_caption1()
+    |> \.textColor .~ UIColor.ksr_text_navy_600
+    |> \.lineBreakMode .~ NSLineBreakMode.byWordWrapping
+    |> \.textAlignment .~ NSTextAlignment.center
+    |> \.numberOfLines .~ 0
+}
+
+private let cancellationReasonTextFieldStyle: TextFieldStyle = { textField in
+  textField
+    |> formFieldStyle
+    |> \.backgroundColor .~ UIColor.white
+    |> \.borderStyle .~ UITextField.BorderStyle.roundedRect
+}
+
+private let cancellationDetailsTextLabelStyle: LabelStyle = { label in
+  label
+    |> \.font .~ UIFont.ksr_callout()
+    |> \.lineBreakMode .~ NSLineBreakMode.byWordWrapping
+    |> \.textAlignment .~ NSTextAlignment.center
+    |> \.numberOfLines .~ 0
 }
--- Library/ViewModels/CancelPledgeViewModel.swift
+++ Library/ViewModels/CancelPledgeViewModel.swift
@@ -1,8 +1,11 @@
 import Foundation
 import KsApi
 import ReactiveSwift
+import Prelude
 
-public protocol CancelPledgeViewModelOutputs {}
+public protocol CancelPledgeViewModelOutputs {
+  var cancellationDetailsTextLabelValue: Signal<(amount: String, projectName: String), Never> { get }
+}
 
 public protocol CancelPledgeViewModelInputs {
   func configure(with project: Project, backing: Backing)
@@ -16,7 +19,19 @@ public protocol CancelPledgeViewModelType {
 
 public final class CancelPledgeViewModel: CancelPledgeViewModelType, CancelPledgeViewModelInputs,
   CancelPledgeViewModelOutputs {
-  public init() {}
+  public init() {
+    let initialData = Signal.combineLatest(self.configureWithProjectAndBackingProperty.signal.skipNil(),
+                                           self.viewDidLoadProperty.signal)
+      .map(first)
+
+    self.cancellationDetailsTextLabelValue = initialData
+      .map { project, backing in
+        let formattedAmount = Format.currency(backing.amount,
+                                     country: project.country,
+                                     omitCurrencyCode: project.stats.omitUSCurrencyCode)
+        return (formattedAmount, project.name)
+    }
+  }
 
   private let configureWithProjectAndBackingProperty = MutableProperty<(Project, Backing)?>(nil)
   public func configure(with project: Project, backing: Backing) {
@@ -27,6 +42,8 @@ public final class CancelPledgeViewModel: CancelPledgeViewModelType, CancelPledg
   public func viewDidLoad() {
     self.viewDidLoadProperty.value = ()
   }
+
+  public let cancellationDetailsTextLabelValue: Signal<(amount: String, projectName: String), Never>
 
   public var inputs: CancelPledgeViewModelInputs { return self }
   public var outputs: CancelPledgeViewModelOutputs { return self }
