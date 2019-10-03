51966e893dc61cf5eee1234ff2a6e6022ab97984
Pavel Dusatko <pavel.dusatko@gmail.com>
2019-10-03 10:53:51 -0700

    [NT-206] Reward received toggle business logic (#866)
    * Move view model property to the top of the properties list
    
    * Prepare reward received vm
    
    * Hook up reward received view model
    
    * Update snapshot tests
    
    * Hook up toggle to actual api call
    
    * Hide reward received toggle section appropriately
    
    * Format code
    
    * Use reactive extension instead of observing value change

--- Kickstarter-iOS/Views/Controllers/ManageViewPledgeRewardReceivedViewController.swift
+++ Kickstarter-iOS/Views/Controllers/ManageViewPledgeRewardReceivedViewController.swift
@@ -1,8 +1,40 @@
+import KsApi
 import Library
 import Prelude
 import UIKit
 
 final class ManageViewPledgeRewardReceivedViewController: ToggleViewController {
+  // MARK: - Properties
+
+  private let viewModel: ManageViewPledgeRewardReceivedViewModelType
+    = ManageViewPledgeRewardReceivedViewModel()
+
+  // MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.toggle.addTarget(
+      self,
+      action: #selector(ManageViewPledgeRewardReceivedViewController.toggleValueDidChange(_:)),
+      for: .valueChanged
+    )
+
+    self.viewModel.inputs.viewDidLoad()
+  }
+
+  // MARK: - Actions
+
+  @objc private func toggleValueDidChange(_ toggle: UISwitch) {
+    self.viewModel.inputs.rewardReceivedToggleTapped(isOn: toggle.isOn)
+  }
+
+  // MARK: - Configuration
+
+  public func configureWith(project: Project) {
+    self.viewModel.inputs.configureWith(project)
+  }
+
   // MARK: - Styles
 
   override func bindStyles() {
@@ -15,5 +47,13 @@ final class ManageViewPledgeRewardReceivedViewController: ToggleViewController {
     _ = self.toggle
       |> checkoutSwitchControlStyle
       |> \.accessibilityLabel %~ { _ in Strings.Reward_received() }
+  }
+
+  // MARK: - View model
+
+  override func bindViewModel() {
+    super.bindViewModel()
+
+    self.toggle.rac.on = self.viewModel.outputs.rewardReceived
   }
 }
