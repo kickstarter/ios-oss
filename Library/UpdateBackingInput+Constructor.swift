f2b515344ca012c77a9dfe34dde2992897bdc826
Justin Swart <justinswart@users.noreply.github.com>
2019-10-08 14:14:14 -0700

    [NT-331] Perform Update Backing Mutation Request (#874)
    * Add mutation
    
    * Fix test
    
    * Add to Service
    
    * Fix compiler error
    
    * Fix find-replace error
    
    * Enable/disable confirm button when values change
    
    * Use pledgeAmount
    
    * Update title
    
    * Update backing request with error/success messaging
    
    * Fix test
    
    * Add tests
    
    * Fix tests
    
    * Use String for Location ID
    
    * Make tests clearer, split backing out into its own signal
    
    * Move message to PledgeViewModel
    
    * Use observeForControllerAction, make function private
    
    * Remove GraphIDBridging from Location model, syntax improvements on PledgeViewModel

---
+++ Library/UpdateBackingInput+Constructor.swift
@@ -0,0 +1,38 @@
+import Foundation
+import KsApi
+
+extension UpdateBackingInput {
+  internal static func input(
+    from updateBackingData: UpdateBackingData
+  ) -> UpdateBackingInput {
+    let pledgeAmount: String? = updateBackingData.pledgeAmount.flatMap { pledgeAmount in
+      pledgeAmountString(withAmount: pledgeAmount, shippingRule: updateBackingData.shippingRule)
+    }
+    let backingId: String = updateBackingData.backing.graphID
+    let locationId: String? = updateBackingData.shippingRule.flatMap { "\($0.location.id)" }
+    let rewardId: String? = updateBackingData.reward.graphID
+
+    return UpdateBackingInput(
+      amount: pledgeAmount,
+      applePay: nil,
+      id: backingId,
+      locationId: locationId,
+      paymentSourceId: nil,
+      rewardId: rewardId
+    )
+  }
+}
+
+private func pledgeAmountString(withAmount amount: Double, shippingRule: ShippingRule?) -> String {
+  let pledgeAmountDecimal = Decimal(amount)
+  var shippingAmountDecimal: Decimal = Decimal()
+
+  if let shippingRule = shippingRule, shippingRule.cost > 0 {
+    shippingAmountDecimal = Decimal(shippingRule.cost)
+  }
+
+  let pledgeTotal = NSDecimalNumber(decimal: pledgeAmountDecimal + shippingAmountDecimal)
+  let formattedPledgeTotal = Format.decimalCurrency(for: pledgeTotal.doubleValue)
+
+  return formattedPledgeTotal
+}
