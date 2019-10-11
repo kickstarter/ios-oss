1e0b44c055aa2395b277da533bcdb7203394e805
Isabel Barrera <ifbarrera@me.com>
2019-10-11 12:55:05 -0400

    Adding accessibility label for cancellation reason text field

--- Kickstarter-iOS/Views/Controllers/CancelPledgeViewController.swift
+++ Kickstarter-iOS/Views/Controllers/CancelPledgeViewController.swift
@@ -125,6 +125,7 @@ final class CancelPledgeViewController: UIViewController, MessageBannerViewContr
 
     _ = self.cancellationReasonTextField
       |> cancellationReasonTextFieldStyle
+      |> UITextField.lens.accessibilityLabel %~ { _ in Strings.Cancellation_reason() }
       |> UITextField.lens.placeholder %~ { _ in Strings.Tell_us_why_optional() }
 
     _ = self.cancellationReasonDisclaimerLabel
