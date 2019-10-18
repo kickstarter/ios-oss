44623743366d3bbb0484df377e60b17d9f5331f0
Nino Collaco <scollaco@gmail.com>
2019-10-18 11:24:05 -0400

    Added missing assertion

--- Library/ViewModels/ManagePledgePaymentMethodViewModelTests.swift
+++ Library/ViewModels/ManagePledgePaymentMethodViewModelTests.swift
@@ -35,6 +35,7 @@ final class ManagePledgePaymentMethodViewModelTests: TestCase {
     self.vm.inputs.configureWith(value: Backing.PaymentSource.applePay)
 
     self.cardImageName.assertValue("icon--apple-pay")
+    self.cardNumberAccessibilityLabel.assertLastValue("Visa, Card ending in 1111")
     self.cardNumberTextShortStyle.assertLastValue("Ending in 1111")
     self.expirationDateText.assertValue("Expires 10/2019")
   }
