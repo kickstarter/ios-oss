2b081835550d58036830e8a7b66108ccbcd3963f
christella <cdolm92@gmail.com>
2019-10-22 18:09:32 -0400

    vm tests

--- Library/ViewModels/PledgeViewModelTests.swift
+++ Library/ViewModels/PledgeViewModelTests.swift
@@ -257,6 +257,8 @@ final class PledgeViewModelTests: TestCase {
 
       let project = Project.template
         |> Project.lens.dates.deadline .~ date!.timeIntervalSince1970
+        |> Project.lens.stats.currentCurrency .~ Currency.USD.rawValue
+        |> Project.lens.stats.currency .~ Currency.USD.rawValue
       let reward = Reward.template
         |> Reward.lens.shipping.enabled .~ true
 
@@ -285,6 +287,55 @@ final class PledgeViewModelTests: TestCase {
         "If the project reaches its funding goal, you will be charged on November 1, 2019.",
         "If the project reaches its funding goal, you will be charged on November 1, 2019."
       ])
+    }
+  }
+
+  func testUpdateContext_ConfirmationLabelShowsTotalAmount() {
+    let dateComponents = DateComponents()
+      |> \.month .~ 11
+      |> \.day .~ 1
+      |> \.year .~ 2_019
+      |> \.timeZone .~ TimeZone.init(secondsFromGMT: 0)
+
+    let calendar = Calendar(identifier: .gregorian)
+      |> \.timeZone .~ TimeZone.init(secondsFromGMT: 0)!
+
+    withEnvironment(calendar: calendar, locale: Locale(identifier: "en")) {
+      let date = AppEnvironment.current.calendar.date(from: dateComponents)
+
+      let project = Project.template
+        |> Project.lens.dates.deadline .~ date!.timeIntervalSince1970
+        |> Project.lens.stats.currentCurrency .~ Currency.USD.rawValue
+        |> Project.lens.stats.currency .~ Currency.HKD.rawValue
+        |> Project.lens.country .~ .hk
+      let reward = Reward.template
+        |> Reward.lens.shipping.enabled .~ true
+
+      self.vm.inputs.configureWith(project: project, reward: reward, refTag: .projectPage, context: .update)
+      self.vm.inputs.viewDidLoad()
+
+      self.confirmationLabelHidden.assertValues([false])
+      self.confirmationLabelAttributedText.assertDidNotEmitValue()
+
+      scheduler.advance(by: .milliseconds(10))
+
+      self.confirmationLabelAttributedText.assertValueCount(1)
+      self.confirmationLabelText.assertValues([
+        "If the project reaches its funding goal, you will be charged HK$ 10 on November 1, 2019."
+        ])
+
+      self.vm.inputs.traitCollectionDidChange()
+      self.vm.inputs.traitCollectionDidChange()
+
+      self.confirmationLabelAttributedText.assertValueCount(1, "Debounces signals")
+
+      scheduler.advance(by: .milliseconds(10))
+
+      self.confirmationLabelAttributedText.assertValueCount(2)
+      self.confirmationLabelText.assertValues([
+        "If the project reaches its funding goal, you will be charged HK$ 10 on November 1, 2019.",
+        "If the project reaches its funding goal, you will be charged HK$ 10 on November 1, 2019."
+        ])
     }
   }
 
