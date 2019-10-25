d5962ef97170fbe2e117b74ef6b49e82a7b279cc
Isabel Barrera <ifbarrera@me.com>
2019-10-25 12:32:48 -0400

    Screenshot tests

--- Kickstarter-iOS/Views/Controllers/PledgeViewControllerTests.swift
+++ Kickstarter-iOS/Views/Controllers/PledgeViewControllerTests.swift
@@ -10,6 +10,8 @@ final class PledgeViewControllerTests: TestCase {
 
     AppEnvironment.pushEnvironment(mainBundle: Bundle.framework)
     UIView.setAnimationsEnabled(false)
+
+    self.recordMode = true
   }
 
   override func tearDown() {
@@ -19,7 +21,7 @@ final class PledgeViewControllerTests: TestCase {
     super.tearDown()
   }
 
-  func testView() {
+  func testView_NeedsConversion_IsFalse() {
     combos(Language.allLanguages, [Device.phone4_7inch, Device.pad]).forEach { language, device in
       withEnvironment(language: language) {
         let controller = PledgeViewController.instantiate()
@@ -33,7 +35,25 @@ final class PledgeViewControllerTests: TestCase {
     }
   }
 
-  func testView_UpdateContext() {
+  func testView_NeedsConversion_IsTrue() {
+    let project = Project.template
+      |> Project.lens.stats.currentCurrency .~ Project.Country.gb.currencyCode
+      |> Project.lens.stats.currentCurrencyRate .~ 2.0
+
+    combos(Language.allLanguages, [Device.phone4_7inch, Device.pad]).forEach { language, device in
+      withEnvironment(language: language) {
+        let controller = PledgeViewController.instantiate()
+        controller.configureWith(project: project, reward: .template, refTag: nil, context: .pledge)
+        let (parent, _) = traitControllers(device: device, orientation: .portrait, child: controller)
+
+        self.scheduler.run()
+
+        FBSnapshotVerifyView(parent.view, identifier: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+
+  func testView_UpdateContext_NeedsConversion_IsFalse() {
     let reward = Reward.template
       |> (Reward.lens.shipping .. Reward.Shipping.lens.enabled) .~ true
 
@@ -51,7 +71,28 @@ final class PledgeViewControllerTests: TestCase {
     }
   }
 
-  func testView_ChangePaymentMethodContext() {
+  func testView_UpdateContext_NeedsConversion_IsTrue() {
+    let project = Project.template
+      |> Project.lens.stats.currentCurrency .~ Project.Country.gb.currencyCode
+      |> Project.lens.stats.currentCurrencyRate .~ 2.0
+    let reward = Reward.template
+      |> (Reward.lens.shipping .. Reward.Shipping.lens.enabled) .~ true
+
+    combos(Language.allLanguages, [Device.phone4_7inch, Device.pad]).forEach { language, device in
+      withEnvironment(language: language) {
+        let controller = PledgeViewController.instantiate()
+        controller.configureWith(project: project, reward: reward, refTag: nil, context: .update)
+        let (parent, _) = traitControllers(device: device, orientation: .portrait, child: controller)
+
+        self.scheduler.advance(by: .milliseconds(10))
+        self.scheduler.run()
+
+        FBSnapshotVerifyView(parent.view, identifier: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+
+  func testView_ChangePaymentMethodContext_NeedsConversion_IsTrue() {
     let reward = Reward.postcards
       |> Reward.lens.shipping.enabled .~ true
 
@@ -67,6 +108,41 @@ final class PledgeViewControllerTests: TestCase {
           |> Backing.lens.shippingAmount .~ 5
           |> Backing.lens.amount .~ 700
       )
+
+    let response = UserEnvelope<GraphUserCreditCard>(me: GraphUserCreditCard.template)
+    let apiService = MockService(fetchGraphCreditCardsResponse: response)
+
+    combos(Language.allLanguages, [Device.phone4_7inch, Device.pad]).forEach { language, device in
+      withEnvironment(apiService: apiService, currentUser: .template, language: language) {
+        let controller = PledgeViewController.instantiate()
+        controller.configureWith(
+          project: project, reward: reward, refTag: nil, context: .changePaymentMethod
+        )
+        let (parent, _) = traitControllers(device: device, orientation: .portrait, child: controller)
+
+        self.scheduler.run()
+
+        FBSnapshotVerifyView(parent.view, identifier: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+
+  func testView_ChangePaymentMethodContext_NeedsConversion_IsFalse() {
+    let reward = Reward.postcards
+      |> Reward.lens.shipping.enabled .~ true
+
+    let project = Project.template
+      |> Project.lens.state .~ .live
+      |> Project.lens.personalization.isBacking .~ true
+      |> Project.lens.personalization.backing .~ (
+        .template
+          |> Backing.lens.paymentSource .~ Backing.PaymentSource.template
+          |> Backing.lens.status .~ .pledged
+          |> Backing.lens.reward .~ reward
+          |> Backing.lens.rewardId .~ reward.id
+          |> Backing.lens.shippingAmount .~ 5
+          |> Backing.lens.amount .~ 700
+    )
 
     let response = UserEnvelope<GraphUserCreditCard>(me: GraphUserCreditCard.template)
     let apiService = MockService(fetchGraphCreditCardsResponse: response)
