a502a2cbe6cd2167f9ca76509fe329ad4cc3b8f8
christella <cdolm92@gmail.com>
2019-10-31 19:11:29 -0400

    test not needed

--- Library/ViewModels/PledgeViewModelTests.swift
+++ Library/ViewModels/PledgeViewModelTests.swift
@@ -3463,18 +3463,4 @@ final class PledgeViewModelTests: TestCase {
 
     XCTAssertEqual(["Update Pledge Button Clicked"], self.trackingClient.events)
   }
-
-  func testTrackingEvents_DoesNotEmit_ContextIsChangePaymentMethod() {
-    self.vm.inputs.configureWith(
-      project: .template, reward: .template, refTag: nil,
-      context: .changePaymentMethod
-    )
-    self.vm.inputs.viewDidLoad()
-
-    XCTAssertEqual([], self.trackingClient.events)
-
-    self.vm.inputs.submitButtonTapped()
-
-    XCTAssertEqual([], self.trackingClient.events)
-  }
 }
