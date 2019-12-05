65603267c0da53087d2e4ae6910a4351daa4905e
Justin Swart <justinswart@users.noreply.github.com>
2019-12-05 15:44:59 -0500

    Rename to KoalaTrackingClient.swift to TrackingClient.swift

---
+++ Library/Tracking/TrackingClientConfigurationTests.swift
@@ -0,0 +1,33 @@
+//
+//  TrackingClientConfigurationTests.swift
+//  Library-iOSTests
+//
+//  Created by Justin Swart on 12/5/19.
+//  Copyright © 2019 Kickstarter. All rights reserved.
+//
+
+import XCTest
+
+class TrackingClientConfigurationTests: TestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
