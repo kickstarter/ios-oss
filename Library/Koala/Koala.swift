3cdef16d5040419d07ede27be049137061bf8e0e
Isabel Barrera <ifbarrera@me.com>
2019-12-09 14:28:42 -0500

    Formatting

--- Library/Koala/Koala.swift
+++ Library/Koala/Koala.swift
@@ -2156,8 +2156,10 @@ private func properties(userActivity: NSUserActivity) -> [String: Any] {
 
 // MARK: - Discovery Properties
 
-private func discoveryProperties(from params: DiscoveryParams,
-                                 prefix _: String = "discover_") -> [String: Any] {
+private func discoveryProperties(
+  from params: DiscoveryParams,
+  prefix _: String = "discover_"
+) -> [String: Any] {
   var result: [String: Any] = [:]
   var unprefixedResult: [String: Any] = [:]
 
