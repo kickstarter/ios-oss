325111221df1df9464e3a1ab09d881abe9ce9868
Isabel Barrera <isabel@kickstarter.com>
2019-12-10 09:59:41 -0500

    [NT-636] "Fix" button on Manage Pledge screen for errored backings (#982)
    * Fix button on errored pledges
    
    * Tests
    
    * Formatting
    
    * Screenshot tests
    
    * Rerecording tests
    
    * formatting

--- Library/ViewModels/ManagePledgePaymentMethodViewModelTests.swift
+++ Library/ViewModels/ManagePledgePaymentMethodViewModelTests.swift
@@ -12,6 +12,8 @@ final class ManagePledgePaymentMethodViewModelTests: TestCase {
   private let cardNumberAccessibilityLabel = TestObserver<String, Never>()
   private let cardNumberTextShortStyle = TestObserver<String, Never>()
   private let expirationDateText = TestObserver<String, Never>()
+  private let fixButtonHidden = TestObserver<Bool, Never>()
+  private let notifyDelegateFixButtonTapped = TestObserver<Void, Never>()
 
   internal override func setUp() {
     super.setUp()
@@ -20,10 +22,12 @@ final class ManagePledgePaymentMethodViewModelTests: TestCase {
     self.vm.outputs.cardNumberAccessibilityLabel.observe(self.cardNumberAccessibilityLabel.observer)
     self.vm.outputs.cardNumberTextShortStyle.observe(self.cardNumberTextShortStyle.observer)
     self.vm.outputs.expirationDateText.observe(self.expirationDateText.observer)
+    self.vm.outputs.fixButtonHidden.observe(self.fixButtonHidden.observer)
+    self.vm.outputs.notifyDelegateFixButtonTapped.observe(self.notifyDelegateFixButtonTapped.observer)
   }
 
   func testPaymentSourceInfo() {
-    self.vm.inputs.configureWith(value: Backing.PaymentSource.template)
+    self.vm.inputs.configureWith(value: Backing.template)
 
     self.cardImageName.assertLastValue("icon--visa")
     self.cardNumberAccessibilityLabel.assertLastValue("Visa, Card ending in 1111")
@@ -32,7 +36,10 @@ final class ManagePledgePaymentMethodViewModelTests: TestCase {
   }
 
   func testApplePay() {
-    self.vm.inputs.configureWith(value: Backing.PaymentSource.applePay)
+    let backing = Backing.template
+      |> Backing.lens.paymentSource .~ Backing.PaymentSource.applePay
+
+    self.vm.inputs.configureWith(value: backing)
 
     self.cardImageName.assertValue("icon--apple-pay")
     self.cardNumberAccessibilityLabel.assertLastValue("Apple Pay, Visa, Card ending in 1111")
@@ -41,11 +48,42 @@ final class ManagePledgePaymentMethodViewModelTests: TestCase {
   }
 
   func testGooglePay() {
-    self.vm.inputs.configureWith(value: Backing.PaymentSource.googlePay)
+    let backing = Backing.template
+      |> Backing.lens.paymentSource .~ Backing.PaymentSource.googlePay
+
+    self.vm.inputs.configureWith(value: backing)
 
     self.cardImageName.assertValue("icon--google-pay")
     self.cardNumberAccessibilityLabel.assertLastValue("Google Pay, Visa, Card ending in 4111")
     self.cardNumberTextShortStyle.assertLastValue("Ending in 4111")
     self.expirationDateText.assertValue("Expires 10/2019")
+  }
+
+  func testFixButton_IsNotHidden() {
+    let backing = Backing.template |> Backing.lens.status .~ .errored
+
+    self.vm.inputs.configureWith(value: backing)
+
+    self.fixButtonHidden.assertValues([false])
+  }
+
+  func testFixButton_IsHidden() {
+    self.vm.inputs.configureWith(value: Backing.template)
+
+    self.fixButtonHidden.assertValues([true])
+  }
+
+  func testFixButtonTapped() {
+    let backing = Backing.template |> Backing.lens.status .~ .errored
+
+    self.vm.inputs.configureWith(value: backing)
+
+    self.fixButtonHidden.assertValues([false])
+
+    self.notifyDelegateFixButtonTapped.assertDidNotEmitValue()
+
+    self.vm.inputs.fixButtonTapped()
+
+    self.notifyDelegateFixButtonTapped.assertDidEmitValue()
   }
 }
