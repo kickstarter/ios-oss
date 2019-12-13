fea0404c5e904995b6c47056df83cdc7e2bbf173
Isabel Barrera <isabel@kickstarter.com>
2019-12-13 11:53:30 -0500

    [NT-687] "Tab Bar Clicked" Event (#988)
    * Tab Bar Item Clicked Event
    
    * Making RootViewModel part of Library framework
    
    * Fixing tests
    
    * Project file changes
    
    * Tests
    
    * Moving RootViewModel and RootViewModelTests
    
    * Moving files
    
    * Removing manual equatable conformance

--- Kickstarter-iOS/Library/NSBundle-Framework.swift
+++ Kickstarter-iOS/Library/NSBundle-Framework.swift
@@ -4,6 +4,6 @@ extension Bundle {
   /// Returns an NSBundle pinned to the framework target. We could choose anything for the `forClass`
   /// parameter as long as it is in the framework target.
   public static var framework: Bundle {
-    return Bundle(for: RootViewModel.self)
+    return Bundle(for: AppDelegateViewModel.self)
   }
 }
