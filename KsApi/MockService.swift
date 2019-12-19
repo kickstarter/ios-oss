7a8e82423b6e4702ec427a1880a2e5ba5dfe36a5
Justin Swart <justinswart@users.noreply.github.com>
2019-12-19 13:41:59 -0800

    Xcode 11 dependency updates

--- KsApi/MockService.swift
+++ KsApi/MockService.swift
@@ -342,7 +342,7 @@
 
       self.backingUpdate = backingUpdate
 
-      self.fetchGraphCategoriesResponse = fetchGraphCategoriesResponse ?? (.template
+      self.fetchGraphCategoriesResponse = fetchGraphCategoriesResponse ?? (RootCategoriesEnvelope.template
         |> RootCategoriesEnvelope.lens.categories .~ [
           .art,
           .filmAndVideo,
@@ -1529,3 +1529,19 @@
     }
   }
 #endif
+
+private extension Result {
+  var value: Success? {
+    switch self {
+    case let .success(value): return value
+    case .failure: return nil
+    }
+  }
+
+  var error: Failure? {
+    switch self {
+    case .success: return nil
+    case let .failure(error): return error
+    }
+  }
+}
