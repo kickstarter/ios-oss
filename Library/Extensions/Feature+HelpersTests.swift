0a3a8fdf6c24547d8c551c9587c5a1abfbb53840
Justin Swart <justinswart@users.noreply.github.com>
2019-12-20 12:02:13 -0800

    Add language and logged_in properties, add feature flag. (#1001)

--- Library/Extensions/Feature+HelpersTests.swift
+++ Library/Extensions/Feature+HelpersTests.swift
@@ -82,4 +82,30 @@ final class FeatureHelpersTests: TestCase {
       XCTAssertFalse(featureNativeCheckoutPledgeViewIsEnabled())
     }
   }
+
+  // MARK: - Qualtrics
+
+  func testFeatureQualtrics_isTrue() {
+    let config = Config.template
+      |> \.features .~ [Feature.qualtrics.rawValue: true]
+
+    withEnvironment(config: config) {
+      XCTAssertTrue(featureQualtricsIsEnabled())
+    }
+  }
+
+  func testFeatureQualtrics_isFalse() {
+    let config = Config.template
+      |> \.features .~ [Feature.qualtrics.rawValue: false]
+
+    withEnvironment(config: config) {
+      XCTAssertFalse(featureQualtricsIsEnabled())
+    }
+  }
+
+  func testFeatureQualtrics_isFalse_whenNil() {
+    withEnvironment(config: .template) {
+      XCTAssertFalse(featureQualtricsIsEnabled())
+    }
+  }
 }
