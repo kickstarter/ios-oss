232ee34fb4896767b2465529a11e7b6f3de05958
Nino Collaco <scollaco@gmail.com>
2020-01-13 17:06:06 -0500

    SwiftFormat

--- Library/EquatableHelpersTests.swift
+++ Library/EquatableHelpersTests.swift
@@ -1,8 +1,7 @@
-import XCTest
 @testable import Library
+import XCTest
 
 final class EquatableHelpersTests: TestCase {
-
   func testIsAnyOf() {
     XCTAssertTrue("one".isAny(of: "one", "two", "three"))
     XCTAssertFalse("five".isAny(of: "one", "two", "three"))
