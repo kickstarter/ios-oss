f1af08cd4e1c8f08bd932096da3efe8cc8bcdf8f
Isabel Barrera <ifbarrera@me.com>
2020-01-21 14:16:43 -0500

    Add New Card Button Clicked

--- Library/Tracking/KoalaTests.swift
+++ Library/Tracking/KoalaTests.swift
@@ -766,9 +766,14 @@ final class KoalaTests: TestCase {
     let loggedInUser = User.template
     let koala = Koala(client: client, loggedInUser: loggedInUser)
 
-    koala.trackAddNewCardButtonClicked(project: .template)
+    koala.trackAddNewCardButtonClicked(project: .template, reward: .template)
+
+    let props = client.properties.last
 
     XCTAssertEqual(["Add New Card Button Clicked"], client.events)
+
+    assertProjectProperties(props)
+    assertPledgeProperties(props)
   }
 
   func testTrackManagePledgeOptionClicked_CancelPledgeSelected() {
