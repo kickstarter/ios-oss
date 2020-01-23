569c31e95732f55a46f70d1bfcd80296f42aead4
Justin Swart <justinswart@users.noreply.github.com>
2020-01-22 17:20:18 -0800

    Ensure that getVariationKey is called for admin users

--- Library/OptimizelyClientType.swift
+++ Library/OptimizelyClientType.swift
@@ -2,15 +2,28 @@ import Foundation
 
 public protocol OptimizelyClientType: AnyObject {
   func activate(experimentKey: String, userId: String, attributes: [String: Any?]?) throws -> String
+  func getVariationKey(experimentKey: String, userId: String, attributes: [String: Any?]?) throws -> String
 }
 
 extension OptimizelyClientType {
   public func variant(
     for experiment: OptimizelyExperiment.Key,
-    userId: String
+    userId: String,
+    isAdmin: Bool
   ) -> OptimizelyExperiment.Variant {
+    let variationString: String?
+    if isAdmin {
+      variationString = try? self.getVariationKey(
+        experimentKey: experiment.rawValue, userId: userId, attributes: nil
+      )
+    } else {
+      variationString = try? self.activate(
+        experimentKey: experiment.rawValue, userId: userId, attributes: nil
+      )
+    }
+
     guard
-      let variation = try? self.activate(experimentKey: experiment.rawValue, userId: userId, attributes: nil),
+      let variation = variationString,
       let variant = OptimizelyExperiment.Variant(rawValue: variation)
     else {
       return .control
