61426eec496198704e4c9e6590e1fd74a72bd3dd
Nino Collaco <scollaco@gmail.com>
2020-02-26 11:21:07 -0500

    SwiftFormat

--- Library/ViewModels/LandingPageViewModelTests.swift
+++ Library/ViewModels/LandingPageViewModelTests.swift
@@ -6,10 +6,8 @@ import UIKit
 import XCTest
 
 internal final class LandingPageViewModelTests: TestCase {
-
   private let dismissViewController = TestObserver<(), Never>()
   private let viewModel: LandingPageViewModelType = LandingPageViewModel()
-
 
   override func setUp() {
     super.setUp()
@@ -29,9 +27,8 @@ internal final class LandingPageViewModelTests: TestCase {
     let userDefaults = MockKeyValueStore()
 
     withEnvironment(currentUser: nil, userDefaults: userDefaults) {
-
       let hasSeenLandingPageKey = AppKeys.hasSeenLandingPage.rawValue
-      
+
       XCTAssertFalse(userDefaults.bool(forKey: hasSeenLandingPageKey))
 
       self.viewModel.inputs.ctaButtonTapped()
