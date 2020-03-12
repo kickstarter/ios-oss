893da528fb20eeea175197b5b04e2397352ad316
Isabel Barrera <ifbarrera@me.com>
2020-03-12 12:06:36 -0400

    Updating the dispatch interval for Optimizely

--- Kickstarter-iOS/AppDelegate.swift
+++ Kickstarter-iOS/AppDelegate.swift
@@ -155,8 +155,8 @@ internal final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     self.viewModel.outputs.configureOptimizely
       .observeForUI()
-      .observeValues { [weak self] key, logLevel in
-        self?.configureOptimizely(with: key, logLevel: logLevel)
+      .observeValues { [weak self] key, logLevel, dispatchInterval in
+        self?.configureOptimizely(with: key, logLevel: logLevel, dispatchInterval: dispatchInterval)
       }
 
     self.viewModel.outputs.configureAppCenterWithData
@@ -323,8 +323,17 @@ internal final class AppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Functions
 
-  private func configureOptimizely(with key: String, logLevel: OptimizelyLogLevelType) {
-    let optimizelyClient = OptimizelyClient(sdkKey: key, defaultLogLevel: logLevel.logLevel)
+  private func configureOptimizely(
+    with key: String,
+    logLevel: OptimizelyLogLevelType,
+    dispatchInterval: TimeInterval
+  ) {
+    let eventDispatcher = DefaultEventDispatcher(timerInterval: dispatchInterval)
+    let optimizelyClient = OptimizelyClient(
+      sdkKey: key,
+      eventDispatcher: eventDispatcher,
+      defaultLogLevel: logLevel.logLevel
+    )
 
     optimizelyClient.start { [weak self] result in
       let shouldUpdateClient = self?.viewModel.inputs.optimizelyConfigured(with: result)
