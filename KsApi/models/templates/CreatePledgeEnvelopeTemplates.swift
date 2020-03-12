edba8a338a5354639f127a69010b0a9fa0f468ad
Isabel Barrera <ifbarrera@me.com>
2020-03-12 10:06:15 -0400

    Delete unused CreatePledgeEnvelopeModel (#1099)

--- KsApi/models/templates/CreatePledgeEnvelopeTemplates.swift
+++
@@ -1,7 +0,0 @@
-extension CreatePledgeEnvelope {
-  internal static let template = CreatePledgeEnvelope(
-    checkoutUrl: "checkouts/1/payments",
-    newCheckoutUrl: "checkouts/1/payments/new",
-    status: 200
-  )
-}
