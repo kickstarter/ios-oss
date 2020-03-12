b012f3c9361a996acd4f02348d014f44b0d83201
christella <cdolm92@gmail.com>
2020-03-12 13:40:00 -0400

    uses isErrored

--- Library/ViewModels/PledgeCreditCardViewModel.swift
+++ Library/ViewModels/PledgeCreditCardViewModel.swift
@@ -106,7 +106,7 @@ public final class PledgeCreditCardViewModel: PledgeCreditCardViewModelInputs,
       .map { $0 && $1 }
       .skipRepeats()
 
-    let erroredPledge = self.pledgeCreditCardValueProperty.signal.skipNil().map { $0.erroredPledge }
+    let erroredPledge = self.pledgeCreditCardValueProperty.signal.skipNil().map { $0.isErrored }
 
     self.fixIconIsHidden = Signal.combineLatest(cardIsSelected, erroredPledge)
       .map { $0 && $1 }
