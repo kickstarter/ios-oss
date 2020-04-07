af45d824c79a42ea7752751d422a4fe4b95bf3c9
christella <cdolm92@gmail.com>
2020-04-07 11:15:56 -0400

    swift format

--- Library/ISO8601DateFormatter.swift
+++ Library/ISO8601DateFormatter.swift
@@ -3,8 +3,8 @@ import Foundation
 public extension ISO8601DateFormatter {
   private static var currentCachedFormatter: ISO8601DateFormatter?
   static func cachedFormatter() -> ISO8601DateFormatter {
-    let formatter = currentCachedFormatter ?? ISO8601DateFormatter()
-    currentCachedFormatter = formatter
+    let formatter = self.currentCachedFormatter ?? ISO8601DateFormatter()
+    self.currentCachedFormatter = formatter
     return formatter
   }
 }
