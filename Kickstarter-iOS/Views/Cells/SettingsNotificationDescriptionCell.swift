2e1f02d67a70a0e894f9f98b24b45395751e279a
christella <cdolm92@gmail.com>
2020-04-28 17:26:19 -0400

    delete file

--- Kickstarter-iOS/Views/Cells/SettingsNotificationDescriptionCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SettingsNotificationDescriptionCell.swift
-//  Kickstarter-iOS
-//
-//  Created by Christella Dolmo on 4/28/20.
-//  Copyright © 2020 Kickstarter. All rights reserved.
-//
-
-import Foundation
