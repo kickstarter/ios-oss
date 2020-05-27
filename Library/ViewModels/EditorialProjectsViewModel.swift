27848b3515179e649b4a465ac90dfaf0e25e8551
Justin Swart <justinswart@users.noreply.github.com>
2020-05-27 08:53:14 -0700

    [NT-1203] Lights On Editorial Collection (#1197)
    * [NT-1221] Lights On Editorial Card (#1185)
    
    * wip
    
    * new copy
    
    * new snapshots
    
    * swift format
    
    * remove old code
    
    * corrected test
    
    * translated copy
    
    * reused cell
    
    * swift format
    
    * pr feedback
    
    * smapshot tests
    
    * correct image
    
    * [NT-1236, NT-1237] Lights on editorial list view and implementation (#1194)
    
    * wip
    
    * new copy
    
    * new snapshots
    
    * swift format
    
    * remove old code
    
    * corrected test
    
    * new copy
    
    * translated copy
    
    * reused cell
    
    * swift format
    
    * wip
    
    * wip
    
    * pr feedback
    
    * smapshot tests
    
    * snapshots
    
    * delete file
    
    * removed copy
    
    * missing copy
    
    * snapshots
    
    * swift format
    
    * changed tag id
    
    * sorted by distance
    
    * test refactor
    
    * fix test
    
    * Tidy up
    
    * Fix icon
    
    * Snapshots
    
    Co-authored-by: Justin Swart <justinswart@users.noreply.github.com>
    
    * [NT-1224] Add Editorial Lights On Optimizely feature flag (#1196)
    
    * Add Optimizely lights on feature flag, remove Go Rewardless references
    
    * Snapshots
    
    * Formatting
    
    * Remove extra underscore
    
    * Remove table view header, fix typo, update snapshots
    
    * Oops
    
    * [NT-1254] Lights On Copy Updates (#1208)
    
    * Add Optimizely lights on feature flag, remove Go Rewardless references
    
    * Snapshots
    
    * Formatting
    
    * Remove extra underscore
    
    * Remove table view header, fix typo, update snapshots
    
    * Oops
    
    * Update copy
    
    * Fix tests
    
    * Fix layout
    
    Co-authored-by: Christella <cdolm92@gmail.com>

--- Library/ViewModels/EditorialProjectsViewModel.swift
+++ Library/ViewModels/EditorialProjectsViewModel.swift
@@ -107,16 +107,13 @@ public class EditorialProjectsViewModel: EditorialProjectsViewModelType,
 
 private func editorialImageName(for tagId: DiscoveryParams.TagID) -> String {
   switch tagId {
-  case .goRewardless:
-    if AppEnvironment.current.device.userInterfaceIdiom == .pad {
-      return "go-rewardless-modal-large"
-    }
-    return "go-rewardless-modal"
+  case .lightsOn:
+    return "lights-on"
   }
 }
 
 private func editorialTitleLabelText(for tagId: DiscoveryParams.TagID) -> String {
   switch tagId {
-  case .goRewardless: return Strings.This_holiday_season_support_a_project_for_no_reward()
+  case .lightsOn: return Strings.Show_up_for_the_spaces_you_love()
   }
 }
