ef206be2ed114d015450159aef61dc0ee575ba78
Justin Swart <justinswart@users.noreply.github.com>
2020-09-17 17:40:13 -0700

    Fix user settings regression

--- KsApi/queries/UserQueriesTests.swift
+++ KsApi/queries/UserQueriesTests.swift
@@ -7,11 +7,11 @@ final class UserQueriesTests: XCTestCase {
     let query = Query.user(accountQueryFields())
 
     XCTAssertEqual(
-      "me { chosenCurrency email hasPassword id imageUrl: imageUrl(blur: false, width: 1024) isAppleConnected isDeliverable isEmailVerified name }",
+      "me { chosenCurrency email hasPassword id imageUrl: imageUrl(blur: false, width: 1024) isAppleConnected isDeliverable isEmailVerified name uid }",
       query.description
     )
     XCTAssertEqual(
-      "{ me { chosenCurrency email hasPassword id imageUrl: imageUrl(blur: false, width: 1024) isAppleConnected isDeliverable isEmailVerified name } }",
+      "{ me { chosenCurrency email hasPassword id imageUrl: imageUrl(blur: false, width: 1024) isAppleConnected isDeliverable isEmailVerified name uid } }",
       Query.build(NonEmptySet(query))
     )
   }
@@ -20,11 +20,11 @@ final class UserQueriesTests: XCTestCase {
     let query = Query.user(changeEmailQueryFields())
 
     XCTAssertEqual(
-      "me { email id imageUrl: imageUrl(blur: false, width: 1024) isDeliverable isEmailVerified name }",
+      "me { email id imageUrl: imageUrl(blur: false, width: 1024) isDeliverable isEmailVerified name uid }",
       query.description
     )
     XCTAssertEqual(
-      "{ me { email id imageUrl: imageUrl(blur: false, width: 1024) isDeliverable isEmailVerified name } }",
+      "{ me { email id imageUrl: imageUrl(blur: false, width: 1024) isDeliverable isEmailVerified name uid } }",
       Query.build(NonEmptySet(query))
     )
   }
@@ -33,11 +33,11 @@ final class UserQueriesTests: XCTestCase {
     let query = Query.user(storedCardsQueryFields())
 
     XCTAssertEqual(
-      "me { id storedCards { nodes { expirationDate id lastFour type } totalCount } }",
+      "me { chosenCurrency id imageUrl: imageUrl(blur: false, width: 1024) name storedCards { nodes { expirationDate id lastFour type } totalCount } uid }",
       query.description
     )
     XCTAssertEqual(
-      "{ me { id storedCards { nodes { expirationDate id lastFour type } totalCount } } }",
+      "{ me { chosenCurrency id imageUrl: imageUrl(blur: false, width: 1024) name storedCards { nodes { expirationDate id lastFour type } totalCount } uid } }",
       Query.build(NonEmptySet(query))
     )
   }
@@ -45,11 +45,11 @@ final class UserQueriesTests: XCTestCase {
   func testBackingsQuery() {
     let query = Query.user(backingsQueryFields(status: BackingState.errored.rawValue))
     XCTAssertEqual(
-      "me { backings(status: errored) { nodes { errorReason id project { finalCollectionDate name pid slug } status } totalCount } id }",
+      "me { backings(status: errored) { nodes { errorReason id project { finalCollectionDate name pid slug } status } totalCount } id imageUrl: imageUrl(blur: false, width: 1024) name uid }",
       query.description
     )
     XCTAssertEqual(
-      "{ me { backings(status: errored) { nodes { errorReason id project { finalCollectionDate name pid slug } status } totalCount } id } }",
+      "{ me { backings(status: errored) { nodes { errorReason id project { finalCollectionDate name pid slug } status } totalCount } id imageUrl: imageUrl(blur: false, width: 1024) name uid } }",
       Query.build(NonEmptySet(query))
     )
   }
