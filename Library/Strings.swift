f84f4ce9be471df335a5e034cf299bf6aafd4709
Hari Singh <15615702+singhhari@users.noreply.github.com>
2020-09-22 11:47:34 -0400

    [NT-1524] Update backed reward copy. Add tests for RewardsCollectionViewControllerTests (#1299)
    * Update backed reward copy. Add tests for RewardsCollectionViewControllerTests.
    
    * Adding slather.yml file to ignore Strings.swift

--- Library/Strings.swift
+++ Library/Strings.swift
@@ -11261,6 +11261,23 @@ Veuillez réessayer ultérieurement."
     )
   }
   /**
+   "Your selection"
+
+   - **en**: "Your selection"
+   - **de**: "Your selection"
+   - **es**: "Your selection"
+   - **fr**: "Votre sélection"
+   - **ja**: "Your selection"
+  */
+  public static func Your_selection() -> String {
+    return localizedString(
+      key: "Your_selection",
+      defaultValue: "Your selection",
+      count: nil,
+      substitutions: [:]
+    )
+  }
+  /**
    "Your shipping destination"
 
    - **en**: "Your shipping destination"
