63b59404895a36371bc080e9874088050cc10ff2
Hari Singh <15615702+singhhari@users.noreply.github.com>
2020-09-25 11:54:25 -0400

    Hide bonus if backer pledges without reward (#1306)

--- KsApi/models/graphql/adapters/Backing+GraphBackingTests.swift
+++ KsApi/models/graphql/adapters/Backing+GraphBackingTests.swift
@@ -1,9 +1,21 @@
 @testable import KsApi
+import Prelude
 import XCTest
 
 final class Backing_GraphBackingTests: XCTestCase {
   func test() {
     // TODO: consider testing more variations
     XCTAssertNotNil(Backing.backing(from: .template))
+  }
+
+  func test_noReward() {
+    let graphBacking = GraphBacking.template
+      |> \.reward .~ nil
+
+    let backing = Backing.backing(from: graphBacking)
+
+    XCTAssertNotNil(backing)
+
+    XCTAssertEqual(backing?.reward?.isNoReward, true)
   }
 }
