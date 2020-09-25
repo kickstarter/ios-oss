3bbb921c5f673a955f389067063c5e6a586b461a
Justin Swart <justinswart@users.noreply.github.com>
2020-09-25 10:43:07 -0700

    Fix ISO date format (#1305)

--- KsApi/extensions/DateFormatter+ISOFormatter.swift
+++ KsApi/extensions/DateFormatter+ISOFormatter.swift
@@ -3,7 +3,9 @@ import Foundation
 extension DateFormatter {
   static var isoDateFormatter: DateFormatter {
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-DD"
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
     return dateFormatter
   }
 }
