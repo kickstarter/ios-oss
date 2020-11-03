61967ede24cc9f7676dfcd1f23780980435d6384
Hari Singh <15615702+singhhari@users.noreply.github.com>
2020-11-03 11:47:02 -0500

    Adding email verification feature flags

--- Library/Extensions/Feature+HelpersTests.swift
+++ Library/Extensions/Feature+HelpersTests.swift
@@ -30,4 +30,56 @@ final class FeatureHelpersTests: TestCase {
       XCTAssertFalse(featureQualtricsIsEnabled())
     }
   }
+
+  // MARK: - Email Verification Flow
+
+  func testFeatureEmailVerificationFlow_isTrue() {
+    let config = Config.template
+      |> \.features .~ [Feature.emailVerificationFlow.rawValue: true]
+
+    withEnvironment(config: config) {
+      XCTAssertTrue(featureEmailVerificationFlowIsEnabled())
+    }
+  }
+
+  func testFeatureEmailVerificationFlow_isFalse() {
+    let config = Config.template
+      |> \.features .~ [Feature.emailVerificationFlow.rawValue: false]
+
+    withEnvironment(config: config) {
+      XCTAssertFalse(featureEmailVerificationFlowIsEnabled())
+    }
+  }
+
+  func testFeatureEmailVerificationFlow_isFalse_whenNil() {
+    withEnvironment(config: .template) {
+      XCTAssertFalse(featureEmailVerificationFlowIsEnabled())
+    }
+  }
+
+  // MARK: - Email Verification Skip
+
+  func testFeatureEmailVerificationSkip_isTrue() {
+    let config = Config.template
+      |> \.features .~ [Feature.emailVerificationSkip.rawValue: true]
+
+    withEnvironment(config: config) {
+      XCTAssertTrue(featureEmailVerificationSkipIsEnabled())
+    }
+  }
+
+  func testFeatureEmailVerificationSkip_isFalse() {
+    let config = Config.template
+      |> \.features .~ [Feature.emailVerificationSkip.rawValue: false]
+
+    withEnvironment(config: config) {
+      XCTAssertFalse(featureEmailVerificationSkipIsEnabled())
+    }
+  }
+
+  func testFeatureEmailVerificationSkip_isFalse_whenNil() {
+    withEnvironment(config: .template) {
+      XCTAssertFalse(featureEmailVerificationSkipIsEnabled())
+    }
+  }
 }
