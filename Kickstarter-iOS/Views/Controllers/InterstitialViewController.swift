0b004db8600777b09b403031f805a160d6595c52
Andrew Batutin <andrey@kickstarter.com>
2020-11-05 18:01:14 +0200

    added InterstitialViewController

---
+++ Kickstarter-iOS/Views/Controllers/InterstitialViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InterstitialViewController.swift
+//  Kickstarter-iOS
+//
+//  Created by admin on 05.11.2020.
+//  Copyright © 2020 Kickstarter. All rights reserved.
+//
+
+import Foundation
