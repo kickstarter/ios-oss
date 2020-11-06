f7db44571fb7c25a5f11c6b5d19bbbfdbf0be7ce
Andrew Batutin <andrey@kickstarter.com>
2020-11-06 14:12:09 +0200

    Fixed ShippingRulesEnvelope key mapping

--- KsApi/models/ShippingRulesEnvelope.swift
+++ KsApi/models/ShippingRulesEnvelope.swift
@@ -2,5 +2,9 @@ import Curry
 import Runes
 
 public struct ShippingRulesEnvelope: Swift.Decodable {
+  enum CodingKeys: String, CodingKey {
+    case shippingRules = "shipping_rules"
+  }
+
   public let shippingRules: [ShippingRule]
 }
