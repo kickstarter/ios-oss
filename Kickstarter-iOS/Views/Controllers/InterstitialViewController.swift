98d3d11dfaca484bbe0efdeef9f244f6b4a51f38
Andrew Batutin <andrey@kickstarter.com>
2020-11-13 18:13:50 +0200

    removed .idea files, put back iboutlets

--- Kickstarter-iOS/Views/Controllers/InterstitialViewController.swift
+++ Kickstarter-iOS/Views/Controllers/InterstitialViewController.swift
@@ -5,6 +5,13 @@ import UIKit
 
 internal final class InterstitialViewController: UIViewController{
   
+  @IBOutlet fileprivate var mailIcon: UIImageView!
+  @IBOutlet fileprivate var laterButton: UIButton!
+  @IBOutlet fileprivate var resendEmailButton: UIButton!
+  @IBOutlet fileprivate var verifyEmailLable: UILabel!
+  @IBOutlet fileprivate var checkInboxLable: UILabel!
+
+  
   fileprivate let viewModel: InterstitialViewModel = InterstitialViewModel()
   
   internal static func instantiate() -> InterstitialViewController {
