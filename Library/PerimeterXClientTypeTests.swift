b7409aee015a3683faf656de5f000a6abb7c07e8
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-02-19 16:29:49 -0500

    Adding/refactoring tests.

--- Library/PerimeterXClientTypeTests.swift
+++ Library/PerimeterXClientTypeTests.swift
@@ -3,7 +3,7 @@ import Prelude
 import XCTest
 
 final class PerimeterXClientTypeTests: TestCase {
-  func testHeaders_NoError() {
+  func testHeaders() {
     let mockPXClient = MockPerimeterXClient()
 
     XCTAssertEqual(mockPXClient.headers(), ["PX-AUTH-TEST": "foobar"])
@@ -13,5 +13,47 @@ final class PerimeterXClientTypeTests: TestCase {
     let mockPXClient = MockPerimeterXClient()
 
     XCTAssertNotEqual(mockPXClient.headers(), ["PX-INCORRECT-HEADER": "foobar"])
+  }
+
+  func testHandleError_StatusCode_403() {
+    let mockPXClient = MockPerimeterXClient()
+    let data = "test".data(using: .utf8, allowLossyConversion: false)
+    guard let blockResponse = HTTPURLResponse(
+      url: URL(string: "http://api.ksr.com/v1/test?key=value")!,
+      statusCode: 403,
+      httpVersion: nil,
+      headerFields: nil
+    ) else {
+      XCTFail("Could not unwrap HTTPURLResponse.")
+      return
+    }
+
+    mockPXClient.handleError(blockResponse: blockResponse, and: data!)
+
+    self.scheduler.advance()
+
+    XCTAssertEqual(mockPXClient.pxblockType, .captcha)
+    XCTAssertTrue(mockPXClient.handleErrorCalled)
+  }
+
+  func testHandleError_StatusCode_200() {
+    let mockPXClient = MockPerimeterXClient()
+    let data = "test".data(using: .utf8, allowLossyConversion: false)
+    guard let blockResponse = HTTPURLResponse(
+      url: URL(string: "http://api.ksr.com/v1/test?key=value")!,
+      statusCode: 200,
+      httpVersion: nil,
+      headerFields: nil
+    ) else {
+      XCTFail("Could not unwrap HTTPURLResponse.")
+      return
+    }
+
+    mockPXClient.handleError(blockResponse: blockResponse, and: data!)
+
+    self.scheduler.advance()
+
+    XCTAssertEqual(mockPXClient.pxblockType, .valid)
+    XCTAssertFalse(mockPXClient.handleErrorCalled)
   }
 }
--- Library/TestHelpers/MockPerimeterXClient.swift
+++ Library/TestHelpers/MockPerimeterXClient.swift
@@ -1,9 +1,24 @@
 import Foundation
 
+internal enum MockPXBlockType: String {
+  case captcha
+  case valid
+}
+
 internal class MockPerimeterXClient: PerimeterXClientType {
+  var handleErrorCalled: Bool = false
+  var pxblockType: MockPXBlockType?
+
   func headers() -> [String: String] {
     return ["PX-AUTH-TEST": "foobar"]
   }
 
-  func handleError(blockResponse _: HTTPURLResponse, and _: Data) {}
+  func handleError(blockResponse: HTTPURLResponse, and _: Data) {
+    if blockResponse.statusCode == 403 {
+      self.pxblockType = .captcha
+      self.handleErrorCalled = true
+      return
+    }
+    self.pxblockType = .valid
+  }
 }
