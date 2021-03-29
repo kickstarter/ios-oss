4177727f4990560f64a90c81b7f45263d9d58ed6
moderateepheezy <moderateepheezy@gmail.com>
2021-03-29 23:55:20 +0100

    Update the TimeInterval test value

--- Library/TimeInterval+ISO8601DateTests.swift
+++ Library/TimeInterval+ISO8601DateTests.swift
@@ -5,14 +5,12 @@ class TimeInterval_ISO8601DateTests: XCTestCase {
   func testToISO8601DateTimeString() {
     let timeInterval = 1_506_897_315.0
 
-    let iSO8601DateTimeString = timeInterval.toISO8601DateTimeString()
-    XCTAssertEqual(iSO8601DateTimeString, "2017-10-01T22:35:15Z")
+    XCTAssertEqual(timeInterval.toISO8601DateTimeString(), "2017-10-01T22:35:15Z")
   }
 
   func testToISO8601DateTimeString_Future() {
     let timeInterval = 1_680_078_655.0
 
-    let iSO8601DateTimeString = timeInterval.toISO8601DateTimeString()
-    XCTAssertEqual(iSO8601DateTimeString, "2023-03-29T08:30:55Z")
+    XCTAssertEqual(timeInterval.toISO8601DateTimeString(), "2023-03-29T08:30:55Z")
   }
 }
