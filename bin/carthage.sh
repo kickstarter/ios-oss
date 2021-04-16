2d3eb93caa40f598edddddfaa43dd612c396327d
Justin Swart <justinswart@users.noreply.github.com>
2021-04-16 16:30:46 -0500

    Remove brew config

--- bin/carthage.sh
+++ bin/carthage.sh
@@ -25,7 +25,6 @@ if [ -z "${CIRCLECI:-}" ]; then
   carthage "$@"
 # Else if running on CircleCI and no cache found, ensure latest carthage and build resolved dependencies
 elif ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
-  brew config
   brew upgrade carthage
   echo "Resolving dependencies"
   carthage bootstrap --platform iOS
