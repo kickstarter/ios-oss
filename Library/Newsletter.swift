335b7003edb1cbffaabdd58ff00a3f3f3b14bc37
Justin Swart <justinswart@users.noreply.github.com>
2021-05-07 10:27:37 -0700

    Updated translations and snapshots

--- Library/Newsletter.swift
+++ Library/Newsletter.swift
@@ -13,23 +13,23 @@ public enum Newsletter: CaseIterable {
   public var displayableName: String {
     switch self {
     case .arts:
-      return localizedString(key: "Kickstarter_Arts", defaultValue: "Kickstarter Arts")
+      return Strings.Kickstarter_Arts()
     case .games:
-      return localizedString(key: "Kickstarter_Games", defaultValue: "Kickstarter Games")
+      return Strings.Kickstarter_Games()
     case .happening:
       return Strings.profile_settings_newsletter_happening()
     case .invent:
-      return localizedString(key: "Kickstarter_Invent", defaultValue: "Kickstarter Invent")
+      return Strings.Kickstarter_Invent()
     case .promo:
       return Strings.Announcements()
     case .weekly:
       return Strings.profile_settings_newsletter_weekly()
     case .films:
-      return localizedString(key: "Kickstarter_on_Film", defaultValue: "Kickstarter on Film")
+      return Strings.Kickstarter_on_Film()
     case .publishing:
       return Strings.profile_settings_newsletter_publishing()
     case .alumni:
-      return localizedString(key: "Working_on_it", defaultValue: "Working on it")
+      return Strings.Working_on_it()
     case .music:
       return Strings.Kickstarter_Music()
     }
@@ -54,10 +54,7 @@ public enum Newsletter: CaseIterable {
     case .publishing:
       return Strings.Welcome_to_our_library_Peruse_the_stacks_with_us()
     case .alumni:
-      return localizedString(
-        key: "A_monthly_pep_talk_to_boost_your_creative_practice_for_creators_of_all_types",
-        defaultValue: "A monthly pep talk to boost your creative practice, for creators of all types."
-      )
+      return Strings.A_monthly_pep_talk_to_boost_your_creative_practice_for_creators_of_all_types()
     case .music:
       return Strings.Its_like_the_radio_but_nothing_sucks_and_also_its_a_newsletter()
     }
