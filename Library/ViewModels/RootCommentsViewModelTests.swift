4fde57f4ab4b906ef29d2c776263c13887dc867d
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-05-11 12:07:09 -0400

    Updating view model test file.

--- Library/ViewModels/RootCommentsViewModelTests.swift
+++ Library/ViewModels/RootCommentsViewModelTests.swift
@@ -9,19 +9,7 @@ import XCTest
 internal final class RootCommentsViewModelTests: TestCase {
   internal let vm: RootCommentsViewModelType = RootCommentsViewModel()
 
-  /// Remove when ready for development
-  internal let viewDidLoadTestOutput = TestObserver<Bool, Never>()
-
   override func setUp() {
     super.setUp()
-
-    self.vm.outputs.viewDidLoadTestOutput.observe(self.viewDidLoadTestOutput.observer)
-  }
-
-  func testViewDidLoad() {
-    self.vm.inputs.viewDidLoad()
-    self.scheduler.advance()
-
-    self.viewDidLoadTestOutput.assertValues([true], "View did load emits.")
   }
 }
