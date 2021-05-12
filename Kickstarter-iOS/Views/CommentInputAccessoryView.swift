e139732b17634e0c9a257b73b3d6cd5bc6ff8c31
Mubarak Sadoon <mubarak@kickstarter.com>
2021-05-12 11:39:24 -0400

    accessory view file

--- Kickstarter-iOS/Views/CommentInputAccessoryView.swift
+++ Kickstarter-iOS/Views/CommentInputAccessoryView.swift
@@ -1,58 +1,62 @@
 import UIKit
 
-// No view models, yet. --> Wait for Toby's stuff
-// No tests yet. --> Wait for Toby's stuff
-// See if you can fix that overpadding on hitting return in the text view. Think about enabling scrolling only once the text view has reached its maximum height, maybe a little less.
-// See if you can make the textview's delegate reactive.
-
-// Note: This code is not final. It is for demonstration purposes only.
-class CommentInputAccessoryView: UIView, UITextViewDelegate {
-   lazy private let commentTextView: UITextView {
-      let textView = UITextView()
-      
-      textView.backgroundColor = .lightGray
-      textView.translatesAutoresizingMaskIntoConstraints = false
-      textView.delegate = self
-      textView.isScrollEnabled = false
-      
-      return textView
-    }
+// NOTE: This code is not final. It is for demonstration purposes only. (Ie. No tests, no view models, styles until we merge  with https://kickstarter.atlassian.net/browse/NT-1893)
+class CommentInputAccessoryView: UIView {
+  // MARK: Properties
+  private let commentTextView = UITextView()
+  // TODO: 100 pixels here seems to be 200 pixels as described in the ticket above. May want to reconfirm if this is in points.
+  private let maximumTextViewHeight: CGFloat = 100.0
+  private let scrollTextViewThresholdHeight: CGFloat = 80.0
   
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-      
-        self.addSubview(commentTextView)
-        self.constrainView(commentTextView)
-        self.autoresizingMask = UIView.AutoresizingMask.flexibleHeight
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override var intrinsicContentSize: CGSize {
-        let textSize = self.commentTextView.sizeThatFits(CGSize(width: self.commentTextView.bounds.width,
-                                                                height: CGFloat.greatestFiniteMagnitude))
-        return CGSize(width: self.bounds.width,
-                      height: textSize.height)
-    }
-
-    // MARK: UITextViewDelegate
-    func textViewDidChange(_ textView: UITextView) {
-        // Re-calculate intrinsicContentSize when text changes
-        self.invalidateIntrinsicContentSize()
-    }
+  // MARK: Initializers
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    self.autoresizingMask = UIView.AutoresizingMask.flexibleHeight
+    self.setupTextView()
+    self.addSubview(commentTextView)
+    self.constrainView(commentTextView)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: View lifecycle
+  override var intrinsicContentSize: CGSize {
+    let textSize = self.commentTextView.sizeThatFits(CGSize(width: self.commentTextView.bounds.width,
+                                                            height: CGFloat.greatestFiniteMagnitude))
+    let contentSize = CGSize(width: self.bounds.width, height: textSize.height)
+    
+    return contentSize
+  }
   
   // MARK: Helpers
-
+  private func setupTextView() {
+    commentTextView.backgroundColor = .lightGray
+    commentTextView.translatesAutoresizingMaskIntoConstraints = false
+    commentTextView.delegate = self
+    commentTextView.isScrollEnabled = false
+  }
+  
   private func constrainView(_ view: UITextView) {
     self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|",
                                                        options: [],
                                                        metrics: nil,
                                                        views: ["view": view]))
-    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view(<=100)]|",
+    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view(<=\(maximumTextViewHeight))]|",
                                                        options: [],
                                                        metrics: nil,
                                                        views: ["view": view]))
+  }
+}
+
+// MARK: UITextViewDelegate
+// TODO: Is there a better reactive way to do this? I know RxSwift had things like textField.rx.didChange properties that could be subscribed to...
+extension CommentInputAccessoryView: UITextViewDelegate {
+  func textViewDidChange(_ textView: UITextView) {
+    invalidateIntrinsicContentSize()
+    
+    commentTextView.isScrollEnabled = bounds.height >= scrollTextViewThresholdHeight
   }
 }
