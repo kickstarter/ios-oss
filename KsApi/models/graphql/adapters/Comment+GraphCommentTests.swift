513a32eed99b478789160f73dfee0a84d94e1446
Justin Swart <justinswart@users.noreply.github.com>
2021-05-13 09:43:15 -0700

    Remove nested function

--- KsApi/models/graphql/adapters/Comment+GraphCommentTests.swift
+++ KsApi/models/graphql/adapters/Comment+GraphCommentTests.swift
@@ -3,18 +3,16 @@ import XCTest
 
 final class Comment_GraphCommentTests: XCTestCase {
   func test() {
-    func test() {
-      let comment = Comment.comment(from: .template)
-
-      XCTAssertEqual(comment.id, "VXNlci0yMDU3OTc4MTQ2")
-      XCTAssertEqual(
-        comment.body,
-        "I hope you guys all remembered to write in Bat Boy/Bigfoot on your ballots! Bat Boy 2020!!"
-      )
-      XCTAssertEqual(comment.replyCount, 4)
-      XCTAssertEqual(comment.author.id, "VXNlci0xOTE1MDY0NDY3")
-      XCTAssertEqual(comment.author.name, "Author McGee")
-      XCTAssertEqual(comment.author.isCreator, false)
-    }
+    let comment = Comment.comment(from: .template)
+    
+    XCTAssertEqual(comment.id, "VXNlci0yMDU3OTc4MTQ2")
+    XCTAssertEqual(
+      comment.body,
+      "I hope you guys all remembered to write in Bat Boy/Bigfoot on your ballots! Bat Boy 2020!!"
+    )
+    XCTAssertEqual(comment.replyCount, 4)
+    XCTAssertEqual(comment.author.id, "VXNlci0xOTE1MDY0NDY3")
+    XCTAssertEqual(comment.author.name, "Author McGee")
+    XCTAssertEqual(comment.author.isCreator, false)
   }
 }
