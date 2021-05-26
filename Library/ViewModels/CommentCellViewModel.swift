df7630a5c0a24e8cc0e4bd7cd66984b788cca89d
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-05-26 17:59:23 -0400

    Minor formatting updates

--- Library/ViewModels/CommentCellViewModel.swift
+++ Library/ViewModels/CommentCellViewModel.swift
@@ -14,20 +14,20 @@ public protocol CommentCellViewModelOutputs {
   /// Emits author's badge for a comment
   var authorBadge: Signal<Comment.AuthorBadge, Never> { get }
 
-  /// Emits text containing author's fullname or username
-  var authorName: Signal<String, Never> { get }
-
   /// Emits a url to the comment author's image
   var authorImageURL: Signal<URL, Never> { get }
+
+  /// Emits text containing author's fullname or username
+  var authorName: Signal<String, Never> { get }
 
   /// Emits text containing comment body
   var body: Signal<String, Never> { get }
 
-  /// Emits a Bool determining if the reply and flag buttons in the bottomColumnStackView are hidden
-  var replyButtonIsHidden: Signal<Bool, Never> { get }
-
   /// Emits text  relative time the comment was posted
   var postTime: Signal<String, Never> { get }
+
+  /// Emits a Bool determining if the reply and flag buttons in the bottomColumnStackView are hidden
+  var replyButtonIsHidden: Signal<Bool, Never> { get }
 }
 
 public protocol CommentCellViewModelType {
@@ -93,10 +93,10 @@ public final class CommentCellViewModel:
 
   public let authorBadge: Signal<Comment.AuthorBadge, Never>
   public var authorImageURL: Signal<URL, Never>
-  public let body: Signal<String, Never>
-  public let replyButtonIsHidden: Signal<Bool, Never>
   public let authorName: Signal<String, Never>
+  public let body: Signal<String, Never>
   public let postTime: Signal<String, Never>
+  public let replyButtonIsHidden: Signal<Bool, Never>
 
   public var inputs: CommentCellViewModelInputs { self }
   public var outputs: CommentCellViewModelOutputs { self }
