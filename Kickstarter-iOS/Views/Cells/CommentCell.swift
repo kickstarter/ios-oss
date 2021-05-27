8e3fb5beac00152120a5536fcfafebfe10248407
moderateepheezy <moderateepheezy@gmail.com>
2021-05-27 19:36:24 +0100

    Add View Replies option to CommentCell

--- Kickstarter-iOS/Views/Cells/CommentCell.swift
+++ Kickstarter-iOS/Views/Cells/CommentCell.swift
@@ -14,6 +14,12 @@ final class CommentCell: UITableViewCell, ValueCell {
 
   private lazy var flagButton = { UIButton(frame: .zero) }()
   private lazy var replyButton = { UIButton(frame: .zero) }()
+  private lazy var rightArrowImageView = { UIImageView(frame: .zero)
+    |> \.image .~ Library.image(named: "arrow-right-large")
+  }()
+
+  private lazy var viewRepliesLabel: UILabel = { UILabel(frame: .zero) }()
+  private lazy var viewRepliesStackView: UIStackView = { UIStackView(frame: .zero) }()
 
   private lazy var rootStackView = {
     UIStackView(frame: .zero)
@@ -56,6 +62,20 @@ final class CommentCell: UITableViewCell, ValueCell {
 
     _ = self.flagButton
       |> UIButton.lens.image(for: .normal) %~ { _ in Library.image(named: "flag") }
+
+    _ = self.viewRepliesStackView
+      |> viewRepliesStackViewStyle
+
+    // TODO: Internationalized in the near future.
+
+    _ = self.viewRepliesLabel
+      |> \.text .~ localizedString(
+        key: "View_replies",
+        defaultValue: "View replies"
+      )
+      |> \.textColor .~ UIColor.ksr_support_400
+      |> \.font .~ UIFont.ksr_subhead()
+      |> \.adjustsFontForContentSizeCategory .~ true
   }
 
   // MARK: - Configuration
@@ -67,7 +87,16 @@ final class CommentCell: UITableViewCell, ValueCell {
   }
 
   private func configureViews() {
-    _ = ([self.commentCellHeaderStackView, self.bodyTextView, self.bottomColumnStackView], self.rootStackView)
+    _ =
+      ([
+        self.commentCellHeaderStackView,
+        self.bodyTextView,
+        self.viewRepliesStackView,
+        self.bottomColumnStackView
+      ], self.rootStackView)
+      |> ksr_addArrangedSubviewsToStackView()
+
+    _ = ([self.viewRepliesLabel, UIView(), self.rightArrowImageView], self.viewRepliesStackView)
       |> ksr_addArrangedSubviewsToStackView()
 
     _ = ([self.replyButton, UIView(), self.flagButton], self.bottomColumnStackView)
@@ -78,6 +107,11 @@ final class CommentCell: UITableViewCell, ValueCell {
     _ = (self.rootStackView, self.contentView)
       |> ksr_addSubviewToParent()
       |> ksr_constrainViewToMarginsInParent()
+
+    NSLayoutConstraint.activate([
+      self.rightArrowImageView.widthAnchor.constraint(equalToConstant: Styles.grid(1)),
+      self.rightArrowImageView.heightAnchor.constraint(equalToConstant: Styles.grid(2))
+    ])
   }
 
   // MARK: - View model
@@ -89,6 +123,20 @@ final class CommentCell: UITableViewCell, ValueCell {
 }
 
 // MARK: Styles
+
+private let viewRepliesStackViewStyle: StackViewStyle = { stackVew in
+  stackVew
+    |> \.axis .~ .horizontal
+    |> \.alignment .~ .center
+    |> \.layoutMargins .~
+    .init(top: Styles.grid(3), left: Styles.grid(2), bottom: Styles.grid(3), right: Styles.grid(3))
+    |> \.isLayoutMarginsRelativeArrangement .~ true
+    |> \.insetsLayoutMarginsFromSafeArea .~ false
+    |> \.spacing .~ Styles.grid(1)
+    |> \.layer.borderWidth .~ 1
+    |> \.layer.borderColor .~ UIColor.ksr_support_100.cgColor
+    |> roundedStyle(cornerRadius: 2)
+}
 
 // TODO: Internationalized in the near future.
 
