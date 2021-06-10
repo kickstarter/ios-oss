489c528353ce3b26b856e788ad9aeac87c883ee5
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-06-10 19:30:50 -0400

    Fixing Strings documentation.

--- Library/Strings.swift
+++ Library/Strings.swift
@@ -5164,10 +5164,10 @@ with friends."
    "<a href="%{community_link}">Learn more about comment guidelines</a>"
 
    - **en**: "<a href="%{community_link}">Learn more about comment guidelines</a>"
-   - **de**: "<a href="%{community_link}">Learn more about comment guidelines</a>"
+   - **de**: "<a href="%{community_link}">Mehr Info zu den Kommentar-Richtlinien</a>"
    - **es**: "<a href="%{community_link}">Más información sobre las reglas de los comentarios</a>"
    - **fr**: "<a href="%{community_link}">En savoir plus sur les directives applicables aux commentaires</a>"
-   - **ja**: "<a href="%{community_link}">Learn more about comment guidelines</a>"
+   - **ja**: "<a href="%{community_link}">コメントのガイドラインについて詳細を見る</a>"
   */
   public static func Learn_more_about_comment_guidelines(community_link: String) -> String {
     return localizedString(
