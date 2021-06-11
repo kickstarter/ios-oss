c4108318cd16b11dc582c19e5bce926b02c2e2a0
Afees Lawal <moderateepheezy@gmail.com>
2021-06-11 01:29:22 +0100

    [NT-2017] Updates – Show new comments experience (#1511)
    * * Use the new comments UI for Updates
    
    * Update map logic
    
    * Refactor implementation of feature flag for Comment Threading
    
    * Undo-redo
    
    * Remove unused imports

---
+++ Library/OptimizelyFeature+Helpers.swift
@@ -0,0 +1,4 @@
+public func featureCommentThreadingIsEnabled() -> Bool {
+  AppEnvironment.current.optimizelyClient?
+    .isFeatureEnabled(featureKey: OptimizelyFeature.Key.commentThreading.rawValue) ?? false
+}
