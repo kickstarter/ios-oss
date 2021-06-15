589e87e6a0f8dfb2c831124970108c457cb9b764
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-06-15 12:30:04 -0400

    Removing login with apple killswitch feature flag and related code.

--- Library/OptimizelyFeature.swift
+++ Library/OptimizelyFeature.swift
@@ -5,6 +5,5 @@ public enum OptimizelyFeature {
     case commentFlaggingEnabled = "ios_comment_threading_comment_flagging"
     case commentThreading = "ios_comment_threading"
     case commentThreadingRepliesEnabled = "ios_comment_threading_reply_buttons"
-    case signInWithAppleKillswitch = "ios_sign_in_with_apple_killswitch"
   }
 }
