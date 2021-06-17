5647e5290812fc9e8097e3a6474b3a8c38e55390
Mubarak Sadoon <msadoon@gmail.com>
2021-06-17 11:05:39 -0400

    reverted all changes, rebasing from NT-2006

--- Kickstarter-iOS/Library/OptimizelyResultTypeTests.swift
+++ Kickstarter-iOS/Library/OptimizelyResultTypeTests.swift
@@ -10,7 +10,7 @@ final class OptimizelyResultTypeTests: XCTestCase {
 
   func testIsSuccess_ReturnsFalse() {
     let result = OptimizelyResult<Any>.failure(OptimizelyError.generic)
-    
+
     XCTAssertEqual("Unknown reason", result.hasError?.localizedDescription)
   }
 }
