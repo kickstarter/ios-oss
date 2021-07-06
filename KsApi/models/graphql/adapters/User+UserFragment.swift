2b85aec2fcf3c03820f94125cdfa7f5447723b3d
Justin Swart <justinswart@users.noreply.github.com>
2021-07-05 17:26:04 -0700

    Adapt Backing from BackingFragment

---
+++ KsApi/models/graphql/adapters/User+UserFragment.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+extension User {
+  /**
+   Returns a minimal `User` from a `GraphUser`
+   */
+  static func user(from userFragment: GraphAPI.UserFragment) -> User? {
+    guard let id = decompose(id: userFragment.id) else { return nil }
+
+    return User(
+      avatar: Avatar(
+        large: userFragment.imageUrl,
+        medium: userFragment.imageUrl,
+        small: userFragment.imageUrl
+      ),
+      erroredBackingsCount: nil,
+      facebookConnected: nil,
+      id: id,
+      isAdmin: nil,
+      isEmailVerified: nil,
+      isFriend: nil,
+      location: nil,
+      name: userFragment.name,
+      needsFreshFacebookToken: nil,
+      newsletters: NewsletterSubscriptions(),
+      notifications: Notifications(),
+      optedOutOfRecommendations: nil,
+      showPublicProfile: nil,
+      social: nil,
+      stats: Stats(),
+      unseenActivityCount: nil
+    )
+  }
+}
