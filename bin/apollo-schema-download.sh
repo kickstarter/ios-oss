d5bd9fb9fbb297d0ebee25b081d0848a5f43d5ee
Justin Swart <justinswart@users.noreply.github.com>
2021-07-14 16:55:47 -0700

    Install Apollo

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+npm install -D apollo
 apollo schema:download --endpoint=https://www.kickstarter.com/graph KsApi/graphql-schema.json --header "Authorization: token ${GRAPHQL_API_AUTH_TOKEN}"
