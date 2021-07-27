0d255e6fe138f2f3a841f6db252ebd0508710fff
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-07-26 20:44:32 -0400

    Adding a small test to bump coverage.

--- KsApi/models/graphql/adapters/GraphMutationWatchProjectResponseEnvelope+WatchProjectMutation.DataTests.swift
+++ KsApi/models/graphql/adapters/GraphMutationWatchProjectResponseEnvelope+WatchProjectMutation.DataTests.swift
@@ -20,4 +20,20 @@ final class GraphMutationWatchProjectResponseEnvelope_WatchProjectMutationTests:
     XCTAssertEqual(envelope?.watchProject.project.id, "id")
     XCTAssertEqual(envelope?.watchProject.project.isWatched, true)
   }
+
+  func test_envelopeFrom_ReturnsNil() {
+    let dict: [String: Any] = [
+      "wrongKey": [
+        "clientMutationId": nil,
+        "project": [
+          "id": "id",
+          "isWatched": true
+        ]
+      ]
+    ]
+
+    let data = GraphAPI.WatchProjectMutation.Data(unsafeResultMap: dict)
+
+    XCTAssertNil(GraphMutationWatchProjectResponseEnvelope.from(data))
+  }
 }
