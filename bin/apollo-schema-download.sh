de45109bfbcfbd7b5e4f4c48c2c920453e099163
Yatrik Solanki <yatrik@kickstarter.com>
2021-08-06 18:14:03 -0400

    Swap argument order

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 npm install -D apollo
-npx apollo schema:download --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --header "Authorization: token ${GRAPHQL_API_AUTH_TOKEN}" --header "Content-Type: application/json"
+npx apollo schema:download --endpoint=https://staging.kickstarter.com/graph --header "Authorization: Token ${GRAPHQL_API_AUTH_TOKEN}" KsApi/graphql-schema.json
