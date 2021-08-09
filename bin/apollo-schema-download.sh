c88cf212cfe1edc65cb0a12b4cb0a459d5fed351
Mubarak Sadoon <msadoon@gmail.com>
2021-08-09 14:25:29 -0400

    added target and included fragment files

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 npm install -D apollo
-npx apollo codegen:generate --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json
+npx apollo codegen:generate --endpoint=https://staging.kickstarter.com/graph --target=swift --includes=./**/*.graphql KsApi/graphql-schema.json
