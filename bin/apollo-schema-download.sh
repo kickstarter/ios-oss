7f52091df9041812539684e2d43bfbec62fe2a82
Yatrik Solanki <yatrik@kickstarter.com>
2021-08-09 11:56:23 -0400

    Fix quotes

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -3,4 +3,4 @@ curl --location --request POST "https://staging.kickstarter.com/graph" \
   --header "User-Agent: Kickstarter iOS" \
   --header "Authorization: Token ${GRAPHQL_API_AUTH_TOKEN}" \
   --header "Content-Type: application/json" \
-  --data-raw "{"query":"{\n  __schema {\n    types {\n      name\n    }\n  }\n}","variables":{}}" > graphql-schema.json
+  --data-raw '{"query":"{\n  __schema {\n    types {\n      name\n    }\n  }\n}","variables":{}}' > graphql-schema.json
