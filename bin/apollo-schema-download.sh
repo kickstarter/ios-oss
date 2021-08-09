82583bcc4ae523ec9e1776a09b6d06f0faa579a7
Mubarak Sadoon <msadoon@gmail.com>
2021-08-09 11:50:56 -0400

    added introspection query to download script

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 npm install -D apollo
-npx apollo schema:download --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --header "Authorization: token ${GRAPHQL_API_AUTH_TOKEN}"
+npx apollo schema:download --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --header "Authorization: token ${GRAPHQL_API_AUTH_TOKEN}" --header 'User-Agent: Kickstarter iOS' --header 'Content-Type: application/json' --header "Authorization: token ${GRAPHQL_API_AUTH_TOKEN}" --data-raw '{"query":"{\n  __schema {\n    types {\n      name\n    }\n  }\n}","variables":{}}'
