890fc0e9a8586da982efacb57957c07276f096e3
Mubarak Sadoon <msadoon@gmail.com>
2021-08-09 14:38:49 -0400

    moved lookup location of fragment files

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 npm install -D apollo
-npx apollo codegen:generate --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --target=swift --includes=./**/*.graphql
+npx apollo codegen:generate --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --target=swift --includes=../**/*.graphql
