d87fc51415b42faa366fd9b0f083aeb88e81d6f6
Mubarak Sadoon <msadoon@gmail.com>
2021-08-09 12:33:01 -0400

    trigger build with new headers.

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 npm install -D apollo
-npx apollo schema:download --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --header "Authorization: Basic Y3JlYXRpdmU6c3R1ZHlpbmdhdGV3aW50ZXJmdW5ueQ==" --header "User-Agent: Kickstarter iOS" --header "Content-Type: application/json" --header "Authorization: token ${GRAPHQL_API_AUTH_TOKEN}" --data-raw "{'query':'{\n  __schema {\n    types {\n      name\n    }\n  }\n}','variables':{}}"
+npx apollo schema:download --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json --header "Authorization: Basic Y3JlYXRpdmU6c3R1ZHlpbmdhdGV3aW50ZXJmdW5ueQ==" --header "User-Agent: Kickstarter iOS" --header "Content-Type: application/json" --header "User-Agent: Kickstarter iOS" --data-raw "{'query':'{\n  __schema {\n    types {\n      name\n    }\n  }\n}','variables':{}}"
