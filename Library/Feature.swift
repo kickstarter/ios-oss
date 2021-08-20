7f1cc3186457060f5f9d9a48752d8e4c2bcc7814
Hari Singh <15615702+singhhari@users.noreply.github.com>
2021-08-20 13:39:32 -0400

    Removing Braze feature flag and all references to it.

--- Library/Feature.swift
+++ Library/Feature.swift
@@ -1,14 +1,12 @@
 import Foundation
 
 public enum Feature: String, CaseIterable {
-  case braze = "ios_braze"
   case segment = "ios_segment"
 }
 
 extension Feature: CustomStringConvertible {
   public var description: String {
     switch self {
-    case .braze: return "Braze"
     case .segment: return "Segment"
     }
   }
