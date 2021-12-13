0afb314fdb22f839ad6c58568893fd8985f9b511
Mubarak Sadoon <msadoon@gmail.com>
2021-12-13 18:00:36 -0500

    fixed up tests, added new ones

---
+++ KsApi/mutations/templates/query/FetchCommentRepliesQueryTemplate.swift
@@ -0,0 +1,9 @@
+//
+//  FetchCommentRepliesQueryTemplate.swift
+//  KsApiTests
+//
+//  Created by Mubarak Sadoon on 12/13/21.
+//  Copyright © 2021 Kickstarter. All rights reserved.
+//
+
+import Foundation
