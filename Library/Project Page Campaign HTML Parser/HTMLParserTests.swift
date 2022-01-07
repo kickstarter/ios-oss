119c2daa18a53f1bd63230cedad698548dd1230d
Mubarak Sadoon <msadoon@gmail.com>
2022-01-07 18:04:20 -0500

    added some test cases, still validing tests against the parser. No UI components built yet.

--- Library/Project Page Campaign HTML Parser/HTMLParserTests.swift
+++ Library/Project Page Campaign HTML Parser/HTMLParserTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class HTMLParserTests: TestCase {
   let htmlParser = HTMLParser()
 
-  func testHTMLParser_WithValidNonGifImage_Success() {
+  func testHTMLParser_WithValidNonGIFImage_Success() {
     let viewElements = self.htmlParser.parse(html: HTMLParserTemplates.validNonGIFImage.data)
 
     guard let viewElement = viewElements.first as? ImageViewElement else {
@@ -16,6 +16,7 @@ final class HTMLParserTests: TestCase {
     XCTAssertEqual(
       viewElement.src,
       "https://ksr-qa-ugc.imgix.net/assets/033/981/078/6a3036d55ab3c3d6f271ab0b5c532912_original.png?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1624426643&amp;auto=format&amp;gif-q=50&amp;lossless=true&amp;s=aaa772a0ea57e4697c14311f1f2e0086"
+        .htmlStripped()
     )
     XCTAssertNil(viewElement.href)
 
@@ -27,4 +28,110 @@ final class HTMLParserTests: TestCase {
 
     XCTAssertTrue(existingCaption.isEmpty)
   }
+
+  func testHTMLParser_WithValidGIFImage_Success() {
+    let viewElements = self.htmlParser.parse(html: HTMLParserTemplates.validGIFImage.data)
+
+    guard let viewElement = viewElements.first as? ImageViewElement else {
+      XCTFail("image view element should be created.")
+
+      return
+    }
+
+    XCTAssertEqual(
+      viewElement.src,
+      "https://ksr-qa-ugc.imgix.net/assets/033/915/794/8dca97fb0636aeb1a4a937025f369e7e_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1623894386&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=cde086d146601f4d9c6fe07e0d93bb84"
+        .htmlStripped()
+    )
+    XCTAssertNil(viewElement.href)
+
+    guard let existingCaption = viewElement.caption else {
+      XCTFail("image caption should exist")
+
+      return
+    }
+
+    XCTAssertTrue(existingCaption.isEmpty)
+  }
+
+  func testHTMLParser_WithValidImageWithCaption_Success() {
+    let viewElements = self.htmlParser.parse(html: HTMLParserTemplates.validImageWithCaption.data)
+
+    guard let viewElement = viewElements.first as? ImageViewElement else {
+      XCTFail("image view element should be created.")
+
+      return
+    }
+
+    XCTAssertEqual(
+      viewElement.src,
+      "https://ksr-qa-ugc.imgix.net/assets/035/418/752/b1fe3dc3ff2aa64161aaf7cd6def0b97_original.jpg?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1635677740&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=6f32811c554177afaafc447642d83788"
+        .htmlStripped()
+    )
+
+    XCTAssertNil(viewElement.href)
+
+    guard let existingCaption = viewElement.caption else {
+      XCTFail("image caption should exist")
+
+      return
+    }
+
+    XCTAssertEqual(existingCaption, "Viktor Pushkarev using lino-cutting to create the cover art.")
+  }
+
+  func testHTMLParser_WithValidImageWithCaptionAndLink_Success() {
+    let viewElements = self.htmlParser.parse(html: HTMLParserTemplates.validImageWithCaptionAndLink.data)
+
+    guard let viewElement = viewElements.last as? ImageViewElement else {
+      XCTFail("image view element should be created.")
+
+      return
+    }
+
+    XCTAssertEqual(
+      viewElement.src,
+      "https://ksr-qa-ugc.imgix.net/assets/034/488/736/c35446a93f1f9faedd76e9db814247bf_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1628654686&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=061483d5e8fac13bd635b67e2ae8a258"
+        .htmlStripped()
+    )
+
+    XCTAssertEqual(
+      viewElement.href,
+      "https://producthype.co/most-powerful-crowdfunding-newsletter/?utm_source=ProductHype&amp;utm_medium=Banner&amp;utm_campaign=Homi"
+        .htmlStripped()
+    )
+
+    guard let existingCaption = viewElement.caption else {
+      XCTFail("image caption should exist")
+
+      return
+    }
+
+    XCTAssertEqual(existingCaption, "Viktor Pushkarev using lino-cutting to create the cover art.")
+  }
+
+  /**
+   func testHTMLParser_WithValidVideo_Success() {
+     let viewElements = self.htmlParser.parse(html: HTMLParserTemplates.validVideo.data)
+
+     guard let viewElement = viewElements.first as? VideoViewElement else {
+       XCTFail("image view element should be created.")
+
+       return
+     }
+
+     XCTAssertEqual(
+       viewElement.src, "https://ksr-qa-ugc.imgix.net/assets/034/488/736/c35446a93f1f9faedd76e9db814247bf_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1628654686&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=061483d5e8fac13bd635b67e2ae8a258".htmlStripped())
+
+     XCTAssertEqual(viewElement.href, "https://producthype.co/most-powerful-crowdfunding-newsletter/?utm_source=ProductHype&amp;utm_medium=Banner&amp;utm_campaign=Homi")
+
+     guard let existingCaption = viewElement.caption else {
+       XCTFail("image caption should exist")
+
+       return
+     }
+
+     XCTAssertEqual(existingCaption, "Viktor Pushkarev using lino-cutting to create the cover art.")
+   }
+   */
 }
--- Library/Project Page Campaign HTML Parser/Templates/HTMLParserTemplates.swift
+++ Library/Project Page Campaign HTML Parser/Templates/HTMLParserTemplates.swift
@@ -20,6 +20,12 @@ public enum HTMLParserTemplates {
     switch self {
     case .validNonGIFImage:
       return self.validNonGIFImage
+    case .validGIFImage:
+      return self.validGIFImage
+    case .validImageWithCaption:
+      return self.validImageWithCaption
+    case .validImageWithCaptionAndLink:
+      return self.validImageWithCaptionAndLink
     case .errored:
       return self.erroredStory
     default:
@@ -29,10 +35,31 @@ public enum HTMLParserTemplates {
 
   // MARK: Private Properties
 
+  private var validImageWithCaptionAndLink: String {
+    """
+    <a href="https://producthype.co/most-powerful-crowdfunding-newsletter/?utm_source=ProductHype&amp;utm_medium=Banner&amp;utm_campaign=Homi" target="_blank" rel="noopener">
+      <div class="template asset" contenteditable="false" data-alt-text="" data-caption=\"Viktor Pushkarev using lino-cutting to create the cover art.\" data-id="34488736">\n
+        <figure>\n <img alt="" class="fit js-lazy-image" data-src="https://ksr-qa-ugc.imgix.net/assets/034/488/736/c35446a93f1f9faedd76e9db814247bf_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1628654686&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=061483d5e8fac13bd635b67e2ae8a258" src="https://ksr-qa-ugc.imgix.net/assets/034/488/736/c35446a93f1f9faedd76e9db814247bf_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1628654686&amp;auto=format&amp;frame=1&amp;q=92&amp;s=463cb21e97dd89bd564e6fc898ea6075">\n </figure>\n\n </div>\n </a>\n\n
+    """
+  }
+
+  private var validImageWithCaption: String {
+    """
+    <div class=\"template asset\" contenteditable=\"false\" data-alt-text=\"\" data-caption=\"Viktor Pushkarev using lino-cutting to create the cover art.\" data-id=\"35418752\">\n<figure>\n<img alt=\"\" class=\"fit\" src=\"https://ksr-qa-ugc.imgix.net/assets/035/418/752/b1fe3dc3ff2aa64161aaf7cd6def0b97_original.jpg?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1635677740&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=6f32811c554177afaafc447642d83788\">\n<figcaption class=\"px2\">Viktor Pushkarev using lino-cutting to create the cover art.</figcaption>\n</figure>\n\n</div>\n\n\n
+    """
+  }
+
   private var validNonGIFImage: String {
     """
     <div class="template asset" contenteditable="false" data-alt-text="" data-caption="" data-id="33981078">\n
       <figure>\n <img alt="" class="fit" src="https://ksr-qa-ugc.imgix.net/assets/033/981/078/6a3036d55ab3c3d6f271ab0b5c532912_original.png?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1624426643&amp;auto=format&amp;gif-q=50&amp;lossless=true&amp;s=aaa772a0ea57e4697c14311f1f2e0086">\n </figure>\n\n </div>\n\n\n
+    """
+  }
+
+  private var validGIFImage: String {
+    """
+    <div class="template asset" contenteditable="false" data-alt-text="" data-caption="" data-id="33915794">\n
+      <figure>\n <img alt="" class="fit js-lazy-image" data-src="https://ksr-qa-ugc.imgix.net/assets/033/915/794/8dca97fb0636aeb1a4a937025f369e7e_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1623894386&amp;auto=format&amp;gif-q=50&amp;q=92&amp;s=cde086d146601f4d9c6fe07e0d93bb84" src="https://ksr-qa-ugc.imgix.net/assets/033/915/794/8dca97fb0636aeb1a4a937025f369e7e_original.gif?ixlib=rb-4.0.2&amp;w=700&amp;fit=max&amp;v=1623894386&amp;auto=format&amp;frame=1&amp;q=92&amp;s=22f83142462421f48d7adca547d22367">\n </figure>\n\n </div>\n\n\n
     """
   }
 
