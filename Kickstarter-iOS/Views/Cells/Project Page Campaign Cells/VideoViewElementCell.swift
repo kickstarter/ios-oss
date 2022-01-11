d5b287fd3e3c538333a618c1382e21d1ee2f6f4c
Mubarak Sadoon <msadoon@gmail.com>
2022-01-11 17:23:08 -0500

    removed attributed text function in TextViewElement, will be added in with UI component.

--- Kickstarter-iOS/Views/Cells/Project Page Campaign Cells/VideoViewElementCell.swift
+++ Kickstarter-iOS/Views/Cells/Project Page Campaign Cells/VideoViewElementCell.swift
@@ -1,97 +1,95 @@
-// import Foundation
-// import Library
-// import UIKit
-// import AVKit
-// import youtube_ios_player_helper
-//
-// class VideoViewElementCell: UITableViewCell {
-//
-//    private var videoPlayerLayer: AVPlayerLayer?
-//    private var embedPlayerView: YTPlayerView?
-//    private var videoPlayerGestureRecognizer: UIGestureRecognizer?
-//
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//
-//        self.initialize()
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//
-//        self.initialize()
-//    }
-//
-//    override func prepareForReuse() {
-//        videoPlayerLayer?.player?.replaceCurrentItem(with: nil)
-//        videoPlayerLayer?.removeFromSuperlayer()
-//        videoPlayerLayer = nil
-//
-//        embedPlayerView?.stopVideo()
-//        embedPlayerView?.removeFromSuperview()
-//        embedPlayerView = nil
-//
-//        if let videoPlayerGestureRecognizer = videoPlayerGestureRecognizer {
-//            removeGestureRecognizer(videoPlayerGestureRecognizer)
-//        }
-//    }
-//
-//    func initialize() {
-//        backgroundColor = UIColor.gray
-//    }
-//
-//    func configureWith(value: VideoViewElement) {
-//        // The first will play the high quality video (0: high - 1: base)
-//        // TODO: Be able to remember where the user stopped playing the video
-//        guard let urlString = value.sourceUrls.first, let url = URL(string: urlString) else {
-//            return
-//        }
-//
-//        // Example Urls
-//        // https://youtu.be/DEG1qicbAmU
-//        // https://www.youtube.com/watch?v=i0uU5IV_4ZA
-//        if url.host == "youtu.be", let videoId = url.pathComponents.last {
-//            configureYouTubeVideo(videoId: videoId)
-//        } else if url.host == "www.youtube.com", let queryItems = URLComponents(string: urlString)?.queryItems {
-//            let videoIdItem = queryItems.first { $0.name == "v" }
-//            if let videoId = videoIdItem?.value {
-//                configureYouTubeVideo(videoId: videoId)
-//            }
-//        } else {
-//            configureVideo(videoUrl: url)
-//        }
-//    }
-//
-//    private func configureVideo(videoUrl: URL) {
-//        let videoPlayer = AVPlayer(url: videoUrl)
-//        videoPlayerLayer = AVPlayerLayer(player: videoPlayer)
-//
-//        if let videoPlayerLayer = videoPlayerLayer {
-//            videoPlayerLayer.frame = bounds
-//            layer.addSublayer(videoPlayerLayer)
-//        }
-//
-//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(togglePlay))
-//        videoPlayerGestureRecognizer = tapGestureRecognizer
-//        addGestureRecognizer(tapGestureRecognizer)
-//    }
-//
-//    private func configureYouTubeVideo(videoId: String) {
-//        // TODO ADD Youtube Player to the element to be able to preload the elements
-//        let videoPlayerView = YTPlayerView()
-//        embedPlayerView = videoPlayerView
-//        videoPlayerView.load(withVideoId: videoId)
-//
-//        addSubview(videoPlayerView)
-//        videoPlayerView.snp.makeConstraints { (make) in
-//            make.leading.trailing.top.bottom.equalToSuperview()
-//        }
-//    }
-//
-//    @IBAction func togglePlay() {
-//        if let player = videoPlayerLayer?.player {
-//            player.rate == 1 ? player.pause() : player.play()
-//        }
-//    }
-//
-// }
+import AVKit
+import Foundation
+import Library
+import UIKit
+import youtube_ios_player_helper
+
+class VideoViewElementCell: UITableViewCell {
+  private var videoPlayerLayer: AVPlayerLayer?
+  private var embedPlayerView: YTPlayerView?
+  private var videoPlayerGestureRecognizer: UIGestureRecognizer?
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+    self.initialize()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+    self.initialize()
+  }
+
+  override func prepareForReuse() {
+    self.videoPlayerLayer?.player?.replaceCurrentItem(with: nil)
+    self.videoPlayerLayer?.removeFromSuperlayer()
+    self.videoPlayerLayer = nil
+
+    self.embedPlayerView?.stopVideo()
+    self.embedPlayerView?.removeFromSuperview()
+    self.embedPlayerView = nil
+
+    if let videoPlayerGestureRecognizer = videoPlayerGestureRecognizer {
+      removeGestureRecognizer(videoPlayerGestureRecognizer)
+    }
+  }
+
+  func initialize() {
+    backgroundColor = UIColor.gray
+  }
+
+  func configureWith(value: VideoViewElement) {
+    // The first will play the high quality video (0: high - 1: base)
+    // TODO: Be able to remember where the user stopped playing the video
+    guard let urlString = value.sourceUrls.first, let url = URL(string: urlString) else {
+      return
+    }
+
+    // Example Urls
+    // https://youtu.be/DEG1qicbAmU
+    // https://www.youtube.com/watch?v=i0uU5IV_4ZA
+    if url.host == "youtu.be", let videoId = url.pathComponents.last {
+      self.configureYouTubeVideo(videoId: videoId)
+    } else if url.host == "www.youtube.com", let queryItems = URLComponents(string: urlString)?.queryItems {
+      let videoIdItem = queryItems.first { $0.name == "v" }
+      if let videoId = videoIdItem?.value {
+        self.configureYouTubeVideo(videoId: videoId)
+      }
+    } else {
+      self.configureVideo(videoUrl: url)
+    }
+  }
+
+  private func configureVideo(videoUrl: URL) {
+    let videoPlayer = AVPlayer(url: videoUrl)
+    videoPlayerLayer = AVPlayerLayer(player: videoPlayer)
+
+    if let videoPlayerLayer = videoPlayerLayer {
+      videoPlayerLayer.frame = bounds
+      layer.addSublayer(videoPlayerLayer)
+    }
+
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.togglePlay))
+    self.videoPlayerGestureRecognizer = tapGestureRecognizer
+    addGestureRecognizer(tapGestureRecognizer)
+  }
+
+  private func configureYouTubeVideo(videoId: String) {
+    // TODO: ADD Youtube Player to the element to be able to preload the elements
+    let videoPlayerView = YTPlayerView()
+    embedPlayerView = videoPlayerView
+    videoPlayerView.load(withVideoId: videoId)
+
+    addSubview(videoPlayerView)
+    videoPlayerView.snp.makeConstraints { make in
+      make.leading.trailing.top.bottom.equalToSuperview()
+    }
+  }
+
+  @IBAction func togglePlay() {
+    if let player = videoPlayerLayer?.player {
+      player.rate == 1 ? player.pause() : player.play()
+    }
+  }
+}
--- Library/Project Page Campaign HTML Parser/View Elements/TextViewElement.swift
+++ Library/Project Page Campaign HTML Parser/View Elements/TextViewElement.swift
@@ -3,97 +3,99 @@ import UIKit
 struct TextViewElement: ViewElement {
   var components: [TextComponent]
 
-  func attributedText(bodySize: Int,
-                      headerSize: Int) -> NSAttributedString {
-    let completedAttributedText = NSMutableAttributedString()
-    let bulletPrefix = "•  "
+  /** TODO: Test in https://kickstarter.atlassian.net/browse/NTV-340
+   func attributedText(bodySize: Int,
+                       headerSize: Int) -> NSAttributedString {
+     let completedAttributedText = NSMutableAttributedString()
+     let bulletPrefix = "•  "
 
-    for textItem in self.components {
-      var componentText = textItem.text
-      // TODO: This will be external URL attached to a tap gesture on the label...need to connect this to the label accurately.
-      let href = textItem.link ?? ""
+     for textItem in self.components {
+       var componentText = textItem.text
+       // TODO: This will be external URL attached to a tap gesture on the label...need to connect this to the label accurately.
+       let href = textItem.link ?? ""
 
-      // - The end list style will be applied only to the LAST child of the LI element
-      if textItem.styles.contains(.listEnd) {
-        let updatedLastListBulletText = bulletPrefix + componentText + "\n"
+       // - The end list style will be applied only to the LAST child of the LI element
+       if textItem.styles.contains(.listEnd) {
+         let updatedLastListBulletText = bulletPrefix + componentText + "\n"
 
-        componentText = updatedLastListBulletText
-      }
+         componentText = updatedLastListBulletText
+       }
 
-      let currentAttributedText = NSMutableAttributedString(string: componentText)
-      let fullRange = (componentText as NSString).localizedStandardRange(of: componentText)
-      let baseFontSize = CGFloat(bodySize)
-      let baseFont = UIFont.ksr_body(size: baseFontSize)
-      let headerFontSize = CGFloat(headerSize)
-      let headerFont = UIFont.ksr_body(size: headerFontSize).bolded
+       let currentAttributedText = NSMutableAttributedString(string: componentText)
+       let fullRange = (componentText as NSString).localizedStandardRange(of: componentText)
+       let baseFontSize = CGFloat(bodySize)
+       let baseFont = UIFont.ksr_body(size: baseFontSize)
+       let headerFontSize = CGFloat(headerSize)
+       let headerFont = UIFont.ksr_body(size: headerFontSize).bolded
 
-      textItem.styles.forEach { textStyleType in
-        switch textStyleType {
-        case .bold:
-          let attributes = [
-            NSAttributedString.Key.font: baseFont.bolded,
-            NSAttributedString.Key.foregroundColor: UIColor.ksr_support_700
-          ]
+       textItem.styles.forEach { textStyleType in
+         switch textStyleType {
+         case .bold:
+           let attributes = [
+             NSAttributedString.Key.font: baseFont.bolded,
+             NSAttributedString.Key.foregroundColor: UIColor.ksr_support_700
+           ]
 
-          currentAttributedText.addAttributes(attributes, range: fullRange)
-        case .emphasis:
-          let attributes = [
-            NSAttributedString.Key.font: baseFont.italicized,
-            NSAttributedString.Key.foregroundColor: UIColor.ksr_support_700
-          ]
+           currentAttributedText.addAttributes(attributes, range: fullRange)
+         case .emphasis:
+           let attributes = [
+             NSAttributedString.Key.font: baseFont.italicized,
+             NSAttributedString.Key.foregroundColor: UIColor.ksr_support_700
+           ]
 
-          currentAttributedText.addAttributes(attributes, range: fullRange)
-        case .link:
-          let attributes: String.Attributes = [
-            .font: UIFont.ksr_subhead(size: baseFontSize),
-            .underlineStyle: NSUnderlineStyle.single.rawValue,
-            .foregroundColor: UIColor.ksr_create_700
-          ]
+           currentAttributedText.addAttributes(attributes, range: fullRange)
+         case .link:
+           let attributes: String.Attributes = [
+             .font: UIFont.ksr_subhead(size: baseFontSize),
+             .underlineStyle: NSUnderlineStyle.single.rawValue,
+             .foregroundColor: UIColor.ksr_create_700
+           ]
 
-          currentAttributedText.addAttributes(attributes, range: fullRange)
-        case .list:
-          let defaultAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.ksr_callout()
-          ]
+           currentAttributedText.addAttributes(attributes, range: fullRange)
+         case .list:
+           let defaultAttributes: [NSAttributedString.Key: Any] = [
+             .font: UIFont.ksr_callout()
+           ]
 
-          let paragraphStyle = NSMutableParagraphStyle()
-          paragraphStyle.headIndent = (bulletPrefix as NSString).size(withAttributes: defaultAttributes).width
-          paragraphStyle.paragraphSpacing = Styles.grid(1)
+           let paragraphStyle = NSMutableParagraphStyle()
+           paragraphStyle.headIndent = (bulletPrefix as NSString).size(withAttributes: defaultAttributes).width
+           paragraphStyle.paragraphSpacing = Styles.grid(1)
 
-          let updatedTextWithBulletAndNewLine = bulletPrefix + componentText + "\n"
+           let updatedTextWithBulletAndNewLine = bulletPrefix + componentText + "\n"
 
-          let fullRangeWithBulletAndNewline = (updatedTextWithBulletAndNewLine as NSString)
-            .localizedStandardRange(of: updatedTextWithBulletAndNewLine)
+           let fullRangeWithBulletAndNewline = (updatedTextWithBulletAndNewLine as NSString)
+             .localizedStandardRange(of: updatedTextWithBulletAndNewLine)
 
-          let bulletedListAttributes: [NSAttributedString.Key: Any] = [.paragraphStyle: paragraphStyle]
+           let bulletedListAttributes: [NSAttributedString.Key: Any] = [.paragraphStyle: paragraphStyle]
 
-          let updatedCurrentAttributedText =
-            NSMutableAttributedString(string: updatedTextWithBulletAndNewLine)
+           let updatedCurrentAttributedText =
+             NSMutableAttributedString(string: updatedTextWithBulletAndNewLine)
 
-          updatedCurrentAttributedText
-            .addAttributes(bulletedListAttributes, range: fullRangeWithBulletAndNewline)
+           updatedCurrentAttributedText
+             .addAttributes(bulletedListAttributes, range: fullRangeWithBulletAndNewline)
 
-        case .header:
-          let attributes = [
-            NSAttributedString.Key.font: headerFont,
-            NSAttributedString.Key.foregroundColor: UIColor.ksr_support_700
-          ]
+         case .header:
+           let attributes = [
+             NSAttributedString.Key.font: headerFont,
+             NSAttributedString.Key.foregroundColor: UIColor.ksr_support_700
+           ]
 
-          currentAttributedText.addAttributes(attributes, range: fullRange)
-        default:
-          let baseFontAttributes = [
-            NSAttributedString.Key.font: baseFont,
-            NSAttributedString.Key.foregroundColor:
-              UIColor.ksr_support_700
-          ]
+           currentAttributedText.addAttributes(attributes, range: fullRange)
+         default:
+           let baseFontAttributes = [
+             NSAttributedString.Key.font: baseFont,
+             NSAttributedString.Key.foregroundColor:
+               UIColor.ksr_support_700
+           ]
 
-          currentAttributedText.addAttributes(baseFontAttributes, range: fullRange)
-        }
+           currentAttributedText.addAttributes(baseFontAttributes, range: fullRange)
+         }
 
-        completedAttributedText.append(currentAttributedText)
-      }
-    }
+         completedAttributedText.append(currentAttributedText)
+       }
+     }
 
-    return completedAttributedText
-  }
+     return completedAttributedText
+   }
+   */
 }
