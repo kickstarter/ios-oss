a6e6ff92bd780940d7e938bf9b35456fd565b967
Mubarak Sadoon <msadoon@gmail.com>
2022-04-18 14:47:49 -0400

    updated apollo version in npm download script

--- bin/apollo-schema-download.sh
+++ bin/apollo-schema-download.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-npm install -D apollo
-npx apollo schema:download --header="Content-Type: application/json" --header="User-Agent: Kickstarter iOS" --endpoint=https://staging.kickstarter.com/graph KsApi/graphql-schema.json
+npm install -D apollo@2.11.1
+npx apollo schema:download --header="Content-Type: application/json" --header="User-Agent: Kickstarter iOS" --endpoint="https://staging.kickstarter.com/graph" KsApi/graphql-schema.json
