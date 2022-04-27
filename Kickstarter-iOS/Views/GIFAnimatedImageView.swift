c0bdd3b18051e9b23b626b5512ce3f2a8b38ae58
Mubarak Sadoon <msadoon@gmail.com>
2022-04-27 16:56:59 -0400

    removed unecessary variables and functions from GIFAnimatedImageView

--- Kickstarter-iOS/Views/GIFAnimatedImageView.swift
+++ Kickstarter-iOS/Views/GIFAnimatedImageView.swift
@@ -1,15 +1,3 @@
 import Kingfisher
-import UIKit
 
-final class GIFAnimatedImageView: AnimatedImageView {
-  private var scaledImageSize: CGSize = .zero
-
-//  override var intrinsicContentSize: CGSize {
-//    self.scaledImageSize
-//  }
-
-  func setImageWith(_ image: UIImage?, scaledImageSize _: CGSize) {
-    self.image = image
-    // self.scaledImageSize = scaledImageSize
-  }
-}
+final class GIFAnimatedImageView: AnimatedImageView {}
