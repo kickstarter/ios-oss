926f1ec9adce85b50183634ceb474a4d9d85b412
Mubarak Sadoon <msadoon@gmail.com>
2022-06-27 12:50:08 -0400

    remove KickstarterUITests scheme and target, moved tests from KickstarterUITests to UIViewController+URLTests. Next step is update ci.

--- Library/UIViewController+URLTests.swift
+++ Library/UIViewController+URLTests.swift
@@ -42,4 +42,40 @@ final class UIViewControllerURLTests: TestCase {
       XCTAssertFalse(mockApplication.openUrlWasCalled)
     }
   }
+  
+  func testGoToHttpScheme() {
+    guard let url = URL(string: "http://www.kickstarter.com") else {
+      XCTFail("URL cannot be nil")
+      return
+    }
+
+    let vc = UIViewController()
+
+    let window = UIWindow()
+    window.rootViewController = vc
+    window.makeKeyAndVisible()
+
+    vc.goTo(url: url)
+
+    XCTAssertTrue(vc.presentedViewController is SFSafariViewController)
+  }
+
+  func testGoToHttpsScheme() {
+    guard let url = URL(string: "https://www.kickstarter.com") else {
+      XCTFail("URL cannot be nil")
+      return
+    }
+
+    let vc = UIViewController()
+
+    let window = UIWindow()
+    window.rootViewController = vc
+    window.makeKeyAndVisible()
+
+    vc.goTo(url: url)
+
+    XCTAssertNotNil(vc.presentedViewController)
+
+    XCTAssertTrue(vc.presentedViewController is SFSafariViewController)
+  }
 }
