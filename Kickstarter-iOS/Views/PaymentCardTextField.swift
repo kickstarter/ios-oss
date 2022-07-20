b27eb2416d98d71f62678adb3a0a586e091247b6
Mubarak Sadoon <msadoon@gmail.com>
2022-07-20 11:54:28 -0400

    wrapped STPPaymentCardTextField in our own class so Settings.storyboard isn't dependant on Stripe directly.

--- Kickstarter-iOS/Views/PaymentCardTextField.swift
+++ Kickstarter-iOS/Views/PaymentCardTextField.swift
@@ -1,9 +1,3 @@
-//
-//  File.swift
-//  Kickstarter-Framework-iOS
-//
-//  Created by Mubarak Sadoon on 2022-07-20.
-//  Copyright © 2022 Kickstarter. All rights reserved.
-//
+import Stripe
 
-import Foundation
+public final class PaymentCardTextField: STPPaymentCardTextField { }
