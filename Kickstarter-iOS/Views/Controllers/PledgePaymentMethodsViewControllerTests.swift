6e0c1b0433a3ba8b22e7c9622edb35a47a3dae64
Mubarak Sadoon <msadoon@gmail.com>
2022-08-24 12:53:50 -0400

    updated view tests, refactored logic around show load indicator to be simpler more manageable.

---
+++ Kickstarter-iOS/Views/Controllers/PledgePaymentMethodsViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  PledgePaymentMethodsViewControllerTests.swift
+//  Kickstarter-Framework-iOSTests
+//
+//  Created by Mubarak Sadoon on 2022-08-24.
+//  Copyright © 2022 Kickstarter. All rights reserved.
+//
+
+import Foundation
