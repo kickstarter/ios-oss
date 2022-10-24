a51d40f696f52928249c9b87d31532f1c08bdeea
Mubarak Sadoon <msadoon@gmail.com>
2022-10-24 17:39:06 -0400

    base view in SwiftUI for ChangeEmailViewController
    Still missing navigation bar content, but it is launching inside UIHostingViewController, so that's good news!

--- Kickstarter-iOS/Features/ChangeEmail/Controller/ChangeEmailView.swift
+++ Kickstarter-iOS/Features/ChangeEmail/Controller/ChangeEmailView.swift
@@ -1,23 +1,82 @@
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct ChangeEmailView: View {
   @State var emailText: String
-  
-    var body: some View {
-      NavigationView {
-        List {
-          HStack {
-            Text("Current Email")
-            Spacer()
-            TextField("title", text: $emailText)
+  @State private var newEmailText = ""
+  @State private var password = ""
+  @Environment(\.defaultMinListRowHeight) var minListRow
+
+  init(emailText: String) {
+    self.emailText = emailText
+  }
+
+  var body: some View {
+    List {
+      Group {
+        VStack(spacing: 0) {
+          Color(.ksr_support_100)
+            .frame(width: .infinity, height: minListRow, alignment: .center)
+
+          ZStack(alignment: .center) {
+            entryField(
+              textLabelText: "Current email",
+              textFieldText: $emailText,
+              placeholder: "Current email"
+            )
+            Color(.ksr_cell_separator).frame(width: .infinity, height: 1)
+              .offset(x: 0, y: minListRow / 2)
+              .alignmentGuide(HorizontalAlignment.center) { dimensions in
+                dimensions[HorizontalAlignment.center]
+              }
+          }
+
+          Color(.ksr_support_100)
+            .frame(width: .infinity, height: minListRow, alignment: .center)
+
+          ZStack(alignment: .center) {
+            entryField(textLabelText: "New email", textFieldText: $newEmailText, placeholder: "Email address")
+          }
+
+          ZStack(alignment: .center) {
+            entryField(textLabelText: "Current password", textFieldText: $password, placeholder: "Password")
+            Color(.ksr_cell_separator).frame(width: .infinity, height: 1)
+              .offset(x: 0, y: minListRow / 2)
+              .alignmentGuide(HorizontalAlignment.center) { dimensions in
+                dimensions[HorizontalAlignment.center]
+              }
           }
         }
       }
+      .listRowSeparator(.hidden)
+      .listRowInsets(EdgeInsets())
     }
-}
+    .background(Color(.ksr_support_100))
+    .listStyle(.plain)
+  }
 
-struct ChangeEmailView_Previews: PreviewProvider {
-    static var previews: some View {
-      ChangeEmailView(emailText: "msadoon@gmail.com")
+  @ViewBuilder
+  private func entryField(textLabelText: String,
+                          textFieldText: Binding<String>,
+                          placeholder: String) -> some View {
+    HStack {
+      Text(textLabelText)
+        .frame(
+          maxWidth: .infinity,
+          alignment: .leading
+        )
+        .font(Font(UIFont.ksr_body()))
+        .foregroundColor(Color(.ksr_support_700))
+      Spacer()
+      TextField("", text: textFieldText, prompt: Text(placeholder))
+        .frame(
+          maxWidth: .infinity,
+          alignment: .trailing
+        )
+        .font(Font(UIFont.ksr_body()))
+        .foregroundColor(Color(.ksr_support_700))
+        .multilineTextAlignment(.trailing)
     }
+    .padding(12)
+  }
 }
