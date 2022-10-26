763105afd703dfb8f119ef056e15eb0f3718916d
Scott Clampet <scottclampet@18045-KSR.local>
2022-10-26 14:23:28 -0400

    formatting

--- Kickstarter-iOS/Features/ThanksProjects/Controller/ThanksViewController.swift
+++ Kickstarter-iOS/Features/ThanksProjects/Controller/ThanksViewController.swift
@@ -220,7 +220,8 @@ internal final class ThanksViewController: UIViewController, UITableViewDelegate
   }
 
   fileprivate func showRatingAlert() {
-    if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+    if let scene = UIApplication.shared.connectedScenes
+      .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
       SKStoreReviewController.requestReview(in: scene)
     }
   }
