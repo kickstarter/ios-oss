77e4fc7578603568b7de496c52e1c32ccf4e9fd9
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2023-01-17 11:47:56 -0500

    [WEB-669] Facebook Conversions API Feature Flag (#1766)
    * add facebook conversions api feature flag
    
    * add facebook conversions api feature flag
    
    * fix test

--- Library/OptimizelyFeature.swift
+++ Library/OptimizelyFeature.swift
@@ -3,6 +3,7 @@ import Foundation
 public enum OptimizelyFeature: String, CaseIterable {
   case commentFlaggingEnabled = "ios_comment_threading_comment_flagging"
   case consentManagementDialogEnabled = "ios_consent_management_dialog"
+  case facebookConversionsAPI = "ios_facebook_conversions_api"
   case facebookLoginDeprecationEnabled = "ios_facebook_deprecation"
   case paymentSheetEnabled = "ios_payment_sheet"
   case projectPageStoryTabEnabled = "project_page_v2_story"
@@ -15,6 +16,7 @@ extension OptimizelyFeature: CustomStringConvertible {
     switch self {
     case .commentFlaggingEnabled: return "Comment Flagging"
     case .consentManagementDialogEnabled: return "Consent Management Dialog"
+    case .facebookConversionsAPI: return "Facebook Conversions API"
     case .facebookLoginDeprecationEnabled: return "Facebook Login Deprecation"
     case .paymentSheetEnabled: return "Payment Sheet"
     case .projectPageStoryTabEnabled: return "Project Page Story Tab"
