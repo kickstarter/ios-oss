01b4f0edecba877d8b5f8c57e767cc2e17979ed4
Mubarak Sadoon <msadoon@gmail.com>
2023-02-01 19:08:46 -0500

    formatting

--- Kickstarter-iOS/Features/LoginTout/Controller/LoginToutViewController.swift
+++ Kickstarter-iOS/Features/LoginTout/Controller/LoginToutViewController.swift
@@ -200,13 +200,13 @@ public final class LoginToutViewController: UIViewController, MFMailComposeViewC
         AppEnvironment.login(accessTokenEnv)
 
         /** FIXME: Not releasing this yet - re-test functionality as it failed regression testing on release-5.6.1 (see confluence regression testing document)
-        guard featureFacebookLoginDeprecationEnabled(),
-          let needsPassword = accessTokenEnv.user.needsPassword,
-          needsPassword else {
-          strongSelf.pushSetYourPasswordViewController()
+         guard featureFacebookLoginDeprecationEnabled(),
+           let needsPassword = accessTokenEnv.user.needsPassword,
+           needsPassword else {
+           strongSelf.pushSetYourPasswordViewController()
 
-          return
-        }
+           return
+         }
          */
 
         strongSelf.viewModel.inputs.environmentLoggedIn()
