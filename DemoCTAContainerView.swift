01d99f0d663d7f4e57145ea88f6042a6dc0c883e
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2023-09-06 12:31:08 -0500

    remove button title feedback

--- DemoCTAContainerView.swift
+++ DemoCTAContainerView.swift
@@ -89,7 +89,6 @@ final class DemoCTAContainerView: UIView {
 
     _ = self.submitButton
       |> greenButtonStyle
-      |> UIButton.lens.title(for: .normal) %~ { _ in Strings.two_factor_buttons_submit() }
 
     _ = self.rootStackView
       |> rootStackViewStyle
