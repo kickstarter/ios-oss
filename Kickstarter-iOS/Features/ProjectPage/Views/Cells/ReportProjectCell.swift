2b0b91614aa672e156ab1093ff2e9533aa921288
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2023-09-21 22:35:04 -0500

    create ReportProjectCell to display swiftui view
    * UITableView cell will be added to the Project Page Table View
    * Used to display a new swiftui view for the report this project label and already reported label

---
+++ Kickstarter-iOS/Features/ProjectPage/Views/Cells/ReportProjectCell.swift
@@ -0,0 +1,29 @@
+import Library
+import SwiftUI
+import UIKit
+
+internal final class ReportProjectCell: UITableViewCell, ValueCell {
+  internal func configureWith(value projectFlagged: Bool) {
+    if #available(iOS 15.0, *) {
+      let hostingController = UIHostingController(rootView: ReportThisProjectLabelView(flagged: projectFlagged))
+
+      hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+      hostingController.view.backgroundColor = .clear
+      
+      self.contentView.addSubview(hostingController.view)
+
+      NSLayoutConstraint.activate([
+        hostingController.view.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 12),
+        hostingController.view.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+        hostingController.view.leadingAnchor
+          .constraint(equalTo: self.contentView.leadingAnchor, constant: 12),
+        hostingController.view.trailingAnchor
+          .constraint(equalTo: self.contentView.trailingAnchor, constant: -12)
+      ])
+    }
+
+    self.selectionStyle = .none
+
+    self.setNeedsLayout()
+  }
+}
---
+++ Kickstarter-iOS/Features/ProjectPage/Views/ReportThisProjectLabelView.swift
@@ -0,0 +1,63 @@
+import Library
+import SwiftUI
+
+@available(iOS 15.0, *)
+struct ReportThisProjectLabelView: View {
+  let flagged: Bool
+
+  var body: some View {
+    if flagged {
+      EmptyView()
+    } else {
+      HStack {
+        Text(Strings.Report_this_project_to())
+          .font(Font(UIFont.ksr_subhead()))
+
+        Spacer()
+
+        Image("chevron-right")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 10, height: 10)
+      }
+      .padding(10)
+    }
+  }
+
+  private struct AlreadyReportedView: View {
+    
+    var body: some View {
+      HStack(alignment: .top, spacing: 10) {
+        Image("info")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 20, height: 20)
+          .foregroundColor(Color(.ksr_support_500))
+
+//        Text(
+//          html: Strings.It_looks(
+//            our_rules: HelpType.prohibitedItems
+//              .url(withBaseUrl: AppEnvironment.current.apiService.serverConfig.webBaseUrl)?
+//              .absoluteString ?? "",
+//            community_guidelines: HelpType.prohibitedItems
+//              .url(withBaseUrl: AppEnvironment.current.apiService.serverConfig.webBaseUrl)?
+//              .absoluteString ?? ""
+//          ),
+//          with: ReportProjectHyperLinkType.communityGuidelines.stringLiteral()
+//        )
+        Text("")
+        .font(Font(UIFont.ksr_caption1()))
+      }
+      .padding()
+      .background(Color(.ksr_support_100))
+      .cornerRadius(15)
+    }
+  }
+}
+
+@available(iOS 15.0, *)
+struct ReportProjectView_Previews: PreviewProvider {
+  static var previews: some View {
+    ReportThisProjectLabelView(flagged: false)
+  }
+}
