18549e8fbc69c103d3e9ce0f78d0c7d644648655
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2023-09-27 11:39:17 -0500

    set accessibility traits and cell's traitCollection

--- Kickstarter-iOS/Features/ProjectPage/Views/Cells/ReportProjectCell.swift
+++ Kickstarter-iOS/Features/ProjectPage/Views/Cells/ReportProjectCell.swift
@@ -1,26 +1,47 @@
 import Library
+import Prelude
 import SwiftUI
 import UIKit
 
 internal final class ReportProjectCell: UITableViewCell, ValueCell {
-  let isIpad = AppEnvironment.current.device.userInterfaceIdiom == .pad
-
   internal func configureWith(value projectFlagged: Bool) {
+    self.setupTableViewCellStyle(projectFlagged: projectFlagged)
     self.setupReportProjectLabelView(projectFlagged: projectFlagged)
-
-    self.selectionStyle = .none
-    self.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
-    self.setNeedsLayout()
   }
 
   internal override func layoutSubviews() {
     super.layoutSubviews()
   }
 
+  internal override func bindStyles() {
+    super.bindStyles()
+
+    self.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+    self.setNeedsLayout()
+  }
+
+  // MARK: - Private Methods
+
+  private func setupTableViewCellStyle(projectFlagged: Bool) {
+    let accessibilityTraits = projectFlagged
+      ? UIAccessibilityTraits.staticText
+      : UIAccessibilityTraits.button
+
+    _ = self
+      |> baseTableViewCellStyle()
+      |> ReportProjectCell.lens.accessibilityTraits .~ accessibilityTraits
+      |> ReportProjectCell.lens.contentView.layoutMargins %~~ { _, cell in
+        cell.traitCollection.isRegularRegular
+          ? .init(topBottom: Styles.gridHalf(5), leftRight: Styles.grid(16))
+          : .init(topBottom: Styles.gridHalf(5), leftRight: Styles.gridHalf(7))
+      }
+  }
+
   private func setupReportProjectLabelView(projectFlagged: Bool) {
     if #available(iOS 15.0, *) {
       DispatchQueue.main.async {
-        let hostingController = UIHostingController(rootView: ReportProjectLabelView(flagged: projectFlagged))
+        let hostingController =
+          UIHostingController(rootView: ReportProjectLabelView(flagged: projectFlagged))
 
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
         hostingController.view.backgroundColor = .clear
@@ -28,19 +49,13 @@ internal final class ReportProjectCell: UITableViewCell, ValueCell {
         self.contentView.addSubview(hostingController.view)
 
         NSLayoutConstraint.activate([
-          hostingController.view.topAnchor
-            .constraint(equalTo: self.contentView.topAnchor, constant: self.isIpad ? Styles.grid(5) : 12),
-          hostingController.view.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+          hostingController.view.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 12),
+          hostingController.view.bottomAnchor
+            .constraint(equalTo: self.contentView.bottomAnchor, constant: -12),
           hostingController.view.leadingAnchor
-            .constraint(
-              equalTo: self.contentView.leadingAnchor,
-              constant: self.isIpad ? Styles.grid(16) : 12
-            ),
+            .constraint(equalTo: self.contentView.leadingAnchor, constant: 12),
           hostingController.view.trailingAnchor
-            .constraint(
-              equalTo: self.contentView.trailingAnchor,
-              constant: self.isIpad ? -Styles.grid(16) : -12
-            )
+            .constraint(equalTo: self.contentView.trailingAnchor, constant: -12)
         ])
       }
     }
