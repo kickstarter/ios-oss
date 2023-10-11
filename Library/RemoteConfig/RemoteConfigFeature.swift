d90d518467da3587dde362489899bebc97f7fbf4
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2023-10-11 12:29:25 -0500

    [MBL-993] Dark Mode Feature Flag (#1865)
    * add new feature flag
    
    * add install-rosetta step to deploy_alpha

--- Library/RemoteConfig/RemoteConfigFeature.swift
+++ Library/RemoteConfig/RemoteConfigFeature.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public enum RemoteConfigFeature: String, CaseIterable {
   case consentManagementDialogEnabled = "consent_management_dialog"
+  case darkModeEnabled = "dark_mode"
   case facebookLoginInterstitialEnabled = "facebook_interstitial"
   case reportThisProjectEnabled = "report_this_project"
   case useOfAIProjectTab = "use_of_ai_project_tab"
@@ -11,6 +12,7 @@ extension RemoteConfigFeature: CustomStringConvertible {
   public var description: String {
     switch self {
     case .consentManagementDialogEnabled: return "Consent Management Dialog"
+    case .darkModeEnabled: return "Dark Mode"
     case .facebookLoginInterstitialEnabled: return "Facebook Login Interstitial"
     case .reportThisProjectEnabled: return "Report This Project"
     case .useOfAIProjectTab: return "Use of AI Project Tab"
