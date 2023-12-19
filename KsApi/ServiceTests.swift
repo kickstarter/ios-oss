341a12f2786a842d47373d67ce45f819bc52c129
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2023-12-19 10:56:49 -0500

    MBL-1016: Implement API V1 request method in Combine (#1904)
    * MBL-1016: Implement base request method for API V1 using Combine
    
    * Delete unused method in reactive NSURLSession extension
    
    * Deleted unused method in Service+DecodeHelpers
    
    * Add test for new decodeModel method to ServiceTests
    
    * Add a test for comparing the Combine and ReactiveSwift versions of Service.request
    
    * #1904 - Rename decodeModel to decodeModelToSignal and decodeModelToResult
    
    * #1904: Rename addFileUploadToRequest to requestWithFileUpload

--- KsApi/ServiceTests.swift
+++ KsApi/ServiceTests.swift
@@ -67,7 +67,7 @@ final class ServiceTests: XCTestCase {
     let expectedResult = try! JSONDecoder().decode(MySwiftModel.self, from: jsonData.data(using: .utf8)!)
 
     let model: SignalProducer<MySwiftModel, ErrorEnvelope> = Service()
-      .decodeModel(jsonData.data(using: .utf8)!)
+      .decodeModelToSignal(jsonData.data(using: .utf8)!)
     let result = try! model.single()?.get()
     XCTAssertEqual(result, expectedResult)
   }
@@ -84,7 +84,7 @@ final class ServiceTests: XCTestCase {
     """
 
     let model: SignalProducer<MySwiftModel, ErrorEnvelope> = Service()
-      .decodeModel(jsonData.data(using: .utf8)!)
+      .decodeModelToSignal(jsonData.data(using: .utf8)!)
     XCTAssertThrowsError(try model.single()?.get(), "wrong key should throw an error")
   }
 
@@ -100,7 +100,7 @@ final class ServiceTests: XCTestCase {
     """
 
     let model: SignalProducer<MySwiftModel, ErrorEnvelope> = Service()
-      .decodeModel(jsonData.data(using: .utf8)!)
+      .decodeModelToSignal(jsonData.data(using: .utf8)!)
     XCTAssertThrowsError(try model.single()?.get(), "wrong key should throw an error")
   }
 
@@ -115,60 +115,119 @@ final class ServiceTests: XCTestCase {
     }
     """
     let data = jsonData.data(using: .utf8)!
-    let model: SignalProducer<MySwiftModel?, ErrorEnvelope> = Service().decodeModel(data: data)
+    let model: SignalProducer<MySwiftModel?, ErrorEnvelope> = Service().decodeModelToSignal(data: data)
     let result = try! model.single()?.get()
     XCTAssertNil(result)
   }
 
-  func test_Array_SwiftDecodeModel_ValidModel() {
+  func testSwiftDecodeModel_ValidModel_Combine() {
+    let expectedResult = MySwiftModel(
+      array: ["string1", "string2"],
+      bool: true,
+      dict: ["key1": "value1", "key2": "value2"],
+      id: 5,
+      name: "Swift Name"
+    )
+
     let jsonData: String = """
-    [
-       {
-          "array":[
-             "string1",
-             "string2"
-          ],
-          "bool":true,
-          "dict":{
-             "key1":"value1",
-             "key2":"value2"
-          },
-          "id":5,
-          "name":"Swift Name"
-       },
-       {
-          "array":[
-             "string1",
-             "string2"
-          ],
-          "bool":true,
-          "dict":{
-             "key1":"value1",
-             "key2":"value2"
-          },
-          "id":6,
-          "name":"Swift Name"
+    {
+        "array": ["string1", "string2"],
+        "bool": true,
+        "dict": {"key1": "value1", "key2": "value2"},
+        "id": 5,
+        "name": "Swift Name"
+    }
+    """
+
+    let data = jsonData.data(using: .utf8)!
+    let result: Result<MySwiftModel?, ErrorEnvelope> = Service()
+      .decodeModelToResult(data: data, ofType: MySwiftModel.self)
+
+    switch result {
+    case let .success(decodedModel):
+      if let decodedModelUnwrapped = decodedModel {
+        XCTAssertEqual(expectedResult, decodedModelUnwrapped)
+      } else {
+        XCTFail("Decoded model is nil")
+      }
+    case .failure: XCTFail("Failed to decode model")
+    }
+  }
+
+  func testSwiftDecodeModel_InvalidModel_Combine() {
+    let jsonData: String = """
+    {
+        "notAValidKey": "foo"
+    }
+    """
+
+    let data = jsonData.data(using: .utf8)!
+    let result: Result<MySwiftModel?, ErrorEnvelope> = Service()
+      .decodeModelToResult(data: data, ofType: MySwiftModel.self)
+
+    switch result {
+    case .success: XCTFail("Decode should have failed.")
+    case let .failure(error): XCTAssertNotNil(error)
+    }
+  }
+
+  /*
+   These two tests shouldn't actually *run* in our testing environment, because we don't want to make real network requests
+   in our real tests. But it's a useful little utility for comparing the Combine and ReactiveSwift version of the
+   API V1 call Request.
+   */
+
+  /*
+   func testCompareRequest_Successful_RAC_vs_Combine() {
+     let service = Service(serverConfig: ServerConfig.production)
+     var project = Project.template
+     project.id = 152885112
+
+     let rac_publisher = service.fetchProject(project: project)
+     let combine_publisher = service.fetchProject_combine(project: project)
+
+     let combine_observer = CombineTestObserver<Project, ErrorEnvelope>()
+     combine_observer.observe(combine_publisher)
+
+     _ = XCTWaiter.wait(for: [expectation(description: "Wait 10 seconds for the server call.")], timeout: 10.0)
+
+     let r1 = rac_publisher.first()
+     let r2 = combine_observer.events.first
+
+     if case let .success(p1) = r1 {
+       if case let .value(p2) = r2 {
+         XCTAssertEqual(p1, p2)
+         return
        }
-    ]
-    """
+     }
 
-    let expectedResult = try! JSONDecoder().decode([MySwiftModel].self, from: jsonData.data(using: .utf8)!)
+     XCTFail()
+   }
 
-    let model: SignalProducer<[MySwiftModel], ErrorEnvelope> = Service()
-      .decodeModels(jsonData.data(using: .utf8)!)
-    let result = try! model.single()?.get()
-    XCTAssertEqual(result, expectedResult)
-  }
+   func testCompareRequest_Failure_RAC_vs_Combine() {
+     let service = Service(serverConfig: ServerConfig.production)
+     var project = Project.template
+     project.id = 1 //Not a real project ID
 
-  func test_Array_SwiftDecodeModel_EmptyArray() {
-    let jsonData: String = """
-    [
-    ]
-    """
+     let rac_publisher = service.fetchProject(project: project)
+     let combine_publisher = service.fetchProject_combine(project: project)
 
-    let model: SignalProducer<[MySwiftModel], ErrorEnvelope> = Service()
-      .decodeModels(jsonData.data(using: .utf8)!)
-    let result = try! model.single()?.get()
-    XCTAssertEqual(result?.count, 0)
-  }
+     let combine_observer = CombineTestObserver<Project, ErrorEnvelope>()
+     combine_observer.observe(combine_publisher)
+
+     _ = XCTWaiter.wait(for: [expectation(description: "Wait 10 seconds for the server call.")], timeout: 10.0)
+
+     let r1 = rac_publisher.first()
+     let r2 = combine_observer.events.first
+
+     if case let .failure(e1) = r1 {
+       if case let .error(e2) = r2 {
+         XCTAssertEqual(e1.errorMessages, e2.errorMessages)
+         return
+       }
+     }
+
+     XCTFail()
+   }
+   */
 }
