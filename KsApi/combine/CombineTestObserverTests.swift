2342cc39a3d797e96b28a0758b1f74ed7549533d
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2024-01-29 16:23:20 -0500

    MBL-1017: Add features to CombineTestObserver (#1920)
    * MBL-1017: Flesh out CombineTestObserver so it's roughly equivalent to TestObserver
    
    * Add a bit more coverage to appease CodeCov
    
    * Change 'we' to 'I' language in assert

---
+++ KsApi/combine/CombineTestObserverTests.swift
@@ -0,0 +1,64 @@
+import Combine
+import XCTest
+
+final class ConcreteError: Error {
+  var message: String
+
+  init(message: String) {
+    self.message = message
+  }
+}
+
+final class CombineTestObserverTests: XCTestCase {
+  var publisher = PassthroughSubject<Bool, ConcreteError>()
+  var observer = CombineTestObserver<Bool, ConcreteError>()
+
+  override func setUp() {
+    self.publisher = PassthroughSubject<Bool, ConcreteError>()
+    self.observer = CombineTestObserver<Bool, ConcreteError>()
+
+    self.observer.observe(self.publisher)
+  }
+
+  func testValues() {
+    self.publisher.send(true)
+    self.observer.assertValue(true)
+    self.observer.assertDidEmitValue()
+
+    self.publisher.send(false)
+    self.observer.assertLastValue(false)
+
+    self.observer.assertValues([true, false])
+    self.observer.assertValueCount(2)
+
+    self.observer.assertDidNotFail()
+    self.observer.assertDidNotComplete()
+    self.observer.assertDidNotTerminate()
+  }
+
+  func testFailure() {
+    self.publisher.send(true)
+    self.observer.assertValue(true)
+
+    let msg = "failure :("
+    let error = ConcreteError(message: msg)
+
+    publisher.send(completion: Subscribers.Completion.failure(error))
+
+    self.observer.assertDidFail()
+    XCTAssertEqual(self.observer.failedError?.message, msg)
+
+    // n.B. in Combine, a publisher also finishes and cannot continue
+    // after an error occurs.
+    self.observer.assertDidComplete()
+  }
+
+  func testCompletion() {
+    self.publisher.send(false)
+    self.observer.assertValue(false)
+
+    self.publisher.send(completion: Subscribers.Completion.finished)
+    self.observer.assertDidComplete()
+    self.observer.assertDidNotFail()
+  }
+}
