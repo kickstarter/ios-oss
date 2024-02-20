d4cff733c7916b9ccf5d7730f2994a897944cb22
Amy <a.dyer@kickstarter.com>
2024-02-20 11:17:20 -0500

    MBL-1159: Exchange temporary token for OAuth token and login

--- Library/OAuth.swift
+++ Library/OAuth.swift
@@ -1,10 +1,19 @@
 import AuthenticationServices
+import Combine
 import FirebaseCrashlytics
 import Foundation
 import KsApi
 
+public enum OAuthAuthorizationResult {
+  case loggedIn
+  case failure(errorMessage: String)
+  case cancelled
+}
+
 public struct OAuth {
   public init() {}
+
+  private static var cancellables = Set<AnyCancellable>()
 
   static let redirectScheme = "ksrauth2"
 
@@ -28,7 +37,8 @@ public struct OAuth {
     return components?.url
   }
 
-  public static func createAuthorizationSession() -> ASWebAuthenticationSession? {
+  public static func createAuthorizationSession(onComplete: @escaping (OAuthAuthorizationResult) -> Void)
+    -> ASWebAuthenticationSession? {
     do {
       let verifier = try PKCE.createCodeVerifier(byteLength: 32)
       if !PKCE.checkCodeVerifier(verifier) {
@@ -44,8 +54,47 @@ public struct OAuth {
       let session = ASWebAuthenticationSession(
         url: url,
         callbackURLScheme: OAuth.redirectScheme
-      ) { _, _ in
-        // TODO: MBL-1159: Exchange information in callback for credentials, then login.
+      ) { url, error in
+        guard error == nil else {
+          if let authenticationError = error as? ASWebAuthenticationSessionError,
+            authenticationError.code == .canceledLogin {
+            onComplete(.cancelled)
+          } else {
+            onComplete(.failure(errorMessage: Strings.Something_went_wrong_please_try_again()))
+          }
+
+          return
+        }
+
+        guard let code = codeFromRedirectURL(url) else {
+          onComplete(.failure(errorMessage: Strings.Something_went_wrong_please_try_again()))
+          return
+        }
+
+        let params = OAuthTokenExchangeParams(temporaryToken: code, codeVerifier: verifier)
+
+        AppEnvironment.current.apiService.exchangeTokenForOAuthToken(params: params)
+          .receive(on: RunLoop.main)
+          .flatMap { response in
+            let token = response.token
+            // TODO: This would be neater if we can just return the V1 user from the exchange endpoint.
+
+            // Return a publisher that emits a tuple of (token, user) when the user request completes
+            return Just(token).setFailureType(to: ErrorEnvelope.self)
+              .zip(AppEnvironment.current.apiService.fetchUserSelf_combine(withToken: token))
+          }
+          .sink { result in
+            if case let .failure(error) = result {
+              let message = error.errorMessages.first ?? Strings.login_errors_unable_to_log_in()
+              onComplete(.failure(errorMessage: message))
+            }
+          } receiveValue: { token, user in
+            let accessEnvelope = AccessTokenEnvelope(accessToken: token, user: user)
+            AppEnvironment.login(accessEnvelope)
+
+            onComplete(.loggedIn)
+
+          }.store(in: &cancellables)
       }
 
       return session
@@ -54,5 +103,14 @@ public struct OAuth {
       Crashlytics.crashlytics().record(error: error)
       return nil
     }
+  }
+
+  private static func codeFromRedirectURL(_ url: URL?) -> String? {
+    guard let redirectURL = url else {
+      return nil
+    }
+
+    let components = URLComponents(url: redirectURL, resolvingAgainstBaseURL: false)
+    return components?.queryItems?.first(where: { $0.name == "code" })?.value
   }
 }
