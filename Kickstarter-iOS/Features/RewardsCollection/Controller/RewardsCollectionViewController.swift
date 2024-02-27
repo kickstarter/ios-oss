d60ee5288db9987566b4d2367b97261a41824eeb
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2024-02-27 10:36:50 -0500

    add navigation to Confirm Details when PCP flag enabled and Project is in PCP phase

--- Kickstarter-iOS/Features/RewardsCollection/Controller/RewardsCollectionViewController.swift
+++ Kickstarter-iOS/Features/RewardsCollection/Controller/RewardsCollectionViewController.swift
@@ -33,6 +33,7 @@ final class RewardsCollectionViewController: UICollectionViewController {
   }()
 
   public weak var pledgeViewDelegate: PledgeViewControllerDelegate?
+  public weak var confirmDetailsViewControllerDelegate: ConfirmDetailsViewControllerDelegate?
 
   private lazy var rewardsCollectionFooterView: RewardsCollectionViewFooter = {
     RewardsCollectionViewFooter(frame: .zero)
@@ -170,7 +171,13 @@ final class RewardsCollectionViewController: UICollectionViewController {
     self.viewModel.outputs.goToPledge
       .observeForControllerAction()
       .observeValues { [weak self] data in
-        self?.goToPledge(data: data)
+        guard let self else { return }
+
+        if featurePostCampaignPledgeEnabled(), data.project.isInPostCampaignPledgingPhase {
+          self.goToConfirmDetails(data: data)
+        } else {
+          self.goToPledge(data: data)
+        }
       }
 
     self.viewModel.outputs.rewardsCollectionViewFooterIsHidden
@@ -284,6 +291,15 @@ final class RewardsCollectionViewController: UICollectionViewController {
     pledgeViewController.configure(with: data)
 
     self.navigationController?.pushViewController(pledgeViewController, animated: true)
+  }
+
+  private func goToConfirmDetails(data: PledgeViewData) {
+    let vc = ConfirmDetailsViewController.instantiate()
+    vc.delegate = self.confirmDetailsViewControllerDelegate
+    vc.configure(with: data)
+    vc.title = self.title
+
+    self.navigationController?.pushViewController(vc, animated: true)
   }
 
   private func showEditRewardConfirmationPrompt(title: String, message: String) {
