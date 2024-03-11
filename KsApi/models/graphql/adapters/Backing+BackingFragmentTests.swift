06aed575c8d1e04b9d99f8f519a046f61e469b76
Amy <a.dyer@kickstarter.com>
2024-03-11 17:11:40 -0400

    MBL-1226: Add stripeCardId to templates using UserStoredCardsFragment fragment

--- KsApi/models/graphql/adapters/Backing+BackingFragmentTests.swift
+++ KsApi/models/graphql/adapters/Backing+BackingFragmentTests.swift
@@ -494,7 +494,8 @@ private func backingDictionary() -> [String: Any] {
             "expirationDate": "2023-01-01",
             "id": "6",
             "lastFour": "4242",
-            "type": "VISA"
+            "type": "VISA",
+            "stripeCardId": "pm_1OtGFX4VvJ2PtfhK3Gp00SWK",
           }
         ],
         "totalCount": 1
@@ -676,7 +677,8 @@ private func backingDictionary() -> [String: Any] {
               "expirationDate": "2023-01-01",
               "id": "6",
               "lastFour": "4242",
-              "type": "VISA"
+              "type": "VISA",
+              "stripeCardId": "pm_1OtGFX4VvJ2PtfhK3Gp00SWK",
             }
           ],
           "totalCount": 1
