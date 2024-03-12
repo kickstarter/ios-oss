2959734ff0dc62ee5d3207e1ae6d90cd40cf7beb
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2024-03-12 11:01:31 -0400

    Create the summary table view with datasource and table cells

---
+++ Kickstarter-iOS/Features/PledgePaymentMethods/Controller/PostCampaignPledgeRewardsSummaryTotalViewController.swift
@@ -0,0 +1,151 @@
+import KsApi
+import Library
+import Prelude
+import UIKit
+
+final class PostCampaignPledgeRewardsSummaryTotalViewController: UIViewController {
+  // MARK: - Properties
+
+  private lazy var titleAndTotalStackView: UIStackView = { UIStackView(frame: .zero) }()
+  private lazy var amountLabel: UILabel = { UILabel(frame: .zero) }()
+  private lazy var rootStackView: UIStackView = {
+    UIStackView(frame: .zero)
+      |> \.translatesAutoresizingMaskIntoConstraints .~ false
+  }()
+
+  private lazy var titleLabel: UILabel = { UILabel(frame: .zero) }()
+  private lazy var totalConversionLabel: UILabel = { UILabel(frame: .zero) }()
+  private lazy var totalStackView: UIStackView = { UIStackView(frame: .zero) }()
+  private let viewModel: PledgeSummaryViewModelType = PledgeSummaryViewModel()
+
+  // MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.configureSubviews()
+
+    self.viewModel.inputs.viewDidLoad()
+  }
+
+  // MARK: - Styles
+
+  override func bindStyles() {
+    super.bindStyles()
+
+    _ = self.view
+      |> checkoutBackgroundStyle
+
+    _ = self.rootStackView
+      |> rootStackViewStyle
+
+    _ = self.titleAndTotalStackView
+      |> adaptableStackViewStyle(
+        self.traitCollection.preferredContentSizeCategory.isAccessibilityCategory
+      )
+      |> titleAndTotalStackViewStyle
+
+    _ = self.totalStackView
+      |> totalStackViewStyle(self.traitCollection.preferredContentSizeCategory.isAccessibilityCategory)
+
+    _ = self.titleLabel
+      |> titleLabelStyle
+
+    _ = self.amountLabel
+      |> amountLabelStyle
+
+    _ = self.totalConversionLabel
+      |> totalConversionLabelStyle
+  }
+
+  private func configureSubviews() {
+    _ = (self.rootStackView, self.view)
+      |> ksr_addSubviewToParent()
+      |> ksr_constrainViewToEdgesInParent()
+
+    _ = ([self.titleAndTotalStackView], self.rootStackView)
+      |> ksr_addArrangedSubviewsToStackView()
+
+    _ = ([self.titleLabel, self.totalStackView], self.titleAndTotalStackView)
+      |> ksr_addArrangedSubviewsToStackView()
+
+    _ = ([self.amountLabel, self.totalConversionLabel], self.totalStackView)
+      |> ksr_addArrangedSubviewsToStackView()
+  }
+
+  // MARK: - View model
+
+  internal override func bindViewModel() {
+    super.bindViewModel()
+
+    self.amountLabel.rac.attributedText = self.viewModel.outputs.amountLabelAttributedText
+    self.totalConversionLabel.rac.text = self.viewModel.outputs.totalConversionLabelText
+  }
+
+  // MARK: - Configuration
+
+  internal func configure(with data: PledgeSummaryViewData) {
+    self.viewModel.inputs.configure(with: data)
+  }
+}
+
+// MARK: - Styles
+
+private let amountLabelStyle: LabelStyle = { (label: UILabel) in
+  _ = label
+    |> \.adjustsFontForContentSizeCategory .~ true
+    |> \.textAlignment .~ NSTextAlignment.right
+    |> \.adjustsFontSizeToFitWidth .~ true
+    |> \.isAccessibilityElement .~ true
+    |> \.minimumScaleFactor .~ 0.75
+    |> \.backgroundColor .~ .ksr_white
+
+  return label
+}
+
+private let titleAndTotalStackViewStyle: StackViewStyle = { (stackView: UIStackView) in
+  stackView
+    |> \.alignment .~ UIStackView.Alignment.top
+    |> \.backgroundColor .~ .ksr_white
+    |> \.layoutMargins .~ UIEdgeInsets(leftRight: Styles.gridHalf(4))
+}
+
+private let rootStackViewStyle: StackViewStyle = { (stackView: UIStackView) in
+  stackView
+    |> verticalStackViewStyle
+    |> \.spacing .~ Styles.grid(3)
+    |> \.backgroundColor .~ .ksr_white
+    |> \.translatesAutoresizingMaskIntoConstraints .~ false
+    |> \.isLayoutMarginsRelativeArrangement .~ true
+    |> \.layoutMargins .~ UIEdgeInsets(topBottom: Styles.grid(3), leftRight: Styles.grid(4))
+}
+
+private let titleLabelStyle: LabelStyle = { (label: UILabel) -> UILabel in
+  _ = label
+    |> checkoutTitleLabelStyle
+    |> \.font .~ .ksr_subhead().bolded
+    |> \.text %~ { _ in Strings.Total() }
+    |> \.backgroundColor .~ .ksr_white
+
+  return label
+}
+
+private let totalConversionLabelStyle: LabelStyle = { label in
+  _ = label
+    |> \.font .~ .ksr_caption1()
+    |> \.textColor .~ .ksr_support_400
+    |> \.backgroundColor .~ .ksr_white
+
+  return label
+}
+
+private func totalStackViewStyle(_ isAccessibilityCategory: Bool) -> StackViewStyle {
+  return { stackView in
+    stackView
+      |> verticalStackViewStyle
+      |> \.backgroundColor .~ .ksr_white
+      |> \.spacing .~ Styles.grid(1)
+      |> \.alignment .~
+      (isAccessibilityCategory ? UIStackView.Alignment.leading : UIStackView.Alignment.trailing)
+  }
+}
---
+++ Library/ViewModels/PostCampaignPledgeRewardsSummaryViewModel.swift
@@ -0,0 +1,245 @@
+import KsApi
+import Prelude
+import ReactiveSwift
+import UIKit
+
+public enum PostCampaignRewardsSummaryItem {
+  case header(PledgeExpandableHeaderRewardCellData)
+  case reward(PledgeExpandableHeaderRewardCellData)
+
+  public var data: PledgeExpandableHeaderRewardCellData {
+    switch self {
+    case let .header(data): return data
+    case let .reward(data): return data
+    }
+  }
+
+  public var isHeader: Bool {
+    switch self {
+    case .header: return true
+    case .reward: return false
+    }
+  }
+
+  public var isReward: Bool {
+    switch self {
+    case .header: return false
+    case .reward: return true
+    }
+  }
+}
+
+public struct PostCampaignRewardsSummaryViewData {
+  public let rewards: [Reward]
+  public let selectedQuantities: SelectedRewardQuantities
+  public let projectCountry: Project.Country
+  public let omitCurrencyCode: Bool
+  public let shipping: PledgeShippingSummaryViewData
+}
+
+public protocol PostCampaignPledgeRewardsSummaryViewModelInputs {
+  func configureWith(rewardsData: PostCampaignRewardsSummaryViewData, bonusAmount: Double?,
+                     pledgeData: PledgeSummaryViewData)
+  func viewDidLoad()
+}
+
+public protocol PostCampaignPledgeRewardsSummaryViewModelOutputs {
+  var configurePledgeTotalViewWithData: Signal<PledgeSummaryViewData, Never> { get }
+  var loadRewardsIntoDataSource: Signal<[PostCampaignRewardsSummaryItem], Never> { get }
+}
+
+public protocol PostCampaignPledgeRewardsSummaryViewModelType {
+  var inputs: PostCampaignPledgeRewardsSummaryViewModelInputs { get }
+  var outputs: PostCampaignPledgeRewardsSummaryViewModelOutputs { get }
+}
+
+public final class PostCampaignPledgeRewardsSummaryViewModel: PostCampaignPledgeRewardsSummaryViewModelType,
+  PostCampaignPledgeRewardsSummaryViewModelInputs, PostCampaignPledgeRewardsSummaryViewModelOutputs {
+  public init() {
+    let data = Signal.combineLatest(
+      self.configureWithDataProperty.signal.skipNil(),
+      self.viewDidLoadProperty.signal
+    )
+    .map(first)
+
+    let rewardData = data.map { $0.0 }
+    let rewards = data.map(\.0.rewards)
+    let selectedQuantities = data.map(\.0.selectedQuantities)
+    let bonusAmount = data.map(\.1)
+
+    let latestRewardDeliveryDate = rewards.map { rewards in
+      rewards
+        .compactMap { $0.estimatedDeliveryOn }
+        .reduce(0) { accum, value in max(accum, value) }
+    }
+
+    let estimatedDeliveryString = latestRewardDeliveryDate.map { date -> String? in
+      guard date > 0 else { return nil }
+
+      let dateString = Format.date(
+        secondsInUTC: date,
+        template: DateFormatter.monthYear,
+        timeZone: UTCTimeZone
+      )
+
+      return Strings.backing_info_estimated_delivery_date(delivery_date: dateString)
+    }
+    .skipNil()
+
+    let total: Signal<Double, Never> = Signal.combineLatest(
+      rewards,
+      selectedQuantities
+    )
+    .map { rewards, selectedQuantities in
+      rewards.reduce(0.0) { total, reward in
+        let totalForReward = reward.minimum
+          .multiplyingCurrency(Double(selectedQuantities[reward.id] ?? 0))
+
+        return total.addingCurrency(totalForReward)
+      }
+    }
+
+    self.loadRewardsIntoDataSource = Signal.zip(
+      rewardData,
+      selectedQuantities,
+      estimatedDeliveryString,
+      bonusAmount,
+      total
+    )
+    .map(items)
+
+    self.configurePledgeTotalViewWithData = data.map { $0.2 }
+  }
+
+  private let configureWithDataProperty =
+    MutableProperty<(PostCampaignRewardsSummaryViewData, Double?, PledgeSummaryViewData)?>(nil)
+  public func configureWith(
+    rewardsData: PostCampaignRewardsSummaryViewData,
+    bonusAmount: Double?,
+    pledgeData: PledgeSummaryViewData
+  ) {
+    self.configureWithDataProperty.value = (rewardsData, bonusAmount, pledgeData)
+  }
+
+  private let viewDidLoadProperty = MutableProperty(())
+  public func viewDidLoad() {
+    self.viewDidLoadProperty.value = ()
+  }
+
+  public let configurePledgeTotalViewWithData: Signal<PledgeSummaryViewData, Never>
+  public let loadRewardsIntoDataSource: Signal<[PostCampaignRewardsSummaryItem], Never>
+
+  public var inputs: PostCampaignPledgeRewardsSummaryViewModelInputs { return self }
+  public var outputs: PostCampaignPledgeRewardsSummaryViewModelOutputs { return self }
+}
+
+// MARK: - Functions
+
+private func items(
+  with data: PostCampaignRewardsSummaryViewData,
+  selectedQuantities: SelectedRewardQuantities,
+  estimatedDeliveryString: String,
+  bonusAmount: Double?,
+  total _: Double
+) -> [PostCampaignRewardsSummaryItem] {
+  // MARK: Header
+
+  let headerItem = PostCampaignRewardsSummaryItem.header((
+    text: estimatedDeliveryString,
+    amount: NSAttributedString(string: "")
+  ))
+
+  // MARK: Rewards
+
+  let rewardItems = data.rewards.compactMap { reward -> PostCampaignRewardsSummaryItem? in
+    guard let title = reward.title else { return nil }
+
+    let quantity = selectedQuantities[reward.id] ?? 0
+
+    let itemString = quantity > 1 ? "\(Format.wholeNumber(quantity)) x \(title)" : title
+
+    let amountAttributedText = attributedRewardCurrency(
+      with: data.projectCountry, amount: reward.minimum, omitUSCurrencyCode: data.omitCurrencyCode
+    )
+
+    return PostCampaignRewardsSummaryItem.reward((
+      text: itemString,
+      amount: amountAttributedText
+    ))
+  }
+
+  // MARK: Shipping
+
+  let shippingAmountAttributedText = attributedRewardCurrency(
+    with: data.projectCountry, amount: data.shipping.total, omitUSCurrencyCode: data.omitCurrencyCode
+  )
+  // TODO: [MBL-1217])https://kickstarter.atlassian.net/browse/MBL-1217) Update hardcoded string with translations
+  let shippingItem = PostCampaignRewardsSummaryItem.reward((
+    text: "Shipping to \(data.shipping.locationName)",
+    amount: shippingAmountAttributedText
+  ))
+
+  var items = [headerItem] + rewardItems
+  items.append(shippingItem)
+
+  // MARK: Bonus
+
+  if let bonus = bonusAmount {
+    let bonusAmountAttributedText = attributedRewardCurrency(
+      with: data.projectCountry, amount: bonus, omitUSCurrencyCode: data.omitCurrencyCode
+    )
+
+    let bonusItem = PostCampaignRewardsSummaryItem.reward((
+      text: Strings.Bonus_support(),
+      amount: bonusAmountAttributedText
+    ))
+
+    items.append(bonusItem)
+  }
+
+  return items
+}
+
+private func attributedHeaderCurrency(
+  with projectCountry: Project.Country,
+  amount: Double,
+  omitUSCurrencyCode: Bool
+) -> NSAttributedString? {
+  let defaultAttributes = checkoutCurrencyDefaultAttributes()
+    .withAllValuesFrom([.foregroundColor: UIColor.ksr_support_400])
+  let superscriptAttributes = checkoutCurrencySuperscriptAttributes()
+  guard
+    let attributedCurrency = Format.attributedCurrency(
+      amount,
+      country: projectCountry,
+      omitCurrencyCode: omitUSCurrencyCode,
+      defaultAttributes: defaultAttributes,
+      superscriptAttributes: superscriptAttributes,
+      maximumFractionDigits: 0,
+      minimumFractionDigits: 0
+    ) else { return nil }
+
+  return attributedCurrency
+}
+
+private func attributedRewardCurrency(
+  with projectCountry: Project.Country,
+  amount: Double,
+  omitUSCurrencyCode: Bool
+) -> NSAttributedString {
+  let currencyString = Format.currency(
+    amount,
+    country: projectCountry,
+    omitCurrencyCode: omitUSCurrencyCode,
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0
+  )
+
+  return NSAttributedString(
+    string: currencyString,
+    attributes: [
+      .foregroundColor: UIColor.ksr_support_400,
+      .font: UIFont.ksr_subhead().bolded
+    ]
+  )
+}
