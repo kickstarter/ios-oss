25d6ff4c8862afa273ff44d54f5fc7247cedea90
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2024-03-27 13:33:41 -0400

    MBL-1285: Add ApplePay into post-campaign pledge controller (#2002)
    * MBL-1285: Add ApplePay into post-campaign pledge controller
    
    * Changes per review #2002: create separate path for post-campaign ApplePay summary items
    
    This increases the clarity of the post-campaign ApplePay flow, because the post-campaign data is set up in a slightly different way.
    I also added some tests for this behavior to document/clarify it.

--- Library/PKPaymentRequestHelpersTests.swift
+++ Library/PKPaymentRequestHelpersTests.swift
@@ -166,30 +166,77 @@ final class PKPaymentRequestHelpersTests: XCTestCase {
     XCTAssertEqual(paymentRequest.paymentSummaryItems[3].type, .final)
   }
 
-  func testPaymentRequest_postCampaignPledgingEnabled() {
+  func testPaymentRequest_fromPostCampaignPaymentAuthorizationData_withReward() {
     var project = Project.template
-    project.country = .us
+    project.country = .jp
+    project.stats.currency = Project.Country.jp.currencyCode
+    project.isInPostCampaignPledgingPhase = true
+
+    let reward = Reward.template
+    let merchantId = "merchant_id"
+
+    let data = PostCampaignPaymentAuthorizationData(
+      project: project,
+      hasNoReward: false,
+      subtotal: 100,
+      bonus: 200,
+      shipping: 300,
+      total: 500,
+      merchantIdentifier: merchantId
+    )
+
+    let paymentRequest = PKPaymentRequest.paymentRequest(for: data)
+    XCTAssertEqual(paymentRequest.merchantIdentifier, merchantId)
+    XCTAssertEqual(paymentRequest.merchantCapabilities, .capability3DS)
+    XCTAssertEqual(paymentRequest.countryCode, "JP")
+    XCTAssertEqual(paymentRequest.currencyCode, "JPY")
+    XCTAssertEqual(paymentRequest.shippingType, .shipping)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems.count, 4)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[0].label, "Reward")
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[0].amount.doubleValue, 100)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[0].type, .final)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[1].label, "Bonus")
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[1].amount.doubleValue, 200)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[1].type, .final)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[2].label, "Shipping")
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[2].amount.doubleValue, 300)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[2].type, .final)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[3].label, "Kickstarter")
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[3].amount.doubleValue, 500)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[3].type, .final)
+  }
+
+  func testPaymentRequest_fromPostCampaignPaymentAuthorizationData_noReward() {
+    var project = Project.template
+    project.country = .ca
+    project.stats.currency = Project.Country.ca.currencyCode
     project.isInPostCampaignPledgingPhase = true
 
     let reward = Reward.noReward
     let merchantId = "merchant_id"
 
-    let mockRemoteConfig = MockRemoteConfigClient()
-    mockRemoteConfig.features = [
-      RemoteConfigFeature.postCampaignPledgeEnabled.rawValue: true
-    ]
+    let data = PostCampaignPaymentAuthorizationData(
+      project: project,
+      hasNoReward: true,
+      subtotal: 500,
+      bonus: 0,
+      shipping: 0,
+      total: 500,
+      merchantIdentifier: merchantId
+    )
 
-    withEnvironment(remoteConfigClient: mockRemoteConfig) {
-      let paymentRequest = PKPaymentRequest.paymentRequest(
-        for: project,
-        reward: reward,
-        allRewardsTotal: 100,
-        additionalPledgeAmount: 50,
-        allRewardsShippingTotal: 0,
-        merchantIdentifier: merchantId
-      )
-
-      XCTAssertEqual(paymentRequest.paymentSummaryItems.last?.label, "Kickstarter")
-    }
+    let paymentRequest = PKPaymentRequest.paymentRequest(for: data)
+    XCTAssertEqual(paymentRequest.merchantIdentifier, merchantId)
+    XCTAssertEqual(paymentRequest.merchantCapabilities, .capability3DS)
+    XCTAssertEqual(paymentRequest.countryCode, "CA")
+    XCTAssertEqual(paymentRequest.currencyCode, "CAD")
+    XCTAssertEqual(paymentRequest.shippingType, .shipping)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems.count, 2)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[0].label, "Total")
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[0].amount.doubleValue, 500)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[0].type, .final)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[1].label, "Kickstarter")
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[1].amount.doubleValue, 500)
+    XCTAssertEqual(paymentRequest.paymentSummaryItems[1].type, .final)
   }
 }
