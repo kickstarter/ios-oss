eaffab07537f0dbdcd3491f325bf200fec65e3d2
Amy <a.dyer@kickstarter.com>
2024-03-28 13:01:17 -0400

    MBL-1285: Integrate ApplePay into Post-Campaign Checkout, using STPApplePayContext

--- KsApi/models/graphql/adapters/GraphAPI.ApplePay+ApplePayParams.swift
+++ KsApi/models/graphql/adapters/GraphAPI.ApplePay+ApplePayParams.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension GraphAPI.ApplePayInput {
+public extension GraphAPI.ApplePayInput {
   static func from(_ input: ApplePayParams?) -> GraphAPI.ApplePayInput? {
     guard let input = input else { return nil }
     return GraphAPI.ApplePayInput(
