58fd9854a87c4d5b905782ca572efd7e9c3b9351
Scott Clampet <s.clampet@kickstarter.com>
2024-04-29 13:38:14 -0600

    Add setup intent func to StripeIntentServiceType

--- Library/StripeIntentService.swift
+++ Library/StripeIntentService.swift
@@ -5,6 +5,8 @@ import ReactiveSwift
 public protocol StripeIntentServiceType {
   static func createPaymentIntent(for projectId: String, pledgeTotal: Double)
     -> SignalProducer<PaymentIntentEnvelope, ErrorEnvelope>
+  static func createSetupIntent(for projectId: String?, context: GraphAPI.StripeIntentContextTypes)
+    -> SignalProducer<ClientSecretEnvelope, ErrorEnvelope>
 }
 
 /// This is the module that creates either a Stripe payment intent or a setup intent.
@@ -29,5 +31,25 @@ public struct StripeIntentService: StripeIntentServiceType {
         amountDollars: String(format: "%.2f", pledgeTotal),
         digitalMarketingAttributed: nil
       ))
+  }
+
+  /**
+   Returns a signal producer that emits a `ClientSecretEnvelope` or `ErrorEnvelope` value representing whether or not a payment intent was created and returned successfully.
+   The returned producer emits once and completes.
+
+   - parameter for: The types to register that we will request permissions for.
+   - parameters:
+     - projectId: The optional GraphID of a project
+     - context: The context for which this intent is being created
+   */
+
+  public static func createSetupIntent(
+    for projectId: String?,
+    context: GraphAPI.StripeIntentContextTypes
+  ) -> SignalProducer<ClientSecretEnvelope, ErrorEnvelope> {
+    AppEnvironment.current.apiService
+      .createStripeSetupIntent(
+        input: CreateSetupIntentInput(projectId: projectId, context: context)
+      )
   }
 }
