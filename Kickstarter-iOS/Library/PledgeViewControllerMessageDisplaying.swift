d1ece62e6e4f19aeaf69f58a3c169445bf57d130
Ingerid Fosli <i.fosli@kickstarter.com>
2024-06-27 10:42:40 -0600

    [MBL-1422] Handle errors without always dismissing (#2090)
    * Refactor messageBannerView to be able to dismiss itself
    
    * Keep user on checkout screen if adding new card errors
    
    * Rename bannerAndParentVC to bannerAndViewController

--- Kickstarter-iOS/Library/PledgeViewControllerMessageDisplaying.swift
+++ Kickstarter-iOS/Library/PledgeViewControllerMessageDisplaying.swift
@@ -2,6 +2,10 @@ import Foundation
 import UIKit
 
 protocol PledgeViewControllerMessageDisplaying: AnyObject {
-  func pledgeViewController(_ pledgeViewController: UIViewController, didErrorWith message: String)
+  func pledgeViewController(
+    _ pledgeViewController: UIViewController,
+    didErrorWith message: String,
+    error: Error?
+  )
   func pledgeViewController(_ pledgeViewController: UIViewController, didSucceedWith message: String)
 }
