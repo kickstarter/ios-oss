e495aca6c2808bb85a86dbf0a933ebac1d876f2d
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2024-07-16 13:37:38 -0500

    [MBL-1599] Adds the Shipping Dropdown Selector to the Rewards Carousel. (#2095)
    * WIP: populate shipping countries using the project's rewards
    
    * No longer using individually passed in Rewards
    
    * update PledgeShippingLocationViewModelTests
    
    * add PledgeShippingLocationViewController to Rewards screen
    
    * update snapshots + clean up
    
    * snapshot test updates
    
    * new orthogonals
    
    * formatting clean up

--- Library/ViewModels/RewardsCollectionViewModel.swift
+++ Library/ViewModels/RewardsCollectionViewModel.swift
@@ -13,6 +13,7 @@ public protocol RewardsCollectionViewModelInputs {
   func confirmedEditReward()
   func rewardCellShouldShowDividerLine(_ show: Bool)
   func rewardSelected(with rewardId: Int)
+  func shippingRuleSelected(_ shippingRule: ShippingRule)
   func traitCollectionDidChange(_ traitCollection: UITraitCollection)
   func viewDidAppear()
   func viewDidLayoutSubviews()
@@ -22,6 +23,7 @@ public protocol RewardsCollectionViewModelInputs {
 
 public protocol RewardsCollectionViewModelOutputs {
   var configureRewardsCollectionViewFooterWithCount: Signal<Int, Never> { get }
+  var configureShippingLocationViewWithData: Signal<PledgeShippingLocationViewData, Never> { get }
   var flashScrollIndicators: Signal<Void, Never> { get }
   var goToAddOnSelection: Signal<PledgeViewData, Never> { get }
   var goToPledge: Signal<PledgeViewData, Never> { get }
@@ -29,6 +31,7 @@ public protocol RewardsCollectionViewModelOutputs {
   var reloadDataWithValues: Signal<[RewardCardViewData], Never> { get }
   var rewardsCollectionViewFooterIsHidden: Signal<Bool, Never> { get }
   var scrollToBackedRewardIndexPath: Signal<IndexPath, Never> { get }
+  var shippingLocationViewHidden: Signal<Bool, Never> { get }
   var showEditRewardConfirmationPrompt: Signal<(String, String), Never> { get }
   var title: Signal<String, Never> { get }
 
@@ -78,6 +81,23 @@ public final class RewardsCollectionViewModel: RewardsCollectionViewModelType,
       .map { $0.count }
 
     self.flashScrollIndicators = self.viewDidAppearProperty.signal
+
+    // MARK: Shipping Location
+
+    self.shippingLocationViewHidden = project
+      .map { project in
+        projectHasShippableRewards(project) == false
+      }
+
+    // Only shown for regular non-add-ons based rewards
+    self.configureShippingLocationViewWithData = Signal.combineLatest(
+      project,
+      self.shippingLocationViewHidden.filter(isFalse)
+    )
+    .map { project, _ in
+      // TODO: Reward will be removed from  ShippingLocationViewData once we remove the selector from Add-Ons
+      (project, project.rewards[0], false, nil)
+    }
 
     let selectedRewardFromId = rewards
       .takePairWhen(self.rewardSelectedWithRewardIdProperty.signal.skipNil())
@@ -299,6 +319,11 @@ public final class RewardsCollectionViewModel: RewardsCollectionViewModelType,
     self.rewardSelectedWithRewardIdProperty.value = rewardId
   }
 
+  private let (shippingRuleSelectedSignal, shippingRuleSelectedObserver) = Signal<ShippingRule, Never>.pipe()
+  public func shippingRuleSelected(_ shippingRule: ShippingRule) {
+    self.shippingRuleSelectedObserver.send(value: shippingRule)
+  }
+
   private let traitCollectionChangedProperty = MutableProperty<UITraitCollection?>(nil)
   public func traitCollectionDidChange(_ traitCollection: UITraitCollection) {
     self.traitCollectionChangedProperty.value = traitCollection
@@ -324,6 +349,7 @@ public final class RewardsCollectionViewModel: RewardsCollectionViewModelType,
     self.viewWillAppearProperty.value = ()
   }
 
+  public let configureShippingLocationViewWithData: Signal<PledgeShippingLocationViewData, Never>
   public let configureRewardsCollectionViewFooterWithCount: Signal<Int, Never>
   public let flashScrollIndicators: Signal<Void, Never>
   public let goToAddOnSelection: Signal<PledgeViewData, Never>
@@ -332,6 +358,7 @@ public final class RewardsCollectionViewModel: RewardsCollectionViewModelType,
   public let reloadDataWithValues: Signal<[RewardCardViewData], Never>
   public let rewardsCollectionViewFooterIsHidden: Signal<Bool, Never>
   public let scrollToBackedRewardIndexPath: Signal<IndexPath, Never>
+  public var shippingLocationViewHidden: Signal<Bool, Never>
   public let showEditRewardConfirmationPrompt: Signal<(String, String), Never>
   public let title: Signal<String, Never>
 
@@ -345,6 +372,11 @@ public final class RewardsCollectionViewModel: RewardsCollectionViewModelType,
 }
 
 // MARK: - Functions
+
+private func projectHasShippableRewards(_ project: Project) -> Bool {
+  project.rewards
+    .contains(where: { $0.isUnRestrictedShippingPreference || $0.isRestrictedShippingPreference })
+}
 
 private func titleForContext(_ context: RewardsCollectionViewContext, project: Project) -> String {
   if currentUserIsCreator(of: project) {
