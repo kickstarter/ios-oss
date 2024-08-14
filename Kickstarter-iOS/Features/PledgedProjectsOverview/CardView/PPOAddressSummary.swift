21e525c0b9f2ba49c74104aa536149a414680fc8
Steve Streza <s.streza@kickstarter.com>
2024-08-14 11:06:42 -0700

    Add address summary for PPO

---
+++ Kickstarter-iOS/Features/PledgedProjectsOverview/CardView/PPOAddressSummary.swift
@@ -0,0 +1,46 @@
+import Foundation
+import SwiftUI
+
+struct PPOAddressSummary: View {
+  let address: String
+
+  var body: some View {
+    HStack(alignment: .firstTextBaseline) {
+      // TODO: Localize
+      Text("Shipping address")
+        .lineLimit(nil)
+        .font(Font(Constants.labelFont))
+        .foregroundStyle(Color(Constants.labelColor))
+        .frame(width: Constants.labelWidth, alignment: .leading)
+        .multilineTextAlignment(.leading)
+
+      Text(self.address)
+        .lineLimit(nil)
+        .multilineTextAlignment(.leading)
+        .font(Font(Constants.addressFont))
+        .foregroundStyle(Color(Constants.addressColor))
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    .frame(maxWidth: .infinity)
+  }
+
+  private enum Constants {
+    static let labelWidth: CGFloat = 85
+    static let labelFont = UIFont.ksr_caption1().bolded
+    static let labelColor = UIColor.ksr_black
+    static let addressFont = UIFont.ksr_caption1()
+    static let addressColor = UIColor.ksr_black
+  }
+}
+
+#Preview {
+  VStack(spacing: 28) {
+    PPOAddressSummary(address: """
+      Firsty Lasty
+      123 First Street, Apt #5678
+      Los Angeles, CA 90025-1234
+      United States
+    """)
+  }
+  .padding(28)
+}
