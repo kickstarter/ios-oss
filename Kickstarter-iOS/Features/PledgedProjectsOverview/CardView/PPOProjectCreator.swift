14fc6b1a24ebb025c15458a8cbc8e1a544feaa2d
Steve Streza <s.streza@kickstarter.com>
2024-08-19 18:00:16 -0700

    Code review comments

--- Kickstarter-iOS/Features/PledgedProjectsOverview/CardView/PPOProjectCreator.swift
+++ Kickstarter-iOS/Features/PledgedProjectsOverview/CardView/PPOProjectCreator.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Library
 import SwiftUI
 
 struct PPOProjectCreator: View {
@@ -13,21 +14,25 @@ struct PPOProjectCreator: View {
         .frame(maxWidth: .infinity, alignment: .leading)
         .lineLimit(1)
 
-      // TODO: Localize
-      Text("Send a message")
-        .font(Font(Constants.sendMessageFont))
-        .foregroundStyle(Color(Constants.sendMessageColor))
-        .frame(alignment: .trailing)
-        .lineLimit(1)
+      Button(action: {
+        // TODO: Action
+      }, label: {
+        // TODO: Localize
+        Text("Send a message")
+      })
+      .font(Font(Constants.sendMessageFont))
+      .foregroundStyle(Color(Constants.sendMessageColor))
+      .frame(alignment: .trailing)
+      .lineLimit(1)
 
       Spacer()
-        .frame(width: 6)
+        .frame(width: Styles.grid(1))
 
       Image("chevron-right")
         .resizable()
         .scaledToFit()
-        .frame(width: 10, height: 10)
-        .offset(y: 2)
+        .frame(width: Constants.chevronSize, height: Constants.chevronSize)
+        .offset(Constants.chevronOffset)
         .foregroundStyle(Color(Constants.sendMessageColor))
     }
     .frame(maxWidth: .infinity)
@@ -38,6 +43,8 @@ struct PPOProjectCreator: View {
     static let createdByColor = UIColor.ksr_support_400
     static let sendMessageFont = UIFont.ksr_caption2()
     static let sendMessageColor = UIColor.ksr_create_700
+    static let chevronSize: CGFloat = 10
+    static let chevronOffset = CGSize(width: 0, height: 2)
   }
 }
 
