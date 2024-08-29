a75ba87bbc382b23023a9b50b1153096071e918a
Steve Streza <s.streza@kickstarter.com>
2024-08-28 17:23:42 -0700

    Undo PPOView changes

--- Kickstarter-iOS/Features/PledgedProjectsOverview/PPOView.swift
+++ Kickstarter-iOS/Features/PledgedProjectsOverview/PPOView.swift
@@ -7,45 +7,34 @@ struct PPOView: View {
   @AccessibilityFocusState private var isBannerFocused: Bool
 
   var body: some View {
-    GeometryReader { _ in
+    GeometryReader { reader in
       ScrollView {
-        switch self.viewModel.results {
-        case .loading, .someLoaded, .allLoaded:
-          ForEach(self.viewModel.results.values) { viewModel in
-            PPOProjectCard(viewModel: viewModel)
-          }
-          .padding(.vertical)
-        case .unloaded:
-          Text("Not loaded")
-        case .empty:
-          PPOEmptyStateView(tabBarController: self.tabBarController)
-        case let .error(error):
-          Text("Failed: \(error)")
+        Text(self.viewModel.greeting)
+        // TODO: Show empty state view if user is logged in and has no PPO updates.
+        //      PPOEmptyStateView(tabBarController: self.tabBarController)
+
+        // TODO: Remove this button once we're showing cards instead.
+        Button("Show banner") {
+          self.viewModel.shouldSendSampleMessage()
         }
       }
-      .refreshable {
-        self.viewModel.pullToRefresh()
-      }
       .frame(maxWidth: .infinity, alignment: .center)
-      .onAppear(perform: {
-        self.viewModel.viewDidAppear()
+      .overlay(alignment: .bottom) {
+        MessageBannerView(viewModel: self.$viewModel.bannerViewModel)
+          .frame(
+            minWidth: reader.size.width,
+            idealWidth: reader.size.width,
+            alignment: .bottom
+          )
+          .animation(.easeInOut, value: self.viewModel.bannerViewModel != nil)
+          .accessibilityFocused(self.$isBannerFocused)
+      }
+
+      .onChange(of: self.viewModel.bannerViewModel, perform: { _ in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+          self.isBannerFocused = self.viewModel.bannerViewModel != nil
+        }
       })
-//      .overlay(alignment: .bottom) {
-//        MessageBannerView(viewModel: self.$viewModel.bannerViewModel)
-//          .frame(
-//            minWidth: reader.size.width,
-//            idealWidth: reader.size.width,
-//            alignment: .bottom
-//          )
-//          .animation(.easeInOut, value: self.viewModel.bannerViewModel != nil)
-//          .accessibilityFocused(self.$isBannerFocused)
-//      }
-//
-//      .onChange(of: self.viewModel.bannerViewModel, perform: { _ in
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-//          self.isBannerFocused = self.viewModel.bannerViewModel != nil
-//        }
-//      })
     }
   }
 }
