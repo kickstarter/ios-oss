6976e5f37e747d53290dc90dea51446b5f8a8650
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2024-09-10 13:21:58 -0700

    [MBL-1719] Include Selected Reward Quantities In Total Estimated Shipping Range (#2152)

--- Library/SharedFunctionsTests.swift
+++ Library/SharedFunctionsTests.swift
@@ -725,4 +725,38 @@ final class SharedFunctionsTests: TestCase {
 
     XCTAssertEqual(estimatedShipping, "$3-$12")
   }
+
+  func test_estimatedShippingText_IncludesSelectedQuantities() {
+    let rewardShippingRule = ShippingRule.template
+      |> ShippingRule.lens.estimatedMin .~ Money(amount: 1)
+      |> ShippingRule.lens.estimatedMax .~ Money(amount: 10)
+    let addOnShippingRule = ShippingRule.template
+      |> ShippingRule.lens.estimatedMin .~ Money(amount: 1)
+      |> ShippingRule.lens.estimatedMax .~ Money(amount: 5)
+
+    let reward = Reward.template
+      |> Reward.lens.shipping .~ (.template |> Reward.Shipping.lens.enabled .~ true)
+      |> Reward.lens.shippingRules .~ [rewardShippingRule]
+      |> Reward.lens.id .~ 99
+    let addOn = Reward.template
+      |> Reward.lens.shipping .~ (.template |> Reward.Shipping.lens.enabled .~ true)
+      |> Reward.lens.id .~ 5
+      |> Reward.lens.shippingRules .~ [addOnShippingRule]
+
+    let project = Project.template
+
+    let selectedQuantities: SelectedRewardQuantities = [
+      reward.id: 1,
+      addOn.id: 2
+    ]
+
+    let estimatedShipping = estimatedShippingText(
+      for: [reward, addOn],
+      project: project,
+      selectedShippingRule: .template,
+      selectedQuantities: selectedQuantities
+    )
+
+    XCTAssertEqual(estimatedShipping, "$3-$20")
+  }
 }
