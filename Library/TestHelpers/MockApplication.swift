dd335646a005b4013aeb3f70ed96ec23a6ac3f96
Jovani Luna <jluna@arkusnexus.com>
2024-09-23 17:39:54 -0700

    fixed linter warings

--- Library/TestHelpers/MockApplication.swift
+++ Library/TestHelpers/MockApplication.swift
@@ -14,20 +14,20 @@ final class MockApplication: UIApplicationType {
   }
 
   #if compiler(>=6.0) // Compiler flag for Xcode >= 16
-  func open(
-    _: URL,
-    options _: [UIApplication.OpenExternalURLOptionsKey: Any],
-    completionHandler _: (@MainActor @Sendable (Bool) -> Void)?
-  ) {
-    self.openUrlWasCalled = true
-  }
+    func open(
+      _: URL,
+      options _: [UIApplication.OpenExternalURLOptionsKey: Any],
+      completionHandler _: (@MainActor @Sendable (Bool) -> Void)?
+    ) {
+      self.openUrlWasCalled = true
+    }
   #else
-  func open(
-    _: URL,
-    options _: [UIApplication.OpenExternalURLOptionsKey: Any],
-    completionHandler _: ((Bool) -> Void)?
-  ) {
-    self.openUrlWasCalled = true
-  }
+    func open(
+      _: URL,
+      options _: [UIApplication.OpenExternalURLOptionsKey: Any],
+      completionHandler _: ((Bool) -> Void)?
+    ) {
+      self.openUrlWasCalled = true
+    }
   #endif
 }
