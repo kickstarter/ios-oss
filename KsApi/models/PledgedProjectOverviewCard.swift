84742e97f88c6792cb92b44d5ee6756328f175ca
Steve Streza <s.streza@kickstarter.com>
2024-10-02 16:56:20 -0700

    Major refactor of data pipeline for PPO types to support populating Project, including new type for PledgedProjectOverviewCard

--- KsApi/models/PledgedProjectOverviewCard.swift
+++ KsApi/models/PledgedProjectOverviewCard.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct PledgedProjectOverviewCard {
+public struct PledgedProjectOverviewCard: Identifiable, Equatable {
   public let isUnread: Bool
   public let alerts: [Alert]
   public let imageURL: URL
@@ -11,9 +11,28 @@ public struct PledgedProjectOverviewCard {
   public let address: String?
   public let actions: (Action, Action?)
   public let tierType: TierType
-  public let parentSize: CGSize
 
-  public enum TierType {
+  // MARK: - Identifiable
+
+  public let id = UUID()
+
+  // MARK: - Equatable
+
+  // For some reason this isn't generated because of the `actions` tuple
+  // If that ever is fixed, this can be removed in favor of a synthesized Equatable implementation
+  public static func == (lhs: PledgedProjectOverviewCard, rhs: PledgedProjectOverviewCard) -> Bool {
+    lhs.isUnread == rhs.isUnread &&
+      lhs.alerts == rhs.alerts &&
+      lhs.imageURL == rhs.imageURL &&
+      lhs.title == rhs.title &&
+      lhs.project == rhs.project &&
+      lhs.pledge == rhs.pledge &&
+      lhs.creatorName == rhs.creatorName &&
+      lhs.address == rhs.address &&
+      lhs.actions == rhs.actions
+  }
+
+  public enum TierType: Equatable {
     case fixPayment
     case authenticateCard
     case openSurvey
@@ -58,7 +77,7 @@ public struct PledgedProjectOverviewCard {
       }
     }
 
-    public enum Style: Identifiable {
+    public enum Style: Identifiable, Equatable {
       case green
       case red
       case black
@@ -92,7 +111,7 @@ public struct PledgedProjectOverviewCard {
       "\(self.type)-\(self.icon)-\(self.message)"
     }
 
-    public enum AlertType: Identifiable {
+    public enum AlertType: Identifiable, Equatable {
       case time
       case alert
 
@@ -106,7 +125,7 @@ public struct PledgedProjectOverviewCard {
       }
     }
 
-    public enum AlertIcon: Identifiable {
+    public enum AlertIcon: Identifiable, Equatable {
       case warning
       case alert
 
@@ -119,5 +138,42 @@ public struct PledgedProjectOverviewCard {
         }
       }
     }
+  }
+}
+
+extension PledgedProjectOverviewCard.Alert {
+  init?(flag: GraphAPI.PpoCardFragment.Flag) {
+    let alertType: PledgedProjectOverviewCard.Alert.AlertType? = switch flag.type {
+    case "alert":
+      .alert
+    case "time":
+      .time
+    default:
+      nil
+    }
+
+    let alertIcon: PledgedProjectOverviewCard.Alert.AlertIcon? = switch flag.icon {
+    case "alert":
+      .alert
+    case "warning":
+      .warning
+    default:
+      nil
+    }
+    let message = flag.message
+
+    guard let alertType, let alertIcon, let message else {
+      return nil
+    }
+
+    self = .init(type: alertType, icon: alertIcon, message: message)
+  }
+}
+
+extension GraphAPI.MoneyFragment: Equatable {
+  public static func == (lhs: KsApi.GraphAPI.MoneyFragment, rhs: KsApi.GraphAPI.MoneyFragment) -> Bool {
+    return lhs.amount == rhs.amount &&
+      lhs.currency == rhs.currency &&
+      lhs.symbol == rhs.symbol
   }
 }
