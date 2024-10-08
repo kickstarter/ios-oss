5050b5feb1b79e0d469c8144d4609cb7002088bd
Steve Streza <s.streza@kickstarter.com>
2024-10-07 17:07:14 -0700

    Fix test

--- Kickstarter-iOS/Features/PledgedProjectsOverview/PPOEmptyStateViewTests.swift
+++ Kickstarter-iOS/Features/PledgedProjectsOverview/PPOEmptyStateViewTests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 final class PPOEmptyStateViewTests: TestCase {
   func testEmptyStateView() {
-    let view = PPOEmptyStateView(viewModel: nil).frame(width: 320, height: 500)
+    let view = PPOEmptyStateView().frame(width: 320, height: 500)
     // TODO: Record multiple snapshots once translations are available (MBL-1558)
     assertSnapshot(matching: view, as: .image, named: "lang_en")
   }
