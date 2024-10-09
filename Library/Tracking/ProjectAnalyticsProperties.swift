64ae422fc881ed36c08e11910855d9f0aad4f4fb
Steve Streza <s.streza@kickstarter.com>
2024-10-08 19:45:51 -0700

    Add ProjectAnalyticsProperties

--- Library/Tracking/ProjectAnalyticsProperties.swift
+++ Library/Tracking/ProjectAnalyticsProperties.swift
@@ -48,133 +48,134 @@ extension Project: ProjectAnalyticsProperties {
   public var categoryAnalyticsName: String? {
     self.category.analyticsName
   }
-
+  
   public var categoryParentAnalyticsName: String? {
     self.category.parentAnalyticsName
   }
-
+  
   public var categoryParentId: String? {
     self.category.parentId.flatMap(String.init)
   }
-
+  
   public var countryCode: String {
     self.country.countryCode
   }
-
+  
   public var creatorId: String? {
     String(self.creator.id)
   }
-
+  
   public var creatorIsRepeatCreator: Bool? {
     self.creator.isRepeatCreator
   }
-
+  
   public var datesDeadline: TimeInterval? {
     self.dates.deadline
   }
-
+  
   public var datesLaunchedAt: TimeInterval? {
     self.dates.launchedAt
   }
-
+  
   public func datesDuration(using calendar: Calendar) -> Int? {
     self.dates.duration(using: calendar)
   }
-
+  
   public func datesHoursRemaining(from date: Date, using calendar: Calendar) -> Int? {
     self.dates.hoursRemaining(from: date, using: calendar)
   }
-
+  
   public var hasVideo: Bool {
     self.video != nil
   }
-
+  
   public var personalizationIsStarred: Bool? {
     self.personalization.isStarred
   }
-
+  
   public var personalizationIsBacking: Bool? {
     self.personalization.isBacking
   }
-
+  
   public var rewardsCount: Int {
     self.rewards.filter { $0 != .noReward }.count
   }
-
+  
   public var stateValue: String {
     self.state.rawValue
   }
-
+  
   public var statsBackerCount: Int {
     self.stats.backersCount
   }
-
+  
   public var statsCommentsCount: Int? {
     self.stats.commentsCount
   }
-
+  
   public var statsCurrency: String {
     self.stats.currency
   }
-
+  
   public var statsPercentFunded: Int {
     self.stats.percentFunded
   }
-
+  
   public var statsPledged: Int {
     self.stats.pledged
   }
-
+  
   public var statsTotalAmountPledgedUsdCurrency: Float? {
     self.stats.totalAmountPledgedUsdCurrency
   }
-
+  
   public var statsGoalUsdCurrency: Float? {
     self.stats.goalUsdCurrency
   }
-
+  
   public var statsUpdatesCount: Int? {
     self.stats.updatesCount
   }
 }
 
+
 extension GraphAPI.ProjectAnalyticsFragment: ProjectAnalyticsProperties {
   public var prelaunchActivated: Bool? {
     self.isPrelaunchActivated
   }
-
+  
   public var categoryAnalyticsName: String? {
     self.category?.analyticsName
   }
-
+  
   public var categoryParentAnalyticsName: String? {
     self.category?.parentCategory?.analyticsName
   }
-
+  
   public var categoryParentId: String? {
     self.category?.parentCategory?.id
   }
-
+  
   public var countryCode: String {
     self.country.code.rawValue
   }
-
+  
   public var creatorId: String? {
     self.creator?.id
   }
-
+  
   public var creatorIsRepeatCreator: Bool? {
     (self.creator?.createdProjects?.totalCount ?? 0) >= 2
   }
-
+  
   public var datesDeadline: TimeInterval? {
     self.deadlineAt.flatMap(TimeInterval.init)
   }
-
+  
   public var datesLaunchedAt: TimeInterval? {
     self.launchedAt.flatMap(TimeInterval.init)
   }
-
+  
   public func datesDuration(using calendar: Calendar = .current) -> Int? {
     guard let deadlineDateValue = self.datesDeadline,
           let launchedAtDateValue = self.datesLaunchedAt else {
@@ -204,66 +205,66 @@ extension GraphAPI.ProjectAnalyticsFragment: ProjectAnalyticsProperties {
   public var id: Int {
     self.pid
   }
-
+  
   public var hasAddOns: Bool {
     (self.addOns?.totalCount ?? 0) > 0
   }
-
+  
   public var hasVideo: Bool {
     self.video != nil
   }
-
+  
   public var personalizationIsStarred: Bool? {
     self.isWatched
   }
-
+  
   public var personalizationIsBacking: Bool? {
     self.backing != nil
   }
-
+  
   public var rewardsCount: Int {
     self.rewards?.totalCount ?? 0
   }
-
+  
   public var stateValue: String {
     self.state.rawValue
   }
-
+  
   public var statsBackerCount: Int {
     self.backersCount
   }
-
+  
   public var statsCommentsCount: Int? {
     self.commentsCount
   }
-
+  
   public var statsCurrency: String {
     self.currency.rawValue
   }
-
+  
   public var statsPercentFunded: Int {
     self.percentFunded
   }
-
+  
   public var statsPledged: Int {
     Int(self.pledged.amount ?? "") ?? 0
   }
-
+  
   public var statsTotalAmountPledgedUsdCurrency: Float? {
     let amount = self.pledged.amount.flatMap(Float.init)
-    return amount.flatMap { amount in self.usdExchangeRate.map { amount * Float($0) } }
+    return amount.flatMap({ amount in self.usdExchangeRate.map { amount * Float($0) } })
   }
-
+  
   public var statsGoalUsdCurrency: Float? {
     let amount = self.goal?.amount.flatMap(Float.init)
-    return amount.flatMap { amount in self.usdExchangeRate.map { amount * Float($0) } }
+    return amount.flatMap({ amount in self.usdExchangeRate.map { amount * Float($0) } })
   }
-
+  
   public var statsUpdatesCount: Int? {
     self.posts?.totalCount
   }
-
+  
   public var tags: [String]? {
-    self.projectTags.compactMap { $0?.name }
+    self.projectTags.compactMap({ $0?.name })
   }
 }
