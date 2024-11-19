53b1b162b8183a5aa418428cb67426783a6ceeb4
Steve Streza <s.streza@kickstarter.com>
2024-11-18 18:16:18 -0800

    Update mock for ATT

--- Library/TestHelpers/MockAppTrackingTransparency.swift
+++ Library/TestHelpers/MockAppTrackingTransparency.swift
@@ -1,19 +1,34 @@
 import Library
+import ReactiveSwift
 
 class MockAppTrackingTransparency: AppTrackingTransparencyType {
   public private(set) var advertisingIdentifier: String?
-  public var shouldRequestAuthStatus = true
   public var requestAndSetAuthorizationStatusFlag = false
+
+  public var shouldRequestAuthStatus: Bool {
+    get {
+      self.authorizationStatusProperty.value == .notDetermined
+    }
+    set {
+      self.authorizationStatusProperty.value = (newValue ? .notDetermined : .authorized)
+    }
+  }
 
   func updateAdvertisingIdentifier() {
     self.advertisingIdentifier = self.shouldRequestAuthStatus ? "advertisingIdentifer" : nil
   }
 
   func requestAndSetAuthorizationStatus() {
+    self.authorizationStatusProperty.value = .authorized
     self.advertisingIdentifier = self.requestAndSetAuthorizationStatusFlag ? "advertisingIdentifier" : nil
   }
 
   func shouldRequestAuthorizationStatus() -> Bool {
     self.shouldRequestAuthStatus
+  }
+
+  private var authorizationStatusProperty = MutableProperty<AppTrackingAuthorization>(.notDetermined)
+  var authorizationStatus: Signal<AppTrackingAuthorization, Never> {
+    self.authorizationStatusProperty.signal
   }
 }
