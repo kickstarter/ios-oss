d6b34d95ad836dc0748ef37e0643a0bdf77a7275
Jovani Luna <j.luna.c@kickstarter.com>
2024-11-27 15:42:27 -0800

    Some change

--- Kickstarter-iOS/Features/PledgePaymentMethods/Views/Cells/PledgePaymentMethodCell.swift
+++ Kickstarter-iOS/Features/PledgePaymentMethods/Views/Cells/PledgePaymentMethodCell.swift
@@ -24,7 +24,7 @@ final class PledgePaymentMethodCell: UITableViewCell, ValueCell {
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
-
+    // forcing changes POC
     self.configureSubviews()
     self.setupConstraints()
     self.bindViewModel()
