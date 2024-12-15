52a6aa23f20482dba90163141f3e9d4bbd25ff97
Jovani Luna <j.luna.c@kickstarter.com>
2024-12-15 08:04:25 -0800

    More tests

--- Library/ViewModels/PledgePaymentPlansOptionViewModelTest.swift
+++ Library/ViewModels/PledgePaymentPlansOptionViewModelTest.swift
@@ -175,7 +175,7 @@ final class PledgePaymentPlansOptionViewModelTest: TestCase {
   }
 }
 
-private func mockPaymentIncrements() -> [PledgePaymentIncrement] {
+internal func mockPaymentIncrements() -> [PledgePaymentIncrement] {
   let amount = PledgePaymentIncrementAmount(amount: 250.0, currency: "USD")
   let scheduledCollection = TimeInterval(1_553_731_200)
   return [
