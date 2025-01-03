ec1d48b26e5b540a4b9d71a4cd5e8e3134ff84d2
Amy <a.dyer@kickstarter.com>
2025-01-03 14:46:57 -0500

    Add loading output to PLOTPledgeViewModel

--- Library/ViewModels/PLOTPledgeViewModel.swift
+++ Library/ViewModels/PLOTPledgeViewModel.swift
@@ -5,10 +5,27 @@ import ReactiveSwift
 public protocol PLOTPledgeViewModelOutputs {
   var showPledgeOverTimeUI: Signal<Bool, Never> { get }
   var pledgeOverTimeConfigData: Signal<PledgePaymentPlansAndSelectionData, Never> { get }
+  var pledgeOverTimeIsLoading: Signal<Bool, Never> { get }
 
   // Visible only for testing
   var buildPaymentPlanInputs: Signal<(String, String), Never> { get }
 }
+
+/**
+ A component view model for Pledge Over Time in the pledge checkout flow.
+ Creates one BuildPaymentPlanQuery and uses those results to create data for the UI.
+
+ Inputs:
+  - `project`: A project.
+  - `pledgeTotal`: Total amount to pledge, a double.
+
+ Both inputs need to send at least one event for any outputs to send.
+
+ Outputs:
+  - `showPledgeOverTimeUI`:  Whether the PLOT module should be shown. Sends one or more events; the PLOT module should disappear if an error occurs in the BuildPaymentPlanQuery.
+  - `pledgeOverTimeConfigData`: Info needed to display the PLOT module. Sends only _one_ event.
+  - `pledgeOverTimeIsLoading`: Whether the PLOT module is loading. Sends one or more events, since loading can start and stop.
+ */
 
 public struct PLOTPledgeViewModel: PLOTPledgeViewModelOutputs {
   init(project: Signal<Project, Never>, pledgeTotal: Signal<Double, Never>) {
@@ -45,6 +62,13 @@ public struct PLOTPledgeViewModel: PLOTPledgeViewModelOutputs {
       pledgeOverTimeQuery.errors().map(value: false)
     )
 
+    self.pledgeOverTimeIsLoading = Signal.merge(
+      pledgeOverTimeUIEnabled,
+      pledgeOverTimeQuery.values().map(value: false),
+      pledgeOverTimeQuery.errors().map(value: false)
+    )
+    .skipRepeats()
+
     self.pledgeOverTimeConfigData = pledgeOverTimeQuery
       .values()
       .compactMap { $0.project?.paymentPlan }
@@ -67,6 +91,7 @@ public struct PLOTPledgeViewModel: PLOTPledgeViewModelOutputs {
 
   public let showPledgeOverTimeUI: Signal<Bool, Never>
   public let pledgeOverTimeConfigData: Signal<PledgePaymentPlansAndSelectionData, Never>
+  public let pledgeOverTimeIsLoading: Signal<Bool, Never>
   public let buildPaymentPlanInputs: Signal<(String, String), Never>
 
   public var outputs: PLOTPledgeViewModelOutputs { return self }
