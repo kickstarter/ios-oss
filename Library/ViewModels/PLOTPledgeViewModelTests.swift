1158d594b9a4f5861032353c2148a32eebb59ac4
Amy <a.dyer@kickstarter.com>
2025-01-03 14:46:57 -0500

    Add loading output to PLOTPledgeViewModel

--- Library/ViewModels/PLOTPledgeViewModelTests.swift
+++ Library/ViewModels/PLOTPledgeViewModelTests.swift
@@ -12,12 +12,16 @@ final class PLOTPledgeViewModelTests: TestCase {
   var vm: PLOTPledgeViewModel?
 
   let buildPaymentPlanInputs = TestObserver<(String, String), Never>()
+  let pledgeOverTimeIsLoading = TestObserver<Bool, Never>()
+  let showPledgeOverTimeUI = TestObserver<Bool, Never>()
 
   override func setUp() {
     super.setUp()
 
     self.vm = PLOTPledgeViewModel(project: self.projectSignal, pledgeTotal: self.pledgeTotalSignal)
     self.vm!.outputs.buildPaymentPlanInputs.observe(self.buildPaymentPlanInputs.observer)
+    self.vm!.outputs.pledgeOverTimeIsLoading.observe(self.pledgeOverTimeIsLoading.observer)
+    self.vm!.outputs.showPledgeOverTimeUI.observe(self.showPledgeOverTimeUI.observer)
   }
 
   func testViewModel_callsBuildPaymentPlanQuery_withCorrectSlugAndPledgeTotal() {
@@ -67,6 +71,74 @@ final class PLOTPledgeViewModelTests: TestCase {
       self.pledgeTotalObserver.send(value: 2.0)
       self.pledgeTotalObserver.send(value: 3.0)
       self.buildPaymentPlanInputs.assertValueCount(1)
+    }
+  }
+
+  func testViewModel_sendsLoadingEvent_whenPledgeOverTimeIsEnabled() {
+    let queryData = try! GraphAPI.BuildPaymentPlanQuery
+      .Data(jsonString: buildPaymentPlanQueryJson(eligible: true))
+    let mockApiService = MockService(buildPaymentPlanResult: .success(queryData))
+
+    let mockConfigClient = MockRemoteConfigClient()
+    mockConfigClient.features = [
+      RemoteConfigFeature.pledgeOverTime.rawValue: true
+    ]
+
+    withEnvironment(apiService: mockApiService, remoteConfigClient: mockConfigClient) {
+      let project = Project.template
+        |> Project.lens.slug .~ "some-slug"
+        |> Project.lens.isPledgeOverTimeAllowed .~ true
+
+      self.projectObserver.send(value: project)
+      self.pledgeTotalObserver.send(value: 100.0)
+
+      self.showPledgeOverTimeUI.assertValues([true])
+      self.pledgeOverTimeIsLoading.assertValues([true, false])
+    }
+  }
+
+  func testViewModel_doesntLoad_whenPledgeOverTimeIsDisabled() {
+    let queryData = try! GraphAPI.BuildPaymentPlanQuery
+      .Data(jsonString: buildPaymentPlanQueryJson(eligible: true))
+    let mockApiService = MockService(buildPaymentPlanResult: .success(queryData))
+
+    let mockConfigClient = MockRemoteConfigClient()
+    mockConfigClient.features = [
+      RemoteConfigFeature.pledgeOverTime.rawValue: false
+    ]
+
+    withEnvironment(apiService: mockApiService, remoteConfigClient: mockConfigClient) {
+      let project = Project.template
+        |> Project.lens.slug .~ "some-slug"
+        |> Project.lens.isPledgeOverTimeAllowed .~ false
+
+      self.projectObserver.send(value: project)
+      self.pledgeTotalObserver.send(value: 100.0)
+
+      self.pledgeOverTimeIsLoading.assertValues([false])
+      self.showPledgeOverTimeUI.assertValues([false])
+    }
+  }
+
+  func testViewModel_hidesUIAndStopsLoading_whenBuildPaymentPlanQueryFails() {
+    let mockApiService = MockService(buildPaymentPlanResult: .failure(.couldNotParseJSON))
+
+    let mockConfigClient = MockRemoteConfigClient()
+    mockConfigClient.features = [
+      RemoteConfigFeature.pledgeOverTime.rawValue: true
+    ]
+
+    withEnvironment(apiService: mockApiService, remoteConfigClient: mockConfigClient) {
+      let project = Project.template
+        |> Project.lens.slug .~ "some-slug"
+        |> Project.lens.isPledgeOverTimeAllowed .~ true
+
+      self.projectObserver.send(value: project)
+      self.pledgeTotalObserver.send(value: 100.0)
+
+      self.pledgeOverTimeIsLoading.assertValues([true, false])
+      self.buildPaymentPlanInputs.assertDidEmitValue()
+      self.showPledgeOverTimeUI.assertValues([true, false])
     }
   }
 }
