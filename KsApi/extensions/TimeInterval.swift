10f06d05277c268e3a253c32d1674942bf635c5d
Scott Clampet <s.clampet@kickstarter.com>
2025-01-08 09:48:28 -0600

    keep TimeInterval extension in one place

--- KsApi/extensions/TimeInterval.swift
+++ KsApi/extensions/TimeInterval.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public extension TimeInterval {
+  func toISO8601DateTimeString() -> String {
+    return ISO8601DateFormatter().string(from: Date(timeIntervalSince1970: self))
+  }
+
   static func from(ISO8601DateTimeString string: String) -> TimeInterval? {
     return ISO8601DateFormatter().date(from: string)?.timeIntervalSince1970
   }
--- Library/TimeInterval+ISO8601Date.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public extension TimeInterval {
-  func toISO8601DateTimeString() -> String {
-    return ISO8601DateFormatter().string(from: Date(timeIntervalSince1970: self))
-  }
-}
