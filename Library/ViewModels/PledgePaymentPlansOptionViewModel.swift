1f1739e190fc8f7f1c5572aedf8987eed81faa4f
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2025-01-08 13:46:25 -0500

    MBL-1943: Show loading UI when PLOT is loading (#2231)
    * MBL-1943: Show loading UI when PLOT is loading
    
    * Update comment
    
    * Fix code comment typo

--- Library/ViewModels/PledgePaymentPlansOptionViewModel.swift
+++ Library/ViewModels/PledgePaymentPlansOptionViewModel.swift
@@ -54,6 +54,11 @@ public struct PledgePaymentIncrementFormatted: Equatable {
   public var scheduledCollection: String
 }
 
+public enum SelectionIndicatorImageName: String {
+  case selected = "icon-payment-method-selected"
+  case unselected = "icon-payment-method-unselected"
+}
+
 public protocol PledgePaymentPlansOptionViewModelInputs {
   func configureWith(data: PledgePaymentPlanOptionData)
   func optionTapped()
@@ -93,7 +98,7 @@ public final class PledgePaymentPlansOptionViewModel:
     self.selectionIndicatorImageName = configData
       .map {
         $0.selectedType == $0.type ?
-          "icon-payment-method-selected" : "icon-payment-method-unselected"
+          SelectionIndicatorImageName.selected.rawValue : SelectionIndicatorImageName.unselected.rawValue
       }
 
     self.titleText = configData.map { getTitleText(by: $0.type) }
