a1973e5897238e16fb787f2cd4e57ca8720c703d
Steve Streza <s.streza@kickstarter.com>
2025-01-15 16:48:32 -0800

    Remove server feature accessor and fixed warning

--- Library/Extensions/ServerFeature+Helpers.swift
+++ Library/Extensions/ServerFeature+Helpers.swift
@@ -1,17 +1,13 @@
 import Foundation
 import KsApi
 
-public func serverFeaturePledgedProjectsOverviewIsEnabled() -> Bool {
-  return ServerFeature.pledgeProjectsOverviewIos_2024.isEnabled()
-}
-
 extension ServerFeature {
   fileprivate func isEnabled(in environment: Environment = AppEnvironment.current) -> Bool {
     environment.currentUserServerFeatures?.contains(self) ?? false
   }
 }
 
-extension Set: EncodableType where Element == ServerFeature {
+extension Set: KsApi.EncodableType where Element == ServerFeature {
   public func encode() -> [String: Any] {
     ["features": Array(self).map { $0.rawValue }]
   }
