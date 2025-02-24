de2c2271bc30f76538be3f36242468cde6e947f5
Amy <a.dyer@kickstarter.com>
2025-02-24 15:16:33 -0500

    #2292: Add typealias TabBarItemStyle

--- Library/Styles/TabBarItemStyles.swift
+++ Library/Styles/TabBarItemStyles.swift
@@ -5,9 +5,11 @@ public let tabBarSelectedColor = UIColor.ksr_create_700
 public let tabBarTintColor = UIColor.ksr_white
 public let tabBarAvatarSize = CGSize(width: 25, height: 25)
 
+public typealias TabBarItemStyle = (UITabBarItem) -> UITabBarItem
+
 private let paddingY: CGFloat = 6.0
 
-public let activityTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
+public let activityTabBarItemStyle: TabBarItemStyle = { button in
   button.title = Strings.tabbar_activity()
   button.image = image(named: "tabbar-icon-activity")
   button.image = image(named: "tabbar-icon-activity-selected")
@@ -15,7 +17,7 @@ public let activityTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
   return button
 }
 
-public let dashboardTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
+public let dashboardTabBarItemStyle: TabBarItemStyle = { button in
   button.title = Strings.tabbar_dashboard()
   button.image = image(named: "tabbar-icon-dashboard")
   button.selectedImage = image(named: "tabbar-icon-dashboard-selected")
@@ -23,7 +25,7 @@ public let dashboardTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem i
   return button
 }
 
-public let homeTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
+public let homeTabBarItemStyle: TabBarItemStyle = { button in
   button.title = Strings.Explore()
   button.image = image(named: "tabbar-icon-home")
   button.selectedImage = image(named: "tabbar-icon-home-selected")
@@ -31,11 +33,11 @@ public let homeTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
   return button
 }
 
-public func profileTabBarItemStyle(isLoggedIn: Bool) -> (UITabBarItem) -> UITabBarItem {
+public func profileTabBarItemStyle(isLoggedIn: Bool) -> TabBarItemStyle {
   let imageName = isLoggedIn ? "tabbar-icon-profile-logged-in" : "tabbar-icon-profile-logged-out"
   let accLabel = isLoggedIn ? Strings.tabbar_profile() : Strings.tabbar_login()
 
-  return { (button: UITabBarItem) -> UITabBarItem in
+  return { button in
     button.title = Strings.tabbar_profile()
     button.image = image(named: imageName)
     button.image = image(named: "tabbar-icon-profile-selected")
@@ -45,7 +47,7 @@ public func profileTabBarItemStyle(isLoggedIn: Bool) -> (UITabBarItem) -> UITabB
   }
 }
 
-public let searchTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
+public let searchTabBarItemStyle: TabBarItemStyle = { button in
   button.title = Strings.tabbar_search()
   button.image = image(named: "tabbar-icon-search")
   button.selectedImage = image(named: "tabbar-icon-search-selected")
@@ -53,7 +55,7 @@ public let searchTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
   return button
 }
 
-public let backingsTabBarItemStyle = { (button: UITabBarItem) -> UITabBarItem in
+public let backingsTabBarItemStyle: TabBarItemStyle = { button in
   button.title = Strings.tabbar_backings()
   button.image = image(named: "tabbar-icon-backings")
   button.selectedImage = image(named: "tabbar-icon-backings-selected")
