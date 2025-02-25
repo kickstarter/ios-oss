1dca5051dd183a40ab8286c08624ab2683882e2d
Amy <a.dyer@kickstarter.com>
2025-02-25 11:12:53 -0500

    MBL-2075: Add backingActionCount to PPOUserSettings

--- KsApi/models/graphql/GraphUserSetup.swift
+++ KsApi/models/graphql/GraphUserSetup.swift
@@ -4,4 +4,5 @@ public struct GraphUserSetup: Decodable, Equatable {
   public var email: String?
   public var enabledFeatures: Set<ServerFeature>
   public var ppoHasAction: Bool?
+  public var backingActionCount: Int?
 }
