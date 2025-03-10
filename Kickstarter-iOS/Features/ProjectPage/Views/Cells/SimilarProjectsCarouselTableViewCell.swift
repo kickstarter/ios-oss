4a682cbf52f781b4585347469691262098f9bfe6
Scott Clampet <s.clampet@kickstarter.com>
2025-03-10 14:26:27 -0400

    Add snapshots

---
+++ Kickstarter-iOS/Features/ProjectPage/Views/Cells/SimilarProjectsCarouselTableViewCell.swift
@@ -0,0 +1,63 @@
+import KsApi
+import Library
+import UIKit
+
+/*
+ A container for the Similar Projects Carousel.
+
+ Right now this just contains the projects card, but will eventualy contain the UICollectionView carousel that displays multiple similar project cards.
+ */
+
+final class SimilarProjectsCarouselTableViewCell: UITableViewCell, ValueCell {
+  private lazy var projectCardView: SimilarProjectsCardView = { SimilarProjectsCardView(frame: .zero) }()
+
+  // MARK: - Lifecycle
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+    self.configureViews()
+    self.bindStyles()
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  internal override func layoutSubviews() {
+    super.layoutSubviews()
+  }
+
+  private func configureViews() {
+    self.contentView.addSubview(self.projectCardView)
+
+    NSLayoutConstraint.activate([
+      self.projectCardView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+      self.projectCardView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+      self.projectCardView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+      self.projectCardView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+    ])
+  }
+
+  // MARK: - Configuration
+
+  func configureWith(value: Project) {
+    self.projectCardView.configureWith(value: value)
+
+    self.layoutIfNeeded()
+  }
+
+  override func bindStyles() {
+    applyBaseCellStyle(self)
+  }
+}
+
+private func applyBaseCellStyle(_ cell: UITableViewCell) {
+  cell.contentView.layoutMargins = .init(topBottom: Styles.grid(3), leftRight: Styles.grid(12))
+  cell.contentView.preservesSuperviewLayoutMargins = false
+  cell.backgroundColor = .ksr_white
+  cell.layoutMargins = .init(all: 0.0)
+  cell.preservesSuperviewLayoutMargins = false
+  cell.selectionStyle = .none
+}
---
+++ Kickstarter-iOS/Features/ProjectPage/Views/SimilarProjectsCardViewTests.swift
@@ -0,0 +1,138 @@
+@testable import Kickstarter_Framework
+@testable import KsApi
+@testable import Library
+import SnapshotTesting
+import XCTest
+
+final class SimilarProjectsCardViewTests: TestCase {
+  override func setUp() {
+    super.setUp()
+    AppEnvironment.pushEnvironment(mainBundle: Bundle.framework)
+    UIView.setAnimationsEnabled(false)
+  }
+
+  override func tearDown() {
+    AppEnvironment.popEnvironment()
+    UIView.setAnimationsEnabled(true)
+
+    super.tearDown()
+  }
+
+  func testView_ProjectState_Live() {
+    orthogonalCombos([Language.en], [Device.phone4_7inch]).forEach { language, device in
+      withEnvironment(language: language) {
+        let view = SimilarProjectsCardView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        var project = Project.template
+        project.state = .live
+
+        view.configureWith(value: project)
+
+        let (parent, _) = traitControllers(
+          device: device,
+          orientation: .portrait,
+          child: wrappedViewController(subview: view, device: device)
+        )
+        parent.view.frame.size.height = 300
+
+        self.scheduler.advance(by: .seconds(1))
+
+        assertSnapshot(matching: parent.view, as: .image, named: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+
+  func testView_ProjectState_Successful() {
+    orthogonalCombos([Language.en], [Device.phone4_7inch]).forEach { language, device in
+      withEnvironment(language: language) {
+        let view = SimilarProjectsCardView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        var project = Project.template
+        project.state = .successful
+
+        view.configureWith(value: project)
+
+        let (parent, _) = traitControllers(
+          device: device,
+          orientation: .portrait,
+          child: wrappedViewController(subview: view, device: device)
+        )
+        parent.view.frame.size.height = 300
+
+        self.scheduler.advance(by: .seconds(1))
+
+        assertSnapshot(matching: parent.view, as: .image, named: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+
+  func testView_ProjectState_Failed() {
+    orthogonalCombos([Language.en], [Device.phone4_7inch]).forEach { language, device in
+      withEnvironment(language: language) {
+        let view = SimilarProjectsCardView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        var project = Project.template
+        project.state = .failed
+
+        view.configureWith(value: project)
+
+        let (parent, _) = traitControllers(
+          device: device,
+          orientation: .portrait,
+          child: wrappedViewController(subview: view, device: device)
+        )
+        parent.view.frame.size.height = 300
+
+        self.scheduler.advance(by: .seconds(1))
+
+        assertSnapshot(matching: parent.view, as: .image, named: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+
+  func testView_Project_IsPrelaunch() {
+    orthogonalCombos([Language.en], [Device.phone4_7inch]).forEach { language, device in
+      withEnvironment(language: language) {
+        let view = SimilarProjectsCardView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        var project = Project.template
+        project.state = .live
+        project.prelaunchActivated = true
+        project.dates.launchedAt = -5
+
+        view.configureWith(value: project)
+
+        let (parent, _) = traitControllers(
+          device: device,
+          orientation: .portrait,
+          child: wrappedViewController(subview: view, device: device)
+        )
+        parent.view.frame.size.height = 300
+
+        self.scheduler.advance(by: .seconds(1))
+
+        assertSnapshot(matching: parent.view, as: .image, named: "lang_\(language)_device_\(device)")
+      }
+    }
+  }
+}
+
+private func wrappedViewController(subview: UIView, device: Device) -> UIViewController {
+  let controller = UIViewController(nibName: nil, bundle: nil)
+  let (parent, _) = traitControllers(device: device, orientation: .portrait, child: controller)
+
+  controller.view.addSubview(subview)
+
+  NSLayoutConstraint.activate([
+    subview.leadingAnchor.constraint(equalTo: controller.view.layoutMarginsGuide.leadingAnchor),
+    subview.topAnchor.constraint(equalTo: controller.view.layoutMarginsGuide.topAnchor),
+    subview.trailingAnchor.constraint(equalTo: controller.view.layoutMarginsGuide.trailingAnchor),
+    subview.bottomAnchor.constraint(equalTo: controller.view.layoutMarginsGuide.bottomAnchor)
+  ])
+
+  return parent
+}
