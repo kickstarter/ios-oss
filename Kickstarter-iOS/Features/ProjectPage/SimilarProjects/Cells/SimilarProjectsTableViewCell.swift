f6c485ae4a0d6ec7542430d15ce8f7ccfac73aaf
Scott Clampet <s.clampet@kickstarter.com>
2025-03-12 11:19:00 -0400

    Add title label and make sure table cell accounts for collection view size constraints

--- Kickstarter-iOS/Features/ProjectPage/SimilarProjects/Cells/SimilarProjectsTableViewCell.swift
+++ Kickstarter-iOS/Features/ProjectPage/SimilarProjects/Cells/SimilarProjectsTableViewCell.swift
@@ -4,26 +4,26 @@ import UIKit
 
 class SimilarProjectsTableViewCell: UITableViewCell, ValueCell {
   // MARK: - Properties
-  
+
   private lazy var collectionView: UICollectionView = UICollectionView(
     frame: .zero,
     collectionViewLayout: layout
   )
   private let layout = UICollectionViewFlowLayout()
-  
-  private lazy var headerView: SimilarProjectsCollectionViewHeaderView = SimilarProjectsCollectionViewHeaderView()
 
   private let dataSource: SimilarProjectsCollectionViewDataSource = SimilarProjectsCollectionViewDataSource()
+
+  private lazy var titleLabel: UILabel = { UILabel(frame: .zero) }()
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
 
     self.collectionView.dataSource = self.dataSource
-    self.collectionView.delegate = self
     self.collectionView.registerCellClass(SimilarProjectsCollectionViewCell.self)
 
     self.configureSubviews()
     self.bindStyles()
+    self.updateConstraints()
   }
 
   @available(*, unavailable)
@@ -31,44 +31,83 @@ class SimilarProjectsTableViewCell: UITableViewCell, ValueCell {
     fatalError("init(coder:) has not been implemented")
   }
 
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    
-    collectionView.frame = contentView.bounds
-  }
-
   override func bindStyles() {
     super.bindStyles()
-    
+
+    applyBaseCellStyle(self)
+    applyTitleLabelStyle(self.titleLabel)
     applyCollectionViewStyle(self.collectionView)
     applyCollectionViewLayoutStyle(self.layout)
-    applyCollectionViewHeaderStyle(self.headerView)
   }
-  
+
+  override func systemLayoutSizeFitting(
+    _: CGSize,
+    withHorizontalFittingPriority _: UILayoutPriority,
+    verticalFittingPriority _: UILayoutPriority
+  ) -> CGSize {
+    self.contentView.frame = self.bounds
+    self.contentView.layoutIfNeeded()
+
+    return self.collectionView.contentSize
+  }
+
   private func configureSubviews() {
     self.contentView.addSubview(self.collectionView)
+    self.contentView.addSubview(self.titleLabel)
+  }
+
+  override func updateConstraints() {
+    NSLayoutConstraint.activate([
+      self.titleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+      self.titleLabel.leadingAnchor.constraint(
+        equalTo: self.contentView.leadingAnchor,
+        constant: Styles.grid(3)
+      ),
+      self.collectionView.topAnchor.constraint(
+        equalTo: self.titleLabel.bottomAnchor
+      ),
+      self.collectionView.leadingAnchor.constraint(
+        equalTo: self.contentView.leadingAnchor,
+        constant: Styles.grid(3)
+      ),
+      self.collectionView.trailingAnchor.constraint(
+        equalTo: self.contentView.trailingAnchor,
+        constant: -Styles.grid(3)
+      ),
+      self.collectionView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+    ])
+
+    super.updateConstraints()
   }
 
   func configureWith(value: Project) {
-    self.dataSource.load([value, value, value, value, value])
+    self.dataSource.load([value, value, value, value])
     self.collectionView.reloadData()
+    self.layoutIfNeeded()
   }
 }
 
+// MARK: - Styles
+
+private func applyBaseCellStyle(_ cell: UITableViewCell) {
+  cell.contentView.layoutMargins = .init(topBottom: Styles.grid(3), leftRight: Styles.grid(12))
+  cell.contentView.preservesSuperviewLayoutMargins = false
+  cell.backgroundColor = .ksr_white
+  cell.preservesSuperviewLayoutMargins = false
+  cell.selectionStyle = .none
+}
+
 private func applyCollectionViewStyle(_ collectionView: UICollectionView) {
-  collectionView.showsHorizontalScrollIndicator = false
   collectionView.backgroundColor = .clear
+  collectionView.contentInset = .init(all: 0)
+  collectionView.showsHorizontalScrollIndicator = false
+  collectionView.translatesAutoresizingMaskIntoConstraints = false
 }
 
 private func applyCollectionViewLayoutStyle(_ layout: UICollectionViewFlowLayout) {
   layout.scrollDirection = .horizontal
-  layout.itemSize = CGSize(width: 200, height: 400)
   layout.minimumInteritemSpacing = 10
-  layout.minimumLineSpacing = 10
-}
-
-private func applyCollectionViewHeaderStyle(_ layout: UICollectionReusableView) {
-  layout.translatesAutoresizingMaskIntoConstraints = false
+  layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
 }
 
 private func applyTitleLabelStyle(_ label: UILabel) {
