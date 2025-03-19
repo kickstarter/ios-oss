438021b452cf8bbd3f84a61c318034d0bd84211f
Scott Clampet <s.clampet@kickstarter.com>
2025-03-19 09:33:07 -0400

    implement a collectionview in SimilarProjectsTableViewCell that uses the new cell and datasource

--- Kickstarter-iOS/Features/ProjectPage/SimilarProjects/Cells/SimilarProjectsTableViewCell.swift
+++ Kickstarter-iOS/Features/ProjectPage/SimilarProjects/Cells/SimilarProjectsTableViewCell.swift
@@ -2,22 +2,40 @@ import KsApi
 import Library
 import UIKit
 
-/*
- A container for the Similar Projects Carousel.
+enum SimilarProjectsCellConstants {
+  static let spacing: CGFloat = Styles.grid(3)
+  static let collectionViewInteritemSpacing: CGFloat = 8.0
+  static let collectionViewItemSize = CGSize(width: 327, height: 279)
+  static let collectionViewBottomSpacing: CGFloat = -Styles.grid(6)
+  static let collectionViewHeight: CGFloat = 350.0
+}
 
- Right now this just contains the projects card, but will eventualy contain the UICollectionView carousel that displays multiple similar project cards.
+/*
+ Contains the SimilarProjectsCarousel Title and UICollectionView.
  */
 
-final class SimilarProjectsCarouselTableViewCell: UITableViewCell, ValueCell {
-  private lazy var projectCardView: SimilarProjectsCardView = { SimilarProjectsCardView(frame: .zero) }()
+class SimilarProjectsTableViewCell: UITableViewCell, ValueCell {
+  // MARK: - Properties
 
-  // MARK: - Lifecycle
+  private let dataSource: SimilarProjectsCollectionViewDataSource = SimilarProjectsCollectionViewDataSource()
+  private let layout = UICollectionViewFlowLayout()
+
+  private lazy var collectionView: UICollectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: layout
+  )
+
+  private lazy var titleLabel: UILabel = { UILabel(frame: .zero) }()
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-    self.configureViews()
+    self.collectionView.dataSource = self.dataSource
+    self.collectionView.registerCellClass(SimilarProjectsCollectionViewCell.self)
+
+    self.configureSubviews()
     self.bindStyles()
+    self.updateConstraints()
   }
 
   @available(*, unavailable)
@@ -25,39 +43,91 @@ final class SimilarProjectsCarouselTableViewCell: UITableViewCell, ValueCell {
     fatalError("init(coder:) has not been implemented")
   }
 
-  internal override func layoutSubviews() {
-    super.layoutSubviews()
-  }
-
-  private func configureViews() {
-    self.contentView.addSubview(self.projectCardView)
-
-    NSLayoutConstraint.activate([
-      self.projectCardView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
-      self.projectCardView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-      self.projectCardView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
-      self.projectCardView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
-    ])
-  }
-
-  // MARK: - Configuration
-
-  func configureWith(value: any SimilarProject) {
-    self.projectCardView.configureWith(value: value)
-
-    self.layoutIfNeeded()
-  }
-
   override func bindStyles() {
+    super.bindStyles()
+
     applyBaseCellStyle(self)
+    applyTitleLabelStyle(self.titleLabel)
+    applyCollectionViewStyle(self.collectionView)
+    applyCollectionViewLayoutStyle(self.layout)
+  }
+
+  /// UITableViewCells have a hard time adjusting to a nested UICollectionView's contentSize.
+  /// This helps make sure that the layout is updated if needed.
+  override func systemLayoutSizeFitting(
+    _: CGSize,
+    withHorizontalFittingPriority _: UILayoutPriority,
+    verticalFittingPriority _: UILayoutPriority
+  ) -> CGSize {
+    self.contentView.frame = self.bounds
+    self.contentView.layoutIfNeeded()
+
+    return self.collectionView.contentSize
+  }
+
+  private func configureSubviews() {
+    self.contentView.addSubview(self.collectionView)
+    self.contentView.addSubview(self.titleLabel)
+  }
+
+  override func updateConstraints() {
+    NSLayoutConstraint.activate([
+      self.titleLabel.topAnchor.constraint(
+        equalTo: self.contentView.topAnchor,
+        constant: SimilarProjectsCellConstants.spacing
+      ),
+      self.titleLabel.leadingAnchor.constraint(
+        equalTo: self.contentView.leadingAnchor,
+        constant: SimilarProjectsCellConstants.spacing
+      ),
+      self.collectionView.topAnchor.constraint(
+        equalTo: self.titleLabel.bottomAnchor,
+        constant: SimilarProjectsCellConstants.spacing
+      ),
+      self.collectionView.leadingAnchor.constraint(
+        equalTo: self.contentView.leadingAnchor,
+        constant: SimilarProjectsCellConstants.spacing
+      ),
+      self.collectionView.trailingAnchor.constraint(
+        equalTo: self.contentView.trailingAnchor,
+        constant: -SimilarProjectsCellConstants.spacing
+      ),
+      self.collectionView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+    ])
+
+    super.updateConstraints()
+  }
+
+  // TODO: Will use the state object to load the collectionview appropriately.
+  func configureWith(value _: SimilarProjectsState) {
+    self.collectionView.reloadData()
+    self.layoutIfNeeded()
   }
 }
 
+// MARK: - Styles
+
 private func applyBaseCellStyle(_ cell: UITableViewCell) {
-  cell.contentView.layoutMargins = .init(topBottom: Styles.grid(3), leftRight: Styles.grid(12))
   cell.contentView.preservesSuperviewLayoutMargins = false
   cell.backgroundColor = .ksr_white
-  cell.layoutMargins = .init(all: 0.0)
   cell.preservesSuperviewLayoutMargins = false
   cell.selectionStyle = .none
+}
+
+private func applyCollectionViewStyle(_ collectionView: UICollectionView) {
+  collectionView.backgroundColor = .clear
+  collectionView.showsHorizontalScrollIndicator = false
+  collectionView.translatesAutoresizingMaskIntoConstraints = false
+}
+
+private func applyCollectionViewLayoutStyle(_ layout: UICollectionViewFlowLayout) {
+  layout.scrollDirection = .horizontal
+  layout.minimumInteritemSpacing = SimilarProjectsCellConstants.collectionViewInteritemSpacing
+  layout.itemSize = SimilarProjectsCellConstants.collectionViewItemSize
+}
+
+private func applyTitleLabelStyle(_ label: UILabel) {
+  label.text = Strings.Similar_projects()
+  label.font = .ksr_title3()
+  label.translatesAutoresizingMaskIntoConstraints = false
 }
