664c189ebd167544ab77b1391eb9fee3ff8bd066
Scott Clampet <110618242+scottkicks@users.noreply.github.com>
2025-03-19 15:30:49 -0400

    [MBL-2166] SPC - Project Cards Loading Cell (#2337)
    * create SimilarProjectsLoadingCollectionViewCell
    
    * use this new loading cell instead of the main project card in SimilarProjectsCollectionViewDataSource when isLoading is true
    
    * make sure the label placeholders also shimmer

--- Kickstarter-iOS/Features/ProjectPage/SimilarProjects/Datasource/SimilarProjectsCollectionViewDataSource.swift
+++ Kickstarter-iOS/Features/ProjectPage/SimilarProjects/Datasource/SimilarProjectsCollectionViewDataSource.swift
@@ -4,7 +4,16 @@ import Library
 import UIKit
 
 final class SimilarProjectsCollectionViewDataSource: ValueCellDataSource {
-  func load(_ values: [any SimilarProject]) {
+  func load(_ values: [any SimilarProject], isLoading: Bool = false) {
+    guard isLoading == false else {
+      /// Sets `[(), ()]` in values so that two cells display to indicate that a collection is loading.
+      return self.set(
+        values: [(), ()],
+        cellClass: SimilarProjectsLoadingCollectionViewCell.self,
+        inSection: 0
+      )
+    }
+
     self.set(
       values: values,
       cellClass: SimilarProjectsCollectionViewCell.self,
@@ -14,6 +23,8 @@ final class SimilarProjectsCollectionViewDataSource: ValueCellDataSource {
 
   override func configureCell(collectionCell cell: UICollectionViewCell, withValue value: Any) {
     switch (cell, value) {
+    case let (cell as SimilarProjectsLoadingCollectionViewCell, value as Void):
+      cell.configureWith(value: value)
     case let (cell as SimilarProjectsCollectionViewCell, value as any SimilarProject):
       cell.configureWith(value: value)
     default:
