686842dad9ab8a8765c956c023495cd9ce7d59d1
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2025-04-01 10:19:50 -0400

    Integrate PaymentMethodsUseCase into NoShippingPledgeViewModel and NoShippingPostCampaignCheckoutViewModel (#2268)
    * Refactor input/output types
    
    * Refactor to include isLoggedIn
    
    * Refactor to add paymentMethodsChangedAndValid
    
    * Integrate PaymentMethodsUseCase into PledgeViewModel
    
    * Tweak a test
    
    * Integrate PaymentMethodsUseCase into PostCampaignCheckoutViewModel
    
    * Appease swift format
    
    * Fix PledgeViewModelTests

--- Library/ViewModels/PostCampaignCheckoutViewModel.swift
+++ Library/ViewModels/PostCampaignCheckoutViewModel.swift
@@ -110,6 +110,11 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
       initialData: initialData.ignoreValues()
     )
 
+    self.paymentMethodsUseCase = PaymentMethodsUseCase(
+      initialData: initialData,
+      isLoggedIn: self.loginSignupUseCase.dataOutputs.isLoggedIn
+    )
+
     // MARK: Calculate totals
 
     let calculatedShippingTotal = Signal.combineLatest(
@@ -215,21 +220,6 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
     let createCheckoutErrors = createCheckoutEvents.errors()
 
     // MARK: Configure views
-
-    self.paymentMethodsViewHidden = self.loginSignupUseCase.dataOutputs.isLoggedIn.map(negate)
-
-    self.configurePaymentMethodsViewControllerWithValue = Signal.combineLatest(
-      project,
-      baseReward,
-      context,
-      refTag,
-      checkoutId
-    )
-    .compactMap { project, reward, context, refTag, checkoutId -> PledgePaymentMethodsValue? in
-      guard let user = AppEnvironment.current.currentUser else { return nil }
-
-      return (user, project, checkoutId, reward, context, refTag)
-    }
 
     self.showWebHelp = Signal.merge(
       self.termsOfUseTappedSignal,
@@ -349,7 +339,7 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
 
     // MARK: Validate Existing Cards
 
-    let selectedCard = self.creditCardSelectedProperty.signal.skipNil()
+    let selectedCard = self.paymentMethodsUseCase.dataOutputs.selectedPaymentSource.skipNil()
 
     let paymentIntentClientSecretForExistingCards = paymentIntentClientSecret
       .takeWhen(selectedCard)
@@ -702,12 +692,10 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
     self.confirmPaymentSuccessfulProperty.value = clientSecret
   }
 
-  private let creditCardSelectedProperty =
-    MutableProperty<PaymentSourceSelected?>(nil)
   public func creditCardSelected(
     source: PaymentSourceSelected
   ) {
-    self.creditCardSelectedProperty.value = source
+    self.paymentMethodsUseCase.creditCardSelected(with: source)
   }
 
   public func goToLoginSignupTapped() {
@@ -758,7 +746,10 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
   // MARK: - Outputs
 
   public let configureEstimatedShippingView: Signal<(String?, String?), Never>
-  public let configurePaymentMethodsViewControllerWithValue: Signal<PledgePaymentMethodsValue, Never>
+  public var configurePaymentMethodsViewControllerWithValue: Signal<PledgePaymentMethodsValue, Never> {
+    return self.paymentMethodsUseCase.configurePaymentMethodsViewControllerWithValue
+  }
+
   public let configurePledgeRewardsSummaryViewWithData: Signal<(
     PostCampaignRewardsSummaryViewData,
     Double?,
@@ -774,7 +765,10 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
   public let processingViewIsHidden: Signal<Bool, Never>
   public let showErrorBannerWithMessage: Signal<String, Never>
   public let showWebHelp: Signal<HelpType, Never>
-  public let paymentMethodsViewHidden: Signal<Bool, Never>
+  public var paymentMethodsViewHidden: Signal<Bool, Never> {
+    return self.paymentMethodsUseCase.paymentMethodsViewHidden
+  }
+
   public let validateCheckoutSuccess: Signal<PaymentSourceValidation, Never>
   public let goToApplePayPaymentAuthorization: Signal<PostCampaignPaymentAuthorizationData, Never>
   public let checkoutComplete: Signal<ThanksPageData, Never>
@@ -786,4 +780,5 @@ public class PostCampaignCheckoutViewModel: PostCampaignCheckoutViewModelType,
   // MARK: - Use cases
 
   private let loginSignupUseCase: LoginSignupUseCase
+  private let paymentMethodsUseCase: PaymentMethodsUseCase
 }
