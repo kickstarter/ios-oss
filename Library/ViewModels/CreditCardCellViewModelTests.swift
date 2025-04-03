c03cbde6025ad48345befecb4f52f28d86249e8a
jovaniks <j.luna.c@kickstarter.com>
2025-04-03 08:59:00 -0700

    Using `image(named:)` helper to avoid issues when load images on test library scope, also added the test target `Library-iOSTests` to Assets (#2368)

--- Library/ViewModels/CreditCardCellViewModelTests.swift
+++ Library/ViewModels/CreditCardCellViewModelTests.swift
@@ -25,42 +25,42 @@ internal final class CreditCardCellViewModelTests: TestCase {
   func testCardInfoForSupportedCards() {
     self.vm.inputs.configureWith(creditCard: UserCreditCards.amex)
 
-    self.cardImage.assertLastValue(UIImage(named: "icon--amex"))
+    self.cardImage.assertLastValue(image(named: "icon--amex"))
     self.cardNumberAccessibilityLabel.assertLastValue("Amex, Card ending in 8882")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 8882")
     self.expirationDateText.assertLastValue("Expires 01/2024")
 
     self.vm.inputs.configureWith(creditCard: UserCreditCards.discover)
 
-    self.cardImage.assertLastValue(UIImage(named: "icon--discover"))
+    self.cardImage.assertLastValue(image(named: "icon--discover"))
     self.cardNumberAccessibilityLabel.assertLastValue("Discover, Card ending in 4242")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 4242")
     self.expirationDateText.assertLastValue("Expires 03/2022")
 
     self.vm.inputs.configureWith(creditCard: UserCreditCards.jcb)
 
-    self.cardImage.assertLastValue(UIImage(named: "icon--jcb"))
+    self.cardImage.assertLastValue(image(named: "icon--jcb"))
     self.cardNumberAccessibilityLabel.assertLastValue("Jcb, Card ending in 2222")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 2222")
     self.expirationDateText.assertLastValue("Expires 01/2022")
 
     self.vm.inputs.configureWith(creditCard: UserCreditCards.masterCard)
 
-    self.cardImage.assertLastValue(UIImage(named: "icon--mastercard"))
+    self.cardImage.assertLastValue(image(named: "icon--mastercard"))
     self.cardNumberAccessibilityLabel.assertLastValue("Mastercard, Card ending in 0000")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 0000")
     self.expirationDateText.assertLastValue("Expires 10/2018")
 
     self.vm.inputs.configureWith(creditCard: UserCreditCards.visa)
 
-    self.cardImage.assertLastValue(UIImage(named: "icon--visa"))
+    self.cardImage.assertLastValue(image(named: "icon--visa"))
     self.cardNumberAccessibilityLabel.assertLastValue("Visa, Card ending in 1111")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 1111")
     self.expirationDateText.assertLastValue("Expires 09/2019")
 
     self.vm.inputs.configureWith(creditCard: UserCreditCards.diners)
 
-    self.cardImage.assertLastValue(UIImage(named: "icon--diners"))
+    self.cardImage.assertLastValue(image(named: "icon--diners"))
     self.cardNumberAccessibilityLabel.assertLastValue("Diners, Card ending in 1212")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 1212")
     self.expirationDateText.assertLastValue("Expires 09/2022")
@@ -69,7 +69,7 @@ internal final class CreditCardCellViewModelTests: TestCase {
   func testSelectedCard() {
     self.vm.inputs.configureWith(creditCard: UserCreditCards.generic)
 
-    self.cardImage.assertValue(UIImage(named: "icon--generic"))
+    self.cardImage.assertValue(image(named: "icon--generic"))
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 1882")
     self.expirationDateText.assertValue("Expires 01/2024")
   }
@@ -77,7 +77,7 @@ internal final class CreditCardCellViewModelTests: TestCase {
   func testCardInfoForUnsupportedCards() {
     self.vm.inputs.configureWith(creditCard: UserCreditCards.generic)
 
-    self.cardImage.assertValue(UIImage(named: "icon--generic"))
+    self.cardImage.assertValue(image(named: "icon--generic"))
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 1882")
     self.expirationDateText.assertValue("Expires 01/2024")
   }
@@ -87,7 +87,7 @@ internal final class CreditCardCellViewModelTests: TestCase {
 
     self.vm.inputs.configureWith(creditCard: unknownCard)
 
-    self.cardImage.assertValue(UIImage(named: "icon--generic"))
+    self.cardImage.assertValue(image(named: "icon--generic"))
     self.cardNumberAccessibilityLabel.assertLastValue("Card ending in 1882")
     self.cardNumberTextLongStyle.assertLastValue("Card ending in 1882")
     self.expirationDateText.assertValue("Expires 01/2024")
