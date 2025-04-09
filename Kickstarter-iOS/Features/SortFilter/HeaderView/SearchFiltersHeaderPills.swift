908502cf06e61ff82832a6c634901bd6002df599
Amy <a.dyer@kickstarter.com>
2025-04-09 14:50:52 -0400

    Extra 3 px

--- Kickstarter-iOS/Features/SortFilter/HeaderView/SearchFiltersHeaderPills.swift
+++ Kickstarter-iOS/Features/SortFilter/HeaderView/SearchFiltersHeaderPills.swift
@@ -5,28 +5,28 @@ private let tintColor = Colors.Text.primary.swiftUIColor()
 private let selectedColor = Colors.Border.subtle.swiftUIColor()
 private let buttonFont = InterFont.headingMD
 
-private struct SearchFiltersPillStyle<S: Shape>: SwiftUI.ButtonStyle {
+private struct SearchFiltersPillStyle: SwiftUI.ButtonStyle {
   let isHighlighted: Bool
-  let shape: S
 
   func makeBody(configuration: Configuration) -> some View {
     configuration.label
       .buttonStyle(.borderless)
       .font(buttonFont.swiftUIFont(size: nil))
-      .padding(EdgeInsets(
-        top: Styles.grid(1),
-        leading: Styles.grid(1),
-        bottom: Styles.grid(1),
-        trailing: Styles.grid(1)
-      ))
       .foregroundStyle(.tint)
-      .compositingGroup()
       .tint(
         tintColor
       )
-      .clipShape(self.shape)
+      .padding(
+        EdgeInsets(
+          top: Styles.grid(2),
+          leading: Styles.grid(2),
+          bottom: Styles.grid(2),
+          trailing: Styles.grid(2)
+        )
+      )
+      .clipShape(Capsule())
       .overlay(
-        self.shape
+        Capsule()
           .stroke(
             configuration.isPressed ? selectedColor : tintColor,
             lineWidth: self.isHighlighted ? 2.0 : 1.0
@@ -40,7 +40,13 @@ internal struct ImagePillButton: View {
   let image: UIImage
   let isHighlighted: Bool
 
-  @ScaledMetric var scale: CGFloat = 1
+  // We want pills with icons to render at the same height
+  // as pills with text, even with large font sizes.
+  @ScaledMetric var scaledSize: CGFloat = buttonFont.defaultSize
+
+  // This extra 3 px was determined via trial-and-error.
+  // It makes this pill render *exactly* the same height as a text pill.
+  let fontPadding = 3.0
 
   var body: some View {
     Button(action: self.action) {
@@ -48,21 +54,13 @@ internal struct ImagePillButton: View {
         .renderingMode(.template)
         .aspectRatio(1.0, contentMode: .fill)
         .frame(
-          width: self.scale * buttonFont.defaultSize,
-          height: self.scale * buttonFont.defaultSize
-        )
-        .padding(
-          EdgeInsets(
-            top: Styles.gridHalf(1),
-            leading: Styles.gridHalf(1),
-            bottom: Styles.gridHalf(1),
-            trailing: Styles.gridHalf(1)
-          )
+          // Always render image pills as square, so the capsule becomes a circle.
+          width: self.scaledSize + self.fontPadding,
+          height: self.scaledSize + self.fontPadding
         )
     }
     .buttonStyle(SearchFiltersPillStyle(
-      isHighlighted: self.isHighlighted,
-      shape: Circle()
+      isHighlighted: self.isHighlighted
     ))
   }
 }
@@ -83,16 +81,9 @@ internal struct DropdownPillButton: View {
             .aspectRatio(contentMode: .fill)
         }
       }
-      .padding(EdgeInsets(
-        top: Styles.gridHalf(1),
-        leading: Styles.grid(1),
-        bottom: Styles.gridHalf(1),
-        trailing: Styles.grid(1)
-      ))
     }
     .buttonStyle(SearchFiltersPillStyle(
-      isHighlighted: self.isHighlighted,
-      shape: Capsule()
+      isHighlighted: self.isHighlighted
     ))
   }
 }
