fd260aa69ec403faf2395b58c1600a3a4053e350
Amy <a.dyer@kickstarter.com>
2025-04-09 10:44:14 -0400

    Remove unused method

--- Library/UseCases/SearchFilterPill.swift
+++ Library/UseCases/SearchFilterPill.swift
@@ -12,14 +12,6 @@ public struct SearchFilterPill: Identifiable {
   public let filterType: FilterType
   public let buttonType: ButtonType
 
-  public var isImage: Bool {
-    if case let .image(string) = buttonType {
-      return true
-    }
-
-    return false
-  }
-
   /// What kind of option the pill represents.
   public enum FilterType {
     case sort
