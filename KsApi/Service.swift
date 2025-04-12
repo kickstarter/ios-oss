ad001a90fedaacf0b30b9e32219d34daf1d28ad8
Steve Streza <s.streza@kickstarter.com>
2025-04-11 17:38:54 -0700

    Rebase cleanup

--- KsApi/Service.swift
+++ KsApi/Service.swift
@@ -746,7 +746,7 @@ public struct Service: ServiceType {
 
   public func incrementVideoCompletion(for project: any HasProjectWebURL) ->
     SignalProducer<VoidEnvelope, ErrorEnvelope> {
-    let producer = request(.incrementVideoCompletion(project: project.serviceProjectWebURL))
+    let producer = request(.incrementVideoCompletion(project: project))
       as SignalProducer<VoidEnvelope, ErrorEnvelope>
 
     return producer
@@ -760,7 +760,7 @@ public struct Service: ServiceType {
 
   public func incrementVideoStart(forProject project: any HasProjectWebURL) ->
     SignalProducer<VoidEnvelope, ErrorEnvelope> {
-    let producer = request(.incrementVideoStart(project: project.serviceProjectWebURL))
+    let producer = request(.incrementVideoStart(project: project))
       as SignalProducer<VoidEnvelope, ErrorEnvelope>
 
     return producer
