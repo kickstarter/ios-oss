a217210c900e418ee81d2e0e3c532a2916ae43b0
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2025-04-25 16:24:01 -0400

    MBL-2220: Implement parent filter modal for search phase 2 (#2399)
    * MBL-2220: Implement parent filter modal for search phase 2
    
    * Suggestion from PR #2399

--- Kickstarter-iOS/Features/SortFilter/FilterCategory/FilterCategoryViewModel.swift
+++ Kickstarter-iOS/Features/SortFilter/FilterCategory/FilterCategoryViewModel.swift
@@ -11,18 +11,12 @@ protocol FilterCategory: Identifiable, Equatable {
 protocol FilterCategoryViewModelInputs {
   associatedtype T: FilterCategory
   func selectCategory(_ category: T, subcategory: T?)
-  func seeResults()
-  func close()
-  func resetSelection()
 }
 
 protocol FilterCategoryViewModelOutputs {
   associatedtype T: FilterCategory
   var selectedCategory: AnyPublisher<CategoryAndSubcategory<T>?, Never> { get }
-  var seeResultsTapped: AnyPublisher<Void, Never> { get }
-  var closeTapped: AnyPublisher<Void, Never> { get }
   var categories: [T] { get }
-  var canReset: Bool { get }
   var isLoading: Bool { get }
   func isCategorySelected(_ category: T) -> Bool
   func isSubcategorySelected(_ category: T?) -> Bool
@@ -63,7 +57,21 @@ class FilterCategoryViewModel<T: FilterCategory>: FilterCategoryViewModelType {
       .assign(to: &self.$currentSubcategory)
 
     if let category = selectedCategory {
+      self.selectInitialCategory(selectedCategory: category)
+    }
+  }
+
+  private func selectInitialCategory(selectedCategory: T) {
+    if let category = self.categories.first(where: { $0 == selectedCategory }) {
       self.selectCategory(category, subcategory: nil)
+      return
+    }
+
+    for category in self.categories {
+      if let subcategory = category.availableSubcategories?.first(where: { $0 == selectedCategory }) {
+        self.selectCategory(category, subcategory: subcategory)
+        return
+      }
     }
   }
 
@@ -77,31 +85,13 @@ class FilterCategoryViewModel<T: FilterCategory>: FilterCategoryViewModelType {
     self.selectedCategorySubject.send(nil)
   }
 
-  func seeResults() {
-    self.seeResultsTappedSubject.send()
-  }
-
-  func close() {
-    self.closeTappedSubject.send()
-  }
-
   // MARK: - Outputs
 
   var selectedCategory: AnyPublisher<CategoryAndSubcategory<T>?, Never> {
     self.selectedCategorySubject.eraseToAnyPublisher()
   }
 
-  var seeResultsTapped: AnyPublisher<Void, Never> {
-    self.seeResultsTappedSubject.eraseToAnyPublisher()
-  }
-
-  var closeTapped: AnyPublisher<Void, Never> {
-    self.closeTappedSubject.eraseToAnyPublisher()
-  }
-
   private let selectedCategorySubject = PassthroughSubject<CategoryAndSubcategory<T>?, Never>()
-  private let seeResultsTappedSubject = PassthroughSubject<Void, Never>()
-  private let closeTappedSubject = PassthroughSubject<Void, Never>()
 
   func isCategorySelected(_ category: T) -> Bool {
     self.currentCategory?.id == category.id
