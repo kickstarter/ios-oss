c915802f01d36b3e5e6c43db2bf83b521cb619fd
Amy <a.dyer@kickstarter.com>
2025-05-14 10:51:56 -0400

    MBL-2357: Refactor new design system colors to use SemanticColor
    - Replace all enum values in Colors with SemanticColor objects
    - Refactor any code that was using those enums
    - Add ColorsView to the Beta page, to view all semantic colors
    - Delete unused color assets from the catalog

--- Kickstarter-iOS/Features/Search/Views/Cell/SearchResultsCountCell.swift
+++ Kickstarter-iOS/Features/Search/Views/Cell/SearchResultsCountCell.swift
@@ -29,12 +29,12 @@ internal final class SearchResultsCountCell: UITableViewCell, ValueCell {
 
     self.selectionStyle = .none
     // FIXME: MBL-2423 this should be surfaceSecondary.
-    self.backgroundColor = Colors.Background.surfacePrimary.adaptive()
+    self.backgroundColor = Colors.Background.Surface.primary.uiColor()
 
     self.titleLabel.constrainViewToMargins(in: self.contentView)
     self.contentView.layoutMargins = UIEdgeInsets(all: 18.0)
 
-    self.titleLabel.textColor = Colors.Text.secondary.adaptive()
+    self.titleLabel.textColor = Colors.Text.secondary.uiColor()
     self.titleLabel.textAlignment = .center
     self.titleLabel.font = InterFont.bodyMD.font()
   }
