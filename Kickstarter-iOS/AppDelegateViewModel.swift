015b0aed0b84d8ef064b744d12c19018efb417e4
Amy <a.dyer@kickstarter.com>
2025-05-15 10:36:02 -0400

    Enable dark mode in Info.plist

--- Kickstarter-iOS/AppDelegateViewModel.swift
+++ Kickstarter-iOS/AppDelegateViewModel.swift
@@ -779,7 +779,7 @@ public final class AppDelegateViewModel: AppDelegateViewModelType, AppDelegateVi
       self.didUpdateRemoteConfigClientProperty.signal.ignoreValues()
     )
     .map { _ in featureDarkModeEnabled() }
-    // .skipRepeats(==)
+    .skipRepeats(==)
   }
 
   public var inputs: AppDelegateViewModelInputs { return self }
