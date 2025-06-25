cc759bb1b438cfa7e29bd3a049f412f14f934e14
Amy <a.dyer@kickstarter.com>
2025-06-25 10:32:07 -0400

    MBL-2521: Add pledged (amount raised) to DiscoveryParams and helpers

--- KsApi/models/DiscoveryParams.swift
+++ KsApi/models/DiscoveryParams.swift
@@ -21,6 +21,7 @@ public struct DiscoveryParams {
   public var state: State?
   public var percentRaised: PercentRaisedBucket?
   public var location: Location?
+  public var pledged: PledgedBucket?
 
   public enum State: String, Decodable {
     case all
@@ -57,6 +58,23 @@ public struct DiscoveryParams {
     case bucket_1
     /// 100% or more
     case bucket_2
+  }
+
+  public enum PledgedBucket: CaseIterable {
+    /// Range from 0 to 1,000 USD
+    case bucket_0
+
+    /// Range from 1,000 to 10,000 USD
+    case bucket_1
+
+    /// Range from 10,000 to 100,000 USD
+    case bucket_2
+
+    /// Range from 100,000 to 1,000,000 USD
+    case bucket_3
+
+    /// Range from 1,000,000 to Infinity USD
+    case bucket_4
   }
 
   public static let defaults = DiscoveryParams(
@@ -221,11 +239,15 @@ extension DiscoveryParams {
       }
     }
 
-    if let raised = self.percentRaised {
+    if let _ = self.percentRaised {
       return false
     }
 
-    if let location = self.location {
+    if let _ = self.location {
+      return false
+    }
+
+    if let _ = self.pledged {
       return false
     }
 
