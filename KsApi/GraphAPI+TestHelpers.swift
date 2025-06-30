41127dd6d15f2b6c31037225e4afc11975a66483
Amy <a.dyer@kickstarter.com>
2025-06-30 17:01:43 -0400

    Make GraphAPI+TestHelpers build

--- KsApi/GraphAPI+TestHelpers.swift
+++ KsApi/GraphAPI+TestHelpers.swift
@@ -1,30 +1,35 @@
 import Apollo
+import ApolloAPI
 import Foundation
+import KsApi
 
 public enum GraphQLSelectionSetStringError: Error {
   case unableToInitData
   case fileNotFoundError
 }
 
-func testGraphObject<T: GraphQLSelectionSet>(data: [String: Any?]) -> T {
-  return T.init(unsafeResultMap: data)
+func testGraphObject<T: GraphAPI.SelectionSet>(data: [String: Any]) throws -> T {
+  return try T.init(data: data)
 }
 
-func testGraphObject<T: GraphQLSelectionSet>(jsonString: String, variables: GraphQLMap? = nil) throws -> T {
+func testGraphObject<T: GraphAPI.SelectionSet>(
+  jsonString: String,
+  variables: GraphQLOperation.Variables? = nil
+) throws -> T {
   guard let data = jsonString.data(using: .utf8) else {
     throw GraphQLSelectionSetStringError.unableToInitData
   }
 
   let json = try JSONSerialization.jsonObject(with: data)
-  return try T.init(jsonObject: json as! JSONObject, variables: variables)
+  return try T.init(data: json as! JSONObject, variables: variables)
 }
 
-func testGraphObject<T: GraphQLSelectionSet>(
+func testGraphObject<T: GraphAPI.SelectionSet>(
   fromResource resource: URL,
-  variables: GraphQLMap? = nil
+  variables: GraphQLOperation.Variables? = nil
 ) throws -> T {
   let jsonData = try! Data(contentsOf: resource)
   let json = try! JSONSerialization.jsonObject(with: jsonData) as! JSONObject
   let data = json["data"]
-  return try T.init(jsonObject: data as! JSONObject, variables: variables)
+  return try T.init(data: data as! JSONObject, variables: variables)
 }
