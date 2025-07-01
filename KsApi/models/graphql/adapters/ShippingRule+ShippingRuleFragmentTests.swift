d85cbf46539077f8f0de204ce6108a697976881d
Amy <a.dyer@kickstarter.com>
2025-07-01 12:03:00 -0400

    MBL-915: Update all fragment tests in KsApi to use testGraphObject instead of initializers

--- KsApi/models/graphql/adapters/ShippingRule+ShippingRuleFragmentTests.swift
+++ KsApi/models/graphql/adapters/ShippingRule+ShippingRuleFragmentTests.swift
@@ -4,28 +4,30 @@ import XCTest
 
 final class ShippingRule_ShippingRuleFragmentTests: XCTestCase {
   func test() {
-    let shippingRuleFragment = GraphAPI.ShippingRuleFragment(
-      cost: GraphAPI.ShippingRuleFragment.Cost(
-        amount: "50",
-        currency: .usd,
-        symbol: "$"
-      ),
-      id: "TG9jYXRpb24tMjM0MjQ3NzU=",
-      location: GraphAPI.ShippingRuleFragment.Location(
-        country: "CA",
-        countryName: "Canada",
-        displayableName: "Canada",
-        id: "TG9jYXRpb24tMjM0MjQ3NzU=",
-        name: "Canada"
-      ),
-      estimatedMin: GraphAPI.ShippingRuleFragment.EstimatedMin(
-        amount: "10.00",
-        currency: GraphAPI.CurrencyCode.usd
-      ),
-      estimatedMax: GraphAPI.ShippingRuleFragment.EstimatedMax(
-        amount: "20.00",
-        currency: GraphAPI.CurrencyCode.usd
-      )
+    let shippingRuleFragment: GraphAPI.ShippingRuleFragment = testGraphObject(
+      data: [
+        "cost": [
+          "amount": "50",
+          "currency": GraphAPI.CurrencyCode.usd,
+          "symbol": "$"
+        ],
+        "id": "TG9jYXRpb24tMjM0MjQ3NzU=",
+        "location": [
+          "country": "CA",
+          "countryName": "Canada",
+          "displayableName": "Canada",
+          "id": "TG9jYXRpb24tMjM0MjQ3NzU=",
+          "name": "Canada"
+        ],
+        "estimatedMin": [
+          "amount": "10.00",
+          "currency": GraphAPI.CurrencyCode.usd
+        ],
+        "estimatedMax": [
+          "amount": "20.00",
+          "currency": GraphAPI.CurrencyCode.usd
+        ]
+      ]
     )
 
     XCTAssertNotNil(ShippingRule.shippingRule(from: shippingRuleFragment))
