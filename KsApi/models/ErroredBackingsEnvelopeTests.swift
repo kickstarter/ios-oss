a0c0379af6bd390df2c3ae5c49bb345adf53e35e
Amy <a.dyer@kickstarter.com>
2025-07-16 13:52:29 -0400

    Add comment on disabled test

--- KsApi/models/ErroredBackingsEnvelopeTests.swift
+++ KsApi/models/ErroredBackingsEnvelopeTests.swift
@@ -4,6 +4,7 @@ import ReactiveSwift
 import XCTest
 
 final class ErroredBackingsEnvelopeTests: XCTestCase {
+  // We don't use ErroredBackingsEnvelope.producer, so no real reason to fix this one.
   func disabled_testErroredBackingsEnvelope_Success() {
     let producer = ErroredBackingsEnvelope.producer(from: FetchUserBackingsQueryTemplate.valid.data)
 
