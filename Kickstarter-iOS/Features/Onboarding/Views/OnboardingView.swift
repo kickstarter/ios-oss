e1ceea8e9a87621a284263f8d024e95171bddfad
Scott Clampet <s.clampet@kickstarter.com>
2025-07-17 16:28:00 -0400

    present ATT popup and navigate to next onboarding item on complete

--- Kickstarter-iOS/Features/Onboarding/Views/OnboardingView.swift
+++ Kickstarter-iOS/Features/Onboarding/Views/OnboardingView.swift
@@ -95,6 +95,9 @@ public struct OnboardingView: View {
       self.viewModel.triggerAppTrackingTransparencyPopup.observeValues {
         self.presentAppTrackingPopup()
       }
+      .onReceive(self.viewModel.triggerAppTrackingTransparencyPopup) {
+        self.presentAppTrackingPopup()
+      }
     }
   }
 
