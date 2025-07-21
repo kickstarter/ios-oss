2c4c22e560ecf434c81bcdcb866cd2abd95543f3
Scott Clampet <s.clampet@kickstarter.com>
2025-07-21 13:47:23 -0400

    present push notifications popup

--- Kickstarter-iOS/Features/Onboarding/Views/OnboardingView.swift
+++ Kickstarter-iOS/Features/Onboarding/Views/OnboardingView.swift
@@ -98,6 +98,9 @@ public struct OnboardingView: View {
       .onReceive(self.viewModel.triggerAppTrackingTransparencyPopup) {
         self.presentAppTrackingPopup()
       }
+      .onReceive(self.viewModel.triggerPushNotificationPermissionDialog) {
+        self.goToNextItem()
+      }
     }
   }
 
