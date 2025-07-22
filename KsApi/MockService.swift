e554209793f85a6b88f2f0f676f11fd51a730c08
Jovani Luna <j.luna.c@kickstarter.com>
2025-07-22 05:20:57 -0700

    Add refunded and refundedAmount to paymentIncrements models and fragments

--- KsApi/MockService.swift
+++ KsApi/MockService.swift
@@ -639,11 +639,7 @@
         return .empty
       }
 
-      let query = GraphAPI.BuildPaymentPlanQuery(
-        slug: projectSlug,
-        amount: pledgeAmount,
-        includeRefundedAmount: false
-      )
+      let query = GraphAPI.BuildPaymentPlanQuery(slug: projectSlug, amount: pledgeAmount, includeRefundedAmount: false)
 
       return client.fetchWithResult(query: query, result: self.buildPaymentPlanResult)
     }
