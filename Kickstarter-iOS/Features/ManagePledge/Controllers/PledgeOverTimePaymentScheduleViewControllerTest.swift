3bc3fa3a9ee5129d995e9dca99fbe26c054414a4
Jovani Luna <j.luna.c@kickstarter.com>
2025-07-24 08:25:16 -0700

    Adding new badge colors

--- Kickstarter-iOS/Features/ManagePledge/Controllers/PledgeOverTimePaymentScheduleViewControllerTest.swift
+++ Kickstarter-iOS/Features/ManagePledge/Controllers/PledgeOverTimePaymentScheduleViewControllerTest.swift
@@ -35,20 +35,35 @@ final class PledgeOverTimePaymentScheduleViewControllerTest: TestCase {
   }
 
   func testView_PaymentSchedule_Expanded() {
+    let darkModeOn = MockRemoteConfigClient()
+    darkModeOn.features = [
+      RemoteConfigFeature.darkModeEnabled.rawValue: true,
+      RemoteConfigFeature.newDesignSystem.rawValue: true
+    ]
+
     let increments = mockPaymentIncrements()
-    orthogonalCombos([Language.en], [Device.pad, Device.phone4_7inch]).forEach { language, device in
-      withEnvironment(language: language) {
+    orthogonalCombos(
+      [Language.en],
+      [Device.pad, Device.phone4_7inch],
+      [UIUserInterfaceStyle.light, UIUserInterfaceStyle.dark]
+    ).forEach { language, device, style in
+      withEnvironment(colorResolver: AppColorResolver(), language: language, remoteConfigClient: darkModeOn) {
         let controller = PledgeOverTimePaymentScheduleViewController.instantiate()
+        controller.overrideUserInterfaceStyle = style
 
         let (parent, _) = traitControllers(device: device, orientation: .portrait, child: controller)
-        parent.view.frame.size.height = 420
+        parent.view.frame.size.height = 520
 
         controller.configure(with: increments)
         controller.collapseToggle()
 
         self.scheduler.advance(by: .seconds(1))
 
-        assertSnapshot(matching: parent.view, as: .image, named: "lang_\(language)_device_\(device)")
+        assertSnapshot(
+          matching: parent.view,
+          as: .image,
+          named: "lang_\(language)_device_\(device)_style_\(style.description)"
+        )
       }
     }
   }
