faf2576c3507d40a113c3ad52c7a6cdbd4f47a91
Amy <a.dyer@kickstarter.com>
2025-08-04 10:52:47 -0400

    MBL-2653: Add Spacing and Dimension to new design system

---
+++ Library/Styles/Spacing/CoreSpacing.swift
@@ -0,0 +1,46 @@
+import CoreFoundation
+
+/// A unit of spacing in our design system. Equal to Spacing/01.
+public let Spacing_01: CGFloat = 4.0
+
+/// A unit of spacing in our design system. Equal to Spacing/02.
+public let Spacing_02: CGFloat = 8.0
+
+/// A unit of spacing in our design system. Equal to Spacing/03.
+public let Spacing_03: CGFloat = 12.0
+
+/// A unit of spacing in our design system. Equal to Spacing/04.
+public let Spacing_04: CGFloat = 16.0
+
+/// A unit of spacing in our design system. Equal to Spacing/05.
+public let Spacing_05: CGFloat = 20.0
+
+/// A unit of spacing in our design system. Equal to Spacing/06.
+public let Spacing_06: CGFloat = 24.0
+
+/// A unit of spacing in our design system. Equal to Spacing/07.
+public let Spacing_07: CGFloat = 28.0
+
+/// A unit of spacing in our design system. Equal to Spacing/08.
+public let Spacing_08: CGFloat = 32.0
+
+/// A unit of spacing in our design system. Equal to Spacing/09.
+public let Spacing_09: CGFloat = 36.0
+
+/// A unit of spacing in our design system. Equal to Spacing/10.
+public let Spacing_10: CGFloat = 40.0
+
+/// A unit of spacing in our design system. Equal to Spacing/11.
+public let Spacing_11: CGFloat = 44.0
+
+/// A unit of spacing in our design system. Equal to Spacing/12.
+public let Spacing_12: CGFloat = 48.0
+
+/// A unit of spacing in our design system. Equal to Spacing/14.
+public let Spacing_14: CGFloat = 56.0
+
+/// A unit of spacing in our design system. Equal to Spacing/16.
+public let Spacing_16: CGFloat = 64.0
+
+/// A unit of spacing in our design system. Equal to Spacing/20.
+public let Spacing_20: CGFloat = 80.0
---
+++ Library/Styles/Spacing/Dimension.swift
@@ -0,0 +1,10 @@
+import CoreFoundation
+
+/// Represents semantic spacing constants in our design system.
+public struct Dimension {
+  public struct CornerRadius {
+    public static let small = Spacing_01
+    public static let medium = Spacing_02
+    public static let large = Spacing_04
+  }
+}
