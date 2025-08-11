6267f3b484b2b3615988983586c200dd842054b1
Amy <a.dyer@kickstarter.com>
2025-08-11 11:34:37 -0400

    Rename global variables Spacing_xx to Spacing.unit_xx

--- Library/Styles/Spacing/Dimension.swift
+++ Library/Styles/Spacing/Dimension.swift
@@ -3,8 +3,8 @@ import CoreFoundation
 /// Represents semantic spacing constants in our design system.
 public struct Dimension {
   public struct CornerRadius {
-    public static let small = Spacing_01
-    public static let medium = Spacing_02
-    public static let large = Spacing_04
+    public static let small = Spacing.unit_01
+    public static let medium = Spacing.unit_02
+    public static let large = Spacing.unit_04
   }
 }
