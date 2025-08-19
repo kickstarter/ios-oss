e0938819c2de614edff0e563d62bd92668a267cb
Amy <a.dyer@kickstarter.com>
2025-08-19 09:54:05 -0400

    Create KDS package

---
+++ KDS/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "KDS",
+  platforms: [
+    .iOS(.v16)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "KDS",
+      targets: ["KDS"]
+    )
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "KDS"
+    ),
+    .testTarget(
+      name: "KDSTests",
+      dependencies: ["KDS"]
+    )
+  ],
+  swiftLanguageModes: [
+    .v5
+  ]
+)
---
+++ KDS/Tests/KDSTests/KDSTests.swift
@@ -0,0 +1,12 @@
+@testable import KDS
+import XCTest
+
+final class KDSTests: XCTestCase {
+  func testExample() throws {
+    // XCTest Documentation
+    // https://developer.apple.com/documentation/xctest
+
+    // Defining Test Cases and Test Methods
+    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+  }
+}
