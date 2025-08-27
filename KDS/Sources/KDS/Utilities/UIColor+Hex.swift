a4cc6e7a0e5f3160af99e44d3b5ffba1e04bf4cf
Amy <a.dyer@kickstarter.com>
2025-08-27 13:53:01 -0400

    Make UIColor.hex private again, fix styles that didn't respect that

--- KDS/Sources/KDS/Utilities/UIColor+Hex.swift
+++ KDS/Sources/KDS/Utilities/UIColor+Hex.swift
@@ -12,7 +12,7 @@ extension UIColor {
     return UIColor(red: r, green: g, blue: b, alpha: a)
   }
 
-  @nonobjc public static func hex(_ value: UInt32) -> UIColor {
+  @nonobjc static func hex(_ value: UInt32) -> UIColor {
     let r = CGFloat((value & 0xFF0000) >> 16) / 255.0
     let g = CGFloat((value & 0xFF00) >> 8) / 255.0
     let b = CGFloat(value & 0xFF) / 255.0
