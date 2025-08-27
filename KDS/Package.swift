69875a8ed06c7900b6199f7dc14b4e12c2edf046
Amy <a.dyer@kickstarter.com>
2025-08-27 13:37:29 -0400

    Move font resources from Library into KDS

--- KDS/Package.swift
+++ KDS/Package.swift
@@ -19,7 +19,10 @@ let package = Package(
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "KDS"
+      name: "KDS",
+      resources: [
+        .process("Fonts/Resources")
+      ]
     ),
     .testTarget(
       name: "KDSTests",
