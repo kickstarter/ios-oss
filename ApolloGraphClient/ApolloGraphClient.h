cb1dec2bc1911f8f44547c7d35e3abe6d60f15d4
Scott Clampet <s.clampet@kickstarter.com>
2025-09-10 09:32:21 -0700

    add a new empty ApolloGraphClient target and scheme that supports Swift 6
    * links KsApi, Library and Apollo

---
+++ ApolloGraphClient/ApolloGraphClient.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApolloGraphClient.
+FOUNDATION_EXPORT double ApolloGraphClientVersionNumber;
+
+//! Project version string for ApolloGraphClient.
+FOUNDATION_EXPORT const unsigned char ApolloGraphClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApolloGraphClient/PublicHeader.h>
+
+
---
+++ ApolloGraphClientTests/ApolloGraphClientTests.swift
@@ -0,0 +1,8 @@
+@testable import ApolloGraphClient
+import Testing
+
+struct ApolloGraphClientTests {
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+}
