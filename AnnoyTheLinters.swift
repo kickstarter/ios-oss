0b5ef2f17b926b78475efb36904d8342d2da9f86
Amy <a.dyer@kickstarter.com>
2025-09-15 12:24:35 -0400

    Appease swiftformat

--- AnnoyTheLinters.swift
+++ AnnoyTheLinters.swift
@@ -1,11 +1,1 @@
-//
-//  AnnoyTheLinters.swift
-//  Kickstarter
-//
-//  Created by Amy Dyer on 9/15/25.
-//  Copyright © 2025 Kickstarter. All rights reserved.
-//
-
-public struct FooBar {
-  
-}   
+public struct FooBar {}
