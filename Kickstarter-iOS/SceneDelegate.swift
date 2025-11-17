4725f56e7434432f75ddbe8cc13d7132c2636413
Steve Streza <s.streza@kickstarter.com>
2025-11-17 09:50:33 -0800

    Initial scene delegate

---
+++ Kickstarter-iOS/SceneDelegate.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(
+      _ scene: UIScene,
+      willConnectTo session: UISceneSession,
+      options connectionOptions: UIScene.ConnectionOptions
+  ) {
+  }
+}
