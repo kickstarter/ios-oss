1311d7fe914d71bfa45509e3981491b2d3009d72
Steve Streza <s.streza@kickstarter.com>
2025-12-03 13:02:32 -0800

    Cleanup and fixing linking errors by propagating GraphAPI into Library-iOS and Kickstarter-Framework

--- ServerDrivenUI/Sources/ServerDrivenUI/ServerDrivenUI.swift
+++ ServerDrivenUI/Sources/ServerDrivenUI/ServerDrivenUI.swift
@@ -1,6 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 public class SDUI {
   public class func name() -> String {
     return "ServerDrivenUI"
--- ServerDrivenUI/Tests/ServerDrivenUITests/ServerDrivenUITests.swift
+++ ServerDrivenUI/Tests/ServerDrivenUITests/ServerDrivenUITests.swift
@@ -2,6 +2,5 @@ import Testing
 @testable import ServerDrivenUI
 
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
   #expect("\(42)" == "42")
 }
