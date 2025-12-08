baa4664bc7831ad7a4316141b8faa9a3104af973
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2025-12-08 12:04:24 -0500

    MBL-2832: Add ShippingLocationsView (#2656)
    This is a SwiftUI view which will replace ShippingRulesTableViewController.

---
+++ Kickstarter-iOS/Features/ShippingRules/ShippingLocationsView.swift
@@ -0,0 +1,112 @@
+import KDS
+import KsApi
+import Library
+import SwiftUI
+
+public func shippingLocationsViewController(
+  withLocations locations: [Location],
+  selectedLocation: Location?,
+  onSelectedLocation: @escaping (Location) -> Void,
+  onCancelled: @escaping () -> Void
+) -> UIViewController {
+  var view = ShippingLocationsView(
+    withLocations: locations,
+    selectedLocation: selectedLocation
+  )
+  view.onSelectedLocation = onSelectedLocation
+  view.onCancelled = onCancelled
+
+  return UIHostingController(rootView: view)
+}
+
+private struct ShippingLocationsView: View {
+  @StateObject private var viewModel: ShippingLocationsViewModel
+  fileprivate var onSelectedLocation: ((Location) -> Void)?
+  fileprivate var onCancelled: (() -> Void)?
+
+  // This pattern can lead to unexpected behavior if this view becomes
+  // a child view of another SwiftUI view. If you're refactoring this to be embedded
+  // in anything _other_ than UIHostingViewController, refactor away this init method.
+  init(withLocations locations: [Location], selectedLocation: Location?) {
+    self._viewModel = StateObject(wrappedValue: ShippingLocationsViewModel(
+      withLocations: locations,
+      selectedLocation: selectedLocation
+    ))
+  }
+
+  @State private var searchText: String = ""
+
+  @ViewBuilder var locationsList: some View {
+    VStack(alignment: .leading, spacing: Spacing.unit_06) {
+      ForEach(self.viewModel.outputs.displayedLocations) { location in
+        Button {
+          self.viewModel.inputs.selectedLocation(location)
+        } label: {
+          ShippingLocationsRow(
+            title: location.localizedName,
+            isSelected: self.viewModel.outputs.isLocationSelected(location)
+          )
+        }
+        .id(location.id)
+      }
+    }
+    .padding(Spacing.unit_06)
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+  }
+
+  public var body: some View {
+    NavigationStack {
+      ScrollViewReader { reader in
+        ScrollView {
+          self.locationsList
+        }
+        .onAppear {
+          if let selectedLocation = self.viewModel.outputs.selectedLocation {
+            reader.scrollTo(selectedLocation.id, anchor: .center)
+          }
+        }
+      }
+      .searchable(
+        text: self.$searchText,
+        placement: .navigationBarDrawer(displayMode: .always)
+      )
+      .navigationTitle(Strings.Location())
+      .toolbar {
+        ToolbarItem(placement: .cancellationAction) {
+          Button(Strings.Cancel()) {
+            if let onCancelled = self.onCancelled {
+              onCancelled()
+            }
+          }
+        }
+      }
+    }
+    .onChange(of: self.searchText) { _, newValue in
+      self.viewModel.inputs.filteredLocations(withTerm: newValue)
+    }
+    .onChange(of: self.viewModel.outputs.selectedLocation) { _, newValue in
+      if let newValue, let onSelectedLocation = self.onSelectedLocation {
+        onSelectedLocation(newValue)
+      }
+    }
+  }
+}
+
+struct ShippingLocationsRow: View {
+  let title: String
+  let isSelected: Bool
+
+  public var body: some View {
+    HStack(spacing: Spacing.unit_02) {
+      Text(self.title)
+        .font(InterFont.bodyLG.swiftUIFont())
+        .multilineTextAlignment(.leading)
+        .foregroundStyle(Colors.Text.primary.swiftUIColor())
+
+      if self.isSelected, let checkmark = Library.image(named: "checkmark") {
+        Spacer()
+        Image(uiImage: checkmark)
+      }
+    }
+  }
+}
---
+++ Kickstarter-iOS/Features/ShippingRules/ShippingLocationsViewModel.swift
@@ -0,0 +1,63 @@
+import KsApi
+import SwiftUICore
+
+protocol ShippingLocationsViewModelInputs {
+  func selectedLocation(_ location: Location)
+  func filteredLocations(withTerm searchTerm: String)
+}
+
+protocol ShippingLocationsViewModelOutputs {
+  func isLocationSelected(_ location: Location) -> Bool
+  var displayedLocations: [Location] { get }
+  var selectedLocation: Location? { get }
+}
+
+protocol ShippingLocationsViewModelType: ObservableObject {
+  var inputs: ShippingLocationsViewModelInputs { get }
+  var outputs: ShippingLocationsViewModelOutputs { get }
+}
+
+class ShippingLocationsViewModel: ObservableObject, ShippingLocationsViewModelType,
+  ShippingLocationsViewModelInputs, ShippingLocationsViewModelOutputs {
+  @Published var displayedLocations: [Location]
+  @Published var selectedLocation: Location?
+
+  // All the possible locations, used for filtering.
+  private var allLocations: [Location]
+
+  init(
+    withLocations locations: [Location],
+    selectedLocation: Location?
+  ) {
+    self.allLocations = locations
+    self.selectedLocation = selectedLocation
+    self.displayedLocations = locations
+  }
+
+  func isLocationSelected(_ location: Location) -> Bool {
+    return location.id == self.selectedLocation?.id
+  }
+
+  func selectedLocation(_ location: Location) {
+    self.selectedLocation = location
+  }
+
+  func filteredLocations(withTerm searchTerm: String) {
+    if searchTerm.isEmpty {
+      self.displayedLocations = self.allLocations
+      return
+    }
+
+    self.displayedLocations = self.allLocations.filter { location in
+      location.localizedName.lowercased().contains(searchTerm.lowercased())
+    }
+  }
+
+  var outputs: any ShippingLocationsViewModelOutputs {
+    return self
+  }
+
+  var inputs: any ShippingLocationsViewModelInputs {
+    return self
+  }
+}
