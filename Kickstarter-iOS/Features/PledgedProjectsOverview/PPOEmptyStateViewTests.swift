32c9bb55f543fd54cefbd68ca585313f309be9f6
Ingerid Fosli <i.fosli@kickstarter.com>
2025-12-23 10:35:05 -0700

    Update empty state view to support funded cards

--- Kickstarter-iOS/Features/PledgedProjectsOverview/PPOEmptyStateViewTests.swift
+++ Kickstarter-iOS/Features/PledgedProjectsOverview/PPOEmptyStateViewTests.swift
@@ -16,17 +16,39 @@ final class PPOEmptyStateViewTests: TestCase {
   func testEmptyStateView() {
     orthogonalCombos(Language.allLanguages, Device.allCases, Orientation.allCases).forEach {
       language, device, orientation in
-      withEnvironment(
-        language: language
-      ) {
+
+      let mockConfigClient = MockRemoteConfigClient()
+      mockConfigClient.features = [
+        RemoteConfigFeature.pledgedProjectsOverviewV2Enabled.rawValue: true
+      ]
+
+      withEnvironment(language: language, remoteConfigClient: mockConfigClient) {
         let size = device.deviceSize(in: orientation)
         let view = PPOEmptyStateView().frame(width: size.width, height: size.height)
         assertSnapshot(
-          matching: view,
+          of: view,
           as: .image,
           named: "lang_\(language.rawValue)_\(device)_\(orientation)"
         )
       }
+    }
+  }
+
+  // The only difference between v1 and v2 is a strings change. Test v1 in english only.
+  func testEmptyStateView_PPOV1() {
+    let mockConfigClient = MockRemoteConfigClient()
+    mockConfigClient.features = [
+      RemoteConfigFeature.pledgedProjectsOverviewV2Enabled.rawValue: false
+    ]
+
+    withEnvironment(language: Language.en, remoteConfigClient: mockConfigClient) {
+      let size = Device.phone5_8inch.deviceSize
+      let view = PPOEmptyStateView().frame(width: size.width, height: size.height)
+      assertSnapshot(
+        of: view,
+        as: .image,
+        named: "lang_en"
+      )
     }
   }
 }
