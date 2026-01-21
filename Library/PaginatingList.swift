91b69aa5f46a50fe30b17b10166ad442a0e986cb
Ingerid Fosli <i.fosli@kickstarter.com>
2026-01-21 12:58:32 -0700

    show progressView when there's more values to show

--- Library/PaginatingList.swift
+++ Library/PaginatingList.swift
@@ -56,9 +56,13 @@ public struct PaginatingList<Data, Cell, Header>: View where
           Spacer()
           ProgressView()
             .controlSize(.large)
-            .id(self.loaderID)
+            // For some reason, a ProgressView in a List won't re-render without
+            // this hacky ID reloading to make it think it's a different instance.
+            // https://stackoverflow.com/questions/75570322/swiftui-progressview-in-list-can-only-be-displayed-once/75570351
+            .id(UUID())
           Spacer()
         }
+        .listRowBackground(Color.clear)
         .listRowSeparator(.hidden, edges: .bottom)
         .onAppear {
           if !self.data.isEmpty {
@@ -73,21 +77,7 @@ public struct PaginatingList<Data, Cell, Header>: View where
     .refreshable {
       await self.onRefresh()
     }
-    .onChange(of: self.data) { _ in
-      self.reloadLoaderID()
-    }
   }
-
-  // For some reason, a ProgressView in a List won't re-render without
-  // this hacky ID reloading to make it think it's a different instance.
-  // https://stackoverflow.com/questions/75570322/swiftui-progressview-in-list-can-only-be-displayed-once/75570351
-  private func reloadLoaderID() {
-    DispatchQueue.main.async {
-      self.loaderID = UUID()
-    }
-  }
-
-  @State private var loaderID = UUID()
 }
 
 extension PaginatingList where Header == EmptyView {
