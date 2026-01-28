2e8b5f59cd2eac938c1c54128dc2b9722c25fd3a
Amy <a.dyer@kickstarter.com>
2026-01-28 16:41:53 -0500

    MBL-2953: Add non-fatal logging for Facebook and Apple login failures

--- Library/AlertError.swift
+++ Library/AlertError.swift
@@ -8,4 +8,32 @@ public enum AlertError {
   case genericFacebookError(envelope: ErrorEnvelope)
   case facebookConnectAccountTaken(envelope: ErrorEnvelope)
   case facebookConnectEmailTaken(envelope: ErrorEnvelope)
+
+  var code: Int {
+    switch self {
+    case .genericError:
+      0
+    case .facebookTokenFail:
+      1
+    case .facebookLoginAttemptFail:
+      2
+    case .genericFacebookError:
+      3
+    case .facebookConnectAccountTaken:
+      4
+    case .facebookConnectEmailTaken:
+      5
+    }
+  }
+
+  // Crashlytics prefers NSErrors
+  public var nsError: NSError {
+    return NSError(
+      domain: "Facebook.AlertError",
+      code: self.code,
+      userInfo: [
+        NSLocalizedDescriptionKey: "Facebook login failure"
+      ]
+    )
+  }
 }
