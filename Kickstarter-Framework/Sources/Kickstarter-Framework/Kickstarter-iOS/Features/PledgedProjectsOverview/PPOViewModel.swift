ff17695372bfda1367335b118e834ceff0037301
Ingerid Fosli <i.fosli@kickstarter.com>
2026-02-11 16:53:40 -0700

    [MBL-2962] Add ppo v3/v4 analytics and fix incorrect total count (#2751)
    * Add new analytics
    
    * Fix total count
    
    * Add comment explaining total count

--- Kickstarter-Framework/Sources/Kickstarter-Framework/Kickstarter-iOS/Features/PledgedProjectsOverview/PPOViewModel.swift
+++ Kickstarter-Framework/Sources/Kickstarter-Framework/Kickstarter-iOS/Features/PledgedProjectsOverview/PPOViewModel.swift
@@ -164,7 +164,7 @@ final class PPOViewModel: ObservableObject, PPOViewModelInputs, PPOViewModelOutp
     let latestLoadedResults = self.paginator.$results
       .compactMap { results in
         results.hasLoaded ? results.values
-          .ppoAnalyticsProperties(total: results.total, page: results.page) : nil
+          .ppoAnalyticsProperties(page: results.page) : nil
       }
 
     // Analytics: When view appears, the next time it loads, send a PPO dashboard open
@@ -255,8 +255,10 @@ final class PPOViewModel: ObservableObject, PPOViewModelInputs, PPOViewModelOutp
         properties: overallProperties
       )
     case .manageLivePledge:
-      // TODO(MBL-2962): Add analytics event.
-      break
+      AppEnvironment.current.ksrAnalytics.trackPPOManageLivePledge(
+        project: cardModel.projectAnalytics,
+        properties: overallProperties
+      )
     }
   }
 
@@ -372,7 +374,7 @@ final class PPOViewModel: ObservableObject, PPOViewModelInputs, PPOViewModelOutp
 }
 
 extension Sequence where Element == PPOProjectCardViewModel {
-  func ppoAnalyticsProperties(total: Int?, page: Int?) -> KSRAnalytics.PledgedProjectOverviewProperties {
+  func ppoAnalyticsProperties(page: Int?) -> KSRAnalytics.PledgedProjectOverviewProperties {
     var paymentFailedCount: Int = 0
     var cardAuthRequiredCount: Int = 0
     var surveyAvailableCount: Int = 0
@@ -380,6 +382,8 @@ extension Sequence where Element == PPOProjectCardViewModel {
     var pledgeManagementCount: Int = 0
 
     var fundedProjectCount: Int = 0
+    var liveProjectCount: Int = 0
+    var unsuccessfulPledgeCount: Int = 0
 
     for viewModel in self {
       switch viewModel.card.tierType {
@@ -396,11 +400,9 @@ extension Sequence where Element == PPOProjectCardViewModel {
       case .surveySubmitted, .pledgeCollected, .addressConfirmed, .awaitingReward, .rewardReceived:
         fundedProjectCount += 1
       case .campaignLive, .campaignFunded, .campaignEnded:
-        // TODO(MBL-2962): Add analytics for live projects.
-        continue
+        liveProjectCount += 1
       case .campaignFailed, .pledgeDropped, .pledgeCanceled:
-        // TODO(MBL-2962): Add analytics for failed/canceled pledges.
-        continue
+        unsuccessfulPledgeCount += 1
       }
     }
 
@@ -411,7 +413,8 @@ extension Sequence where Element == PPOProjectCardViewModel {
       paymentFailedCount: paymentFailedCount,
       cardAuthRequiredCount: cardAuthRequiredCount,
       fundedProjectCount: fundedProjectCount,
-      total: total,
+      liveProjectCount: liveProjectCount,
+      unsuccessfulPledgeCount: unsuccessfulPledgeCount,
       page: page
     )
   }
