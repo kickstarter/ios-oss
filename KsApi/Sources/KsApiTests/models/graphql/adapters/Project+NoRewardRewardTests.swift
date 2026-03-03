321565140127977712e98b40db02a27bf473c2e6
Amy <a.dyer@kickstarter.com>
2026-03-03 11:44:41 -0500

    Add another few assertions to Project_NoRewardRewardTests

--- KsApi/Sources/KsApiTests/models/graphql/adapters/Project+NoRewardRewardTests.swift
+++ KsApi/Sources/KsApiTests/models/graphql/adapters/Project+NoRewardRewardTests.swift
@@ -7,6 +7,9 @@ final class Project_NoRewardRewardTests: XCTestCase {
     let fragment = GraphAPI.NoRewardRewardFragment(minPledge: 1, fxRate: 1.0)
     let reward = Reward.noRewardReward(from: fragment)
 
+    XCTAssertTrue(reward.isNoReward)
+    XCTAssertTrue(reward.hasNoShippingPreference)
+
     XCTAssertEqual(reward.minimum, 1.0)
     XCTAssertEqual(reward.convertedMinimum, 1.0)
   }
@@ -14,6 +17,9 @@ final class Project_NoRewardRewardTests: XCTestCase {
   func test_fxRate_returnsConvertedMinimum() {
     let fragment = GraphAPI.NoRewardRewardFragment(minPledge: 5, fxRate: 1.493454)
     let reward = Reward.noRewardReward(from: fragment)
+
+    XCTAssertTrue(reward.isNoReward)
+    XCTAssertTrue(reward.hasNoShippingPreference)
 
     XCTAssertEqual(reward.minimum, 5.0)
     XCTAssertEqual(reward.convertedMinimum, 7.46727)
