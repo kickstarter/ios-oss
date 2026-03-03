77f08f4e7c45287f12c1f79cd58eb7a1805031b0
Amy <a.dyer@kickstarter.com>
2026-03-03 11:33:33 -0500

    Add a test for Project+NoRewardReward adapter

---
+++ KsApi/Sources/KsApiTests/models/graphql/adapters/Project+NoRewardRewardTests.swift
@@ -0,0 +1,21 @@
+import GraphAPI
+import KsApi
+import XCTest
+
+final class Project_NoRewardRewardTests: XCTestCase {
+  func test_noFxRate_returnsMinimumPledge() {
+    let fragment = GraphAPI.NoRewardRewardFragment(minPledge: 1, fxRate: 1.0)
+    let reward = Reward.noRewardReward(from: fragment)
+
+    XCTAssertEqual(reward.minimum, 1.0)
+    XCTAssertEqual(reward.convertedMinimum, 1.0)
+  }
+
+  func test_fxRate_returnsConvertedMinimum() {
+    let fragment = GraphAPI.NoRewardRewardFragment(minPledge: 5, fxRate: 1.493454)
+    let reward = Reward.noRewardReward(from: fragment)
+
+    XCTAssertEqual(reward.minimum, 5.0)
+    XCTAssertEqual(reward.convertedMinimum, 7.46727)
+  }
+}
