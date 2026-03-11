331c58bb08fd7f9f12370086696bd4f4e2545554
Steve Streza <s.streza@kickstarter.com>
2026-03-11 15:30:09 -0700

    [MBL-2885] [MBL-2886] [MBL-2887] Add styling for Rich Text SDUI (#2770)
    * Add RichTextStyle protocol and implementations
    
    * Add snapshot test
    
    * Fix formatting
    
    * Fix formatting
    
    * Fix formatting
    
    * Fix test
    
    * Tweaks
    
    * Move light/dark styles to helper module
    
    * Styling
    
    * Change Holder to BindingHolder
    
    * Delete dead comment

---
+++ ServerDrivenUI/Sources/ServerDrivenUITestHelpers/LightRichTextStyle.swift
@@ -0,0 +1,27 @@
+import KDS
+import ServerDrivenUI
+import SwiftUI
+
+/// Fixed light theme for rich text with explicit light-mode colors from KDS.
+/// Use for screenshot tests and snapshots so colors are deterministic (no runtime trait resolution).
+public struct LightRichTextStyle: RichTextStyle, Sendable {
+  private let parent = AutomaticRichTextStyle()
+  public init() {}
+
+  // MARK: - RichTextStyle
+
+  public var bodyFont: Font { self.parent.bodyFont }
+  public var bodyColor: AdaptiveColor { self.parent.bodyColor.resolvedForLightMode() }
+  public var heading1Font: Font { self.parent.heading1Font }
+  public var heading2Font: Font { self.parent.heading2Font }
+  public var heading3Font: Font { self.parent.heading3Font }
+  public var heading4Font: Font { self.parent.heading4Font }
+  public var headingColor: AdaptiveColor { self.parent.headingColor.resolvedForLightMode() }
+  public var linkColor: AdaptiveColor { self.parent.linkColor.resolvedForLightMode() }
+  public var linkUnderlined: Bool { self.parent.linkUnderlined }
+  public var backgroundColor: AdaptiveColor { self.parent.backgroundColor.resolvedForLightMode() }
+  public var blockSpacing: CGFloat { self.parent.blockSpacing }
+  public var listIndentation: CGFloat { self.parent.listIndentation }
+  public var contentHorizontalPadding: CGFloat { self.parent.contentHorizontalPadding }
+  public var mediaCornerRadius: CGFloat { self.parent.mediaCornerRadius }
+}
