15c6f4fecf0e1c44056c643adf2c134ebb9ca427
amy-at-kickstarter <146007185+amy-at-kickstarter@users.noreply.github.com>
2026-03-16 14:29:15 -0400

    MBL-1678: Add loading state to Rewards page (#2789)
    * MBL-1678: Add loading cells to Reward carousel
    
    * MBL-1678: Integrate loading state into RewardsCollectionViewController
    
    * Clean up unused delegate path from PledgeShippingLocationViewController to RewardsCollectionViewController
    
    * Fix bug on iOS 18
    
    * Rename showLoadingRewards to showPlaceholderRewardCards

--- Kickstarter-Framework/Sources/Kickstarter-Framework/Kickstarter-iOS/Features/RewardsCollection/Datasource/RewardsCollectionViewDataSource.swift
+++ Kickstarter-Framework/Sources/Kickstarter-Framework/Kickstarter-iOS/Features/RewardsCollection/Datasource/RewardsCollectionViewDataSource.swift
@@ -1,4 +1,5 @@
 import Foundation
+import KDS
 import KsApi
 import Library
 import Prelude
@@ -13,9 +14,20 @@ final class RewardsCollectionViewDataSource: ValueCellDataSource {
     )
   }
 
+  func isLoading(rewardsCount: Int) {
+    let values = (0..<rewardsCount).map { _ in true }
+    self.set(
+      values: values,
+      cellClass: RewardCardLoadingCell.self,
+      inSection: 0
+    )
+  }
+
   override func configureCell(collectionCell cell: UICollectionViewCell, withValue value: Any) {
     switch (cell, value) {
     case let (cell as RewardCell, value as RewardCardViewData):
+      cell.configureWith(value: value)
+    case let (cell as RewardCardLoadingCell, value as Bool):
       cell.configureWith(value: value)
     default:
       assertionFailure("Unrecognized (cell, value) combo.")
