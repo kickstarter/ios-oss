d771a52b904c8b2e1cd5ab02e55655b8a4ebd6c4
Scott Clampet <s.clampet@kickstarter.com>
2026-03-16 13:40:55 -0500

    cleanup unused method

--- Library/Sources/Library/Library/MockStatsigClient.swift
+++ Library/Sources/Library/Library/MockStatsigClient.swift
@@ -3,20 +3,9 @@ import Foundation
 public final class MockStatsigClient: StatsigClientType {
   public var features: [String: Bool] = [:]
 
-  public var initializedUserIDs: [String] = []
-  public var updatedUserIDs: [String] = []
-
   public init() {}
 
-  public func initialize(userID: String?) {
-    if let userID {
-      self.initializedUserIDs.append(userID)
-    }
-  }
-
-  public func updateUser(userID: String) {
-    self.updatedUserIDs.append(userID)
-  }
+  public func initialize(userID _: String?) {}
 
   public func checkGate(for feature: StatsigFeature) -> Bool {
     self.features[feature.rawValue] ?? false
