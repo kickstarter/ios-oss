9596758a41ac0a60e32a6d5348f0819ff1c1138a
Scott Clampet <s.clampet@kickstarter.com>
2026-03-16 11:10:54 -0500

    add a separate Statsig gates section

--- Library/Sources/Library/Library/ViewModels/RemoteConfigFeatureFlagToolsViewModel.swift
+++ Library/Sources/Library/Library/ViewModels/RemoteConfigFeatureFlagToolsViewModel.swift
@@ -4,15 +4,19 @@ import Prelude
 import ReactiveSwift
 
 public typealias RemoteConfigFeatures = [(RemoteConfigFeature, Bool)]
+public typealias StatsigFeatures = [(StatsigFeature, Bool)]
 
 public protocol RemoteConfigFeatureFlagToolsViewModelOutputs {
-  var reloadWithData: Signal<RemoteConfigFeatures, Never> { get }
-  var updateUserDefaultsWithFeatures: Signal<RemoteConfigFeatures, Never> { get }
+  var reloadWithRemoteConfigData: Signal<RemoteConfigFeatures, Never> { get }
+  var updateUserDefaultsWithRemoteConfigFeatures: Signal<RemoteConfigFeatures, Never> { get }
+  var reloadWithStatsigData: Signal<StatsigFeatures, Never> { get }
+  var updateUserDefaultsWithStatsigFeatures: Signal<StatsigFeatures, Never> { get }
 }
 
 public protocol RemoteConfigFeatureFlagToolsViewModelInputs {
   func didUpdateUserDefaults()
   func setFeatureAtIndexEnabled(index: Int, isEnabled: Bool)
+  func setStatsigFeatureAtIndexEnabled(index: Int, isEnabled: Bool)
   func viewDidLoad()
 }
 
@@ -32,18 +36,18 @@ public final class RemoteConfigFeatureFlagToolsViewModel: RemoteConfigFeatureFla
       self.viewDidLoadProperty.signal,
       didUpdateUserDefaultsAndUI
     )
-    .map { _ in RemoteConfigFeature.allCases }
 
     let remoteConfigFeatures = features
+      .map { _ in RemoteConfigFeature.allCases }
       .map { features in
         features.map { feature -> (RemoteConfigFeature, Bool) in
           (feature, isFeatureEnabled(feature))
         }
       }
 
-    self.reloadWithData = remoteConfigFeatures
+    self.reloadWithRemoteConfigData = remoteConfigFeatures
 
-    self.updateUserDefaultsWithFeatures = remoteConfigFeatures
+    self.updateUserDefaultsWithRemoteConfigFeatures = remoteConfigFeatures
       .takePairWhen(self.setFeatureEnabledAtIndexProperty.signal.skipNil())
       .map(unpack)
       .map { features, index, isEnabled -> RemoteConfigFeatures? in
@@ -56,11 +60,44 @@ public final class RemoteConfigFeatureFlagToolsViewModel: RemoteConfigFeatureFla
         return mutatedFeatures
       }
       .skipNil()
+
+    // MARK: Statsig
+
+    let statsigFeatures = features
+      .map { _ in StatsigFeature.allCases }
+      .map { features in
+        features.map { feature -> (StatsigFeature, Bool) in
+          (feature, isStatsigFeatureEnabled(feature))
+        }
+      }
+
+    self.reloadWithStatsigData = statsigFeatures
+
+    self.updateUserDefaultsWithStatsigFeatures = statsigFeatures
+      .takePairWhen(self.setStatsigFeatureEnabledAtIndexProperty.signal.skipNil())
+      .map(unpack)
+      .map { features, index, isEnabled -> StatsigFeatures? in
+        let (feature, _) = features[index]
+        var mutatedFeatures = features
+
+        setStatsigValueInUserDefaults(for: feature, and: isEnabled)
+
+        mutatedFeatures[index] = (feature, isEnabled)
+        return mutatedFeatures
+      }
+      .skipNil()
   }
+
+  // MARK: - Inputs
 
   private let setFeatureEnabledAtIndexProperty = MutableProperty<(Int, Bool)?>(nil)
   public func setFeatureAtIndexEnabled(index: Int, isEnabled: Bool) {
     self.setFeatureEnabledAtIndexProperty.value = (index, isEnabled)
+  }
+
+  private let setStatsigFeatureEnabledAtIndexProperty = MutableProperty<(Int, Bool)?>(nil)
+  public func setStatsigFeatureAtIndexEnabled(index: Int, isEnabled: Bool) {
+    self.setStatsigFeatureEnabledAtIndexProperty.value = (index, isEnabled)
   }
 
   private let didUpdateUserDefaultsProperty = MutableProperty(())
@@ -73,22 +110,39 @@ public final class RemoteConfigFeatureFlagToolsViewModel: RemoteConfigFeatureFla
     self.viewDidLoadProperty.value = ()
   }
 
-  public let reloadWithData: Signal<RemoteConfigFeatures, Never>
-  public let updateUserDefaultsWithFeatures: Signal<RemoteConfigFeatures, Never>
+  // MARK: - Outputs
+
+  public let reloadWithRemoteConfigData: Signal<RemoteConfigFeatures, Never>
+  public let updateUserDefaultsWithRemoteConfigFeatures: Signal<RemoteConfigFeatures, Never>
+  public let reloadWithStatsigData: Signal<StatsigFeatures, Never>
+  public let updateUserDefaultsWithStatsigFeatures: Signal<StatsigFeatures, Never>
 
   public var inputs: RemoteConfigFeatureFlagToolsViewModelInputs { return self }
   public var outputs: RemoteConfigFeatureFlagToolsViewModelOutputs { return self }
 }
 
-// MARK: - Private Helpers
+// MARK: - Private Helpers (Remote Config)
 
 private func isFeatureEnabled(_ feature: RemoteConfigFeature) -> Bool {
   return featureEnabled(feature: feature)
 }
 
-/** Sets the value for the UserDefaults key in the AppEnvironment.
- */
 private func setValueInUserDefaults(for feature: RemoteConfigFeature, and value: Bool) {
+  AppEnvironment.current.userDefaults
+    .remoteConfigFeatureFlags[feature.rawValue] = value
+}
+
+// MARK: - Private Helpers (Statsig)
+
+private func isStatsigFeatureEnabled(_ feature: StatsigFeature) -> Bool {
+  if let valueFromDefaults = AppEnvironment.current.userDefaults
+    .remoteConfigFeatureFlags[feature.rawValue] {
+    return valueFromDefaults
+  }
+  return AppEnvironment.current.statsigClient?.checkGate(for: feature) == true
+}
+
+private func setStatsigValueInUserDefaults(for feature: StatsigFeature, and value: Bool) {
   AppEnvironment.current.userDefaults
     .remoteConfigFeatureFlags[feature.rawValue] = value
 }
