cd4a4a2961a665ee3096e5ae9d68780139d69d6d
Amy <a.dyer@kickstarter.com>
2026-03-30 11:05:27 -0400

    MBL-3155: Show correct title when reward doesn't ship to your location and the project has already been backed

--- Library/Sources/Library/Library/ViewModels/RewardCardContainerViewModel.swift
+++ Library/Sources/Library/Library/ViewModels/RewardCardContainerViewModel.swift
@@ -98,7 +98,7 @@ private func pledgeButtonTitle(data: RewardCardViewData) -> String? {
   let rewardShipsToLocation = rewardCanShip(data.reward, toLocation: data.currentShippingLocation)
 
   // If you're creating a new backing but the reward doesn't ship there, show the 'not available' string.
-  if !rewardShipsToLocation, projectBackingState.canCreateNewPledge {
+  if !rewardShipsToLocation, !isBackingThisReward {
     return Strings.Not_available_in_selected_country()
   }
 
