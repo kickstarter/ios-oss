459e6d874a2018de2ea004fa4dfcd78dd973312e
Scott Clampet <s.clampet@kickstarter.com>
2026-03-30 08:55:00 -0500

    add bottom overlay fields to VideoFeedItems

--- Kickstarter-Framework/Sources/Kickstarter-Framework/Kickstarter-iOS/Features/VideoFeed/Model/VideoFeedItem.swift
+++ Kickstarter-Framework/Sources/Kickstarter-Framework/Kickstarter-iOS/Features/VideoFeed/Model/VideoFeedItem.swift
@@ -2,5 +2,25 @@ import Foundation
 
 public struct VideoFeedItem {
   let id: String
+
+  /// Main title shown in the bottom overlay.
   let title: String
+
+  /// Creator name shown in the bottom overlay.
+  let creator: String
+
+  /// Creator avatar URL used in the right rail.
+  let creatorImageURL: URL?
+
+  /// Stats text shown below the title
+  let statsText: String
+
+  /// Left pill label
+  let categoryPillText: String
+
+  /// Right pill label
+  let secondaryPillText: String
+
+  /// CTA button label
+  let ctaTitle: String
 }
