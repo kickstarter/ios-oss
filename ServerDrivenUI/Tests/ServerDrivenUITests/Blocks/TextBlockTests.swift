b522fbc2be17969cbe6ca57ebfbffd78c5c922e9
Steve Streza <s.streza@kickstarter.com>
2026-04-20 16:35:38 -0700

    Update TextBlock tests for readability and to use UITraitCollection

--- ServerDrivenUI/Tests/ServerDrivenUITests/Blocks/TextBlockTests.swift
+++ ServerDrivenUI/Tests/ServerDrivenUITests/Blocks/TextBlockTests.swift
@@ -11,151 +11,122 @@ struct TextBlockTests {
   @Test(
     "Test basic text rendering",
     .snapshots(record: .failed),
+
     arguments:
-    [ /* Text contents */
-      RichTextElement.Text(text: "Hello world"),
-      RichTextElement
-        .Text(
-          text: "Swift Testing has a clear and expressive API built using macros, so you can declare complex behaviors with a small amount of code."
-        )
-    ], orthogonalCombos(
-      [ /* Color scheme */
-        ColorScheme.dark,
-        ColorScheme.light
-      ], [ /* Content size */
-        DynamicTypeSize.accessibility5,
-        DynamicTypeSize.xxxLarge,
-        DynamicTypeSize.large,
-        DynamicTypeSize.xSmall
-      ]
-    )
+    [helloWorldPlain, longTextPlain],
+    orthogonalCombos(colorSchemes, contentSizes)
   )
   func testBasicText(
     text: RichTextElement.Text,
-    properties: (ColorScheme, DynamicTypeSize),
+    properties: (ColorScheme, UIContentSizeCategory),
   ) async throws {
-    let (colorScheme, typeSize) = properties
-    let view = TextBlock(text: text)
-      .frame(width: 500)
-      .frame(maxHeight: .infinity)
-      .environment(\.colorScheme, colorScheme)
-      .environment(\.dynamicTypeSize, typeSize)
+    let (colorScheme, contentSizeCategory) = properties
+    UITraitCollection(preferredContentSizeCategory: contentSizeCategory).performAsCurrent {
+      let view = TextBlock(text: text)
+        .frame(width: 500)
+        .frame(maxHeight: .infinity)
+        .environment(\.colorScheme, colorScheme)
 
-    assertSnapshot(of: view, as: .image, named: "\(text.text.prefix(10))-\(colorScheme)-\(typeSize)")
+      assertSnapshot(
+        of: view,
+        as: .image,
+        named: "\(text.text.prefix(10))-\(colorScheme)-\(contentSizeCategory)"
+      )
+    }
   }
 
   @Test(
     "Test styled text rendering",
     .snapshots(record: .failed),
+
     arguments:
-    [ /* Text contents */
-      RichTextElement.Text(text: "", children: [
-        .text(RichTextElement.Text(text: "Hello "), nil),
-        .text(RichTextElement.Text(text: "world", styles: [.strong]), nil),
-        .text(RichTextElement.Text(text: "!"), nil)
-      ]),
-      RichTextElement.Text(text: "", children: [
-        .text(RichTextElement.Text(
-          text: "Swift Testing has a "
-        ), nil),
-        .text(RichTextElement.Text(
-          text: "clear",
-          styles: [.strong],
-        ), nil),
-        .text(RichTextElement.Text(
-          text: " and "
-        ), nil),
-        .text(RichTextElement.Text(
-          text: "expressive",
-          styles: [.emphasis]
-        ), nil),
-        .text(RichTextElement.Text(
-          text: " API built using macros, so you can declare complex behaviors with a small amount of code."
-        ), nil)
-      ])
-    ], orthogonalCombos(
-      [ /* Color scheme */
-        ColorScheme.dark,
-        ColorScheme.light
-      ], [ /* Content size */
-        DynamicTypeSize.xSmall,
-        DynamicTypeSize.large,
-        DynamicTypeSize.xxxLarge,
-        DynamicTypeSize.accessibility5
-      ]
-    )
+    [helloWorldStyled, longTextStyled],
+    orthogonalCombos(colorSchemes, contentSizes)
   )
   func testStyles(
     text: RichTextElement.Text,
-    properties: (ColorScheme, DynamicTypeSize),
+    properties: (ColorScheme, UIContentSizeCategory),
   ) async throws {
-    let (colorScheme, typeSize) = properties
-    let view = TextBlock(text: text)
-      .frame(width: 500)
-      .frame(maxHeight: .infinity)
-      .environment(\.colorScheme, colorScheme)
-      .environment(\.dynamicTypeSize, typeSize)
+    let (colorScheme, contentSizeCategory) = properties
+    UITraitCollection(preferredContentSizeCategory: contentSizeCategory).performAsCurrent {
+      let view = TextBlock(text: text)
+        .frame(width: 500)
+        .frame(maxHeight: .infinity)
+        .environment(\.colorScheme, colorScheme)
 
-    let fullText = text.children.compactMap {
-      if case let .text(text, _) = $0 {
-        return text
-      } else {
-        return nil
+      let fullText = text.children.compactMap {
+        if case let .text(text, _) = $0 {
+          return text
+        } else {
+          return nil
+        }
       }
-    }
-    let contents = fullText.map { $0.text }.joined()
+      let contents = fullText.map { $0.text }.joined()
 
-    assertSnapshot(of: view, as: .image, named: "\(contents.prefix(10))-\(colorScheme)-\(typeSize)")
+      assertSnapshot(
+        of: view,
+        as: .image,
+        named: "\(contents.prefix(10))-\(colorScheme)-\(contentSizeCategory)"
+      )
+    }
   }
 
   @Test(
     "Test list item text rendering",
     .snapshots(record: .failed),
+
     arguments:
-    [ /* Text contents */
-      RichTextElement.Text(text: "Hello world"),
-      RichTextElement.Text(text: "", children: [
-        .text(RichTextElement.Text(
-          text: "Swift Testing has a "
-        ), nil),
-        .text(RichTextElement.Text(
-          text: "clear",
-          styles: [.strong],
-        ), nil),
-        .text(RichTextElement.Text(
-          text: " and "
-        ), nil),
-        .text(RichTextElement.Text(
-          text: "expressive",
-          styles: [.emphasis]
-        ), nil),
-        .text(RichTextElement.Text(
-          text: " API built using macros, so you can declare complex behaviors with a small amount of code."
-        ), nil)
-      ])
-    ], orthogonalCombos(
-      [ /* Color scheme */
-        ColorScheme.dark,
-        ColorScheme.light
-      ], [ /* Content size */
-        DynamicTypeSize.accessibility5,
-        DynamicTypeSize.xxxLarge,
-        DynamicTypeSize.large,
-        DynamicTypeSize.xSmall
-      ]
-    )
+    [helloWorldPlain, longTextStyled],
+    orthogonalCombos(colorSchemes, contentSizes)
   )
   func testListItemText(
     text: RichTextElement.Text,
-    properties: (ColorScheme, DynamicTypeSize),
+    properties: (ColorScheme, UIContentSizeCategory),
   ) async throws {
-    let (colorScheme, typeSize) = properties
-    let view = ListItemBlock(text: text)
-      .frame(width: 500)
-      .frame(maxHeight: .infinity)
-      .environment(\.colorScheme, colorScheme)
-      .environment(\.dynamicTypeSize, typeSize)
+    let (colorScheme, contentSizeCategory) = properties
+    UITraitCollection(preferredContentSizeCategory: contentSizeCategory).performAsCurrent {
+      let view = ListItemBlock(text: text)
+        .frame(width: 500)
+        .frame(maxHeight: .infinity)
+        .environment(\.colorScheme, colorScheme)
 
-    assertSnapshot(of: view, as: .image, named: "\(text.text.prefix(10))-\(colorScheme)-\(typeSize)")
+      assertSnapshot(
+        of: view,
+        as: .image,
+        named: "\(text.text.prefix(10))-\(colorScheme)-\(contentSizeCategory)"
+      )
+    }
   }
+}
+
+private let colorSchemes = [ColorScheme.dark, ColorScheme.light]
+private let contentSizes = [
+  UIContentSizeCategory.accessibilityExtraExtraExtraLarge,
+  UIContentSizeCategory.extraExtraExtraLarge,
+  UIContentSizeCategory.large,
+  UIContentSizeCategory.extraSmall
+]
+
+private let helloWorldPlain = makeText("Hello world")
+private let helloWorldStyled = makeText(("Hello ", nil), ("world", .strong), ("!", nil))
+private let longTextPlain =
+  makeText(
+    "Swift Testing has a clear and expressive API built using macros, so you can declare complex behaviors with a small amount of code."
+  )
+private let longTextStyled = makeText(
+  ("Swift Testing has a ", nil),
+  ("clear", .strong),
+  (" and ", nil),
+  ("expressive", .emphasis),
+  (" API built using macros, so you can declare complex behaviors with a small amount of code.", nil)
+)
+
+private func makeText(_ text: String) -> RichTextElement.Text {
+  RichTextElement.Text(text: text)
+}
+
+private func makeText(_ args: (String, RichTextElement.Text.Style?)...) -> RichTextElement.Text {
+  RichTextElement.Text(text: "", children: args.map { text, style in
+    .text(RichTextElement.Text(text: text, link: nil, styles: [style].compactMap { $0 }, children: []), nil)
+  })
 }
