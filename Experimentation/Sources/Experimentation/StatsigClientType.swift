90e79f6824179f29128727bec63f78ac72684579
Amy <a.dyer@kickstarter.com>
2026-04-28 10:58:52 -0400

    Add comment explaining more Statsig types

--- Experimentation/Sources/Experimentation/StatsigClientType.swift
+++ Experimentation/Sources/Experimentation/StatsigClientType.swift
@@ -11,4 +11,10 @@ public protocol StatsigClientType: AnyObject {
   /// Returns a boolean value from an experiment.
   /// May return `nil` if the experiment or key are invalid.
   func boolValue<T: StatsigExperimentProtocol>(forKey key: T.Parameters, inExperiment experiment: T) -> Bool?
+
+  // Statsig supports Bool, Int, String and even Object parameters for experiments.
+  // When and if we need them, we can expand this implementation to support more types.
+
+  // func intValue<T: StatsigExperimentProtocol>(forKey key: T.Parameters, inExperiment experiment: T) -> Int?
+  // func stringValue<T: StatsigExperimentProtocol>(forKey key: T.Parameters, inExperiment experiment: T) -> String?
 }
