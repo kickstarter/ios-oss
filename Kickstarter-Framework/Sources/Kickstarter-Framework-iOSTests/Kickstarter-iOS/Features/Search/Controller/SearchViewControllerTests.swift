63a88537dd728b9c08d3ae9bc7b4f7f19a77b689
Scott Clampet <s.clampet@kickstarter.com>
2026-05-27 12:35:56 -0500

    update snapshots

--- Kickstarter-Framework/Sources/Kickstarter-Framework-iOSTests/Kickstarter-iOS/Features/Search/Controller/SearchViewControllerTests.swift
+++ Kickstarter-Framework/Sources/Kickstarter-Framework-iOSTests/Kickstarter-iOS/Features/Search/Controller/SearchViewControllerTests.swift
@@ -249,9 +249,8 @@ internal final class SearchViewContollerTests: TestCase {
     let mockStatsigClient = MockStatsigWrapper()
     mockStatsigClient.features = [.videoFeed: true]
 
-    // TODO: Update to all languages once translations are in [mbl-3158](https://kickstarter.atlassian.net/browse/MBL-3158)
     orthogonalCombos(
-      [Language.en],
+      Language.allLanguages,
       [Device.phone5_8inch, Device.pad],
       [UIUserInterfaceStyle.light, UIUserInterfaceStyle.dark]
     )
