5fb8c769aed31705f249e7788a8d63ec068daa79
Steve Streza <s.streza@kickstarter.com>
2026-09-15 10:22:57 -0700

    Fix formatting

--- Kickstarter-iOS/AppDelegate.swift
+++ Kickstarter-iOS/AppDelegate.swift
@@ -329,9 +329,9 @@ internal final class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   func application(
-    _ application: UIApplication,
+    _: UIApplication,
     configurationForConnecting connectingSceneSession: UISceneSession,
-    options: UIScene.ConnectionOptions
+    options _: UIScene.ConnectionOptions
   ) -> UISceneConfiguration {
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
--- Kickstarter-iOS/SceneDelegate.swift
+++ Kickstarter-iOS/SceneDelegate.swift
@@ -16,7 +16,7 @@ internal final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(
     _ scene: UIScene,
     willConnectTo _: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
+    options _: UIScene.ConnectionOptions
   ) {
     guard scene is UIWindowScene else { return }
 
